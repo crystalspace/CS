b98689a542e78b2f95660e66c2e71801d13ae366
Serguei Narojnyi <snaar@idirect.ca>
1999-05-08 20:57:14 +0000

    int to short type change on almost every variable; minor updates caused by changes in ndrvdefs.h
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@377 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csnetdrv/sockets/netsdrv.cpp
+++ libs/csnetdrv/sockets/netsdrv.cpp
@@ -39,7 +39,7 @@ csNetworkDriverSockets::csNetworkDriverSockets(ISystem* piSystem)
 	SocksReady = false;
 	dwLastError = 0;
 
-	for(int i = 0; i < CS_NET_MAX_SOCKETS; i++)
+	for(short i = 0; i < CS_NET_MAX_SOCKETS; i++)
 	{
 		SocketListening[CS_NET_MAX_SOCKETS] = false;
 		SocketConnected[CS_NET_MAX_SOCKETS] = false;
@@ -67,12 +67,12 @@ STDMETHODIMP csNetworkDriverSockets::Open()
 {
 	SysPrintf(MSG_INITIALIZATION, "\nNetwork driver stuff:\n");
 
-	if (InitSocks() == CS_NET_SUCCESS) SysPrintf(MSG_INITIALIZATION, "Network driver initialisation finished\n");
+	if (InitSocks() == S_OK) SysPrintf(MSG_INITIALIZATION, "Network driver initialisation finished\n");
 	else SysPrintf(MSG_INITIALIZATION, "Network driver initialisation failed!\n");
 
 	SysPrintf(MSG_INITIALIZATION, "\n");
 
-	return CS_NET_SUCCESS;
+	return S_OK;
 }
 
 STDMETHODIMP csNetworkDriverSockets::Close()
@@ -81,7 +81,7 @@ STDMETHODIMP csNetworkDriverSockets::Close()
 
 	if(SocksReady)
 	{
-		if(ReleaseSocks() != CS_NET_SUCCESS) return CS_NET_FAIL;
+		if(ReleaseSocks() != S_OK) return S_FALSE;
 	}
 	return S_OK;
 }
@@ -90,26 +90,26 @@ STDMETHODIMP csNetworkDriverSockets::Connect(DWORD dwID, CS_NET_ADDRESS *lpNetAd
 {
 	if(!SocksReady)
 	{
-		dwLastError = CS_NET_ERR_NOT_INITIALIZED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_INITIALIZED;
+		return S_FALSE;
 	}
 
 	if(SocketConnected[dwID])
 	{
-		dwLastError = CS_NET_ERR_ALREADY_CONNECTED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_ALREADY_CONNECTED;
+		return S_FALSE;
 	}
 
 #if !defined(NO_SOCKETS_SUPPORT)
 
 	struct sockaddr_in addr;
 	struct hostent *hp;
-	int len;
+	short len;
 
 	addr.sin_family = AF_INET;
 	if((hp = gethostbyname(lpNetAddress->hostnm)) == NULL)
 	{
-		dwLastError = CS_NET_ERR_CANNOT_RESOLVE_NAME;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_RESOLVE_NAME;
 		return S_FALSE;
 	}
 	memcpy((char*)&addr.sin_addr, (char*)hp->h_addr, hp->h_length);
@@ -117,14 +117,14 @@ STDMETHODIMP csNetworkDriverSockets::Connect(DWORD dwID, CS_NET_ADDRESS *lpNetAd
 	len = sizeof(addr);
 	if(connect(Socket[dwID], (struct sockaddr*)&addr, len) < 0)
 	{
-		dwLastError = CS_NET_ERR_CANNOT_CONNECT;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_CONNECT;
+		return S_FALSE;
 	}
 
 #endif
 
 	SocketConnected[dwID] = true;
-	return CS_NET_SUCCESS;
+	return S_OK;
 }
 
 STDMETHODIMP csNetworkDriverSockets::Disconnect(DWORD dwID)
@@ -132,12 +132,12 @@ STDMETHODIMP csNetworkDriverSockets::Disconnect(DWORD dwID)
 	// This is not really a good thing going on here but i had no choice since BSD sockets cannot disconnect
 	if(!SocksReady)
 	{
-		dwLastError = CS_NET_ERR_NOT_INITIALIZED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_INITIALIZED;
+		return S_FALSE;
 	}
 	if (!SocketConnected[dwID]) return S_FALSE;
 	{
-		return CS_NET_SUCCESS;
+		return S_OK;
 	}
 
 #if !defined(NO_SOCKETS_SUPPORT)
@@ -148,21 +148,21 @@ STDMETHODIMP csNetworkDriverSockets::Disconnect(DWORD dwID)
 
 	if(!getsockopt(Socket[dwID], SOL_SOCKET, SO_TYPE, &SockType, &SizeOfSockType))
 	{
-		dwLastError = CS_NET_ERR_CANNON_GET_SOCKOPT;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNON_GET_SOCKOPT;
+		return S_FALSE;
 	}
 
 	if(SockType == SOCK_STREAM) ProtoType = IPPROTO_TCP;
 	else if(SockType == SOCK_DGRAM) ProtoType = IPPROTO_UDP;
 	else
 	{
-		dwLastError = CS_NET_ERR_INVALID_TYPE;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_INVALID_TYPE;
+		return S_FALSE;
 	}
 
 #endif
 
-	if(Kill(dwID) == CS_NET_FAIL) return CS_NET_FAIL;
+	if(Kill(dwID) == S_FALSE) return S_FALSE;
 
 #if !defined(NO_SOCKETS_SUPPORT)
 
@@ -170,28 +170,28 @@ STDMETHODIMP csNetworkDriverSockets::Disconnect(DWORD dwID)
 
 	if(Socket[dwID] == CS_NET_INVALID_SOCKET)
 	{
-		dwLastError = CS_NET_ERR_CANNOT_CREATE;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_CREATE;
+		return S_FALSE;
 	}
 
 #endif
 
 	SocketInitialized[dwID] = true;
 
-	return CS_NET_SUCCESS;
+	return S_OK;
 }
 
 STDMETHODIMP csNetworkDriverSockets::Send(DWORD dwID, DWORD dwBytesToSend, char *lpDataBuffer)
 {
 	if (!SocksReady)
 	{
-		dwLastError = CS_NET_ERR_NOT_INITIALIZED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_INITIALIZED;
+		return S_FALSE;
 	}
 	if (!SocketConnected[dwID]) return S_FALSE;
 	{
-		dwLastError = CS_NET_ERR_NOT_CONNECTED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_CONNECTED;
+		return S_FALSE;
 	}
 
 #if !defined(NO_SOCKETS_SUPPORT)
@@ -200,7 +200,7 @@ STDMETHODIMP csNetworkDriverSockets::Send(DWORD dwID, DWORD dwBytesToSend, char 
 	return S_OK;
 #endif
 
-	dwLastError = CS_NET_ERR_CANNOT_SEND;
+	dwLastError = CS_NET_DRV_ERR_CANNOT_SEND;
 	return S_FALSE;
 }
 
@@ -208,13 +208,13 @@ STDMETHODIMP csNetworkDriverSockets::Receive(DWORD dwID, DWORD *lpdwBytesToRecei
 {
 	if (!SocksReady)
 	{
-		dwLastError = CS_NET_ERR_NOT_INITIALIZED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_INITIALIZED;
+		return S_FALSE;
 	}
 	if (!SocketConnected[dwID]) return S_FALSE;
 	{
-		dwLastError = CS_NET_ERR_NOT_CONNECTED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_CONNECTED;
+		return S_FALSE;
 	}
 
 #if !defined(NO_SOCKETS_SUPPORT)
@@ -223,13 +223,13 @@ STDMETHODIMP csNetworkDriverSockets::Receive(DWORD dwID, DWORD *lpdwBytesToRecei
 	return S_OK;
 #endif
 
-	dwLastError = CS_NET_ERR_CANNOT_SEND;
+	dwLastError = CS_NET_DRV_ERR_CANNOT_SEND;
 	return S_FALSE;
 }
 
 HRESULT csNetworkDriverSockets::InitSocks()
 {
-	if(SocksReady) return CS_NET_SUCCESS;
+	if(SocksReady) return S_OK;
 
 #if defined(OS_WIN32) && !defined(NO_SOCKETS_SUPPORT)
 
@@ -238,66 +238,66 @@ HRESULT csNetworkDriverSockets::InitSocks()
 	
 	if(WSAStartup(wVersionRequested, &wsaData) != 0)
 	{
-		dwLastError = CS_NET_ERR_CANNOT_GET_VERSION;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_GET_VERSION;
+		return S_FALSE;
 	}
 
 	if((LOBYTE(wsaData.wVersion) != 1) || (HIBYTE(wsaData.wVersion) != 1))
 	{
 		WSACleanup();
-		dwLastError = CS_NET_ERR_WRONG_VERSION;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_WRONG_VERSION;
+		return S_FALSE;
 	}
 
 #endif
 
 	SocksReady = true;
-	return CS_NET_SUCCESS;
+	return S_OK;
 }
 
 HRESULT csNetworkDriverSockets::ReleaseSocks()
 {
-	if(!SocksReady) return CS_NET_SUCCESS;
+	if(!SocksReady) return S_OK;
 
 #if defined(OS_WIN32) && !defined(NO_SOCKETS_SUPPORT)
 	if(WSACleanup())
 	{
-		dwLastError = CS_NET_ERR_CANNOT_CLEANUP;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_CLEANUP;
+		return S_FALSE;
 	}
 #endif
 
 	SocksReady = false;
 
-	return CS_NET_SUCCESS;
+	return S_OK;
 }
 
 STDMETHODIMP csNetworkDriverSockets::SetListenState(DWORD dwID, CS_NET_LISTENPARAMS *lpCSListenParams)
 {
 	if(!SocksReady)
 	{
-		dwLastError = CS_NET_ERR_NOT_INITIALIZED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_INITIALIZED;
+		return S_FALSE;
 	}
 	if((dwID >= CS_NET_MAX_SOCKETS) || (!SocketInitialized[dwID]))
 	{
-		dwLastError = CS_NET_ERR_INVALID_SOCKET;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_INVALID_SOCKET;
+		return S_FALSE;
 	}
 	if(SocketConnected[dwID])
 	{
-		dwLastError = CS_NET_ERR_ALREADY_CONNECTED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_ALREADY_CONNECTED;
+		return S_FALSE;
 	}
 	if(SocketListening[dwID])
 	{
-		dwLastError = CS_NET_ERR_ALREADY_LISTENING;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_ALREADY_LISTENING;
+		return S_FALSE;
 	}
 
 #if !defined(NO_SOCKETS_SUPPORT)
 
-	int LocalPort = lpCSListenParams->port;
+	short LocalPort = lpCSListenParams->port;
 	struct sockaddr_in LocalAddress;
 	LocalAddress.sin_family = AF_INET;
 	LocalAddress.sin_addr.s_addr = INADDR_ANY;
@@ -305,21 +305,21 @@ STDMETHODIMP csNetworkDriverSockets::SetListenState(DWORD dwID, CS_NET_LISTENPAR
 
 	if(!bind(Socket[dwID], (struct sockaddr*)&LocalAddress, sizeof(LocalAddress)))
 	{
-		dwLastError = CS_NET_ERR_CANNOT_BIND;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_BIND;
+		return S_FALSE;
 	}
 
 	if(!listen(Socket[dwID], CS_NET_LISTEN_QUEUE_SIZE))
 	{
-		dwLastError = CS_NET_ERR_CANNOT_LISTEN;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_LISTEN;
+		return S_FALSE;
 	}
 
 #endif
 
 	SocketListening[dwID] = true;
 
-	return CS_NET_SUCCESS;
+	return S_OK;
 
 }
 
@@ -327,19 +327,19 @@ STDMETHODIMP csNetworkDriverSockets::Spawn(DWORD *lpdwID /*out*/, DWORD dwType)
 {
 	if(!SocksReady)
 	{
-		dwLastError = CS_NET_ERR_NOT_INITIALIZED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_INITIALIZED;
+		return S_FALSE;
 	}
 
-	int i = 0;
+	short i = 0;
 
 	while(SocketInitialized[i])
 	{
 		i++;
 		if(i >= CS_NET_MAX_SOCKETS)
 		{
-			dwLastError = CS_NET_ERR_LIMIT_REACHED;
-			return CS_NET_FAIL;
+			dwLastError = CS_NET_DRV_ERR_LIMIT_REACHED;
+			return S_FALSE;
 		}
 	}
 
@@ -349,14 +349,14 @@ STDMETHODIMP csNetworkDriverSockets::Spawn(DWORD *lpdwID /*out*/, DWORD dwType)
 	else if(dwType == CS_NET_CONNLESS) Socket[i] = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 	else
 	{
-		dwLastError = CS_NET_ERR_INVALID_TYPE;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_INVALID_TYPE;
+		return S_FALSE;
 	}
 
 	if(Socket[i] == CS_NET_INVALID_SOCKET)
 	{
-		dwLastError = CS_NET_ERR_CANNOT_CREATE;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_CREATE;
+		return S_FALSE;
 	}
 
 #endif
@@ -365,41 +365,41 @@ STDMETHODIMP csNetworkDriverSockets::Spawn(DWORD *lpdwID /*out*/, DWORD dwType)
 
 	*lpdwID = i;
 
-	return CS_NET_SUCCESS;
+	return S_OK;
 }
 
 STDMETHODIMP csNetworkDriverSockets::Kill(DWORD dwID)
 {
 	if(!SocksReady)
 	{
-		dwLastError = CS_NET_ERR_NOT_INITIALIZED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_INITIALIZED;
+		return S_FALSE;
 	}
 	if(dwID >= CS_NET_MAX_SOCKETS)
 	{
-		dwLastError = CS_NET_ERR_INVALID_SOCKET;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_INVALID_SOCKET;
+		return S_FALSE;
 	}
-	if(!SocketInitialized[dwID]) return CS_NET_SUCCESS;
+	if(!SocketInitialized[dwID]) return S_OK;
 
 #if !defined(NO_SOCKETS_SUPPORT)
 	#if defined(OS_WIN32)
 	if(closesocket(Socket[dwID]))
 	{
-		dwLastError = CS_NET_ERR_CANNOT_CLOSE;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_CLOSE;
+		return S_FALSE;
 	}
 	#elif defined(OS_OS2)
 	if(soclose(Socket[dwID]))
 	{
-		dwLastError = CS_NET_ERR_CANNOT_CLOSE;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_CLOSE;
+		return S_FALSE;
 	}
 	#else
 	if(close(Socket[dwID]))
 	{
-		dwLastError = CS_NET_ERR_CANNOT_CLOSE;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_CLOSE;
+		return S_FALSE;
 	}
 	#endif
 #endif
@@ -408,23 +408,23 @@ STDMETHODIMP csNetworkDriverSockets::Kill(DWORD dwID)
 	SocketConnected[dwID] = false;
 	SocketListening[dwID] = false;
 
-	return CS_NET_SUCCESS;
+	return S_OK;
 }
 
 STDMETHODIMP csNetworkDriverSockets::KillAll()
 {
 	if(!SocksReady)
 	{
-		dwLastError = CS_NET_ERR_NOT_INITIALIZED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_INITIALIZED;
+		return S_FALSE;
 	}
 
 	DWORD i = 0;
-	HRESULT hResult = CS_NET_SUCCESS;
+	HRESULT hResult = S_OK;
 
 	while(i < CS_NET_MAX_SOCKETS)
 	{
-		if(Kill(i) == CS_NET_FAIL) hResult = CS_NET_FAIL;
+		if(Kill(i) == S_FALSE) hResult = S_FALSE;
 		i++;
 	}
 
@@ -435,18 +435,18 @@ STDMETHODIMP csNetworkDriverSockets::Accept(DWORD dwLID/*listening socket*/, DWO
 {
 	if(!SocksReady)
 	{
-		dwLastError = CS_NET_ERR_NOT_INITIALIZED;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_INITIALIZED;
+		return S_FALSE;
 	}
 	if(dwLID >= CS_NET_MAX_SOCKETS)
 	{
-		dwLastError = CS_NET_ERR_INVALID_SOCKET;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_INVALID_SOCKET;
+		return S_FALSE;
 	}
 	if(!SocketListening[dwLID])
 	{
-		dwLastError = CS_NET_ERR_NOT_LISTENING;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_NOT_LISTENING;
+		return S_FALSE;
 	}
 
 	*lpdwID = 0;
@@ -457,8 +457,8 @@ STDMETHODIMP csNetworkDriverSockets::Accept(DWORD dwLID/*listening socket*/, DWO
 		if(*lpdwID >= CS_NET_MAX_SOCKETS)
 		{
 			*lpdwID = CS_NET_INVALID_SOCKET;
-			dwLastError = CS_NET_ERR_LIMIT_REACHED;
-			return CS_NET_FAIL;
+			dwLastError = CS_NET_DRV_ERR_LIMIT_REACHED;
+			return S_FALSE;
 		}
 	}
 
@@ -469,13 +469,13 @@ STDMETHODIMP csNetworkDriverSockets::Accept(DWORD dwLID/*listening socket*/, DWO
 	Socket[*lpdwID] = accept(Socket[dwLID], &VisitorAddress, &AddressLen);
 	if(Socket[*lpdwID] == CS_NET_INVALID_SOCKET)
 	{
-		dwLastError = CS_NET_ERR_CANNOT_ACCEPT;
-		return CS_NET_FAIL;
+		dwLastError = CS_NET_DRV_ERR_CANNOT_ACCEPT;
+		return S_FALSE;
 	}
 #endif
 
 	SocketConnected[*lpdwID] = true;
-	return CS_NET_SUCCESS;
+	return S_OK;
 }
 
 STDMETHODIMP csNetworkDriverSockets::GetDriverCaps(CS_NET_DRIVERCAPS *lpCSNetDriverCaps)
@@ -483,7 +483,7 @@ STDMETHODIMP csNetworkDriverSockets::GetDriverCaps(CS_NET_DRIVERCAPS *lpCSNetDri
 	lpCSNetDriverCaps->bConnOriented = true;
 	lpCSNetDriverCaps->bConnLess = true;
 	lpCSNetDriverCaps->iMaxSockets = CS_NET_MAX_SOCKETS;
-	return CS_NET_SUCCESS;
+	return S_OK;
 }
 
 STDMETHODIMP csNetworkDriverSockets::GetLastError()
