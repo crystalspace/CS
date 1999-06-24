a26512d437ef8990653f2fb2678b31b95c0e9867
Andrew Zabolotny <bit@eltech.ru>
1999-06-24 08:21:38 +0000

    80x86 FPU control routines
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@743 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ support/debug/fpu80x86.h
@@ -0,0 +1,72 @@
+/*
+    Copyright (C) 1998 by Jorrit Tyberghein
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __FPU80X86_H__
+#define __FPU80X86_H__
+
+#if defined (PROC_INTEL) && defined (COMP_GCC)
+
+/*
+     ---- ---- --XX XXXX = MCW_EM - exception masks (1=handle exception internally, 0=fault)
+     ---- ---- ---- ---X = EM_INVALID - invalid operation
+     ---- ---- ---- --X- = EM_DENORMAL - denormal operand
+     ---- ---- ---- -X-- = EM_ZERODIVIDE - divide by zero
+     ---- ---- ---- X--- = EM_OVERFLOW - overflow
+     ---- ---- ---X ---- = EM_UNDERFLOW - underflow
+     ---- ---- --X- ---- = EM_INEXACT - rounding was required
+     ---- --XX ---- ---- = MCW_PC - precision control
+     ---- --00 ---- ---- = PC_24 - single precision
+     ---- --10 ---- ---- = PC_53 - double precision
+     ---- --11 ---- ---- = PC_64 - extended precision
+     ---- XX-- ---- ---- = MCW_RC - rounding control
+     ---- 00-- ---- ---- = RC_NEAR - round to nearest
+     ---- 01-- ---- ---- = RC_DOWN - round towards -Inf
+     ---- 10-- ---- ---- = RC_UP - round towards +Inf
+     ---- 11-- ---- ---- = RC_CHOP - round towards zero
+     ---X ---- ---- ---- = MCW_IC - infinity control (obsolete, always affine)
+     ---0 ---- ---- ---- = IC_AFFINE - -Inf < +Inf
+     ---1 ---- ---- ---- = IC_PROJECTIVE - -Inf == +Inf
+*/
+
+static inline unsigned int _control87(unsigned int newcw, unsigned int mask)
+{
+  int oldcw;
+  int tmpcw;
+  asm __volatile__
+  (
+        "       fclex                   \n"     // clear exceptions
+        "       fstcw   %0              \n"     // oldcw = FPU control word
+        "       andl    %0,%%eax        \n"     // eax &= oldcw;
+        "       andl    %1,%%ecx        \n"     // ecx &= newcw
+        "       orl     %%ecx,%%eax     \n"     // eax |= ecx
+        "       movl    %%eax,%4        \n"     // tmpcw = eax
+        "       fldcw   %4              \n"     // load FPU control word
+        : "=m" (oldcw)
+	: "g" (newcw), "a" (~mask), "c" (mask), "m" (tmpcw)
+	: "eax", "ecx", "memory"
+  );
+  return oldcw & 0xffff;
+}
+
+#else
+
+#define _control87(newcw, mask)	0
+
+#endif
+
+#endif // __FPU80X86_H__
