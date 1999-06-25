faf41f698d330c2fa51a957a265fc5ae28a7b532
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
1999-06-25 13:31:02 +0000

    Fixed layout.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@763 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csengine/objects/thing.cpp
+++ libs/csengine/objects/thing.cpp
@@ -72,11 +72,11 @@ void csThing::SetConvex (bool c)
       if (i != center_idx)
       {
         if (obj_verts[i].x < minx) minx = obj_verts[i].x;
-  if (obj_verts[i].x > maxx) maxx = obj_verts[i].x;
+  	if (obj_verts[i].x > maxx) maxx = obj_verts[i].x;
         if (obj_verts[i].y < miny) miny = obj_verts[i].y;
-  if (obj_verts[i].y > maxy) maxy = obj_verts[i].y;
+  	if (obj_verts[i].y > maxy) maxy = obj_verts[i].y;
         if (obj_verts[i].z < minz) minz = obj_verts[i].z;
-  if (obj_verts[i].z > maxz) maxz = obj_verts[i].z;
+  	if (obj_verts[i].z > maxz) maxz = obj_verts[i].z;
       }
     obj_verts[center_idx].Set ((minx+maxx)/2, (miny+maxy)/2, (minz+maxz)/2);
     wor_verts[center_idx].Set ((minx+maxx)/2, (miny+maxy)/2, (minz+maxz)/2);
@@ -149,8 +149,8 @@ csPolygon3D* csThing::IntersectSphere (csVector3& center, float radius, float* p
       hit.z = d*(-C-center.z)+center.z;
       if (p->IntersectRay (center, hit))
       {
-  min_d = d;
-  min_p = p;
+  	min_d = d;
+  	min_p = p;
       }
     }
   }
@@ -256,60 +256,60 @@ void csThing::DrawCurves (csRenderView& rview, bool use_z_buf)
       bool visible = true;
       int k;
       for (k = 0 ; k < 3 ; k++)
-  if (varr[k]->z >= SMALL_Z)
-  {
-    float iz = csCamera::aspect/varr[k]->z;
-    persp[k].x = varr[k]->x * iz + csWorld::shift_x;
-    persp[k].y = varr[k]->y * iz + csWorld::shift_y;
-  }
-  else
-    visible = false;
+        if (varr[k]->z >= SMALL_Z)
+        {
+    	  float iz = csCamera::aspect/varr[k]->z;
+    	  persp[k].x = varr[k]->x * iz + csWorld::shift_x;
+    	  persp[k].y = varr[k]->y * iz + csWorld::shift_y;
+  	}
+        else
+    	  visible = false;
 
       // Draw all triangles.
       if (visible)
       {
-  //-----
-  // Do backface culling.
-  //-----
-  if (csMath2::Area2 (persp [0].x, persp [0].y,
-          persp [1].x, persp [1].y,
-          persp [2].x, persp [2].y) >= 0)
-    continue;
+  	//-----
+  	// Do backface culling.
+  	//-----
+  	if (csMath2::Area2 (persp [0].x, persp [0].y,
+          	persp [1].x, persp [1].y,
+          	persp [2].x, persp [2].y) >= 0)
+    	  continue;
 
-  // Clip triangle
-  int rescount;
-  csVector2 *cpoly = rview.view->Clip (persp, 3, rescount);
-  if (!cpoly) continue;
+  	// Clip triangle
+  	int rescount;
+  	csVector2 *cpoly = rview.view->Clip (persp, 3, rescount);
+  	if (!cpoly) continue;
 
-  poly.num = rescount;
-  int j;
-  for (j = 0; j < 3; j++)
-  {
-    if (varr[j]->z > 0.0001)
-      poly.vertices [j].z = 1 / varr[j]->z;
-    poly.vertices [j].u = texes[j]->x;
-    poly.vertices [j].v = texes[j]->y;
-    if (gouraud)
-    {
-      int lm_idx = control_y[j]*(lm_width+2) + control_x[j];
-      //@@@ NOT EFFICIENT!
-      poly.vertices[j].r = ((float)mapR[lm_idx])/256.;
-      poly.vertices[j].g = ((float)mapG[lm_idx])/256.;
-      poly.vertices[j].b = ((float)mapB[lm_idx])/256.;
-    }
-  }
+  	poly.num = rescount;
+  	int j;
+  	for (j = 0; j < 3; j++)
+  	{
+    	  if (varr[j]->z > 0.0001)
+      	    poly.vertices [j].z = 1 / varr[j]->z;
+    	  poly.vertices [j].u = texes[j]->x;
+    	  poly.vertices [j].v = texes[j]->y;
+    	  if (gouraud)
+    	  {
+      	    int lm_idx = control_y[j]*(lm_width+2) + control_x[j];
+      	    //@@@ NOT EFFICIENT!
+      	    poly.vertices[j].r = ((float)mapR[lm_idx])/256.;
+      	    poly.vertices[j].g = ((float)mapG[lm_idx])/256.;
+      	    poly.vertices[j].b = ((float)mapB[lm_idx])/256.;
+    	  }
+  	}
 
-  for (j = 0; j < rescount; j++)
-  {
-    poly.vertices [j].sx = cpoly [j].x;
-    poly.vertices [j].sy = cpoly [j].y;
-  }
-  CHK (delete[] cpoly);
+  	for (j = 0; j < rescount; j++)
+  	{
+    	  poly.vertices [j].sx = cpoly [j].x;
+    	  poly.vertices [j].sy = cpoly [j].y;
+  	}
+  	CHK (delete[] cpoly);
 
-  //poly.pi_triangle = ; //DPQFIX
+  	//poly.pi_triangle = ; //DPQFIX
         PreparePolygonQuick (&poly, (csVector2 *)persp, gouraud);
-  // Draw resulting polygon
-  rview.g3d->DrawPolygonQuick (poly);
+  	// Draw resulting polygon
+  	rview.g3d->DrawPolygonQuick (poly);
       }
     }
     rview.g3d->FinishPolygonQuick ();
@@ -406,14 +406,14 @@ void csThing::DrawFoggy (csRenderView& d)
 
         long do_edges;
         d.g3d->GetRenderState (G3DRENDERSTATE_EDGESENABLE, do_edges);
-  if (do_edges)
-  {
-      ITextureManager* txtmgr;
-      d.g3d->GetTextureManager (&txtmgr);
-      int white;
-      txtmgr->FindRGB (255, 255, 255, white);
-          csPolygon2D::clipped.Draw (d.g2d, white);
-  }
+  	if (do_edges)
+  	{
+      	  ITextureManager* txtmgr;
+      	  d.g3d->GetTextureManager (&txtmgr);
+      	  int white;
+      	  txtmgr->FindRGB (255, 255, 255, white);
+            csPolygon2D::clipped.Draw (d.g2d, white);
+  	}
       }
     }
     d.SetMirrored (!d.IsMirrored ());
@@ -446,14 +446,14 @@ void csThing::DrawFoggy (csRenderView& d)
 
         long do_edges;
         d.g3d->GetRenderState (G3DRENDERSTATE_EDGESENABLE, do_edges);
-  if (do_edges)
-  {
-      ITextureManager* txtmgr;
-      d.g3d->GetTextureManager (&txtmgr);
-      int white;
-      txtmgr->FindRGB (255, 255, 255, white);
-          csPolygon2D::clipped.Draw (d.g2d, white);
-  }
+  	if (do_edges)
+  	{
+      	  ITextureManager* txtmgr;
+      	  d.g3d->GetTextureManager (&txtmgr);
+      	  int white;
+      	  txtmgr->FindRGB (255, 255, 255, white);
+            csPolygon2D::clipped.Draw (d.g2d, white);
+  	}
       }
     }
     d.g3d->CloseFogObject (GetID ());
@@ -632,9 +632,9 @@ void csThing::MergeTemplate (csThingTemplate* tpl,
   }
 
   for (i=0;i< tpl->GetNumCurveVertices();i++)
-    {
-      AddCurveVertex (tpl->CurveVertex (i),tpl->CurveTexel (i));
-    }
+  {
+    AddCurveVertex (tpl->CurveVertex (i),tpl->CurveTexel (i));
+  }
 
   for (i = 0 ; i < tpl->GetNumCurves () ; i++)
   {
