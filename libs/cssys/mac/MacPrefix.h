3705b5bd652d4c74c57c0579f433ed57a0e32f62
Robert Bate <rbate@mac.com>
1999-07-16 17:06:52 +0000

    Removing defines that are now defined in volatile.h.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@992 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/cssys/mac/MacPrefix.h
+++ libs/cssys/mac/MacPrefix.h
@@ -2,23 +2,9 @@
 
 // some mac specific defines
 
-#define COMP_MWERKS		1
-#define PROC_POWERPC	1
-#define OS_MACOS		1
-
 #define NO_COM_SUPPORT	1
 
 #define __NEED_OPENDIR_PROTOTYPE	1
-
-#define DO_GIF		1
-#define DO_JPG		1
-#define DO_PNG		1
-
-#define DO_SOUND	1
-#define DO_AIFF		1
-#define DO_AU		1
-#define DO_WAV		1
-#define DO_IFF		1
 
 #define SCAN_KEYBOARD		1
 
