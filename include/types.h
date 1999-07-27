6a64ef52d5e96c336a13dde3e97fd48173b51f4a
Eric Sunshine <sunshine@sunshineco.com>
1999-07-27 07:08:36 +0000

    Renamed the multiple-inclusion protection macro from TYPES_H to CS_TYPES_H since TYPES_H is far too common and could potentially conflict. Added copyright and LGPL notice.  Updated comments. Now includes new platform.h which provides definition of NO_BOOL_TYPE if needed, since NO_BOOL_TYPE is no longer specified on the command line.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@1162 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/types.h
+++ include/types.h
@@ -1,24 +1,43 @@
-#ifndef TYPES_H
-#define TYPES_H
+/*
+    Copyright (C) 1998 by Jorrit Tyberghein
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
-//----------------
+#ifndef __CS_TYPES_H
+#define __CS_TYPES_H
+
+#include "platform.h"
+
+//-----------------------------------------------------------------------------
 // If your compiler complains about 'true', 'false', and 'bool' it
 // may be an older C++ compiler which doesn't understand these constructs.
-// In that case, define -DNO_BOOL_TYPE in the makefile or here.
-//----------------
-//#define NO_BOOL_TYPE
+// In that case, set NEED_FAKE_BOOL to 'yes' in the makefile, or define the
+// C-macro NO_BOOL_TYPE in your project file.
+//-----------------------------------------------------------------------------
 #ifdef NO_BOOL_TYPE
-    typedef int bool;
-#   undef true
-#   define true 1
+typedef int bool;
+#undef  true
+#define true 1
+#undef  false
+#define false 0
+#endif
 
-#   undef false
-#   define false 0
-#endif /*NO_BOOL_TYPE*/
-
-//----------------
+//-----------------------------------------------------------------------------
 // Make sure the following six types are correct for your system.
-//----------------
+//-----------------------------------------------------------------------------
 typedef unsigned char UByte;	// Unsigned 8 bit value
 typedef signed char SByte;	// Signed 8 bit value
 typedef unsigned short UShort;	// Unsigned 16 bit value
@@ -30,4 +49,4 @@ typedef signed int SInt; 	// Signed int value (16..32 bit)
 
 typedef unsigned long CS_ID;    // Used for uniquely generated id numbers 
 
-#endif
+#endif // __CS_TYPES_H
