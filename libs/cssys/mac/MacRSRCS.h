5618cd3b5343f2efe69d1f02c79469301463146f
Robert Bate <rbate@mac.com>
1999-07-29 13:22:28 +0000

    New file containing the defines for the resource IDs.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@1214 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ libs/cssys/mac/MacRSRCS.h
@@ -0,0 +1,26 @@
+/*
+    This header file contains all definitions needed for the resources
+ */
+#ifndef MACRSRCS_H
+#define MACRSRCS_H
+
+#define kArrowCursor				128
+
+#define kGeneralErrorDialog			1026
+#define kAskForDepthChangeDialog	1027
+
+#define kErrorStrings				1025
+#define kBadDepthString				1
+#define kNoDSContext				2
+#define kUnableToOpenDSContext		3
+#define kUnableToReserveDSContext	4
+#define kFatalErrorInGlide			5
+#define kFatalErrorInOpenGL2D		6
+#define kFatalErrorInOpenGL3D		7
+#define kFatalErrorOutOfMemory		8
+#define kFatalErrorInDriver2D		9
+#define kFatalErrorInRave2D			10
+#define kFatalErrorInRave3D			11
+#define kFatalErrorCantStartRave	12
+
+#endif // MACRSRCS_H
