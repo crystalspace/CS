19a0a06f6db62e74d1b0572901e389de139f7748
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
1999-08-03 17:54:34 +0000

    Small change.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@1250 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csparser/crossbld.cpp
+++ libs/csparser/crossbld.cpp
@@ -391,6 +391,7 @@ void csCrossBuild_ThingTemplateFactory::Build_TriangleMesh(csThingTemplate& mesh
     ptemp->SetUV (0, buildsource.cor3_uv[0][a], buildsource.cor3_uv[1][a]);
     ptemp->SetUV (1, buildsource.cor3_uv[0][b], buildsource.cor3_uv[1][b]);
     ptemp->SetUV (2, buildsource.cor3_uv[0][c], buildsource.cor3_uv[1][c]);
+    ptemp->SetGouraud ();
     meshsource.AddPolygon (ptemp);
   }
 }
