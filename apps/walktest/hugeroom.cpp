2b3690b60019e92073d9f5a66c66c8fab0efe570
Eric Sunshine <sunshine@sunshineco.com>
1999-08-20 22:42:57 +0000

    Fixed a new "uninitialized variable" compilation warning.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@1396 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/walktest/hugeroom.cpp
+++ apps/walktest/hugeroom.cpp
@@ -99,7 +99,7 @@ csPolygon3D* HugeRoom::create_polygon (csSector* sector, csPolygonSet* thing,
   csMatrix3 t_m;
   csVector3 t_v (0, 0, 0);
 
-  csTextureHandle* tm;
+  csTextureHandle* tm = NULL;
   switch (txt)
   {
     case 0: tm = NULL; break;
