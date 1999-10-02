a034238f11fdc98a38bef026ac6cc8e1acb486ae
Thomas Hieber <thieber@gmx.net>
1999-10-02 08:24:43 +0000

    Moved Win32 to COM emulation. (No more native COM by default)
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@1660 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/cssys/win32/volatile.h
+++ include/cssys/win32/volatile.h
@@ -5,22 +5,23 @@
 #ifndef __VOLATILE_H__
 #define __VOLATILE_H__
 
-#define OS_WIN32
-#define PROC_INTEL
-#define COMP_VC
-#define DO_SOUND
-#define DO_GIF
-#define DO_BMP
-#define DO_TGA
-#define DO_PNG
-#define DO_JPG
-#define DO_AIFF
-#define DO_IFF
-#define DO_WAV
-#define DO_AU
-#define DO_MMX
-#define ZLIB_DLL
-#undef DO_DINPUT_KEYBOARD // undefine this if you have problems with keyboard handling
+  #define OS_WIN32
+  #define PROC_INTEL
+  #define COMP_VC
+  #define DO_SOUND
+  #define DO_GIF
+  #define DO_BMP
+  #define DO_TGA
+  #define DO_PNG
+  #define DO_JPG
+  #define DO_AIFF
+  #define DO_IFF
+  #define DO_WAV
+  #define DO_AU
+  #define DO_MMX
+  #define ZLIB_DLL
+  #define NO_COM_SUPPORT    // undefine this, if you want native COM on Windows.
+  #undef DO_DINPUT_KEYBOARD // undefine this if you have problems with keyboard handling
 
   //#ifdef _DEBUG
     //Right now, Inline assembler doesn' work any more on MSVC, 
