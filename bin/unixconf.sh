53970d3d1ccd9de68e764ed296788a57d66dca5e
Andrew Zabolotny <bit@eltech.ru>
1999-10-28 09:28:02 +0000

    Added Python include/lib directory detection
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@1931 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- bin/unixconf.sh
+++ bin/unixconf.sh
@@ -107,11 +107,22 @@ if [ -n "${MAKEDEP}" ]; then
 fi
 
 # Look where is X11 directory
-# lib/ or include/ ? (Dmitry)
 ([ -d /usr/X11 ] && echo "X11_PATH = /usr/X11") || \
 ([ -d /usr/X11R6 ] && echo "X11_PATH = /usr/X11R6") || \
 ([ -d /usr/openwin ] && echo "X11_PATH = /usr/openwin") || \
 ([ -d /usr/lib/X11 ] && echo "X11_PATH = /usr/lib/X11") || \
 (echo "$0: Cannot find X11 directory!" >&2 && exit 1)
+
+# Find the Python header/library directory
+[ -z "${PYTHON_INC}" ] && PYTHON_INC=`ls -d /usr/include/python* 2>/dev/null`
+[ -z "${PYTHON_INC}" ] && PYTHON_INC=`ls -d /usr/local/include/python* 2>/dev/null`
+[ -z "${PYTHON_LIB}" ] && PYTHON_LIB=`ls -d /usr/lib/python* 2>/dev/null`
+[ -z "${PYTHON_LIB}" ] && PYTHON_LIB=`ls -d /usr/local/lib/python* 2>/dev/null`
+
+if [ -n "${PYTHON_INC}" -a -n "${PYTHON_LIB}" ]; then
+# echo "Found Python headers in ${PYTHON_INC}, libs in ${PYTHON_LIB}" >&2
+  echo "PYTHON_INC = ${PYTHON_INC}"
+  echo "PYTHON_LIB = ${PYTHON_LIB}"
+fi
 
 exec ${SCRIPT_DIR}/booltest.sh ${CXX}
