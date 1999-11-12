caed807b7466fb9774a2f0514d4a1b335658c802
Brandon Ehle <azverkan@yahoo.com>
1999-11-12 04:18:18 +0000

    cspython is working with SCF, so is cspythontest
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2054 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/tests.h
+++ include/tests.h
@@ -1,5 +1,5 @@
 #define TEST_FAILED(msg) { printf(csString("Test Failed: ")+msg+csString("\n")); fatal_exit(-1, 0); }
-#define TEST_SUCCEED() { printf("Test Succeeded!"); }
+#define TEST_SUCCEED() { printf("Test Succeeded!\n"); }
 
 #define TEST_SETUP() \
 void cleanup() {} \
