98b621805cb2a647a649f85946f19f0aa1cfa893
Robert Bate <rbate@mac.com>
1999-11-19 19:56:04 +0000

    Changes for scf.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2171 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/canvas/mac/ISysG2D.h
+++ plugins/video/canvas/mac/ISysG2D.h
@@ -8,24 +8,19 @@
 /// iMacGraphicsInfo interface -- for Mac-specific properties.
 SCF_INTERFACE (iMacGraphicsInfo, 0, 0, 1) : public iBase
 {
+    virtual void ActivateWindow( WindowPtr theWindow, bool active ) = 0;
     ///
-    STDMETHOD(Open)(char* szTitle) = 0;
+    virtual void UpdateWindow( WindowPtr theWindow, bool *updated ) = 0;
     ///
-    STDMETHOD(Close)() = 0;
+    virtual void PointInWindow( Point *thePoint, bool *inWindow ) = 0;
     ///
-    STDMETHOD(ActivateWindow)( WindowPtr theWindow, bool active ) = 0;
+    virtual void DoesDriverNeedEvent( bool *enabled ) = 0;
     ///
-    STDMETHOD(UpdateWindow)( WindowPtr theWindow, bool *updated ) = 0;
+    virtual void SetColorPalette( void ) = 0;
     ///
-    STDMETHOD(PointInWindow)( Point *thePoint, bool *inWindow ) = 0;
+    virtual void WindowChanged( void ) = 0;
     ///
-    STDMETHOD(DoesDriverNeedEvent)( bool *enabled ) = 0;
-    ///
-    STDMETHOD(SetColorPalette)( void ) = 0;
-    ///
-    STDMETHOD(WindowChanged)( void ) = 0;
-    ///
-    STDMETHOD(HandleEvent)( EventRecord *inEvent, bool *outEventWasProcessed ) = 0;
+    virtual void HandleEvent( EventRecord *inEvent, bool *outEventWasProcessed ) = 0;
 };
 
 #endif
