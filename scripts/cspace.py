d82f941e62caab0879c7ccd11f0ae4973037314a
Brandon Ehle <azverkan@yahoo.com>
1999-11-22 05:05:30 +0000

    almost got unreal working
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2185 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- scripts/cspace.py
+++ scripts/cspace.py
@@ -1,54 +1,353 @@
 # This file was created automatically by SWIG.
 import cspacec
-class Vector3Ptr :
+class iBasePtr :
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __repr__(self):
+        return "<C iBase instance>"
+class iBase(iBasePtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class iPlugInPtr(iBasePtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __repr__(self):
+        return "<C iPlugIn instance>"
+class iPlugIn(iPlugInPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class iGraphics3DPtr(iPlugInPtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __repr__(self):
+        return "<C iGraphics3D instance>"
+class iGraphics3D(iGraphics3DPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class iCameraPtr(iBasePtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __repr__(self):
+        return "<C iCamera instance>"
+class iCamera(iCameraPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csVector3Ptr :
     def __init__(self,this):
         self.this = this
         self.thisown = 0
     def __str__(self):
-        val = cspacec.Vector3___str__(self.this)
+        val = cspacec.csVector3___str__(self.this)
         return val
     def __setattr__(self,name,value):
         if name == "x" :
-            cspacec.Vector3_x_set(self.this,value)
+            cspacec.csVector3_x_set(self.this,value)
             return
         if name == "y" :
-            cspacec.Vector3_y_set(self.this,value)
+            cspacec.csVector3_y_set(self.this,value)
             return
         if name == "z" :
-            cspacec.Vector3_z_set(self.this,value)
+            cspacec.csVector3_z_set(self.this,value)
             return
         self.__dict__[name] = value
     def __getattr__(self,name):
         if name == "x" : 
-            return cspacec.Vector3_x_get(self.this)
+            return cspacec.csVector3_x_get(self.this)
         if name == "y" : 
-            return cspacec.Vector3_y_get(self.this)
+            return cspacec.csVector3_y_get(self.this)
         if name == "z" : 
-            return cspacec.Vector3_z_get(self.this)
+            return cspacec.csVector3_z_get(self.this)
         raise AttributeError,name
     def __repr__(self):
-        return "<C Vector3 instance>"
-class Vector3(Vector3Ptr):
+        return "<C csVector3 instance>"
+class csVector3(csVector3Ptr):
     def __init__(self,arg0,arg1,arg2) :
-        self.this = cspacec.new_Vector3(arg0,arg1,arg2)
+        self.this = cspacec.new_csVector3(arg0,arg1,arg2)
         self.thisown = 1
 
 
 
-def Vector3n() :
-    val = Vector3Ptr(cspacec.new_Vector3n())
-    val.thisown = 1
-    return val
 
-def Vector3s(arg0) :
-    val = Vector3Ptr(cspacec.new_Vector3s(arg0))
-    val.thisown = 1
-    return val
+class csBasePtr :
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __del__(self):
+        if self.thisown == 1 :
+            cspacec.delete_csBase(self.this)
+    def __repr__(self):
+        return "<C csBase instance>"
+class csBase(csBasePtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csObjectPtr(csBasePtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def SetName(self,arg0):
+        val = cspacec.csObject_SetName(self.this,arg0)
+        return val
+    def __repr__(self):
+        return "<C csObject instance>"
+class csObject(csObjectPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csTextureHandlePtr(csObjectPtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __repr__(self):
+        return "<C csTextureHandle instance>"
+class csTextureHandle(csTextureHandlePtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csPolygon3DPtr(csObjectPtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def AddVertex(self,arg0,arg1,arg2):
+        val = cspacec.csPolygon3D_AddVertex(self.this,arg0,arg1,arg2)
+        return val
+    def SetTextureSpace(self,arg0,arg1,arg2):
+        val = cspacec.csPolygon3D_SetTextureSpace(self.this,arg0.this,arg1.this,arg2)
+        return val
+    def Vobj(self,arg0):
+        val = cspacec.csPolygon3D_Vobj(self.this,arg0)
+        val = csVector3Ptr(val)
+        return val
+    def __repr__(self):
+        return "<C csPolygon3D instance>"
+class csPolygon3D(csPolygon3DPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csPolygonSetPtr(csObjectPtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def NewPolygon(self,arg0):
+        val = cspacec.csPolygonSet_NewPolygon(self.this,arg0.this)
+        val = csPolygon3DPtr(val)
+        return val
+    def __repr__(self):
+        return "<C csPolygonSet instance>"
+class csPolygonSet(csPolygonSetPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csSectorPtr(csPolygonSetPtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __repr__(self):
+        return "<C csSector instance>"
+class csSector(csSectorPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csWorldPtr :
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def NewSector(self):
+        val = cspacec.csWorld_NewSector(self.this)
+        val = csSectorPtr(val)
+        return val
+    def __setattr__(self,name,value):
+        if name == "view" :
+            cspacec.csWorld_view_set(self.this,value.this)
+            return
+        self.__dict__[name] = value
+    def __getattr__(self,name):
+        if name == "view" : 
+            return csViewPtr(cspacec.csWorld_view_get(self.this))
+        raise AttributeError,name
+    def __repr__(self):
+        return "<C csWorld instance>"
+class csWorld(csWorldPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csLoaderPtr :
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __del__(self):
+        if self.thisown == 1 :
+            cspacec.delete_csLoader(self.this)
+    def LoadTexture(self,arg0,arg1,arg2):
+        val = cspacec.csLoader_LoadTexture(self.this,arg0.this,arg1,arg2)
+        val = csTextureHandlePtr(val)
+        return val
+    def __repr__(self):
+        return "<C csLoader instance>"
+class csLoader(csLoaderPtr):
+    def __init__(self) :
+        self.this = cspacec.new_csLoader()
+        self.thisown = 1
+
+
+
+
+class csTransformPtr :
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __repr__(self):
+        return "<C csTransform instance>"
+class csTransform(csTransformPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csReversibleTransformPtr(csTransformPtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __repr__(self):
+        return "<C csReversibleTransform instance>"
+class csReversibleTransform(csReversibleTransformPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csOrthoTransformPtr(csReversibleTransformPtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __repr__(self):
+        return "<C csOrthoTransform instance>"
+class csOrthoTransform(csOrthoTransformPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csCameraPtr(csOrthoTransformPtr,iCameraPtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def SetPosition(self,arg0):
+        val = cspacec.csCamera_SetPosition(self.this,arg0.this)
+        return val
+    def __repr__(self):
+        return "<C csCamera instance>"
+class csCamera(csCameraPtr):
+    def __init__(self,this):
+        self.this = this
+
+
+
+
+class csViewPtr(csBasePtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def __del__(self):
+        if self.thisown == 1 :
+            cspacec.delete_csView(self.this)
+    def SetSector(self,arg0):
+        val = cspacec.csView_SetSector(self.this,arg0.this)
+        return val
+    def GetCamera(self):
+        val = cspacec.csView_GetCamera(self.this)
+        val = csCameraPtr(val)
+        return val
+    def SetRectangle(self,arg0,arg1,arg2,arg3):
+        val = cspacec.csView_SetRectangle(self.this,arg0,arg1,arg2,arg3)
+        return val
+    def __repr__(self):
+        return "<C csView instance>"
+class csView(csViewPtr):
+    def __init__(self,arg0,arg1) :
+        self.this = cspacec.new_csView(arg0.this,arg1.this)
+        self.thisown = 1
+
+
+
+
+class iSystemPtr(iBasePtr):
+    def __init__(self,this):
+        self.this = this
+        self.thisown = 0
+    def Query_csWorld(self):
+        val = cspacec.iSystem_Query_csWorld(self.this)
+        val = csWorldPtr(val)
+        return val
+    def Query_iGraphics3D(self):
+        val = cspacec.iSystem_Query_iGraphics3D(self.this)
+        val = iGraphics3DPtr(val)
+        return val
+    def __repr__(self):
+        return "<C iSystem instance>"
+class iSystem(iSystemPtr):
+    def __init__(self,this):
+        self.this = this
+
+
 
 
 
 
 #-------------- FUNCTION WRAPPERS ------------------
+
+def GetSystem():
+    val = cspacec.GetSystem()
+    val = iSystemPtr(val)
+    return val
+
+GetFrameHeight = cspacec.GetFrameHeight
+
+GetFrameWidth = cspacec.GetFrameWidth
 
 
 
---
+++ scripts/unrmap.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+
+import types, cPickle
+from string import *;
+
+try:
+	import cspacec;
+	from cspace import *;
+except:
+    	print "Failed to import cspace, can only pickle the map"
+	class Vector3:
+		def __init__(self, x, y, z):
+			print x, y, z
+	
+class Actor:
+	"""An actor object from Unreal"""
+	def __init__(self, attrib):
+	        self.attrib=attrib
+	        self.stack=[]
+	def Add(self, attrib):
+		pass
+	def Push(self, child):
+		self.stack.append(child)
+	def Load(self, room, tm):
+		for x in self.stack:
+			x.Load(room, tm)
+
+def ListToVector3(list):
+	for x in list:
+		csVector3(x[0], x[1], x[2])
+
+class Polygon(Actor):
+    	"""A polygons"""
+	def __init__(self, attrib):
+	        self.attrib=attrib
+	        self.veclist=[]
+	def AddPoint(self, vec):
+		self.veclist.append(float(vec[0]), float(vec[1]), float(vec[2]))
+	def Load(self, room, tm):
+		poly=room.NewPolygon(tm)
+		for x in self.veclist:
+			poly.AddVertex(x[0], x[1], x[2])
+		poly.SetTextureSpace(poly.Vobj(0), poly.Vobj(1), 3);
+		
+class PolyList(Actor):
+    	"""A list of polygons"""
+	pass
+		
+class Brush(Actor):
+    	"""A geometry object from Unreal"""
+	pass
+	
+class Map(Actor):
+	"""A pickleable map"""
+	def Load(self):
+		loader=csLoader()
+		system=GetSystem()
+		world=system.Query_csWorld()
+		room=world.NewSector()
+		tm=loader.LoadTexture(world, 'stone', '/lib/std/stone4.gif')
+		for x in self.stack:
+			x.Load(room, tm)
+		g3d=system.Query_iGraphics3D()
+#		view=csView(world, g3d)
+#		view.SetSector(room)
+#		view.GetCamera().SetPosition(csVector3(0,5,0))
+#		view.SetRectangle(2, 2, GetFrameWidth()-4, GetFrameHeight()-4)
+#		world.view=view
+
+class UnrealMap:
+	"""Import an Unreal Map and Load it into Engine, or pickle it"""
+	def Load(self, file):
+		if(type(file)==types.StringType):
+			file=open(file, 'r')
+		self.stack=[]
+		self.file=file;
+		self.Loop()
+		self.map=self.block
+	def Save(self, file):
+		if(type(file)==types.StringType):
+			file=open(file, 'w+')
+		cPickle.dump(self.map, file)		
+	def Parse(self):
+		self.NextLine()
+		if(not len(self.line)):
+			return 0
+		self.words=split(self.line)
+		if(self.words[0]=='Begin'):
+			self.Push()
+			self.Begin()
+			return 1
+		if(self.words[0]=='End'):
+			self.Pop()
+			return 0
+		self.block.Add(self.Params(0))
+		return 1
+	def NextLine(self):
+		self.line=self.file.readline()
+	def Loop(self):
+		while(self.Parse()):
+			pass
+	def Begin(self):
+		if(self.words[1]=='Map'):
+			attrib=self.Params(2)
+			self.block=Map(attrib)
+			print "Found Map", attrib
+			self.Loop()
+			return
+		if(self.words[1]=='Actor'):
+			self.Actor()
+			return
+		if(self.words[1]=='Brush'):
+			self.block=Brush(self.Params(2))
+			self.Loop()
+			return
+		if(self.words[1]=='PolyList'):
+			self.block=PolyList(self.Params(2))
+			self.Loop()
+			return
+		if(self.words[1]=='Polygon'):
+			self.Polygon()
+			return
+		print "WARNING: Ignoring Unknown: ", self.words[1]
+		self.block=None
+		self.Loop()
+	def Polygon(self):
+		self.block=Polygon(self.Params(2))
+		for i in range(1,4):
+			self.NextLine();
+		for i in range(1,4):
+			self.PolyAdd()
+		self.Loop()
+	def PolyAdd(self):
+		self.NextLine();
+		self.words=split(self.line)
+		vecs=split(self.words[1], ',')
+		self.block.AddPoint(vecs)
+	def Actor(self):
+		attrib=self.Params(2)
+		self.block=Actor(attrib)
+		self.Loop()
+	def Pop(self):
+		if(len(self.stack)):
+#			print "Pop: ", self.stack[0], self.block
+			self.stack[0].Push(self.block)
+			self.block=self.stack[0]
+			del self.stack[0]
+	def Push(self):
+		if(hasattr(self, 'block')):
+			self.stack.insert(0, self.block)
+#			print "Push: ", self.block
+	def Params(self, index):
+		params={}
+		for x in self.words[index:]:
+			word=split(x, '=')
+			if(len(word)==2):
+				params[word[0]]=word[1]
+		return params
+		
+m=UnrealMap()
+m.Load('bluff.t3d')
+m.Save('bluff.pym')
+
+a=cPickle.load(open('bluff.pym','r'))
+a.Load()
+
