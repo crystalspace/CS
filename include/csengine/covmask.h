aded74c1a5deb25a4862a172cc959797f3ba2f73
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
1999-11-22 14:10:30 +0000

    New csCovMask and LUT. This is the bases for the coverage mask tree optimization. Currently not used though.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2188 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/csengine/covmask.h
@@ -0,0 +1,197 @@
+/*
+    Copyright (C) 1999 by Jorrit Tyberghein
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef COVMASK_H
+#define COVMASK_H
+
+// Set to mask4x4, mask8x4 or mask8x8 to select coverage mask size.
+#define CS_CM mask4x4
+
+/**
+ * Class representing one coverage mask.
+ */
+class csCovMask
+{
+private:
+# if CS_CM == mask4x4
+#   define CS_CM_HOR 4
+#   define CS_CM_VER 4
+    UShort out, in;
+# elif CS_CM == mask8x4
+#   define CS_CM_HOR 8
+#   define CS_CM_VER 4
+    ULong out, in;
+# elif CS_CM == mask8x8
+#   define CS_CM_HOR 8
+#   define CS_CM_VER 8
+    ULong out1, out2, in1, in2;
+# endif
+# define CS_CM_BITS (CS_CM_HOR*CS_CM_VER)
+
+public:
+  /// Clear a coverage mask.
+  void Clear ()
+  {
+#   if CS_CM == mask4x4 || CS_CM == mask8x4
+      out = in = 0;
+#   elif CS_CM == mask8x8
+      out1 = out2 = in1 = in2 = 0;
+#   endif
+  }
+
+  /**
+   * Set the state of two coverage mask bits.
+   * 'so' and 'si' are 0 or 1.
+   * 'bit' is the index in the mask (between 0 and CS_CM_BITS-1).
+   */
+  void SetState (int bit, int so, int si)
+  {
+#   if CS_CM == mask4x4 || CS_CM == mask8x4
+      out = (out & ~(1<<bit)) | (so<<bit);
+      in = (in & ~(1<<bit)) | (si<<bit);
+#   elif CS_CM == mask8x8
+      if (bit < 32)
+      {
+	out1 = (out1 & ~(1<<bit)) | (so<<bit);
+	in1 = (in1 & ~(1<<bit)) | (si<<bit);
+      }
+      else
+      {
+	bit -= 32;
+	out2 = (out2 & ~(1<<bit)) | (so<<bit);
+	in2 = (in2 & ~(1<<bit)) | (si<<bit);
+      }
+#   endif
+  }
+};
+
+/**
+ * Class representing one end-point coverage mask.
+ * An end-point coverage mask only holds one bit
+ * per mask point instead of two. In other words, you
+ * only have inside/outside information and not 'on
+ * the edge' information.
+ */
+class csCovMaskSingle
+{
+private:
+# if CS_CM == mask4x4
+  UShort in;
+# elif CS_CM == mask8x4
+  ULong in;
+# elif CS_CM == mask8x8
+  ULong in1, in2;
+# endif
+
+public:
+  /// Clear a coverage mask.
+  void Clear ()
+  {
+#   if CS_CM == mask4x4
+      in = 0;
+#   elif CS_CM == mask8x4
+      in = 0;
+#   elif CS_CM == mask8x8
+      in1 = in2 = 0;
+#   endif
+  }
+
+  /**
+   * Set the state of mask bits.
+   * 's' is 0 or 1.
+   * 'bit' is the index in the mask (between 0 and CS_CM_BITS-1).
+   */
+  void SetState (int bit, int s)
+  {
+#   if CS_CM == mask4x4 || CS_CM == mask8x4
+      in = (in & ~(1<<bit)) | (s<<bit);
+#   elif CS_CM == mask8x8
+      if (bit < 32)
+      {
+	in1 = (in1 & ~(1<<bit)) | (s<<bit);
+      }
+      else
+      {
+	bit -= 32;
+	in2 = (in2 & ~(1<<bit)) | (s<<bit);
+      }
+#   endif
+  }
+};
+
+/**
+ * This class represents a lookup table for our coverage
+ * masks. It is used by the coverage mask tree. The input
+ * for this LUT is a line crossing a box. The two intersections
+ * of the line with the box are given with two edge indices.
+ * An edge index is a number between 0 and n-1 with n beingu
+ * equal to dimension*2+dimension*2.
+ * The returned mask indicates the 'outside' of the line,
+ * the 'inside' of the line, and everything on the line.
+ */
+class csCovMaskLUT
+{
+private:
+  // The dimensions of the box in integer units.
+  int dimension;
+  // Total number of possible edge intersections with box (i.e. 4*dimension).
+  int num_edge_points;
+  // Shift value to use instead of multiplying with num_edge_points.
+  int dim_shift;
+  // LUT.
+  csCovMask* masks;
+  // LUT.
+  csCovMaskSingle* masks_single;
+
+  /// Build the lookup tables.
+  void BuildTables ();
+
+public:
+  /**
+   * Constructor. The 'dimension' argument defines
+   * the number of divisions to make horizontally and
+   * vertically. Note that only powers of two are allowed.
+   */
+  csCovMaskLUT (int dimension);
+
+  /// Destructor.
+  ~csCovMaskLUT ();
+
+  /**
+   * Return a coverage mask for the given line.
+   * 'from' and 'to' are indices of the intersection between
+   * the line and the box edges (between 0 and num_edge_points-1).
+   */
+  csCovMask& GetMask (int from, int to)
+  {
+    return masks[(from<<dim_shift) + to];
+  }
+
+  /**
+   * Return a coverage mask for the given line.
+   * 'from' and 'to' are indices of the intersection between
+   * the line and the box edges (between 0 and num_edge_points-1).
+   */
+  csCovMaskSingle& GetMaskSingle (int from, int to)
+  {
+    return masks_single[(from<<dim_shift) + to];
+  }
+};
+
+#endif /*COVMASK_H*/
+
---
+++ libs/csengine/covmask.cpp
@@ -0,0 +1,233 @@
+/*
+    Copyright (C) 1999 by Jorrit Tyberghein
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "sysdef.h"
+#include "csgeom/math2d.h"
+#include "csengine/covmask.h"
+
+csCovMaskLUT::csCovMaskLUT (int dimension)
+{
+  csCovMaskLUT::dimension = dimension;
+  num_edge_points = dimension*4;
+  CHK (masks = new csCovMask[num_edge_points*num_edge_points]);
+  CHK (masks_single = new csCovMaskSingle[num_edge_points*num_edge_points]);
+  switch (dimension)
+  {
+    case 1: dim_shift = 0+2; break;
+    case 2: dim_shift = 1+2; break;
+    case 4: dim_shift = 2+2; break;
+    case 8: dim_shift = 3+2; break;
+    case 16: dim_shift = 4+2; break;
+    case 32: dim_shift = 5+2; break;
+    case 64: dim_shift = 6+2; break;
+    case 128: dim_shift = 7+2; break;
+    case 256: dim_shift = 8+2; break;
+  }
+  // build lookup table here.
+  BuildTables ();
+}
+
+csCovMaskLUT::~csCovMaskLUT ()
+{
+  CHK (delete [] masks);
+  CHK (delete [] masks_single);
+}
+
+void calc_from_point (int from, int dimension, int& from_ix1, int& from_iy1,
+	int& from_ix2, int& from_iy2)
+{
+  // Calculate two integer x and y's on the edges of the box
+  // corresponding with the 'from' point.
+  // These two positions define the left and right starting
+  // points of the double line segment intersecting the box.
+  if (from < dimension*2)
+  {
+    from_iy1 = (from / dimension) * dimension;
+    from_iy2 = from_iy1;
+    if (from_iy1 == 0)
+    {
+      from_ix2 = from % dimension;
+      from_ix1 = from_ix2+1;
+    }
+    else
+    {
+      from_ix1 = from % dimension;
+      from_ix2 = from_ix1+1;
+    }
+  }
+  else
+  {
+    from_ix1 = ((from-dimension*2) / dimension) * dimension;
+    from_ix2 = from_ix1;
+    if (from_ix1 == 0)
+    {
+      from_iy1 = from % dimension;
+      from_iy2 = from_iy1+1;
+    }
+    else
+    {
+      from_iy2 = from % dimension;
+      from_iy1 = from_iy2+1;
+    }
+  }
+}
+
+void calc_to_point (int to, int dimension, int& to_ix1, int& to_iy1,
+	int& to_ix2, int& to_iy2)
+{
+  // Calculate two integer x and y's on the edges of the box
+  // corresponding with the 'to' point.
+  // These two positions define the left and right ending
+  // points of the double line segment intersecting the box.
+  if (to < dimension*2)
+  {
+    to_iy1 = (to / dimension) * dimension;
+    to_iy2 = to_iy1;
+    if (to_iy1 == 0)
+    {
+      to_ix1 = to % dimension;
+      to_ix2 = to_ix1+1;
+    }
+    else
+    {
+      to_ix2 = to % dimension;
+      to_ix1 = to_ix2+1;
+    }
+  }
+  else
+  {
+    to_ix1 = ((to-dimension*2) / dimension) * dimension;
+    to_ix2 = to_ix1;
+    if (to_ix1 == 0)
+    {
+      to_iy2 = to % dimension;
+      to_iy1 = to_iy2+1;
+    }
+    else
+    {
+      to_iy1 = to % dimension;
+      to_iy2 = to_iy1+1;
+    }
+  }
+}
+
+/*
+   For dimension equal to 4 the possible edge positions
+   are defined as follows:
+
+       0  1  2  3
+      +--+--+--+--+
+    8 |  |  |  |  | 12
+      +--+--+--+--+
+    9 |  |  |  |  | 13
+      +--+--+--+--+
+   10 |  |  |  |  | 14
+      +--+--+--+--+
+   11 |  |  |  |  | 15
+      +--+--+--+--+
+       4  5  6  7
+ */
+void csCovMaskLUT::BuildTables ()
+{
+  int from, to, ix, iy;
+  int bit;
+
+  // Allocate a left and right matrix for all boxes defined in
+  // a coverage mask. See below for info how left and right are filled.
+  CHK (bool* left = new bool[(CS_CM_HOR+1)*(CS_CM_VER+1)]);
+  CHK (bool* right = new bool[(CS_CM_HOR+1)*(CS_CM_VER+1)]);
+  
+  for (from = 0 ; from < num_edge_points ; from++)
+  {
+    // Calculate two integer x and y's on the edges of the box
+    // corresponding with the 'from' point.
+    // These two positions define the left and right starting
+    // points of the double line segment intersecting the box.
+    int from_ix1, from_iy1;
+    int from_ix2, from_iy2;
+    calc_from_point (from, dimension, from_ix1, from_iy1, from_ix2, from_iy2);
+
+    for (to = 0 ; to < num_edge_points ; to++)
+    {
+      // We have an edge from 'from' to 'to'.
+
+      // If 'from' and 'to' are on the same edge then we
+      // skep this case.
+      if (from / dimension == to / dimension) continue;
+
+      // Calculate two integer x and y's on the edges of the box
+      // corresponding with the 'to' point.
+      // These two positions define the left and right ending
+      // points of the double line segment intersecting the box.
+      int to_ix1, to_iy1;
+      int to_ix2, to_iy2;
+      calc_to_point (to, dimension, to_ix1, to_iy1, to_ix2, to_iy2);
+
+      // Now we have a line L1 from (from_ix1,from_iy1) to (to_ix1,to_iy1)
+      // and a line L2 from (from_ix2,from_iy2) to (to_ix2,to_iy2).
+      csVector2 from1 ((float)from_ix1, (float)from_iy1);
+      csVector2 from2 ((float)from_ix2, (float)from_iy2);
+      csVector2 to1 ((float)to_ix1, (float)to_iy1);
+      csVector2 to2 ((float)to_ix2, (float)to_iy2);
+
+      // We traverse all (CS_CM_HOR+1)*(CS_CM_VER+1) pixels in the box
+      // and mark if they are left of L1 and/or right of L2.
+      int dimhor1 = CS_CM_HOR+1;
+      float fdim = (float)dimension;
+      for (ix = 0 ; ix <= CS_CM_HOR ; ix++)
+        for (iy = 0 ; iy <= CS_CM_VER ; iy++)
+	{
+	  csVector2 p (
+	  	(float)ix * fdim / (float)CS_CM_HOR,
+	  	(float)iy * fdim / (float)CS_CM_VER);
+	  left[iy*dimhor1+ix] = csMath2::WhichSide2D (p, from1, to1) < 0;
+	  right[iy*dimhor1+ix] = csMath2::WhichSide2D (p, from2, to2) > 0;
+	}
+
+      // Now we are going to calculate the normal and single coverage
+      // masks based on the 'left' and 'right' tables.
+      int mask_idx = (from << dim_shift) + to;
+      masks[mask_idx].Clear ();
+      masks_single[mask_idx].Clear ();
+      // For every bit in the mask.
+      for (bit = 0 ; bit < CS_CM_BITS ; bit++)
+      {
+	ix = bit % CS_CM_HOR;
+	iy = bit / CS_CM_HOR;
+	// 'so' will be 1 if all points of this position in mask
+	// are completely left of L1.
+	int so = left[iy*dimhor1+ix] && left[iy*dimhor1+ix+1] &&
+		left[(iy+1)*dimhor1+ix] && left[(iy+1)*dimhor1+ix+1];
+	// 'si' will be 1 if all points of this position in mask
+	// are completely right of L2.
+	int si = right[iy*dimhor1+ix] && right[iy*dimhor1+ix+1] &&
+		right[(iy+1)*dimhor1+ix] && right[(iy+1)*dimhor1+ix+1];
+	// 's' will be 1 if there are points of this position in
+	// mask that are completely right of L2.
+	int s = right[iy*dimhor1+ix] || right[iy*dimhor1+ix+1] ||
+		right[(iy+1)*dimhor1+ix] || right[(iy+1)*dimhor1+ix+1];
+	masks[mask_idx].SetState (bit, so, si);
+	masks_single[mask_idx].SetState (bit, s);
+      }
+    }
+  }
+
+  CHK (delete [] left);
+  CHK (delete [] right);
+}
+
