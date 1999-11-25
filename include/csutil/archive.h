a35ba3e1aff0bd2c5ba9a7e3bb3d440a63e88496
Andrew Zabolotny <bit@eltech.ru>
1999-11-25 11:07:08 +0000

    Fixed GetFile() to return NULL for out-of-range indices
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2248 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/archive.h
+++ include/csutil/archive.h
@@ -191,7 +191,7 @@ public:
 
   /// Get Nth file in archive or NULL
   void *GetFile (int no)
-  { return dir.Get (no); }
+  { return (no >= 0) && (no < dir.Length ()) ? dir.Get (no) : NULL; }
 
   /// Find a file in archive; returns a handle or NULL
   void *FindName (const char *name) const;
