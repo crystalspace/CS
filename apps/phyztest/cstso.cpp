56132cd89298cad5dbf0a2ad00d32bb63f5a7290
Michael Ewert <mewert99@yahoo.com>
1999-12-05 00:38:33 +0000

    space time object.  glue between CS and csphyzik lib ( crytal time ) has some weak collision detection code that only works for an object crashing into sector walls.  Only handles one collision point per object
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2327 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ apps/phyztest/cstso.cpp
@@ -0,0 +1,295 @@
+#include "sysdef.h"
+#include "qint.h"
+#include "cssys/system.h"
+#include "walktest/walktest.h"
+#include "walktest/bot.h"
+#include "walktest/infmaze.h"
+#include "walktest/hugeroom.h"
+#include "apps/support/command.h"
+#include "cstools/simpcons.h"
+#include "csengine/camera.h"
+#include "csengine/world.h"
+#include "csengine/csview.h"
+#include "csengine/wirefrm.h"
+#include "csengine/cssprite.h"
+#include "csengine/skeleton.h"
+#include "csengine/triangle.h"
+#include "csengine/polygon.h"
+#include "csengine/light.h"
+#include "csengine/sector.h"
+#include "csengine/csspr2d.h"
+#include "csengine/cdobj.h"
+#include "csengine/collider.h"
+#include "csutil/scanstr.h"
+#include "csparser/impexp.h"
+#include "csobject/dataobj.h"
+#include "cssfxldr/common/snddata.h"
+#include "csparser/snddatao.h"
+#include "csparser/csloader.h"
+#include "csparser/crossbld.h"
+#include "csscript/csscript.h"
+#include "csgeom/math3d.h"
+#include "isndsrc.h"
+#include "isndlstn.h"
+#include "isndbuf.h"
+#include "isndrdr.h"
+#include "igraph3d.h"
+
+#include "csphyzik/phyziks.h"
+//#include "csgeom/math3d.h"
+//#include "csengine/cssprite.h"
+//#include "csengine/collider.h"
+#include "cstso.h"
+
+csRigidSpaceTimeObj *csRigidSpaceTimeObj::space_time_continuum[ MAX_SPACE_TIME_NUM ];
+long csRigidSpaceTimeObj::continuum_end = 0;
+
+// hideously inefficient collision detection/response algorithm
+// just wanted to see some stuff bouncing around for now. 
+
+csSpaceTimeObj::csSpaceTimeObj()
+{
+//  space_time_continuum[continuum_end++] = this;
+  //what_type = ST_SPACETIME; 
+}
+
+csRigidSpaceTimeObj::csRigidSpaceTimeObj( csSprite3D *psprt, ctRigidBody *prb )
+{
+  space_time_continuum[continuum_end++] = this;
+ // what_type = ST_SPACETIME; 
+ // col = pcollide;
+  sprt = psprt;
+  rb = prb;
+  CHK (col = new csCollider (sprt));
+  csColliderPointerObject::SetCollider (*sprt, col, true);
+  what_type = ST_RIGID;
+
+}
+
+// using sloooooooow but accurate collision detection.
+// this can be sped up in many ways.
+// - use eulers method or mid-point for ODE just during CD
+// - use prune and sweep algorithm with axis aligned, sorted bounding boxes
+// - only evolve objects related to colliding objects when searching for c-time
+// - don't forget to handle 'tunneling' problem as well.
+void csRigidSpaceTimeObj::evolve_system( real t1, real t2, ctWorld *time_world, csWorld *space_world )
+{
+static real collision_epsilon = 0.02;
+static real time_epsilon = 0.001;
+real ta, tb;
+ 
+long loops = 200;  //make sure we don't go into an infinite loop
+
+  ta = t1;
+  tb = t2;
+  real min_col_dist;
+
+  while( ta < t2 && loops-- > 0){
+ 
+    time_world->evolve( ta, tb );
+
+    min_col_dist = collision_check( space_world );
+    // if there are objects intersecting
+    if( min_col_dist > 0 ){
+      // if interpenetrations distance is very small => time of impact found
+      // or if the time step is really small
+      if( min_col_dist <= collision_epsilon || ( tb - ta ) <= time_epsilon ){
+        // respond to collision
+        collision_response( space_world );
+        ta = tb;
+        tb = t2;
+      }else{
+        // bisect time backwards to search for time of impact
+        tb -= (tb - ta)*0.5;
+        ctVector3 px = space_time_continuum[0]->rb->get_pos();
+        time_world->rewind();   // rewind state back to ta
+        px = space_time_continuum[0]->rb->get_pos();
+      }
+    // if we have not arrived at the end of our time interval 
+    }else if( tb < t2 ){
+      // search forward in time for time of impact for collision(s)
+      ta = tb;
+      // if we are at our min time step, just finish this time step, collision
+      // will get resolved next time.
+      if( (t2 - ta) <= time_epsilon ){
+        tb = t2;
+        time_world->evolve( ta, tb );
+
+        ta = tb;
+      }else{
+        tb += (t2 - tb)*0.5;
+      }
+    }else{
+      ta = tb;
+      tb = t2;
+    }
+  }
+
+  update_space();
+
+}
+
+void csRigidSpaceTimeObj::update_space()
+{
+ctVector3 px;
+csMatrix3 m; 
+ctMatrix3 M;
+csRigidSpaceTimeObj *sto;
+
+  for( int i = 0; i < continuum_end; i++ ){
+    sto = space_time_continuum[i];
+    px = sto->rb->get_pos();
+
+    csVector3 new_p( px[0], px[1], px[2] );   
+    sto->sprt->SetMove ( new_p );
+
+    M = sto->rb->get_R();   // get orientation for this link
+    // ctMatrix3 and csMatrix3 not directly compatable yet
+    m.Set( M[0][0], M[0][1], M[0][2],
+         M[1][0], M[1][1], M[1][2],
+         M[2][0], M[2][1], M[2][2]);    // set orientation of sprite
+    sto->sprt->SetTransform(m);
+  }
+}
+
+
+real csRigidSpaceTimeObj::collision_check( csWorld *space )
+{
+csCollider *coli;
+//csSprite3D *sprt;
+csSector* first_sector;
+ctMatrix3 M;
+csMatrix3 m;
+csVector3 n;
+csVector3 x;
+ctVector3 X;
+csVector3 trime;
+CDTriangle *wall;
+CDTriangle *htri;
+real max_depth;
+real current_depth;
+csOrthoTransform tfm;
+collision_pair *CD_contact = NULL;
+
+  max_depth = 0;
+
+  for( int i = 0; i < continuum_end; i++ ){
+
+    first_sector = (csSector*)(space_time_continuum[i]->sprt->sectors[0]);
+    
+    // Start collision detection.
+    csCollider::CollideReset ();
+    csCollider::firstHit = false;
+    coli = space_time_continuum[i]->col;
+   // for ( ; num_sectors-- ; )
+    M = space_time_continuum[i]->rb->get_world_to_this();
+    m.Set( M[0][0], M[0][1], M[0][2],
+               M[1][0], M[1][1], M[1][2],
+               M[2][0], M[2][1], M[2][2]);    // orientation of sprite
+//    hits += CollisionDetect (col, first_sector, &m);
+    X = space_time_continuum[i]->rb->get_pos();
+    x.x = X[0]; x.y = X[1]; x.z = X[2];
+    // this IS a transformaition from other to this space.
+    tfm.SetO2T( m );
+    // NOT a traslation from other space to this space.  Actually opposite.
+    tfm.SetO2TTranslation( x );
+
+    // Check collision with this sector.
+    csCollider::numHits = 0;
+    if( first_sector ){
+      csCollider::CollidePair (coli, csColliderPointerObject::GetCollider(*first_sector), &tfm);
+      CD_contact = csCollider::GetCollisions ();
+    }
+
+    space_time_continuum[i]->num_collisions = csCollider::numHits;
+    // determine type of collision and penetration depth
+    if( space_time_continuum[i]->num_collisions == 0 ){
+      // no collision
+    }else{
+      for( int acol = 0; acol < csCollider::numHits; acol++ ){
+        space_time_continuum[i]->cd_contact[acol] = CD_contact[acol];
+
+        space_time_continuum[i]->contact.body_a = space_time_continuum[i]->rb;
+        // here is where the body hit should be recorded as well
+        space_time_continuum[i]->contact.body_b = NULL;
+
+        space_time_continuum[i]->contact.restitution = 0.75;
+
+        wall = CD_contact[acol].tr2;
+        n = ((wall->p2-wall->p1)%(wall->p3-wall->p2)).Unit();
+      //  CsPrintf( MSG_DEBUG_1, "n %f, %f, %f\n", n.x, n.y, n.z );
+        space_time_continuum[i]->contact.n =
+          ctVector3(n.x, n.y, n.z );
+
+    // ignore ojbects that aren't really moving towards collision.  
+    // they may "seep" though floor.
+    if( fabs(space_time_continuum[i]->rb->get_v() * space_time_continuum[i]->contact.n)  < MIN_REAL*1000.0)
+      return 0;
+
+
+        htri = CD_contact[acol].tr1;
+
+        // check each point of this triangle to see which penetrated the most
+       // trime = (m * (htri->p1)) + x;
+        trime = tfm.This2Other( htri->p1 );
+        current_depth = -(trime - wall->p1)*n;
+        // this is the collision point
+        if( current_depth > max_depth ){
+          max_depth = current_depth;
+          space_time_continuum[i]->contact.contact_p = 
+            ctVector3( trime.x, trime.y, trime.z );
+          // add collision point to other object as well, here
+        }// else if( current_depth == max_depth ){
+        // simultaneous collision.  add to collision list
+        //}
+
+      //  trime = (m * (htri->p2)) + x;
+        trime = tfm.This2Other( htri->p2 );
+        current_depth = -(trime - wall->p1)*n;
+       // this is the collision point
+        if( current_depth > max_depth ){
+          max_depth = current_depth;
+          space_time_continuum[i]->contact.contact_p = 
+            ctVector3( trime.x, trime.y, trime.z );
+          
+          // add collision point to other object as well, here
+        }// else if( current_depth == max_depth ){
+        // simultaneous collision.  add to collision list
+        //}
+
+       // trime = (m * (htri->p3)) + x;
+        trime = tfm.This2Other( htri->p3 );
+        current_depth = -(trime - wall->p1)*n;
+        // this is the collision point
+        if( current_depth > max_depth ){
+          max_depth = current_depth;
+          space_time_continuum[i]->contact.contact_p = 
+            ctVector3( trime.x, trime.y, trime.z );
+          
+          // add collision point to other object as well, here
+        }// else if( current_depth == max_depth ){
+        // simultaneous collision.  add to collision list
+        //}
+        
+      }
+
+    }
+
+  }
+
+  return sqrt(max_depth);
+
+}
+
+
+void csRigidSpaceTimeObj::collision_response( csWorld *space )
+{
+csRigidSpaceTimeObj *sto;
+
+  for( int i = 0; i < continuum_end; i++ ){
+    sto = space_time_continuum[i];
+    if( sto->num_collisions > 0 ){
+      sto->rb->resolve_collision( sto->contact );
+    }
+  }
+}
---
+++ apps/phyztest/cstso.h
@@ -0,0 +1,60 @@
+#ifndef SPACETIMEOBJECT_H
+#define SPACETIMEOBJECT_H
+
+// hideously inefficient collision detection/response algorithm
+// just wanted to see some stuff bouncing around for now. 
+
+#include "csengine/collider.h"
+
+class csWorld;
+class csSprite3D;
+class ctRigidBody;
+class ctCollidingContact;
+class ctWorld;
+
+#define MAX_SPACE_TIME_NUM 100
+#define MAX_COL_PER_STO 50
+enum sttype { ST_SPACETIME, ST_RIGID };
+
+enum collisionresult { COL_DONOTHING, COL_SPHEREIMPULSE, COL_DIRECTIONAL_IMPULSE };
+
+class csSpaceTimeObj
+{
+public:
+
+	sttype what_type;
+
+
+
+	csSpaceTimeObj();
+
+};
+
+class csRigidSpaceTimeObj : public csSpaceTimeObj
+{
+public:
+	static csRigidSpaceTimeObj *space_time_continuum[ MAX_SPACE_TIME_NUM ];
+	static long continuum_end = 0;
+
+  static void evolve_system( real t1, real t2, ctWorld *time_world, csWorld *space_world );
+
+	csCollider *col;
+	csSprite3D *sprt;
+	ctRigidBody *rb;
+  
+  collision_pair cd_contact[MAX_COL_PER_STO];
+  int num_collisions;
+
+	ctCollidingContact contact;
+
+	csRigidSpaceTimeObj( csSprite3D *psprt, ctRigidBody *prb );
+
+protected:
+  static void update_space();
+  static real collision_check( csWorld *space );
+  static void collision_response( csWorld *space );
+
+
+};
+
+#endif
