7ca038695742cf2821486f6eb05de630b4b129ac
Michael Ewert <mewert99@yahoo.com>
1999-12-09 07:14:55 +0000

    multiple collisions now supported.  link list pointer added. body_a removed, it is implied by the object being called with it.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2361 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csphyzik/contact.h
+++ include/csphyzik/contact.h
@@ -1,3 +1,23 @@
+/*
+    Dynamics/Kinematics modeling and simulation library.
+    Copyright (C) 1999 by Michael Alexander Ewert
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
 #ifndef CT_COLLIDING_H
 #define CT_COLLIDING_H
 
@@ -11,21 +31,24 @@ class ctCollidingContact
 public:
 
   ctCollidingContact(){
-    body_a = NULL;
     body_b = NULL;
     restitution = 1.0;
+    next = NULL;
   }
 
-  ctPhysicalEntity *body_a;  // body a.  one body involved in the contact
   // set body_b to NULL if it is an immovable object. e.g. the ground.
-  // don't set body_a to NULL however.
   ctPhysicalEntity *body_b;  // body b.  other body involved in the contact
 
   ctVector3 contact_p;  // point of contact in world coords
 
   ctVector3 n;   // collision normal
 
-  real restitution;  // coefficent of restitution.  what % of v bounces back. +ve
+  // coefficent of restitution.  what % of v bounces back. +ve
+  // should be a number between 0.0 and 1.0
+  real restitution;  
+
+  // link to next contact
+  ctCollidingContact *next;
 
 };
 
