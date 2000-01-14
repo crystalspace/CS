a381ae31aede8f169245e69f494cb24e2c05902f
Noah L. Gibbs <angelbob@users.sourceforge.net>
2000-01-14 05:26:23 +0000

    Changed ctPhysicalEntity to have ctEntity as a parent class, with all appropriate changes pertaining to doing so.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2555 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csphyzik/phyzent.h
+++ include/csphyzik/phyzent.h
@@ -25,10 +25,7 @@
 #include "csphyzik/force.h"
 #include "csphyzik/linklist.h"
 #include "csphyzik/refframe.h"
-//#include <iostream>
-
-// flags
-#define CTF_NOREWIND 0x1
+#include "csphyzik/entity.h"
 
 #define DEFAULT_ENTITY_MASS 10.0
 
@@ -37,111 +34,99 @@
 class ctSolver;
 class ctCollidingContact;
 
-// parent class of all physical bodies ( or even some non-physical ones... )
-class ctPhysicalEntity
+// parent class of physical bodies
+class ctPhysicalEntity : public ctEntity
 {
 public:
 /*
-**	Constructors/destructors/statics
+**  Constructors/destructors/statics
 */
-	// default refrence frame will be inertial frame
-	ctPhysicalEntity();
-	// use specified reference frame
-	ctPhysicalEntity( ctReferenceFrame &ref );
-	virtual ~ctPhysicalEntity();
+  // default refrence frame will be inertial frame
+  ctPhysicalEntity();
+  // use specified reference frame
+  ctPhysicalEntity( ctReferenceFrame &ref );
+  virtual ~ctPhysicalEntity();
 
 /*
-**	Member functions
+**  Member functions
 */
 
-	// compute the derivitive of this bodies state vector ( position, velocity... )
-	// from applied forces.  Just passes control to the ctSolver for this object.
-	virtual void solve( real t );
+  //**********  ODE interface ****************
+  // init values that will influence the calculation of the change in state
+  virtual void init_state(){
+    F[0] = 0; F[1] = 0; F[2] = 0;
+    T[0] = 0; T[1] = 0; T[2] = 0;
+  }
+  // return the size of this entity's state vector
+  virtual int get_state_size(){ return PHYSICALENTITY_STATESIZE; }
+  // add this body's state to the state vector buffer passed in. 
+  // increment state buffer to point after added state.  upload
+  virtual int set_state( real *sa );
+  // download state from buffer into this entity
+  virtual int get_state( const real *sa );
+  // add change in state vector over time to state buffer parameter.
+  virtual int set_delta_state( real *state_array ); 
 
-	// set the class responsible for solving change in state wrt time
-	void set_solver( ctSolver *pslv ){ solver = pslv; }
-	
-
-	//**********  ODE interface ****************
-	// init values that will influence the calculation of the change in state
-	virtual void init_state(){ F[0] = 0; F[1] = 0; F[2] = 0; T[0] = 0; T[1] = 0; T[2] = 0; }
-	// return the size of this enities state vector
-	virtual int get_state_size(){ return PHYSICALENTITY_STATESIZE; }// = 0;
-	// add this bodies state to the state vector buffer passed in. 
-	// increment state buffer to point after added state.  upload
-	virtual int set_state( real *sa );
-	// download state from buffer into this entity
-	virtual int get_state( const real *sa );
-	// add change in state vector over time to state buffer parameter.
-	virtual int set_delta_state( real *state_array ); 
-
-	// change orientation in radians
-	virtual void rotate_around_line( ctVector3 &paxis, real ptheta );
-	ctVector3 get_v(){ return v; }
+  // change orientation in radians
+  virtual void rotate_around_line( ctVector3 &paxis, real ptheta );
+  ctVector3 get_v(){ return v; }
   // virtual because v is calculated from P ( momentum ) in rigid bodies
-	virtual void set_v( const ctVector3 &pv );
-	void set_pos( real px, real py, real pz ){ ctVector3 poff( px, py, pz ); RF.set_offset( poff ); }
+  virtual void set_v( const ctVector3 &pv );
+  void set_pos( real px, real py, real pz ) {
+    ctVector3 poff( px, py, pz );
+    RF.set_offset( poff );
+  }
   virtual void set_angular_v( const ctVector3 &pw );
-	ctVector3 get_angular_v(){ return w; }
-	
-	ctVector3 get_F(){ return F; }
-	ctVector3 get_torque(){ return T; }
-	void set_F( const ctVector3 &pF ){ F = pF; }
-	void set_torque( const ctVector3 &pT ){ T = pT; }
-	void add_force( ctForce *f ){ forces.add_link( f ); }
-	void sum_force( const ctVector3 &f ){ F += f; }
-	void sum_torque( const ctVector3 &t ){ T += t; }
-	void remove_force( ctForce *f ){ forces.remove_link( f ); }
+  ctVector3 get_angular_v(){ return w; }
 
-	// add this force to the list of forces that will be applied each frame
-	virtual void apply_given_F( ctForce &frc );
-	void print_force(){/*cout << "F: " << F*F << "\n";*/ }
+  ctVector3 get_F(){ return F; }
+  ctVector3 get_torque(){ return T; }
+  void set_F( const ctVector3 &pF ){ F = pF; }
+  void set_torque( const ctVector3 &pT ){ T = pT; }
+  //void add_force( ctForce *f ){ forces.add_link( f ); }
+  void sum_force( const ctVector3 &f ){ F += f; }
+  void sum_torque( const ctVector3 &t ){ T += t; }
+  //void remove_force( ctForce *f ){ forces.remove_link( f ); }
+
+  // add this force to the list of forces that will be applied each frame
+  virtual void apply_given_F( ctForce &frc );
+  void print_force(){/*cout << "F: " << F*F << "\n";*/ }
 
   // collision routines
   // resolve collision with a body.  warning: original cont may be modified
   virtual void resolve_collision( ctCollidingContact *cont );
   // can use this to impart and impulse to this object.
-  virtual void apply_impulse( ctVector3 impulse_point, ctVector3 impulse_vector );
+  virtual void apply_impulse( ctVector3 impulse_point,
+			      ctVector3 impulse_vector );
   virtual real get_impulse_m(){ return DEFAULT_ENTITY_MASS; }
-  virtual ctMatrix3 get_impulse_I_inv(){ return ctMatrix3( 1.0/get_impulse_m() ); }
-//	bool uses_ODE(){ return use_ODE; }
+  virtual ctMatrix3 get_impulse_I_inv() {
+    return ctMatrix3( 1.0/get_impulse_m() );
+  }
 
-	const ctMatrix3 &get_R(){ return RF.get_R(); }
-	const ctMatrix3 &get_T(){ return RF.get_T(); }
+  const ctMatrix3 &get_R(){ return RF.get_R(); }
+  const ctMatrix3 &get_T(){ return RF.get_T(); }
 #define get_pos get_org_world
-	const ctVector3 &get_org_world(){ return RF.get_offset(); }
-	const ctMatrix3 &get_world_to_this(){ return RF.get_parent_to_this(); }
-	const ctMatrix3 &get_this_to_world(){ return RF.get_this_to_parent(); }
-	void v_this_to_world( ctVector3 &pv ){ RF.this_to_world( pv ); }
-	ctVector3 get_v_this_to_world( ctVector3 &pv ){ 
-		ctVector3 pret = pv;  RF.this_to_world( pret ); return pret;  
-	}
-	
-	ctReferenceFrame *get_RF(){ return &RF; }
-
-  // flags
-  unsigned long flags;
+  const ctVector3 &get_org_world(){ return RF.get_offset(); }
+  const ctMatrix3 &get_world_to_this(){ return RF.get_parent_to_this(); }
+  const ctMatrix3 &get_this_to_world(){ return RF.get_this_to_parent(); }
+  void v_this_to_world( ctVector3 &pv ){ RF.this_to_world( pv ); }
+  ctVector3 get_v_this_to_world( ctVector3 &pv ){ 
+    ctVector3 pret = pv;  RF.this_to_world( pret ); return pret;  
+  }
+  
+  ctReferenceFrame *get_RF(){ return &RF; }
 
 protected:
 
-	ctVector3 v;
-	ctVector3 w;
+  ctVector3 v;
+  ctVector3 w;
 
 
-  //!me gotta try to garauntee that this is not modified in the middle
-	// of an ODE run.......
-	//bool use_ODE;
-	
-	// frame of reference. orientation and position are stored here
-	ctReferenceFrame &RF;
+  // frame of reference. orientation and position are stored here
+  ctReferenceFrame &RF;
 
-	// object responsible for calculating the change in state wrt time 
-	// uses forces list and current state to do this.
-	ctSolver *solver;
-
-	ctVector3 F;	// total added Force for a frame
-	ctVector3 T;	// total Torque for a frame
-	ctLinkList_ctForce forces;   // list of all forces affecting this object
+  ctVector3 F;  // total added Force for a frame
+  ctVector3 T;  // total Torque for a frame
 
 };
 
@@ -152,22 +137,21 @@ class ctSimpleDynamicsSolver;
 class ctDynamicEntity : public ctPhysicalEntity
 {
 public:
-	ctDynamicEntity();
-	ctDynamicEntity( ctReferenceFrame &ref );
-	virtual ~ctDynamicEntity();
-	
-//	virtual void solve( real t );
-	virtual void apply_given_F( ctForce &frc );
+  ctDynamicEntity();
+  ctDynamicEntity( ctReferenceFrame &ref );
+  virtual ~ctDynamicEntity();
 
-	friend class ctSimpleDynamicsSolver;
+  virtual void apply_given_F( ctForce &frc );
 
-	virtual void set_m( real pm );
-	real get_m(){ return m; }
+  friend class ctSimpleDynamicsSolver;
+
+  virtual void set_m( real pm );
+  real get_m(){ return m; }
   virtual real get_impulse_m(){ return m; }
 
 protected:
 
-	real m;			// mass
+  real m;     // mass
 };
 
 #endif
--- include/csphyzik/world.h
+++ include/csphyzik/world.h
@@ -1,6 +1,6 @@
 /*
     Dynamics/Kinematics modeling and simulation library.
-    Copyright (C) 1999 by Michael Alexander Ewert
+    Copyright (C) 1999 by Michael Alexander Ewert & Noah Gibbs
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -22,18 +22,19 @@
 #define PHYZ_WORLD_H
 
 #include "csphyzik/phyztype.h"
+#include "csphyzik/entity.h"
 #include "csphyzik/phyzent.h"
 #include "csphyzik/linklist.h"
 #include "csphyzik/odesolve.h"
 
 enum worldstate { CTWS_NORMAL, CTWS_REWOUND };
 
-enum errorcode { WORLD_NOERR, WORLD_ERR_NULLPARAMETER, WORLD_ERR_NOODE, WORLD_ERR_SHITHAPPEND, WORLD_ERR_OTHERSTUFF };
+enum errorcode { WORLD_NOERR, WORLD_ERR_NULLPARAMETER, WORLD_ERR_NOODE,
+                 WORLD_ERR_SHITHAPPEND, WORLD_ERR_OTHERSTUFF };
 
 #define DEFAULT_INIT_MAX_STATE_SIZE  1024
 #define STATE_RESIZE_EXTRA 256
 
-class ctRigidBody;
 class ctArticulatedBody;
 class ctForce;
 
@@ -41,25 +42,23 @@ class ctWorld : public ctPhysicalEntity
 {
 public:
 
-	ctWorld();
-	virtual ~ctWorld();  //!me delete _lists and ode
+  ctWorld();
+  virtual ~ctWorld();  //!me delete _lists and ode
 
-	void calc_delta_state( real t, const real y[], real ydot[] );
+  void calc_delta_state( real t, const real y[], real ydot[] );
 
   // evolve the state of the system.  calc forces, determine new v, etc..
-	errorcode evolve( real t1, real t2 );
-  
-  // rewind the state of the system to the time just before evolve was called.
-  // pass in the correct time frame
-  errorcode rewind( real t1, real t2 );  
-	
-  void solve( real t );
-	errorcode add_physicalentity( ctPhysicalEntity *pe );
-	errorcode add_rigidbody( ctRigidBody *rb );
-	errorcode add_articulatedbodybase( ctArticulatedBody *ab );
-	errorcode add_enviro_force( ctForce *f );
+  errorcode evolve( real t1, real t2 );
 
-	errorcode delete_articulatedbody( ctArticulatedBody *pbase );
+  // rewind the state of the system to the time just before evolve was called.
+  // pass in the correct time frame.
+  errorcode rewind( real t1, real t2 );  
+
+  void solve( real t );
+  errorcode add_entity( ctEntity *pe );
+  errorcode add_enviro_force( ctForce *f );
+
+  errorcode delete_articulatedbody( ctArticulatedBody *pbase );
 
   // set the ODE solver used to evolve the system
   void set_ODE_solver( OdeSolver *pode ){ 
@@ -68,40 +67,44 @@ public:
   }
 
   // apply the given function to all physical entities in the system.
-  void apply_fuction_to_body_list( void(*fcn)( ctPhysicalEntity *ppe ) );
+  void apply_function_to_body_list( void(*fcn)( ctEntity *pe ) );
 
+  // This function evaluates dydt().  By overriding it in child classes,
+  // ctWorld objects that use nondefault derivatives (like catastrophes,
+  // constraints, and certain forces) can be created.
+  virtual void dydt_eval(real t, const real y[], real dy[]);
 protected:
-	// take state values( position, velocity, orientation, ... ) from 
-	// this worlds physical entities and put them into the array
-	void load_state( real *state_array );
+  // take state values( position, velocity, orientation, ... ) from 
+  // this world's entities and put them into the array
+  void load_state( real *state_array );
 
-	// take state values from array and reintegrate them into physicalentity 
-	// structures of this world.  the reverse operation of load_state.
-	// state_array must have been filled out by load_state.
-	void reintegrate_state( const real *state_array );
-	
-	// put first derivative of all world entities into state 
-	void load_delta_state( real *state_array );
+  // take state values from array and reintegrate them into ctEntity 
+  // structures of this world.  the reverse operation of load_state.
+  // state_array must have been filled out by load_state.
+  void reintegrate_state( const real *state_array );
 
-	void init_state();
+  // put first derivative of all world entities into state 
+  void load_delta_state( real *state_array );
 
-	void collide();
+  void init_state();
+
+  void collide();
 
   void resize_state_vector( long new_size );
-  
+
   // the current state of the world. as in finite state machine state
   worldstate fsm_state;  
   real rewound_from;
 
-	ctLinkList_ctPhysicalEntity body_list;
-	ctLinkList_ctForce enviro_force_list;
+  ctLinkList_ctEntity body_list;
+  ctLinkList_ctForce enviro_force_list;
 
-	OdeSolver *ode_to_math;  // would an equation by any other name smell as sweet?
+  OdeSolver *ode_to_math;  // would an equation by any other name smell as sweet?
 
   // state vectors for ODE interface
   real *y_save;  // used to save state so it can be rewound to pre-ODE state
-	real *y0;
-	real *y1;
+  real *y0;
+  real *y1;
 
   long y_save_size;
   long max_state_size;
