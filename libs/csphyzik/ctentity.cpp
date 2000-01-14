1921bfadf42370b88f3ee59f7340a1982361514e
Noah L. Gibbs <angelbob@users.sourceforge.net>
2000-01-14 05:27:48 +0000

    Added ctEntity as the parent of ctPhysicalEntity, which required creating this file.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2557 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ libs/csphyzik/ctentity.cpp
@@ -0,0 +1,52 @@
+/*
+    Dynamics/Kinematics modeling and simulation library.
+    Copyright (C) 1999 by Michael Alexander Ewert
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#include "csphyzik/entity.h"
+//#include "csphyzik/contact.h"
+//#include "csphyzik/refframe.h"
+//#include "csphyzik/mathutil.h"
+#include "csphyzik/solver.h"
+#include "csphyzik/debug.h"
+
+ctEntity::ctEntity()
+{
+  solver = NULL;
+  flags = 0;
+}
+
+ctEntity::~ctEntity()
+{
+  if( solver )
+    delete solver;
+}
+
+
+// pass control to solver
+void ctEntity::solve( real t )
+{
+  if( solver ){
+    solver->solve( t );
+  }
+}
+
+void ctEntity::apply_given_F( ctForce &frc )
+{
+  // notin
+}
