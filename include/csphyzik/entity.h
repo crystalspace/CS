b33b7ee2088a0a5251de0274c3bac152c45b6856
Noah L. Gibbs <angelbob@users.sourceforge.net>
2000-01-14 05:27:10 +0000

    Added ctEntity as the parent of ctPhysicalEntity, which required creating this file :)
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2556 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/csphyzik/entity.h
@@ -0,0 +1,105 @@
+/*
+    Dynamics/Kinematics modeling and simulation library.
+    Copyright (C) 1999 by Michael Alexander Ewert
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
+#ifndef ctEntity_H
+#define ctEntity_H
+
+#include "csphyzik/phyztype.h"
+#include "csphyzik/force.h"
+#include "csphyzik/linklist.h"
+#include "csphyzik/refframe.h"
+
+// flags
+#define CTF_NOREWIND 0x1
+
+class ctSolver;
+//class ctCollidingContact;
+
+// parent class of all physical bodies ( or even some non-physical ones... )
+class ctEntity
+{
+ public:
+/*
+**      Constructors/destructors/statics
+*/
+  ctEntity();
+  virtual ~ctEntity();
+
+/*
+**      Member functions
+*/
+
+  // Compute the derivative of this body's state vector ( position,
+  // velocity... ) from applied forces.  Just passes control to the
+  // ctSolver for this object.
+  virtual void solve( real t );
+
+  // Set the class responsible for solving change in state wrt time.
+  void set_solver( ctSolver *pslv ){ solver = pslv; }
+
+
+  //**********  ODE interface ****************
+  // Init values that will influence the calculation of the change in state.
+  virtual void init_state() {}
+  // Return the size of this enities state vector
+  virtual int get_state_size() = 0;
+  // Add this body's state to the state vector buffer passed in. 
+  // Increment state buffer to point after added state.  Upload.
+  virtual int set_state( real *sa ) = 0;
+  // download state from buffer into this entity
+  virtual int get_state( const real *sa ) = 0;
+  // add change in state vector over time to state buffer parameter.
+  virtual int set_delta_state( real *state_array ) = 0;
+
+  void add_force( ctForce *f ){ forces.add_link( f ); }
+  void remove_force( ctForce *f ){ forces.remove_link( f ); }
+
+  // add this force to the list of forces that will be applied each frame
+  virtual void apply_given_F( ctForce &frc );
+  void print_force() {}
+
+  // collision routines
+  // resolve collision with a body.  warning: original cont may be modified
+  //virtual void resolve_collision( ctCollidingContact *cont );
+
+  // can use this to impart an impulse to this object.
+  //virtual void apply_impulse( ctVector3 impulse_point,
+  //                          ctVector3 impulse_vector ) = 0;
+
+  // Stereotype of funcs to replace flags array
+  virtual bool will_rewind() { return !(flags & CTF_NOREWIND); }
+  virtual void set_rewind(bool dorewind) {
+    flags &= ~CTF_NOREWIND;
+    if(!dorewind) flags |= CTF_NOREWIND;
+  }
+
+  // flags
+  unsigned long flags;
+
+protected:
+
+  // object responsible for calculating the change in state wrt time 
+  // uses forces list and current state to do this.
+  ctSolver *solver;
+
+  ctLinkList_ctForce forces;   // list of all forces affecting this object
+};
+
+#endif
