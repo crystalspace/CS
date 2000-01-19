3565c72df900b13d2f65034991896af8619d5fcf
Michael Ewert <mewert99@yahoo.com>
2000-01-19 22:51:33 +0000

    instead of giving up under some circumstances, a mediocre solution is given back
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2594 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csphyzik/math/mtrxutil.cpp
+++ libs/csphyzik/math/mtrxutil.cpp
@@ -67,12 +67,22 @@ int max_order_row_index;
 		// elimination:  only eliminate all rows that haven't been reduced already
 		for( target_row = elimcol+1; target_row < dim; target_row++ ){
 			if( A[elimrow][elimcol] == 0.0 ){ //!me should report errors
+        //!me instead of failing I should try to fix it the best I can.
+        //!me much better to have wierd results than to fail completely.
+        //!me Here's what to do:  look up cholesky factorization
+        //!me or do this: try to figure out what vector is missing in 
+        //!me order to get b.  take dot product with b and subract from
+        //!me b for every row ( column? ) then whatever is left is 
+        //!me what this 0 row should be ( might have to take it from 
+        //!me column space to row space first ). Cool!?!?!
+
+
 				factor = 0.0;  // the whole row was 0.0, so fail nicely
 				printf( "shit singular matrix\n");
 				exit(1);
 			}else{
 				if( fabs(A[elimrow][elimcol]) < MIN_REAL ){  
-				exit(1);
+	//			exit(1);
 					factor = MAX_REAL;
 				}else{
 					factor = A[order[target_row]][elimcol]/A[elimrow][elimcol];
@@ -108,84 +118,3 @@ int max_order_row_index;
 	}
 
 }
-
-
-/*
-#define NRANSI
-#include "csphyzik/nrutil.h"
-#define TINY 1.0e-20;
-   
-void ludcmp(float **a, int n, int *indx, float *d)
-{
-	int i,imax,j,k;
-	float big,dum,sum,temp;
-	float *vv;
-   
-	vv=vector(1,n);
-	*d=1.0;
-	for (i=1;i<=n;i++) {
-		big=0.0;
-		for (j=1;j<=n;j++)
-			if ((temp=fabs(a[i-1][j-1])) > big) big=temp;
-		if (big == 0.0) nrerror("Singular matrix in routine ludcmp");
-		vv[i]=1.0/big;
-	}
-	for (j=1;j<=n;j++) {
-		for (i=1;i<j;i++) {
-			sum=a[i-1][j-1];
-			for (k=1;k<i;k++) sum -= a[i-1][k-1]*a[k-1][j-1];
-			a[i-1][j-1]=sum;
-		}
-		big=0.0;
-		for (i=j;i<=n;i++) {
-			sum=a[i-1][j-1];
-			for (k=1;k<j;k++)
-				sum -= a[i-1][k-1]*a[k-1][j-1];
-				a[i-1][j-1]=sum;
-			if ( (dum=vv[i]*fabs(sum)) >= big) {
-				big=dum;
-				imax=i;
-			}
-		}
-		if (j != imax) {
-			for (k=1;k<=n;k++) {
-				dum=a[imax-1][k-1];
-				a[imax-1][k-1]=a[j-1][k-1];
-				a[j-1][k-1]=dum;
-			}
-			*d = -(*d);
-			vv[imax]=vv[j];
-		}
-		indx[j-1]=imax;
-		if (a[j-1][j-1] == 0.0) a[j-1][j-1]=TINY;
-		if (j != n) {
-			dum=1.0/(a[j-1][j-1]);
-			for (i=j+1;i<=n;i++) a[i-1][j-1] *= dum;
-		}
-	}
-	free_vector(vv,1,n);
-}
-#undef TINY
-#undef NRANSI
-
-void lubksb(float **a, int n, int *indx, float b[])
-{
-	int i,ii=0,ip,j;
-	float sum;
-   
-	for (i=1;i<=n;i++) {
-		ip=indx[i-1];
-		sum=b[ip-1];
-		b[ip-1]=b[i-1];
-		if (ii)
-			for (j=ii;j<=i-1;j++) sum -= a[i-1][j-1]*b[j-1];
-		else if (sum) ii=i;
-		b[i-1]=sum;
-	}
-	for (i=n;i>=1;i--) {
-		sum=b[i-1];
-		for (j=i+1;j<=n;j++) sum -= a[i-1][j-1]*b[j-1];
-		b[i-1]=sum/a[i-1][i-1];
-	}
-}
-*/
