0c5edd7e8952d78c1279c44accb138bbda55dabb
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2000-02-24 08:30:26 +0000

    Added math.h and float.h again which Andrew removed. These includes are needed to greatly speed up the mathematical functions. On Linux these includes define built-ins for some of the math functions.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2934 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csgeom/math3d.cpp
+++ libs/csgeom/math3d.cpp
@@ -17,6 +17,8 @@
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
+#include <math.h>
+#include <float.h>
 #include "sysdef.h"
 #include "qint.h"
 #include "csgeom/math3d.h"
