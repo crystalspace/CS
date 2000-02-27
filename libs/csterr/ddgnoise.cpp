51a90062de467d1b13108bcc9a54dbe90adb21c6
link <link@localhost>
2000-02-27 20:58:48 +0000

    Fixed occurrences of 'static variable' that should have been 'static int variable'.  The former breaks on GCC (at least, on version 2.95).
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@2994 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csterr/ddgnoise.cpp
+++ libs/csterr/ddgnoise.cpp
@@ -111,7 +111,7 @@ double ddgNoise::cloud( double *v, double freq, int cc, double cs )
 #define NP 12   /* 2^N */
 #define NM 0xfff
 
-static p[B + B + 2];
+static int p[B + B + 2];
 static double g3[B + B + 2][3];
 static double g2[B + B + 2][2];
 static double g1[B + B + 2];
@@ -154,7 +154,7 @@ double ddgNoise::noise2(double *vec
 {
 	int bx0, bx1, by0, by1, b00, b10, b01, b11;
 	double rx0, rx1, ry0, ry1, *q, sx, sy, a, b, t, u, v;
-	register i, j;
+	register int i, j;
 
 	if (start) {
 		init();
@@ -191,7 +191,7 @@ double ddgNoise::noise3(double *vec)
 {
 	int bx0, bx1, by0, by1, bz0, bz1, b00, b10, b01, b11;
 	double rx0, rx1, ry0, ry1, rz0, rz1, *q, sy, sz, a, b, c, d, t, u, v;
-	register i, j;
+	register int i, j;
 
 	if (start) {
 		init();
