dd0e6f9aabb70aa10c9c40ab3ed0e4f097ce5ad0
Norman Krämer <norman@users.sourceforge.net>
2000-04-17 11:13:33 +0000

    another source of quintillions of warnings removed
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@3456 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/cstools/simpcons.h
+++ include/cstools/simpcons.h
@@ -125,7 +125,7 @@ public:
   void SetMaxLines (int ml = -1);
 
   /// iConsole compatibility methods
-  virtual bool Initialize(iSystem *system) { return true; }
+  virtual bool Initialize(iSystem* /*system*/) { return true; }
   virtual void PutText(const char *text);
   virtual const csString *GetText(int = -1) const { return NULL; } /* Not supported */
   virtual void DeleteText(int = 0, int = -1) { } /* Not supported */
