9c00a53aa8f0e8d6ae066227b46ded93b5ecd04d
Eric Sunshine <sunshine@sunshineco.com>
2000-05-10 22:41:12 +0000

    Re-added the explicit IncRef() on VFS (at least for now).  After Norman removed it, command-line applications began crashing at termination time since they were sending an extra DecRef().  For now, I prefer to have the application leak the VFS object rather than actually crash.  We need to resolve this reference count issue once and for all, though.  Since command line programs crash, but graphic programs don't, I might suspect that one of the plug-in modules (which command-line programs don't use) is leaking a reference to VFS, though a scan through them reveals no obvious errors.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@3780 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/cssys/system.cpp
+++ libs/cssys/system.cpp
@@ -365,6 +365,8 @@ bool csSystemDriver::Initialize (int argc, const char* const argv[], const char 
   // a VFS volume.
   Config = new csIniFile;
   VFS = (iVFS*)LoadPlugIn("crystalspace.kernel.vfs", CS_FUNCID_VFS, "iVFS", 0);
+  if (VFS)
+    VFS->IncRef();
 
   // Initialize configuration file
   if (iConfigName)
