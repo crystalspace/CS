241f8ab5c92b08014046381d6f4e1321d3d08133
Eric Sunshine <sunshine@sunshineco.com>
2000-05-12 10:43:08 +0000

    Fixed bug: Push() wasn't abstract.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@3810 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/inetcmd.h
+++ include/inetcmd.h
@@ -7,27 +7,18 @@ SCF_VERSION (iNetCmd, 0, 0, 1);
 
 struct iNetCmd : public iBase
 {
-  virtual const char *GetName() const =0;
-  virtual void SetName(const char *iName) = 0;
+  virtual const char *GetName() const = 0;
+  virtual void SetName(const char*) = 0;
   virtual int GetNodeType() = 0;
-  virtual void SetLeft(struct iNetCmd *) =0;
-  virtual void SetRight(struct iNetCmd *) =0;
-  virtual void SetNext(struct iNetCmd *) = 0;
+  virtual void SetLeft(struct iNetCmd*) = 0;
+  virtual void SetRight(struct iNetCmd*) = 0;
+  virtual void SetNext(struct iNetCmd*) = 0;
   virtual char *Stringify() = 0;
-
-  virtual iNetCmd *GetLeft() =0;
-  virtual iNetCmd *GetRight() =0;  
-  virtual iNetCmd *GetNext() =0;  
-  virtual void Push(iNetCmd *cmd);
+  virtual iNetCmd *GetLeft() = 0;
+  virtual iNetCmd *GetRight() = 0;  
+  virtual iNetCmd *GetNext() = 0;  
+  virtual void Push(iNetCmd*) = 0;
   virtual int GetValue() = 0;
 };
 
-
-
 #endif
-
-
-
-
-
-
