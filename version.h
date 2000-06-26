899705ac8b45252363d7688a5e65431badbc6505
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2000-06-26 07:11:32 +0000

    Preparation for 0.16.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@4335 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- version.h
+++ version.h
@@ -1,9 +1,9 @@
 #ifndef __CS_VERSION_H__
 #define __CS_VERSION_H__
 
-#define CS_VERSION	"0.15"	/* NOTE: Update docs/texinfo/version.txi  */
-#define CS_SUBVERSION	"002"	/* whenever updating these two values.    */
-#define RELEASE_DATE	"Wed, 23-Feb-2000"
+#define CS_VERSION	"0.16"	/* NOTE: Update docs/texinfo/version.txi  */
+#define CS_SUBVERSION	"001"	/* whenever updating these two values.    */
+#define RELEASE_DATE	"Mon, 26-Jun-2000"
 
 #if defined(OS_SOLARIS)
 #  define OS_VERSION "Solaris"
