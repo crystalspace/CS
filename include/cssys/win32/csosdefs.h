ebf040efd201a07738bf210b35de343ccd3e95fc
Thomas Hieber <thieber@gmx.net>
2000-08-07 19:00:22 +0000

    define CS_DEBUG when building in debug mode
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@4678 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/cssys/win32/csosdefs.h
+++ include/cssys/win32/csosdefs.h
@@ -63,6 +63,7 @@
   #define ASSERT(expression) assert(expression)
   #define VERIFY_SUCCESS(expression) assert(SUCCEEDED(expression))
   #define VERIFY_RESULT(expression, result) assert(expression == result)
+  #define CS_DEBUG
 #else
   #define ASSERT(expression)
   #define VERIFY_SUCCESS(expression) expression
