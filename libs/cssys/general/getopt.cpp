a397f34c6d5acfe1bc92cae85e6047add9d43561
Thomas Hieber <thieber@gmx.net>
2000-08-07 18:57:57 +0000

    removed a warning
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@4676 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/cssys/general/getopt.cpp
+++ libs/cssys/general/getopt.cpp
@@ -274,7 +274,7 @@ exchange (char **argv)
 
 /* Initialize the internal data when the first call is made.  */
 
-static const char *_getopt_initialize (int argc, char *const *argv,
+static const char *_getopt_initialize (int /*argc*/, char* const* /*argv*/,
     const char *optstring)
 {
   /* Start processing options with ARGV-element 1 (since ARGV-element 0
