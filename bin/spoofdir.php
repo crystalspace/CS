03f8f55e399201b43ff4ded784e54b3624e19582
Eric Sunshine <sunshine@sunshineco.com>
2000-09-08 08:28:36 +0000

    Added the standard "free software" license blurb.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@4962 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- bin/spoofdir.php
+++ bin/spoofdir.php
@@ -1,7 +1,26 @@
 <?php
+//=============================================================================
+//
+//    Virtual-Directory Listing & Pretty-Printing Script
+//    Copyright (C) 2000 by Eric Sunshine <sunshine@sunshineco.com>
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+//
+//=============================================================================
 //-----------------------------------------------------------------------------
 // spoofdir.php
-// Copyright (C) 2000 by Eric Sunshine <sunshine@sunshineco.com>
 //
 // This script allows HTTP access to directories and files which are contained
 // both within and outside of the normal Apache 'htdocs' directory tree.  It
