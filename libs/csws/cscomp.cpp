44c747ae1541a00ccdb145d7d470cb071e490a5b
Andrew Zabolotny <bit@eltech.ru>
2000-09-19 05:34:29 +0000

    Fixed a small bug (I'm even not sure it caused any problems)
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@4990 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csws/cscomp.cpp
+++ libs/csws/cscomp.cpp
@@ -1355,6 +1355,7 @@ void csComponent::SetState (int mask, bool enable)
 
   // If our focused status is set, update parent `focused' field
   if ((mask & CSS_FOCUSED)
+   && enabled
    && parent)
     parent->focused = this;
 
