abd673ecfe931f7d8cc39633556ecf3f19971b1f
link <link@localhost>
2000-10-15 23:01:43 +0000

    New csEventCord files.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@5169 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/cssys/csevcord.h
@@ -0,0 +1,78 @@
+/*
+    Copyright (C) 2000 by Jorrit Tyberghein
+    Written by Michael Dale Long.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_EVCORD__
+#define __CS_EVCORD__
+
+#include "csutil/scf.h"
+#include "ievent.h"
+
+struct iPlugIn;
+
+class csEventCord : public iEventCord
+{
+public:
+  DECLARE_IBASE;
+
+  // Create an event cord for a given category/subcategory
+  csEventCord(int category, int subcategory);
+
+  // Insert a plugin into the event cord
+  virtual int Insert(iPlugIn *plugin, int priority);
+
+  // Remove a plugin from the event cord
+  virtual void Remove(iPlugIn *plugin);
+
+  // Get and set whether events are passed to the system event queue
+  virtual bool GetPass() const;
+  virtual void SetPass(bool pass);
+
+  // For the system driver to post events to the cord.  Should not be
+  // used directly.
+  bool PutEvent(iEvent *event);
+
+  // The category and subcategory of this events on this cord
+  const int category, subcategory;
+
+protected:
+  // Pass events to the system queue?
+  volatile bool pass;
+
+  // Linked list of plugins
+  struct PluginData {
+    iPlugIn *plugin;
+    int priority;
+    PluginData *next;
+  };
+
+  // The cord itself
+  volatile PluginData *plugins;
+
+  // Protection against multiple threads accessing the same cord
+  volatile int SpinLock;
+
+  /// Lock the queue for modifications: NESTED CALLS TO LOCK/UNLOCK NOT ALLOWED!
+  inline void Lock ()
+  { while (SpinLock) ; SpinLock++; }
+  /// Unlock the queue
+  inline void Unlock ()
+  { SpinLock--; }
+};
+
+#endif // ! __CS_EVCORD__
---
+++ libs/cssys/csevcord.cpp
@@ -0,0 +1,136 @@
+/*
+    Copyright (C) 2000 by Jorrit Tyberghein
+    Written by Michael Dale Long.
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+#include "csutil/scf.h"
+#include "cssys/system.h"
+#include "cssys/csevcord.h"
+#include "iplugin.h"
+#include "ievent.h"
+
+IMPLEMENT_IBASE(csEventCord)
+  IMPLEMENTS_INTERFACE(iEventCord)
+IMPLEMENT_IBASE_END
+
+csEventCord::csEventCord(int Category, int Subcategory) : category(Category), 
+  subcategory(Subcategory)
+{
+  plugins = NULL;
+  // By default, do not pass events to the system driver
+  pass = false;
+  SpinLock = 0;
+}
+
+int csEventCord::Insert(iPlugIn *plugin, int priority)
+{
+  Lock();
+  // Increment the plugin reference count
+  plugin->IncRef();
+  int retval = 0;
+  if(plugins) { // Add a new record
+    PluginData *last = NULL, *curr = CONST_CAST(PluginData *) (plugins);
+    while (curr) {
+      if(priority>curr->priority)
+	break;
+      last = curr;
+      curr = curr->next;
+      retval++;
+    }
+    if(last) {  // Insert in the middle or at the end
+      last->next = new PluginData;
+      last->next->plugin = plugin;
+      last->next->priority = priority;
+      last->next->next = curr;
+    } else { // Insert at the top
+      plugins = new PluginData;
+      plugins->plugin = plugin;
+      plugins->priority = priority;
+      plugins->next = curr;
+    }
+  } else { // Create first record
+    plugins = new PluginData;
+    plugins->plugin = plugin;
+    plugins->priority = priority;
+    plugins->next = NULL;
+  }
+  Unlock();
+  return retval;
+}
+
+void csEventCord::Remove(iPlugIn *plugin)
+{
+  Lock();
+  PluginData *last = NULL, *curr = CONST_CAST(PluginData *) (plugins);
+  // Walk the list until you find the plugin
+  while(curr) {
+    if(curr->plugin==plugin) {
+      if(last) {
+	// Unlink the plugin from the list and delete it's record
+	last->next = curr->next;
+	curr->plugin->DecRef();
+	delete curr;
+	break;
+      }
+    }
+    last = curr;
+    curr = curr->next;
+  }
+  Unlock();
+}
+
+bool csEventCord::GetPass() const
+{
+  return pass;
+}
+
+void csEventCord::SetPass(bool Pass)
+{
+  pass = Pass;
+}
+
+bool csEventCord::PutEvent(iEvent *event)
+{
+  Lock();
+  // Make sure we have a list
+  if(plugins) {
+    PluginData *curr = CONST_CAST(PluginData *) (plugins);
+    // Walk the list until a plugin returns true
+    while(curr) {
+      if(curr->plugin->HandleEvent(*event))
+	return true;
+      curr = curr->next;
+    }      
+  }
+  Unlock();
+
+  // If we pass events to the system driver, then we return 
+  // false so it will process the event.
+  return !pass;
+}
+
+int csSystemDriver::csEventCordsVector::Find(int Category, int SubCategory)
+{
+  int i;
+  for(i = 0; i < Length(); i++) {
+    csEventCord *cord = Get(i);
+    if(cord->category==Category && cord->subcategory==SubCategory)
+      return i;
+  }
+  return -1;
+}     
