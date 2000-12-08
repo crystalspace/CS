3ff3f7cf49128f7d48b4dc24488e620f6d488d32
Eric Sunshine <sunshine@sunshineco.com>
2000-12-08 20:44:55 +0000

    Moved command.cpp and command.h from apps/support to apps/walktest since these files are only used by WalkTest. Updated makefile and other source files to reflect this change.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@5372 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/css.h
+++ include/css.h
@@ -1,6 +1,6 @@
 /*
     Copyright (C) 1998 by Jorrit Tyberghein
-	CSScript module created by Brandon Ehle
+    CSScript module created by Brandon Ehle
   
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -17,30 +17,30 @@
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-//The king of all include files 
-//Good for being a precompiled header, by utilizing the NO_* macros, while stop a complete build
+// The queen of all include files.  Good for being a precompiled header, by
+// utilizing the NO_* macros, while stop a complete build
 
-#ifndef __CS_H__
-#define __CS_H__
+#ifndef __CSS_H__
+#define __CSS_H__
 
-#ifndef NOTHING
+#ifndef BRANDON_NOTHING
 
-//SysDef
+// SysDef
 #ifndef NO_CSSYSDEF
 #include "cssysdef.h"
 #endif
 
-//SCF
+// SCF
 #ifndef NO_CSSCF
 #include "csutil/scf.h"
 #endif
 
-//CS Sys
+// CS Sys
 #ifndef NO_CSSYS
 #include "cssys/sysdriv.h"
 #endif
 
-//CS Interfaces
+// CS Interfaces
 #ifndef NO_CSINTERFACE
 #include "iimage.h"
 #include "ipolygon.h"
@@ -52,13 +52,13 @@
 #include "ipolyset.h"
 #endif
 
-//CS Geom
+// CS Geom
 #ifndef NO_CSGEOM
 #include "csgeom/math2d.h"
 #include "csgeom/math3d.h"
 #endif
 
-//CS Engine
+// CS Engine
 #ifndef NO_CSENGINE
 #include "csengine/collider.h"
 #include "csengine/csview.h"
@@ -68,25 +68,20 @@
 #include "csengine/polygon.h"
 #endif
 
-//CS Object
+// CS Object
 #ifndef NO_CSOBJECT
 #endif
 
-//CS Util
+// CS Util
 #ifndef NO_CSUTIL
 #include "csutil/inifile.h"
 #endif
 
-//CS Parser
+// CS Parser
 #ifndef NO_CSPARSER
 #include "csparser/csloader.h"
 #endif
 
-//CS Support
-#ifndef NO_SUPPORT
-#include "support/command.h"
-#endif
+#endif // BRANDON_NOTHING
 
-#endif
-
-#endif
+#endif // __CSS_H__
