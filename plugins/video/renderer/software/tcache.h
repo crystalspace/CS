5ec6a6cf92ec2a45bc43946fdcab878669ba73d7
Philip Wyett <philswyett@gmx.co.uk>
2000-12-21 04:12:43 +0000

    Found a bug in ~SoftwareCachedTexture (). Which causes an on exit error in "appptlab" in Windows. Because of possible side effects I have merely commented above the offending line for resolution at a later date/more convenient time..
    Hope that is OK.
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@5409 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/renderer/software/tcache.h
+++ plugins/video/renderer/software/tcache.h
@@ -84,7 +84,11 @@ public:
   /// Destroy the lighted texture
   ~SoftwareCachedTexture ()
   {
+	// BUG: Following code line causes an error upon exit
+	//      in "appptlab", when run in Windows. If line is
+	//      removed it causes lock-ups in "appwalktest"
     source->SetCacheData (mipmap, NULL);
+
     delete [] data;
   }
 
