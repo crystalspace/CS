7fa434bf499729b6b146f1b1f53e9c4849ef75a9
Martin Geisse <mgeisse@gmx.net>
2001-02-08 18:02:53 +0000

    changed DirectInput version number from 0x0700 to 0x0300
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@5707 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/cssys/win32/win32.cpp
+++ libs/cssys/win32/win32.cpp
@@ -36,7 +36,7 @@
 
 // Use new DirectInput keyboard handling by Xavier Trochu (xtrochu@yahoo.com)?
 #ifdef DO_DINPUT_KEYBOARD
-#define DIRECTINPUT_VERSION  0x0700
+#define DIRECTINPUT_VERSION  0x0300
 #include <dinput.h>
 #endif
 
