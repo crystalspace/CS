8e34eadb240452b7bd11df36b1524114af13144f
Robert Bate <rbate@mac.com>
2001-02-22 22:20:23 +0000

    Need new prefix file so we can set the carbon flags.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@5878 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ libs/cssys/mac/MacPrefixCarbon.h
@@ -0,0 +1,11 @@
+// these defines appear in a make file on systems that use one
+
+// some mac specific defines
+
+#define __NEED_OPENDIR_PROTOTYPE	1
+#define MACOS_VOLATILE	1
+
+#define TARGET_API_MAC_OS8		0
+#define TARGET_API_MAC_CARBON	1
+#define TARGET_API_MAC_OSX		0
+#define PM_USE_SESSION_APIS		0
