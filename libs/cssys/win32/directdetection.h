4a54e61cc76e8741833c36f9b2966ec908088946
Philip Wyett <philswyett@gmx.co.uk>
2001-02-24 09:25:17 +0000

    Removed very restrictive code which forces DIRECTINPUT_VERSION to 0x0300 and replaced it with test statement block which uses WINVER to set DIRECTINPUT_VERSION.
    - For Win 98/SE/ME/2000 or above DIRECTINPUT_VERSION is set to
      0x0800. Don't worry if your using a lower DX SDK as any DX SDK 5 or
      above in <dinput.h> uses the '#if DIRECTINPUT_VERSION >=' thus
      makes this method totally backward compatible.
    
    - For Win95 and NT4. DIRECTINPUT_VERSION is set to 0x0300 as it's the
      highest NT4 supports and you cannot seperate NT4 and 95 by version so
      have to be set to the same value.
    
    Have tested on most flavors of Windows and both VC and MingW...
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@5891 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/cssys/win32/directdetection.h
+++ libs/cssys/win32/directdetection.h
@@ -1,17 +1,32 @@
-// DirectDetection.h: interface for the DirectDetection class.
-//
-//////////////////////////////////////////////////////////////////////
+/*
+    Copyright (C) 1998 by Jorrit Tyberghein
 
-#if !defined(AFX_DIRECTDETECTION_H__E850B940_FD15_11D1_933A_0000B43D8331__INCLUDED_)
-#define AFX_DIRECTDETECTION_H__E850B940_FD15_11D1_933A_0000B43D8331__INCLUDED_
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
-#if _MSC_VER >= 1000
-#pragma once
-#endif // _MSC_VER >= 1000
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
 
-// define directInput to be backwards compatible with dx3 input
-#define DIRECTINPUT_VERSION 0x0300
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __DIRECTDETECTION_H__
+#define __DIRECTDETECTION_H__
+
+#if WINVER >= 0x0500
+  #define DIRECTINPUT_VERSION 0x0800
+#else
+  #define DIRECTINPUT_VERSION 0x0300
+#endif
+
 #include <windowsx.h>
+#include <dinput.h>
 #include <ddraw.h>
 #include <d3d.h>
 #include <d3dcaps.h>
@@ -127,4 +142,4 @@ public:
   DirectDetectionDevice * Devices; // list of devices
 };
 
-#endif // !defined(AFX_DIRECTDETECTION_H__E850B940_FD15_11D1_933A_0000B43D8331__INCLUDED_)
+#endif // End of __DIRECTDETECTION_H__
