c66f2a8699ffa615df6989b8a276c8397ed3e7b4
Martin Geisse <mgeisse@gmx.net>
2001-03-01 17:57:51 +0000

    added a simple convenience class that makes using the config manager easier
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@5921 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/csutil/cfgacc.h
@@ -0,0 +1,57 @@
+/*
+    Copyright (C) 2001 by Martin Geisse <mgeisse@gmx.net>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CFGMGR_H__
+#define __CFGMGR_H__
+
+#include "csutil/csvector.h"
+#include "isystem.h"
+struct iConfigFileNew;
+
+/**
+ * This is a simple convenience class that can be used to deal with the
+ * sytem config manager. It adds config files to the manager on demand and
+ * automatically removes them when destroyed. In addition, you can use
+ * the '->' operator to access the system configuration. Note that you must
+ * use the same iSystem for all config files you add.
+ */
+class csConfigAccess
+{
+public:
+  /// constructor
+  csConfigAccess();
+  /// constructor (also adds a config file)
+  csConfigAccess(iSystem *sys, const char *Filename,
+    bool vfs = true, int Priority = iSystem::ConfigPriorityPlugIn);
+  /// destructor
+  ~csConfigAccess();
+
+  /// add a config file
+  void AddConfig(iSystem *sys, const char *Filename,
+    bool vfs = true, int Priority = iSystem::ConfigPriorityPlugIn);
+  /// access to system configuration
+  iConfigFileNew *operator->();
+  /// access to system configuration
+  operator iConfigFileNew* ();
+
+private:
+  iSystem *System;
+  csVector ConfigFiles;
+};
+
+#endif
---
+++ libs/csutil/cfgacc.cpp
@@ -0,0 +1,55 @@
+/*
+    Copyright (C) 2001 by Martin Geisse <mgeisse@gmx.net>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+#include "csutil/cfgacc.h"
+#include "icfgmgr.h"
+
+csConfigAccess::csConfigAccess()
+{
+  System = NULL;
+}
+
+csConfigAccess::csConfigAccess(iSystem *sys, const char *fname,
+  bool vfs, int priority)
+{
+  AddConfig(sys, fname, vfs, priority);
+}
+
+csConfigAccess::~csConfigAccess()
+{
+  for (long i=0; i<ConfigFiles.Length(); i++)
+    System->RemoveConfig((iConfigFileNew*)ConfigFiles.Get(i));
+}
+
+void csConfigAccess::AddConfig(iSystem *sys, const char *fname,
+  bool vfs, int priority)
+{
+  System = sys;
+  ConfigFiles.Push(System->AddConfig(fname, vfs, priority));
+}
+
+iConfigFileNew *csConfigAccess::operator->()
+{
+  return System->GetConfig();
+}
+
+csConfigAccess::operator iConfigFileNew* ()
+{
+  return System->GetConfig();
+}
