a551c8a6e18e96350c6af1f09289f22b6f84816e
Wouter Wijngaards <wouter@users.sourceforge.net>
2001-03-20 10:52:51 +0000

    Added the following methods to explosion particlesystem:   GetNumberParticles, GetCenter, GetPush, GetNrSides, GetPartRadius,   GetLighting, GetSpreadPos, GetSpreadSpeed, GetSpreadAcceleration and   GetFadeSprites.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@6071 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/imesh/explode.h
+++ include/imesh/explode.h
@@ -34,27 +34,47 @@ struct iExplosionState : public iBase
 {
   /// Set the number of particles to use.
   virtual void SetNumberParticles (int num) = 0;
+  /// Get the number of particles
+  virtual int GetNumberParticles () const = 0;
   /// Set the explosion center.
   virtual void SetCenter (const csVector3& center) = 0;
+  /// Get the explosion center
+  virtual const csVector3 &GetCenter () const = 0;
   /// Set the push vector.
   virtual void SetPush (const csVector3& push) = 0;
+  /// Get the push vector.
+  virtual const csVector3& GetPush () const = 0;
   /// Set the number of sides.
   virtual void SetNrSides (int nr_sides) = 0;
+  /// Get the number of sides.
+  virtual int GetNrSides () const = 0;
   /// Set the radius of all particles.
   virtual void SetPartRadius (float part_radius) = 0;
+  /// Get the radius of all particles.
+  virtual float GetPartRadius () const = 0;
   /// Enable or disable lighting.
   virtual void SetLighting (bool l) = 0;
+  /// See if lighting is enabled or disabled.
+  virtual bool GetLighting () const = 0;
   /// Set the spread position.
   virtual void SetSpreadPos (float spread_pos) = 0;
+  /// Get the spread position.
+  virtual float GetSpreadPos () const = 0;
   /// Set the spread speed.
   virtual void SetSpreadSpeed (float spread_speed) = 0;
+  /// Get the spread speed.
+  virtual float GetSpreadSpeed () const = 0;
   /// Set the spread acceleration.
   virtual void SetSpreadAcceleration (float spread_accel) = 0;
+  /// Get the spread acceleration.
+  virtual float GetSpreadAcceleration () const = 0;
   /**
    * Set particles to be scaled to nothing starting at fade_particles msec 
    * before self-destruct.
    */
   virtual void SetFadeSprites (cs_time fade_time) = 0;
+  /// See if particles are faded (returns true), and returns fade time too.
+  virtual bool GetFadeSprites (cs_time& fade_time) const = 0;
   /**
    * Add a light at explosion center. add msec when light starts fading,
    * which is used when time_to_live is set / SelfDestruct is used.
--- plugins/mesh/object/explo/explo.h
+++ plugins/mesh/object/explo/explo.h
@@ -87,60 +87,84 @@ public:
     number = num;
     SetNumber (number);
   }
+  /// Get the number of particles
+  int GetNumberParticles () const {return number;}
   /// Set the explosion center.
   void SetCenter (const csVector3& center)
   {
     initialized = false;
     csExploMeshObject::center = center;
   }
+  /// Get the explosion center
+  const csVector3 &GetCenter () const {return center;}
   /// Set the push vector.
   void SetPush (const csVector3& push)
   {
     initialized = false;
     csExploMeshObject::push = push;
   }
+  /// Get the push vector.
+  const csVector3& GetPush () const {return push;}
   /// Set the number of sides.
   void SetNrSides (int nr_sides)
   {
     initialized = false;
     csExploMeshObject::nr_sides = nr_sides;
   }
+  /// Get the number of sides.
+  int GetNrSides () const {return nr_sides;}
   /// Set the radius of all particles.
   void SetPartRadius (float part_radius)
   {
     initialized = false;
     csExploMeshObject::part_radius = part_radius;
   }
+  /// Get the radius of all particles.
+  float GetPartRadius () const {return part_radius;}
   /// Enable or disable lighting.
   void SetLighting (bool l)
   {
     initialized = false;
     lighted_particles = l;
   }
+  /// See if lighting is enabled or disabled.
+  bool GetLighting () const {return lighted_particles;}
   /// Set the spread position.
   void SetSpreadPos (float spread_pos)
   {
     initialized = false;
     csExploMeshObject::spread_pos = spread_pos;
   }
+  /// Get the spread position.
+  float GetSpreadPos () const {return spread_pos;}
   /// Set the spread speed.
   void SetSpreadSpeed (float spread_speed)
   {
     initialized = false;
     csExploMeshObject::spread_speed = spread_speed;
   }
+  /// Get the spread speed.
+  float GetSpreadSpeed () const {return spread_speed;}
   /// Set the spread acceleration.
   void SetSpreadAcceleration (float spread_accel)
   {
     initialized = false;
     csExploMeshObject::spread_accel = spread_accel;
   }
+  /// Get the spread acceleration.
+  float GetSpreadAcceleration () const {return spread_accel;}
   /**
    * Set particles to be scaled to nothing starting at fade_particles msec 
    * before self-destruct.
    */
   void SetFadeSprites (cs_time fade_time) 
   { scale_particles = true; fade_particles = fade_time; }
+  /// See if particles are faded (returns true), and returns fade time too.
+  bool GetFadeSprites (cs_time& fade_time) const
+  {
+    if(!scale_particles) return false;
+    fade_time = fade_particles; return true;
+  }
 
   /// Explosion has a dynamic light at the center?
   bool HasLight () { return has_light; }
@@ -169,41 +193,81 @@ public:
     {
       scfParent->SetNumberParticles (num);
     }
+    virtual int GetNumberParticles () const 
+    {
+      return scfParent->GetNumberParticles();
+    }
     virtual void SetCenter (const csVector3& center)
     {
       scfParent->SetCenter (center);
+    }
+    virtual const csVector3 &GetCenter () const 
+    {
+      return scfParent->GetCenter ();
     }
     virtual void SetLighting (bool l)
     {
       scfParent->SetLighting (l);
     }
+    virtual bool GetLighting () const 
+    {
+      return scfParent->GetLighting ();
+    }
     virtual void SetPush (const csVector3& push)
     {
       scfParent->SetPush (push);
+    }
+    virtual const csVector3 &GetPush () const 
+    {
+      return scfParent->GetPush ();
     }
     virtual void SetNrSides (int nr_sides)
     {
       scfParent->SetNrSides (nr_sides);
     }
+    virtual int GetNrSides () const 
+    {
+      return scfParent->GetNrSides();
+    }
     virtual void SetPartRadius (float part_radius)
     {
       scfParent->SetPartRadius (part_radius);
+    }
+    virtual float GetPartRadius () const 
+    {
+      return scfParent->GetPartRadius ();
     }
     virtual void SetSpreadPos (float spread_pos)
     {
       scfParent->SetSpreadPos (spread_pos);
     }
+    virtual float GetSpreadPos () const 
+    {
+      return scfParent->GetSpreadPos ();
+    }
     virtual void SetSpreadSpeed (float spread_speed)
     {
       scfParent->SetSpreadSpeed (spread_speed);
+    }
+    virtual float GetSpreadSpeed () const 
+    {
+      return scfParent->GetSpreadSpeed ();
     }
     virtual void SetSpreadAcceleration (float spread_accel)
     {
       scfParent->SetSpreadAcceleration (spread_accel);
     }
+    virtual float GetSpreadAcceleration () const 
+    {
+      return scfParent->GetSpreadAcceleration ();
+    }
     virtual void SetFadeSprites (cs_time fade_time)
     {
       scfParent->SetFadeSprites (fade_time);
+    }
+    virtual bool GetFadeSprites (cs_time& fade_time) const 
+    {
+      return scfParent->GetFadeSprites (fade_time);
     }
     virtual void AddLight (iEngine* engine, iSector* sector, cs_time fade = 200)
     {
