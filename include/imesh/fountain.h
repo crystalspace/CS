193ccfe1e5332d64e6aef47bcb23f32260f02610
Wouter Wijngaards <wouter@users.sourceforge.net>
2001-03-22 11:26:35 +0000

    Added a host of Get methods to the fountain mesh plugin.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@6086 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/imesh/fountain.h
+++ include/imesh/fountain.h
@@ -1,5 +1,6 @@
 /*
     Copyright (C) 2000 by Jorrit Tyberghein
+    Copyright (C) 2001 by W.C.A. Wijngaards
   
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -32,24 +33,44 @@ struct iFountainState : public iBase
 {
   /// Set the number of particles to use.
   virtual void SetNumberParticles (int num) = 0;
+  /// Get the number of particles used.
+  virtual int GetNumberParticles () const = 0;
   /// Set the size of the fountain drops.
   virtual void SetDropSize (float dropwidth, float dropheight) = 0;
+  /// Get the size of the fountain drops.
+  virtual void GetDropSize (float& dropwidth, float& dropheight) const = 0;
   /// Set origin of the fountain.
   virtual void SetOrigin (const csVector3& origin) = 0;
+  /// Get origin of the fountain.
+  virtual const csVector3& GetOrigin () const = 0;
   /// Enable or disable lighting.
   virtual void SetLighting (bool l) = 0;
+  /// See if lighting is enabled.
+  virtual bool GetLighting () const = 0;
   /// Set acceleration.
   virtual void SetAcceleration (const csVector3& accel) = 0;
+  /// Get acceleration.
+  virtual const csVector3& GetAcceleration () const = 0;
   /// Set elevation.
   virtual void SetElevation (float elev) = 0;
+  /// Get elevation.
+  virtual float GetElevation () const = 0;
   /// Set azimuth.
   virtual void SetAzimuth (float azi) = 0;
+  /// Get azimuth.
+  virtual float GetAzimuth () const = 0;
   /// Set opening.
   virtual void SetOpening (float open) = 0;
+  /// Get opening.
+  virtual float GetOpening () const = 0;
   /// Set speed.
   virtual void SetSpeed (float spd) = 0;
+  /// Get speed.
+  virtual float GetSpeed () const = 0;
   /// Set fall time.
   virtual void SetFallTime (float ftime) = 0;
+  /// Get fall time.
+  virtual float GetFallTime () const = 0;
 };
 
 #endif
--- plugins/mesh/object/fountain/fountain.h
+++ plugins/mesh/object/fountain/fountain.h
@@ -78,6 +78,8 @@ public:
 
   /// Set the number of particles to use.
   void SetNumberParticles (int num) { initialized = false; number = num; }
+  /// Get the number of particles used.
+  int GetNumberParticles () const { return number; }
   /// Set the size of the fountain drops.
   void SetDropSize (float dropwidth, float dropheight)
   {
@@ -85,54 +87,76 @@ public:
     drop_width = dropwidth;
     drop_height = dropheight;
   }
+  /// Get the size of the fountain drops.
+  void GetDropSize (float& dropwidth, float& dropheight) const
+  {
+    dropwidth = drop_width;
+    dropheight = drop_height;
+  }
   /// Set origin of the fountain.
   void SetOrigin (const csVector3& origin)
   {
     initialized = false;
     csFountainMeshObject::origin = origin;
   }
+  /// Get origin of the fountain.
+  const csVector3& GetOrigin () const { return origin; }
   /// Enable or disable lighting.
   void SetLighting (bool l)
   {
     initialized = false;
     lighted_particles = l;
   }
+  /// See if lighting is enabled.
+  bool GetLighting () const { return lighted_particles; }
   /// Set acceleration.
   void SetAcceleration (const csVector3& accel)
   {
     initialized = false;
     csFountainMeshObject::accel = accel;
   }
+  /// Get acceleration.
+  const csVector3& GetAcceleration () const { return accel; }
   /// Set elevation.
   void SetElevation (float elev)
   {
     initialized = false;
     elevation = elev;
   }
+  /// Get elevation.
+  float GetElevation () const { return elevation; }
   /// Set azimuth.
   void SetAzimuth (float azi)
   {
     initialized = false;
     azimuth = azi;
   }
+  /// Get azimuth.
+  float GetAzimuth () const { return azimuth; }
   /// Set opening.
   void SetOpening (float open)
   {
     initialized = false;
     opening = open;
   }
+  /// Get opening.
+  float GetOpening () const { return opening; }
   /// Set speed.
   void SetSpeed (float spd)
   {
     initialized = false;
     speed = spd;
   }
+  /// Get speed.
+  float GetSpeed () const { return speed; }
   /// Set fall time.
   void SetFallTime (float ftime)
   {
     initialized = false;
     fall_time = ftime;
   }
+  /// Get fall time.
+  float GetFallTime () const { return fall_time; }
 
   /// Update the particle system.
   virtual void Update (cs_time elapsed_time);
@@ -186,6 +210,46 @@ public:
     virtual void SetFallTime (float ftime)
     {
       scfParent->SetFallTime (ftime);
+    }
+    virtual int GetNumberParticles () const
+    {
+      return scfParent->GetNumberParticles ();
+    }
+    virtual void GetDropSize (float& dropwidth, float& dropheight) const
+    {
+      scfParent->GetDropSize (dropwidth, dropheight);
+    }
+    virtual const csVector3& GetOrigin () const
+    {
+      return scfParent->GetOrigin ();
+    }
+    virtual bool GetLighting () const
+    {
+      return scfParent->GetLighting ();
+    }
+    virtual const csVector3& GetAcceleration () const
+    {
+      return scfParent->GetAcceleration ();
+    }
+    virtual float GetElevation () const
+    {
+      return scfParent->GetElevation ();
+    }
+    virtual float GetAzimuth () const
+    {
+      return scfParent->GetAzimuth ();
+    }
+    virtual float GetOpening () const
+    {
+      return scfParent->GetOpening ();
+    }
+    virtual float GetSpeed () const
+    {
+      return scfParent->GetSpeed ();
+    }
+    virtual float GetFallTime () const
+    {
+      return scfParent->GetFallTime ();
     }
   } scfiFountainState;
   friend class FountainState;
