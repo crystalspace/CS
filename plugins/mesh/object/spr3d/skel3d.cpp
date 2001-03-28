37745a913e71aaec2abdd1d1c9058ef4b7b68868
Philip Wyett <philswyett@gmx.co.uk>
2001-03-28 15:50:08 +0000

    Here is a better fix being that strdup if fails is a NULL return.
    Ok, note to self, beware of VC6 undo... :-) And don't be so
    quick to commit to cvs...Sorry.
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@6143 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/object/spr3d/skel3d.cpp
+++ plugins/mesh/object/spr3d/skel3d.cpp
@@ -121,8 +121,9 @@ void csSkelLimb::ComputeBoundingBox (csPoly3D* source)
 
 void csSkelLimb::SetName (const char *newname)
 {
-  if (name != NULL)
-    name = strdup (newname);
+  if (name) name = NULL;
+
+  name = strdup (newname);
 }
 
 iSkeletonConnection* csSkelLimb::CreateConnection ()
@@ -224,8 +225,8 @@ void csSkelLimbState::Transform (const csTransform& tr, csVector3* source,
 
 void csSkelLimbState::SetName (const char *newname)
 {
-  if (name != NULL)
-    name = strdup (newname);
+  if (name) name = NULL;
+  name = strdup (newname);
 }
 
 void csSkelConnectionState::Transform (const csTransform& tr,
