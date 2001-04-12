ba0a67d5012bfb81291827254d5bc4de2b39b236
Philip Wyett <philswyett@gmx.co.uk>
2001-04-12 17:15:56 +0000

    Fixed VC6 compile error in new terrain plugin within function:
    virtual void SetDirLight (const csVector3& pos, const csColor& col)
    
    Was doing 2 similar tests within if() which VC did not like. Just
    commented out the offending one.
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@6248 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/terrain/object/terrfunc/terrfunc.h
+++ plugins/terrain/object/terrfunc/terrfunc.h
@@ -92,11 +92,7 @@ public:
 
   virtual void SetDirLight (const csVector3& pos, const csColor& col)
   {
-    if (do_dirlight &&
-    	((dirlight - pos) < .00001) &&
-    	ABS (col.red-dirlight_color.red) < .0001 &&
-    	ABS (col.green-dirlight_color.green) < .0001 &&
-	ABS (col.blue-dirlight_color.blue) < .0001)
+    if (do_dirlight /*@@@ && ((dirlight - pos) < .00001)*/ && ABS (col.red-dirlight_color.red) < .0001 && ABS (col.green-dirlight_color.green) < .0001 && ABS (col.blue-dirlight_color.blue) < .0001)
       return;	// Nothing changed.
     do_dirlight = true;
     dirlight = pos;
