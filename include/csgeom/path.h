5a7deeb46f79f6af605106682b260b4e927f26e1
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2001-04-17 20:09:10 +0000

    Added csPath to csgeom library. This is a subclass of csCubicSpline that will interpolate over 9 dimensions (position, up-vector, and forward-vector). This is ideal in combination with csReversibleTransform::LookAt() to have an object or camera move along a path. This code (as well as the cubic spline code) is totally untested at this moment.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@6336 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/csgeom/path.h
@@ -0,0 +1,100 @@
+/*
+    Copyright (C) 2001 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __PATH_H__
+#define __PATH_H__
+
+#include "csgeom/spline.h"
+
+/**
+ * A path in 3D.
+ * An object or camera can use this object to trace a path in 3D.
+ * This is particularly useful in combination with
+ * csReversibleTransform::LookAt().
+ */
+class csPath : public csCubicSpline
+{
+private:
+  void SetVectorAsDimensionValues (int dim, csVector3* v)
+  {
+    int i;
+    float* x, * y, * z;
+    x = new float [GetNumPoints ()];
+    y = new float [GetNumPoints ()];
+    z = new float [GetNumPoints ()];
+    for (i = 0 ; i < GetNumPoints () ; i++)
+    {
+      x[i] = v[i].x;
+      y[i] = v[i].y;
+      z[i] = v[i].z;
+    }
+    SetDimensionValues (dim+0, x);
+    SetDimensionValues (dim+1, y);
+    SetDimensionValues (dim+2, z);
+    delete[] x;
+    delete[] y;
+    delete[] z;
+  }
+
+public:
+  /// Create a path with p points.
+  csPath (int p) : csCubicSpline (9, p) { }
+
+  /// Destroy the path.
+  ~csPath () { }
+
+  /// Set the position vectors (first three dimensions of the cubic spline).
+  void SetPositionVectors (csVector3* v)
+  {
+    SetVectorAsDimensionValues (0, v);
+  }
+  /// Set the up vectors (dimensions 3 to 5).
+  void SetUpVectors (csVector3* v)
+  {
+    SetVectorAsDimensionValues (3, v);
+  }
+  /// Set the forward vectors (dimensions 6 to 8).
+  void SetForwardVectors (csVector3* v)
+  {
+    SetVectorAsDimensionValues (6, v);
+  }
+
+  /// Get the interpolated position.
+  void GetInterpolatedPosition (csVector3& pos)
+  {
+    pos.x = GetInterpolatedDimension (0);
+    pos.y = GetInterpolatedDimension (1);
+    pos.z = GetInterpolatedDimension (2);
+  }
+  /// Get the interpolated up vector.
+  void GetInterpolatedUp (csVector3& pos)
+  {
+    pos.x = GetInterpolatedDimension (3);
+    pos.y = GetInterpolatedDimension (4);
+    pos.z = GetInterpolatedDimension (5);
+  }
+  /// Get the interpolated forward vector.
+  void GetInterpolatedForward (csVector3& pos)
+  {
+    pos.x = GetInterpolatedDimension (6);
+    pos.y = GetInterpolatedDimension (7);
+    pos.z = GetInterpolatedDimension (8);
+  }
+};
+
+#endif // __PATH_H__
