2734ea173107ccd3a3cac0a37b1179d664787690
Christopher Nelson <paradox@bbhc.org>
2001-05-14 21:49:45 +0000

    Updated the lexical analyzer definition, changed  the iString's to csString's since they aren't interchangeable currently.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@6651 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/ivaria/lexan.h
+++ include/ivaria/lexan.h
@@ -3,6 +3,10 @@
 
 #include "cssysdef.h"
 #include "isys/plugin.h"
+#include "iutil/databuff.h"
+#include "iutil/string.h"
+#include "csutil/csstring.h"
+
 
 SCF_VERSION (iRegExp, 0, 0, 1);
 
@@ -45,7 +49,33 @@ struct iLexicalAnalyzer : public iPlugIn
 {
   /// Initializes the bytecode machine
   virtual bool Initialize (iSystem *sys) = 0;
+  
+  /// Register a regular expression for matching
+  virtual bool RegisterRegExp(unsigned int key, iRegExp &re)=0;
 
+  /// Unregister a previously registered expression
+  virtual bool UnregisterRegExp(unsigned int key)=0;
+
+  /// Requests a new (free) key
+  virtual unsigned int GetFreeKey()=0;
+  
+  /// Returns the last error.  Value is only valid when a member function has returned false.
+  virtual unsigned int GetError() = 0;
+  
+  /// Returns the key of the last RE matched
+  virtual unsigned int GetMatchedKey()=0;
+  
+  /// Returns a pointer to the last matched text
+  virtual csString * GetMatchedText()=0;
+  
+  /// Saves the current input stream and state, then resets the current state and uses stream from buf
+  virtual bool PushStream(iDataBuffer &buf)=0;
+  
+  /// Replaces the current input stream and state with the next stream and state on the stack
+  virtual bool PopStream()=0;
+  
+  /// Scans the input stream for a match by one of the RegularExpression's returns 0 on failure, otherwise returns the key of the matched RE.
+  virtual unsigned int Match() = 0;
 };
 
 #endif
