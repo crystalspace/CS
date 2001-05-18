cf5db15b9891af7219e46cecbbb03a4bb19f738b
Wouter Wijngaards <wouter@users.sourceforge.net>
2001-05-18 20:29:25 +0000

    Fixed procedural water, so that it will look nice in software mode. No more pixelized look. By removing the proc.texture.only flag.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@6685 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csfx/prwater.cpp
+++ libs/csfx/prwater.cpp
@@ -38,8 +38,8 @@ csProcWater::csProcWater () : csProcTexture()
   mat_w = 64;
   mat_h = 64;
 
-  texFlags = CS_TEXTURE_3D | CS_TEXTURE_PROC | CS_TEXTURE_NOMIPMAPS | 
-    CS_TEXTURE_PROC_ALONE_HINT;
+  texFlags = CS_TEXTURE_3D | CS_TEXTURE_PROC | CS_TEXTURE_NOMIPMAPS ;
+    //| CS_TEXTURE_PROC_ALONE_HINT;
 }
 
 csProcWater::~csProcWater ()
