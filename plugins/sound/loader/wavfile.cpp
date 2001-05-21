27719b671b1c02ce77a30afd4fc65de27c593873
Martin Geisse <mgeisse@gmx.net>
2001-05-21 18:04:12 +0000

    committed a remake of the WAV loader by Andreas H�fler
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@6709 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/sound/loader/wavfile.cpp
+++ plugins/sound/loader/wavfile.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 1998 by Jorrit Tyberghein    
+    Copyright (C) 2001 by Andreas Höfler <andreas.hoefler@gmx.at>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -28,7 +28,70 @@
 #include "sndload.h"
 
 // Microsoft Wav file loader
-//  support 8 and 16 bits PCM (RIFF)
+// support 8 and 16 bits PCM (RIFF)
+
+/* ====================================
+   short description of wav-file-format (from www.wotsit.org, look there for details)
+   ====================================
+
+  WAV-data is contained within the RIFF-file-format. This file format
+  consists of headers and "chunks".
+
+  A RIFF file has an 8-byte RIFF header.
+
+  struct 
+  {
+    char  id[4];   // identifier string = "RIFF"
+    DWORD len;     // remaining length after this header
+  } riff_hdr;
+
+  The riff_hdr is immediately followed by a 4-byte data type identifier.
+  For .WAV files this is "WAVE" as follows:
+
+  char wave_id[4]; // WAVE file identifier = "WAVE"
+
+  The remaining file consists of "chunks". A chunk is following:
+
+  struct
+  { 
+    char  id[4];   // identifier, e.g. "fmt " or "data"
+    DWORD len;     // remaining chunk length after this header
+  } chunk_hdr;
+
+  The chunk is followed by the data of the chunk.
+
+  Unexpected chunks should be allowed (and ignored) within a RIFF-file.
+
+  For WAV-files following chunks can be expected:
+  (only necessary chunks are listed)
+
+  WAVE Format Chunk, this has to appear before the WAVE Data Chunk:
+
+  struct _FMTchk
+  {
+    char chunk_id[4];  // for the format-chunk of wav-files always "fmt "
+    unsigned long len; // length of this chunk after this 8 bytes of header
+    unsigned short fmt_tag; // format category of file. 0x0001 = Microsoft PCM
+    unsigned short channel; // number of channels (1 = mono, 2 = stereo)
+    unsigned long samples_per_sec; // sampling rate
+    unsigned long avg_bytes_per_sec; // for buffer estimation
+    unsigned short blk_align; // data block size
+    unsigned short bits_per_sample; // sample size  (only if Microsoft PCM)
+  } fmtchk;
+
+  WAVE Data Chunk:
+
+  struct _WAVchk
+  {
+    char chunk_id[4];  // for the data-chunk of wav-files always "data"
+    unsigned long len; // length of this chunk after this 8 bytes of header
+  }
+
+   ====================================
+   End of short format-description
+   ====================================
+
+*/
 
 class csSoundLoader_WAV : public iSoundLoader
 {
@@ -51,110 +114,162 @@ IMPLEMENT_IBASE_END;
 
 IMPLEMENT_FACTORY(csSoundLoader_WAV);
 
-
-struct _WAVchk
+// header of the RIFF-chunk
+struct _RIFFchk
 {
-  char chunk[4];
-  unsigned long len;
-} wavchk;
+  char riff_id[4]; // for RIFF-files always "RIFF"
+  unsigned long len; // length of chunk after this 8 bytes of header
+  char wave_id[4]; // for wav-files always "WAVE"
+} riffchk;
 
-struct _WAVhdr
+// header of the wav-format-chunk
+struct _FMTchk
 {
-  char format[4];
-  unsigned long f_len;
-  char fmt[8];
-  unsigned long fmt_len;
+  char chunk_id[4]; // for the format-chunk of wav-files always "fmt "
+  unsigned long len; // length of this chunk after this 8 bytes of header
   unsigned short fmt_tag;
   unsigned short channel;
   unsigned long samples_per_sec;
   unsigned long avg_bytes_per_sec;
   unsigned short blk_align;
   unsigned short bits_per_sample;
-} wavhdr;
+} fmtchk;
 
-#define setStream(x) {if(x>size) {goto exit_read;} else {index=x;}}
-#define canAddStream(x) {if((index+x)>size) goto exit_read;}
-#define addStream(x) {if((index+x)>size) {goto exit_read;} else {index+=x;}}
-#define Stream buf[index]
+// header of the wav-data-chunk
+struct _WAVchk
+{
+  char chunk_id[4]; // for wav-data-chunk this is always "data"
+  unsigned long len; // length of chunk after this 8 bytes of header
+} wavchk;
 
-iSoundData *csSoundLoader_WAV::LoadSound(void *databuf, ULong size) const {
-  UByte *buf = (UByte*) databuf;
-  int index=0;
 
-  csSoundDataRaw *sb= NULL;
-  char *data=NULL;
-  UByte *ptr;
-  UByte *ptr_end;
 
-  memset(&wavhdr, 0, sizeof(wavhdr));
-  if(size<sizeof(wavhdr)) goto exit_read;
-    
-  if(memcpy(&wavhdr, &Stream, sizeof(wavhdr))==NULL)
-    goto exit_read;
-  canAddStream(sizeof(wavhdr));
-  addStream(sizeof(wavhdr));
+iSoundData* csSoundLoader_WAV::LoadSound (void* databuf, ULong size) const
+{
+  UByte* buf = (UByte*) databuf;
+  UByte* ptr = NULL;
+  UByte* ptr_end = NULL;
+  csSoundDataRaw* sb = NULL;
+  csSoundFormat format;
+  char* data = NULL;
 
-  if(memcmp(wavhdr.format, "RIFF", 4))
-    goto exit_read;
+  int index = 0;
 
-  if(memcmp(wavhdr.fmt, "WAVEfmt ", 8))
-    goto exit_read;
+// check if this is a valid wav-file
+
+  // check if file has the size to be able to contain all necessary chunks
+  if (size < (sizeof (riffchk) + sizeof (fmtchk) + sizeof (wavchk)))
+    return NULL;
+
+  // copy RIFF-header
+  if (memcpy(&riffchk, &buf[0], sizeof (riffchk)) == NULL)
+    return NULL;
+
+  // check RIFF-header
+  if (memcmp (riffchk.riff_id, "RIFF", 4) != 0)
+    return NULL;
+
+  // check WAVE-id
+  if (memcmp (riffchk.wave_id, "WAVE", 4) != 0)
+    return NULL;
   
-#ifdef CS_BIG_ENDIAN
-  wavhdr.f_len = ByteSwap32bit( wavhdr.f_len );
-  wavhdr.fmt_len = ByteSwap32bit( wavhdr.fmt_len );
-  wavhdr.fmt_tag = ByteSwap16bit( wavhdr.fmt_tag );
-  wavhdr.channel = ByteSwap16bit( wavhdr.channel );
-  wavhdr.samples_per_sec = ByteSwap32bit( wavhdr.samples_per_sec );
-  wavhdr.avg_bytes_per_sec = ByteSwap32bit( wavhdr.avg_bytes_per_sec );
-  wavhdr.blk_align = ByteSwap16bit( wavhdr.blk_align );
-  wavhdr.bits_per_sample = ByteSwap16bit( wavhdr.bits_per_sample );
-#endif // CS_BIG_ENDIAN
+// find format-chunk, copy it into struct and make corrections if necessary
 
-  if(!((wavhdr.channel == 1) || (wavhdr.channel == 2)))
-    goto exit_read;
+  // set index after riff-header to the first chunk inside
+  index += sizeof (riffchk);
   
-
-  ptr=&Stream;
-  ptr_end=ptr+size;
-  while(ptr<ptr_end)
+  // find format-chunk
+  bool found = false; // true, if format-chunk was found
+  for ( ;
+       (found == false) && ((index + sizeof (fmtchk)) < size) ;
+       index += fmtchk.len + 8 // +8, because chunk_id + len are not counted in len
+      )
   {
-    memcpy(&wavchk, ptr, sizeof(wavchk));
-#ifdef CS_BIG_ENDIAN
-    wavchk.len = ByteSwap32bit( wavchk.len );
-#endif // CS_BIG_ENDIAN
+    if (memcpy(&fmtchk, &buf[index], sizeof (fmtchk)) == NULL)
+      return NULL;
 
-    ptr+=sizeof(wavchk);
-    if(memcmp(wavchk.chunk, "data", 4)==0)
-      break;
-    ptr+=wavchk.len;
+    if (memcmp(fmtchk.chunk_id, "fmt ", 4) == 0)
+      found = true;
+
+    // correct length of chunk on big endian system
+    #ifdef CS_BIG_ENDIAN     // @@@ correct fmtchk.len on big-endian systems?
+      fmtchk.len = ByteSwap32bit (fmtchk.len);
+    #endif
+  }
+
+  // no format-chunk found -> no valid file
+  if (found == false)
+    return NULL;
+
+  // correct the chunk, if under big-endian system
+  #ifdef CS_BIG_ENDIAN // @@@ is this correct?
+    fmtchk.fmt_tag = ByteSwap16 (fmtchk.fmt_tag);
+    fmtchk.channel = ByteSwap16 (fmtchk.channel);
+    fmtchk.samples_per_sec = ByteSwap32 (fmtchk.samples_per_sec);
+    fmtchk.avg_bytes_per_sec = ByteSwap32 (fmtchk.avg_bytes_per_sec);
+    fmtchk.blk_align = ByteSwap16 (fmtchk.blk_align);
+    fmtchk.bits_per_sample = ByteSwap16 (fmtchk.bits_per_sample);
+  #endif // CS_BIG_ENDIAN
+
+  // check format of file
+
+  // only 1 or 2 channels are valid
+  if (!((fmtchk.channel == 1) || (fmtchk.channel == 2)))
+     return NULL;
+
+  // only Microsoft PCM wave files are valid
+  if (!(fmtchk.fmt_tag == 0x0001))
+    return NULL;
+
+// find wav-data-chunk
+  found = false; // true, if wav-data-chunk was found
+  for ( ;
+       (found == false) && ((index + sizeof (wavchk)) < size) ;
+       index += wavchk.len + 8 // +8, because chunk_id and len are not counted in len
+      )
+  {
+    if (memcpy(&wavchk, &buf[index], sizeof (wavchk)) == NULL)
+      return NULL;
+
+    if (memcmp(wavchk.chunk_id, "data", 4) == 0)
+      found = true;
+
+    // correct length of chunk on big endian systems
+    #ifdef CS_BIG_ENDIAN     // @@@ correct wavchk.len on big-endian systems?
+      wavchk.len = ByteSwap32bit (wavchk.len);
+    #endif
+  }
+
+  // no wav-data-chunk found -> no valid file
+  if (found == false)
+    return NULL;
+
+  // index points now after wav-data, so correct it
+  index -= wavchk.len;
+
+  // make new buffer, which contains the wav-data
+  data = new char[wavchk.len];
+    
+  // copy the wav-data into the buffer
+  if (memcpy(data, &buf[index], wavchk.len)==NULL)
+  {
+    delete[] data;
+    return NULL;
   }
   
-  data=new char[wavchk.len];
-  if(data==NULL)
-    goto exit_read;
-  
-  if(memcpy(data, ptr, wavchk.len)==NULL)
-    goto exit_read;  
+  #ifdef CS_BIG_ENDIAN // @@@ correct?
+  if (fmtchk.bits_per_sample == 16)
+    ByteSwap16bitBuffer ( (unsigned short*)data, wavchk.len / 2);
+  #endif // CS_BIG_ENDIAN
 
-#ifdef CS_BIG_ENDIAN
-  if ( wavhdr.bits_per_sample==16 ) {
-    ByteSwap16bitBuffer( (unsigned short *)data, wavchk.len / 2 );
-  }
-#endif // CS_BIG_ENDIAN
+  // set up format for csSoundDataRaw
+  format.Freq = fmtchk.samples_per_sec;
+  format.Bits = fmtchk.bits_per_sample;
+  format.Channels = fmtchk.channel;
 
+  // set up sound-buffer
+  csSoundDataRaw* rawSound = new csSoundDataRaw(NULL, data,
+    (fmtchk.bits_per_sample == 16) ? (wavchk.len/2)-1 : wavchk.len-1, format);
 
-  csSoundFormat Format;
-  Format.Freq=wavhdr.samples_per_sec;
-  Format.Bits=wavhdr.bits_per_sample;
-  Format.Channels=wavhdr.channel;
-  sb=new csSoundDataRaw(NULL, data,
-    (wavhdr.bits_per_sample==16)?(wavchk.len/2)-1:wavchk.len-1, Format);
-
-  goto exit_ok;
-exit_read:
-  if(data) delete [] data;
-
-exit_ok:
-  return sb;
+  return rawSound;
 }
