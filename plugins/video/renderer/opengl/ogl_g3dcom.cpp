62d7b6371d702650efb846dccacaa24f8a019526
Philip Wyett <philswyett@gmx.co.uk>
2001-05-25 09:38:01 +0000

    Fixed broken fog problem with ATI Radeon cards.
    Jorrit: Has the before this fix screenshot :-)
    
    Tested on both ATI and nVidia GeForce 2 cards
    and all is Ok. Would like to know if is Ok on other
    cards?
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@6764 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/renderer/opengl/ogl_g3dcom.cpp
+++ plugins/video/renderer/opengl/ogl_g3dcom.cpp
@@ -692,9 +692,11 @@ bool csGraphics3DOGLCommon::NewOpen (const char *Title)
   // handle 1D textures very well
   glGenTextures (1, &m_fogtexturehandle);
   glBindTexture (GL_TEXTURE_2D, m_fogtexturehandle);
+
   glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexImage2D (GL_TEXTURE_2D, 0, 4, CS_FOGTABLE_SIZE, 1, 0,
 		GL_RGBA, GL_UNSIGNED_BYTE, transientfogdata);
 
