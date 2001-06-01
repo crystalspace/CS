f427e937ff907893996f7fc5ee7b13f1bd9b6ea8
Christopher Nelson <paradox@bbhc.org>
2001-06-01 20:57:35 +0000

    These are the new general RE parsing routines.  They support alternates, all wildcards, and have rudimentary support for tables.  Nesting alternates and wildcards is allowed.  Silly syntax is ignored, and some syntactical errors are recovered from.  Forgetting ')', ']', or specifiying an unknown named table are fatal.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@6871 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ plugins/cslexan/csrdparse.cpp
@@ -0,0 +1,144 @@
+#include "csrenode.h"
+
+/// Does a foreward lookup to support wildcards
+bool
+SupportWildCards(char **pRE, csRESyntaxTree &tree, csRENode **node)
+{
+  char *p = *pRE;
+  bool  wild_found=false;
+	
+  // Foreward-lookup the next char for special handling of alternates and wildcards
+  switch(*p)
+  { 
+    case '|':    
+      ++p; // skip the or bar, since we've already processed it.
+      wild_found=true;
+      *node = new csREAltNode(*node, tree.Build(&p));
+    break;
+    
+    case '*':
+      ++p; // eat the star
+      wild_found=true;
+      *node = new csREStarNode(*node);
+    break;
+   
+    case '+':
+      ++p; // eat the plus
+      wild_found=true;
+      *node = new csREPlusNode(*node);
+    break;
+
+    case '?':
+      ++p; // eat the qm
+      wild_found=true;
+      *node = new csREQmNode(*node);
+    break;
+  }
+
+  *pRE = p; 	
+   return wild_found;
+} 
+
+/// Builds a character leaf node, with proper checking for alternate branching.
+csRENode *
+BuildCharLeaf(char **pRE, csRESyntaxTree &tree)
+{
+  char     *p = *pRE;
+  csRENode *node = new csRECharLeaf(*p);
+             
+  // Look ahead one, to see if we need to perform alternating.
+  ++p;
+  
+  // Support scoping wild cards (e.g):  (aabb)++ matches "aabbaabb" and "aabbaabbaabb" but not "aabb"
+  while(SupportWildCards(&p, tree, &node));        
+  
+  *pRE = p;
+  return node;
+}
+
+/// Builds a parenthetical branch (infix order override) with support for wildcards and alternate branches.
+csRENode *
+BuildParenBranch(char **pRE, csRESyntaxTree &tree)
+{
+  char     *p = *pRE;
+  csRENode *node;
+  
+  csRENode *top_node = new csRECatNode(tree.Build(&p), tree.Build(&p));
+  
+  // Continue to process until we have finished this set of parentheses.
+  while(*p && *p!=')')
+  { 
+    node = new csRECatNode(top_node, tree.Build(&p));
+    top_node = node;     	
+  }
+  
+  // Support scoping wild cards 
+  while(SupportWildCards(&p, tree, &node));        
+   
+  *pRE = p;
+  return node;
+};
+
+/// Recursive descent parsing of regular expressions
+csRENode *
+csRESyntaxTree::Build(char **regexp)
+{
+  char *p = *regexp;
+  csRENode  *node=0;
+ 
+  switch(*p)
+  {
+     case '[':
+     { // Begin table creation.  
+       
+       
+     	
+     	
+     } // End table creation
+     break;	
+  	
+     case '(':
+      { // Begin parenthetical recursion
+        
+        // Special case: is there only one char inside the parentheses?
+        if (*(p+2) == ')')
+        {  // yes, treat it like it wasn't in parentheses.
+         node = BuildCharLeaf(&p, *this);
+         p+=3; // Set pointer to next character to be consumed.
+        }
+        else
+        { // no, begin recursion
+         node = BuildParenBranch(&p, *this); 
+        }
+        
+      } // End parenthetical recursion 
+      break;     	
+    	
+      default:
+        node = BuildCharLeaf(&p, *this); 
+      break;  	
+    }  // end switch (*p)	
+   
+ // Save the location of our pointer.
+ *regexp = p;
+ 
+ // Return the top node
+ return node;
+}
+
+
+csReSyntaxTree::Compile(char *regexp)
+{
+ char *pRE = regexp;
+ 
+ // Create the initial top node.
+ csRENode *top_node= new csRECatNode(Build(&pRE), Build(&pRE));
+ 
+ // Recursively create nodes, with the new node always being on top.  This allows us to create proper LALR syntax tree.
+ while(*pRE)
+ {
+   csRENode *node = new csRECatNode(top_node, Build(&pRE));	
+   top_node = node;	
+ }
+ 	
+}
---
+++ plugins/cslexan/csrenode.h
@@ -0,0 +1,247 @@
+#ifndef __CSRENODE_H__
+#define __CSRENODE_H__
+
+/*****************************************************************************
+    Copyright (C) 2001 by Christopher Nelson
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+****************************************************************************/
+
+/****** Change log
+  1. Fri Jun 01 09:59:08 AM MDT 2001 paradox <paradox@bbhc.org> Creation of file
+******/
+
+/**********
+   In order to derive the proper parsing states, we must create a syntax-directed parse tree of the regular expression.  Once we
+ finish this tree, we must perform a depth-first traversal of it in order find out our state transition information.  The tree
+ has a few types of nodes:
+ 
+ 	1. cat-node: a cat node is a concantenation node, meaning that this value, if matched, should be concantenated onto our 
+ current string.  This is the most common type of node.  Cat nodes are binary.
+     2. star-node: a star node is a many or none match, equivalent to "*"
+     3. plus-node: a plus node is a one or more match, equivalent to "+"
+     4. alt-node:  matches an alternate.  alternates are binary. equivalent to "|"
+     5. qm-node:   matches one or none.  equivalent to "?"
+     
+   Leaves are always the individual characters to match, unless they are special table leaves.  A table leaf supports "[]", and
+ therefore may match any of the characters in the table.  However, it still only matches ONE character from the table.  
+
+   These classes are for the INTERNAL use of the engine ONLY.  They are NOT intended to be exported by any module.
+
+ */
+ 
+ const unsigned char CSRE_CATNODE 	= 1;
+ const unsigned char CSRE_STARNODE	= 2;
+ const unsigned char CSRE_PLUSNODE	= 3;
+ const unsigned char CSRE_ALTNODE	 = 4;
+ const unsigned char CSRE_QMNODE	  = 5;
+ 
+ const unsigned char CSRE_CHLEAF	  = 6;
+ const unsigned char CSRE_TBLEAF	  = 7;
+ 
+ /// The base class for nodes.  All nodes have a type. 
+ class csRENode
+ {
+ public:
+    /// Returns the type of node this is.
+    virtual unsigned char Type()=0;
+ 
+    /// Gets the left branch, or if this is a unary node, gets the only branch.
+    virtual csRENode *Left()=0;
+    
+    /// Gets the right branch.  Returns NULL if there's none.
+    virtual csReNode *Right()=0;
+    
+    /// Returns an acceptance match for this leaf (if node is a leaf)
+    virtual bool Accept(int ch)
+    { return false; }    
+};
+ 
+ /// The cat-node class.  Contains a pointer to the next level down.  Performs a cat operation.
+ class csRECatNode : public csRENode
+ {
+   csRENode *left, *right;
+ 
+ public:
+   csRECatNode(csRENode *l, csRENode *r):left(l), right(r) {};
+   virtual ~csRECatNode() {};
+   
+ public:
+   virtual unsigned char Type()
+   { return CSRE_CATNODE; }
+ 
+   virtual csRENode *Left()
+   { return left; }
+   
+   virtual csRENode *Right()
+   { return right;  } 
+ };
+ 
+ /// The star-node class.  Contains a pointer to the next level down.  Performs a none or many operation.
+ class csREStarNode : public csRENode
+ {
+   csRENode *child;
+   
+ public:
+   csREStarNode(csRENode *c):child(c) {};
+   virtual ~csREStarNode() {};
+   
+ public:
+   virtual unsigned char Type()
+   { return CSRE_STARNODE; }
+ 
+   virtual csRENode *Left()
+   { return child; }
+   
+   virtual csRENode *Right()
+   { return NULL;  } 
+ };
+ 
+ /// The plus-node class.  Contains a pointer to the next level down.  Performs a one or many operation.
+ class csREPlusNode : public csRENode
+ {
+   csRENode *child;
+    
+ public:
+   csREPlusNode(csRENode *c):child(c) {};
+   virtual ~csREPlusNode() {};
+   
+ public:
+   virtual unsigned char Type()
+   { return CSRE_PLUSNODE; }
+ 
+   virtual csRENode *Left()
+   { return child; }
+   
+   virtual csRENode *Right()
+   { return NULL;  } 
+ };
+ 
+ /// The qm-node class.  Contains a pointer to the next level down.  Performs a cat operation.
+ class csREQmNode : public csRENode
+ {
+   csRENode *child;
+   
+ public:
+   csREQmNode(csRENode *c):child(c) {};
+   virtual ~csREQmNode() {};
+   
+ public:
+   virtual unsigned char Type()
+   { return CSRE_QMNODE; }
+ 
+   virtual csRENode *Left()
+   { return child; }
+   
+   virtual csRENode *Right()
+   { return NULL;  } 
+ };
+ 
+ /// The alt-node class.  Contains a pointer to the next level down.  Performs an Or operation.
+ class csREAltNode : public csRENode
+ {
+   csRENode *left, *right;
+   
+ public:
+   csREAltNode(csRENode *l, csReNode *r):left(l), right(r) {};
+   virtual ~csREAltNode() {};
+   
+ public:
+   virtual unsigned char Type()
+   { return CSRE_ALTNODE; }
+ 
+   virtual csRENode *Left()
+   { return left; }
+   
+   virtual csRENode *Right()
+   { return right;  } 
+ };
+ 
+ 
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ //                  								Leaves
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ 
+ /// The char leaf class.  Contains a character to match.
+ class csRECharLeaf : public csRENode
+ {
+  int c;
+   
+ public:
+   csRECharLeaf(int ch):c(ch) {};
+   virtual ~csRECharLeaf() {};
+   
+ public:
+   virtual unsigned char Type()
+   { return CSRE_CHARLEAF; }
+ 
+   virtual csRENode *Left()
+   { return NULL; }
+   
+   virtual csRENode *Right()
+   { return NULL;  }
+   
+   /// Returns a true if this char matches the stored char.
+   virtual bool Accept(int ch)
+   { return c==ch; } 
+ };
+ 
+ /// The table leaf class.  Contains a set of characters to match.
+ class csRETableLeaf : public csRENode
+ {
+  /// The set of characters that can be matched by this node.
+  char *set;
+  
+  /// If true, then Accept returns true if the char is NOT in this table, otherwise returns true if char IS in the table.
+  bool invert_match;
+   
+ public:
+   csRETableLeaf(char *s, bool invert=false):set(s), invert_match(invert) {};
+   virtual ~csRETableLeaf() { if (set) delete set; };
+   
+ public:
+   virtual unsigned char Type()
+   { return CSRE_TABLELEAF; }
+ 
+   virtual csRENode *Left()
+   { return NULL; }
+   
+   virtual csRENode *Right()
+   { return NULL;  }
+   
+   /// Returns a true if this char matches the stored char.
+   virtual bool Accept(int ch)
+   { 
+     bool matched=false;
+     
+     char mc = (char)ch;
+     char *p = set;
+     
+     // see if the char is in this set.
+     while(*p)
+     {
+       if (*p == mc) 
+       {
+         matched=true;
+         break;
+       }
+     }
+     
+     return (invert_match ? !matched : matched);
+   
+   } 
+ };
+
+#endif 
