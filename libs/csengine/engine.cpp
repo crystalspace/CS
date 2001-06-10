aeea3dbfd351a4a99cbdcc0514f3030b12a96eb6
Norman Krämer <norman@users.sourceforge.net>
2001-06-10 21:38:35 +0000

    Clear now releases all collected objects via ObjRemoveAll (this will assure for instance soundwrappers are decref'd correctly)
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@7019 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csengine/engine.cpp
+++ libs/csengine/engine.cpp
@@ -15,7 +15,6 @@
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
-
 #include "cssysdef.h"
 #include "qint.h"
 #include "csutil/scf.h"
@@ -668,6 +667,9 @@ void csEngine::Clear ()
 
   // Clear all render priorities.
   ClearRenderPriorities ();
+  
+  // remove objects
+  QueryObject ()->ObjRemoveAll ();
 }
 
 iObject *csEngine::QueryObject ()
