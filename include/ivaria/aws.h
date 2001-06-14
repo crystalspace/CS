b45244b2d5e74c9346ab98daf5a11b582aa69984
Christopher Nelson <paradox@bbhc.org>
2001-06-14 19:15:00 +0000

    Updated interface definitions
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@7112 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/ivaria/aws.h
+++ include/ivaria/aws.h
@@ -2,13 +2,23 @@
 #define __IVARIA_AWS_H__
 
 #include "csutil/scf.h"
+#include "csgeom/csrect.h"
+#include "iutil/string.h"
+
+struct iAws;
+struct iAwsPrefs;
+            
 
 SCF_VERSION (iAws, 0, 0, 1);
 
 struct iAws : public iBase
 {
 public:  
-  virtual void Load(const char *def_file)=0;
+  /// Get a pointer to the preference manager
+  virtual iAwsPrefs *GetPrefMgr()=0;
+
+  /// Set the preference manager used by the window system
+  virtual void       SetPrefMgr(iAwsPrefs *pmgr)=0;
 };
 
 
@@ -16,8 +26,33 @@ SCF_VERSION (iAwsPrefs, 0, 0, 1);
 
 struct iAwsPrefs : public iBase
 {
-public:  
+public:
+  /// Invokes the definition parser to load definition files
   virtual void Load(const char *def_file)=0;
+
+  /// Maps a name to an id
+  virtual unsigned long NameToId(char *name)=0;
+    
+  /// Select which skin is the default for components, the skin must be loaded.  True on success, false otherwise.
+  virtual bool SelectDefaultSkin(char *skin_name)=0;
+
+  /// Lookup the value of an int key by name (from the skin def)
+  virtual bool LookupIntKey(char *name, int &val)=0; 
+
+  /// Lookup the value of an int key by id (from the skin def)
+  virtual bool LookupIntKey(unsigned long id, int &val)=0; 
+
+  /// Lookup the value of a string key by name (from the skin def)
+  virtual bool LookupStringKey(char *name, iString *&val)=0; 
+
+  /// Lookup the value of a string key by id (from the skin def)
+  virtual bool LookupStringKey(unsigned long id, iString *&val)=0; 
+
+  /// Lookup the value of a rect key by name (from the skin def)
+  virtual bool LookupRectKey(char *name, csRect &rect)=0; 
+
+  /// Lookup the value of a rect key by id (from the skin def)
+  virtual bool LookupRectKey(unsigned long id, csRect &rect)=0; 
 };
 
 #endif // __IVARIA_AWS_H__
