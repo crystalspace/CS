0e0fb1a3f03a06888f11f35eaa4c6bce3f016a43
Samuel Humphreys <samuelh@ntlworld.com>
2001-08-07 17:11:07 +0000

    Adding interfaces
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@7749 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/ivideo/xextf86vm.h
@@ -0,0 +1,50 @@
+/*
+    Copyright (C) 1998 by Jorrit Tyberghein 
+    Copyright (C) 2001 by Samuel Humphreys
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __IVIDEO_XEXTF86VM_H__
+#define __IVIDEO_XEXTF86VM_H__
+
+#define XK_MISCELLANY 1
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <X11/Xutil.h>
+
+
+SCF_VERSION (iXExtF86VM, 1, 0, 0);
+
+struct iXExtF86VM : public iBase
+{
+  /// Open Video Mode plugin
+  virtual void Open (Display *dpy, int screen_num, 
+		     XVisualInfo *xvis, Colormap cmap) = 0;
+  /// Finish
+  virtual void Close () = 0;
+  /// Set whether in full screen mode
+  virtual void SetFullScreen (Window ctx_win, Window wm_win, bool yesno) = 0;
+  /// Query full screen status
+  virtual bool IsFullScreen () = 0;
+  /// While in full screen jump to next higher resolution mode
+  virtual void IncVideoMode () = 0;
+  /// While in full screen jump to next lower resolution mode
+  virtual void DecVideoMode () = 0;
+  /// Get Current Dimensions
+  virtual void GetDimensions (int &w, int &h) = 0;
+};
+
+#endif // __IVIDEO_XEXTF86VM_H__
---
+++ include/ivideo/xwindow.h
@@ -0,0 +1,64 @@
+/*
+    Copyright (C) 1998 by Jorrit Tyberghein 
+    Copyright (C) 2001 by Samuel Humphreys
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __IVIDEO_XWINDOW_H__
+#define __IVIDEO_XWINDOW_H__
+
+#define XK_MISCELLANY 1
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <X11/Xutil.h>
+
+#include "ivideo/cursor.h"
+
+struct iGraphics2D;
+
+SCF_VERSION (iXWindow, 1, 0, 0);
+
+struct iXWindow : public iBase
+{
+  // These should be inherited from csNativeWindow
+  virtual bool Open () = 0;
+  virtual void Close () = 0;
+
+  virtual void AllowResize (bool iAllow) = 0;
+
+  virtual bool GetFullScreen () = 0;
+  virtual void SetFullScreen (bool yesno) = 0;
+
+  virtual void SetTitle (const char* title) = 0;
+  virtual void SetCanvas (iGraphics2D *canvas) = 0;
+
+
+  // These are X specific
+  virtual Display *GetDisplay () = 0;
+  virtual int GetScreen () = 0;
+  virtual Window GetWindow () = 0;
+  virtual GC GetGC () = 0;
+
+  virtual void SetVisualInfo (XVisualInfo *xvis) = 0;
+  virtual void SetColormap (Colormap cmap) = 0;
+
+
+  // Should be in the window manager...
+  virtual bool SetMousePosition (int x, int y) = 0;
+  virtual bool SetMouseCursor (csMouseCursorID iShape) = 0;
+};
+
+#endif // __IVIDEO_XWINDOW_H__
