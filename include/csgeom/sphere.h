94faa2905b0fb4f9deafbe830f8fb3d9859a7aa9
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2001-08-29 13:49:29 +0000

    Added new csSphere class to csgeom. This class is not yet used but it will be extended to handle operations on bounding spheres. At this moment it has a function to calculate the union of two bounding spheres. This is untested however.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@7953 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/csgeom/sphere.h
@@ -0,0 +1,77 @@
+/*
+    Copyright (C) 2001 by Jorrit Tyberghein
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_SPHERE_H__
+#define __CS_SPHERE_H__
+
+#include "cstypes.h"
+#include "vector3.h"
+
+/**
+ * This class represents a sphere.
+ */
+class csSphere
+{
+private:
+  csVector3 center;
+  float radius;
+
+public:
+  /// Create a new empty sphere at (0,0,0).
+  csSphere ()
+  {
+    center.Set (0, 0, 0);
+    radius = 0;
+  }
+
+  /// Create a new sphere.
+  csSphere (const csVector3& center, float radius)
+  {
+    csSphere::center = center;
+    csSphere::radius = radius;
+  }
+
+  /// Copy Constructor.
+  csSphere (const csSphere& s) { center = s.center; radius = s.radius; }
+
+  /// Get the center of this sphere.
+  csVector3& GetCenter () { return center; }
+  /// Get the center of this sphere.
+  const csVector3& GetCenter () const { return center; }
+  /// Set the center of this sphere.
+  void SetCenter (const csVector3& c) { center = c; }
+  /// Get the radius of this sphere.
+  float GetRadius () const { return radius; }
+  /// Set the radius of this sphere.
+  void SetRadius (float r) { radius = r; }
+
+  /// Calculate the union of this sphere and another.
+  void Union (const csVector3& ocenter, float oradius);
+
+  /// Calculate the union of two spheres.
+  csSphere friend operator+ (const csSphere& s1, const csSphere& s2);
+  /// Calculate the union of this sphere and another one.
+  csSphere& operator+= (const csSphere& s)
+  {
+    Union (s.center, s.radius);
+    return *this;
+  }
+};
+
+#endif // __CS_SPHERE_H__
+
---
+++ libs/csgeom/sphere.cpp
@@ -0,0 +1,63 @@
+/*
+    Copyright (C) 2001 by Jorrit Tyberghein
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include <math.h>
+#include <float.h>
+#include "cssysdef.h"
+#include "qint.h"
+#include "qsqrt.h"
+#include "csgeom/sphere.h"
+#include "csgeom/math3d.h"
+
+//---------------------------------------------------------------------------
+
+csSphere operator+ (const csSphere& s1, const csSphere& s2)
+{
+  csSphere s = s1;
+  s += s2;
+  return s;
+}
+
+void csSphere::Union (const csVector3& ocenter, float oradius)
+{
+  // First calculate distance between centers of two spheres.
+  float distance = qsqrt (csSquaredDist::PointPoint (center, ocenter));
+
+  if (radius >= oradius+distance) // Sphere 2 is inside sphere 1
+  {
+  }
+  else if (oradius >= radius+distance) // Sphere 1 is inside sphere 2
+  {
+    center = ocenter;
+    radius = oradius;
+  }
+  else if (ABS (distance) < SMALL_EPSILON)
+  {
+    // Spheres very close to each other. Because the two tests above
+    // succeeded the spheres are actually equal. So nothing has to be done.
+  }
+  else
+  {
+    csVector3 direction = (center-ocenter) / distance;
+    center = (center + direction*radius + ocenter + direction*oradius) * .5;
+    radius = (radius + oradius + distance) * .5;
+  }
+}
+
+//---------------------------------------------------------------------------
+
