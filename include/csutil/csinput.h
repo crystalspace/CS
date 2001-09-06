799976c75ce8cb0935d9205f010ea41d291e4927
Norman Krämer <norman@users.sourceforge.net>
2001-09-06 11:41:22 +0000

    moved the IBASE from the base class to the subclasses and made it an embedded ibase. The previous let one register an inputdriver as an eventhandler but the refcount was never incremented (because it was an ibase and not embedded ibase). thus it led to a deleted inputdriver while the eventqueue still had the listeners registered (which caused the crash at exit since the eventqueue decrefs the listeners at destruction time). erm, did that explain anything ?
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@8002 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/csinput.h
+++ include/csutil/csinput.h
@@ -35,6 +35,7 @@
 #include "iutil/comp.h"
 struct iEvent;
 struct iEventQueue;
+struct iEventHandler;
 struct iObjectRegistry;
 
 /**
@@ -49,19 +50,14 @@ protected:
   iEventQueue* GetEventQueue();
   virtual void LostFocus() = 0;
   virtual void Post(iEvent*);
-private:
-  struct FocusListener : public iEventHandler
-  {
-    csInputDriver* Parent;
-    SCF_DECLARE_IBASE;
-    FocusListener() { SCF_CONSTRUCT_IBASE(0); }
-    virtual bool HandleEvent(iEvent&);
-  };
-  friend struct FocusListener;
-  FocusListener Listener;
-  iEventQueue* Queue;
+  virtual bool HandleEvent (iEvent &event);
+
+  iEventHandler *Listener;
   void StartListening();
   void StopListening();
+
+private:
+  iEventQueue* Queue;
 };
 
 /**
@@ -103,6 +99,13 @@ public:
 private:
   /// Application lost focus.
   virtual void LostFocus() { Reset(); }
+
+  struct eiEventHandler : public iEventHandler
+  {
+    SCF_DECLARE_EMBEDDED_IBASE (csKeyboardDriver);
+    virtual bool HandleEvent(iEvent &ev) { return scfParent->HandleEvent (ev); }
+  } scfiEventHandler;
+  friend struct eiEventHandler;
 };
 
 /**
@@ -165,6 +168,13 @@ public:
 private:
   /// Application lost focus.
   virtual void LostFocus() { Reset(); }
+
+  struct eiEventHandler : public iEventHandler
+  {
+    SCF_DECLARE_EMBEDDED_IBASE (csMouseDriver);
+    virtual bool HandleEvent(iEvent &ev) { return scfParent->HandleEvent (ev); }
+  } scfiEventHandler;
+  friend struct eiEventHandler;
 };
 
 /**
@@ -214,6 +224,13 @@ public:
 private:
   /// Application lost focus.
   virtual void LostFocus() { Reset(); }
+
+  struct eiEventHandler : public iEventHandler
+  {
+    SCF_DECLARE_EMBEDDED_IBASE (csJoystickDriver);
+    virtual bool HandleEvent(iEvent &ev) { return scfParent->HandleEvent (ev); }
+  } scfiEventHandler;
+  friend struct eiEventHandler;
 };
 
 #endif // __CS_CSINPUT_H__
