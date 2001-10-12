285f68c56299f93737a017871988a526c1e158f0
Norman Krämer <norman@users.sourceforge.net>
2001-10-12 23:09:21 +0000

    iOpenGLInterface
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@8390 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ plugins/video/canvas/openglcommon/iogl.h
@@ -0,0 +1,41 @@
+#ifndef _CS_OPENGLINTERFACE_H_
+#define _CS_OPENGLINTERFACE_H_
+
+/*
+    Copyright (C) 2001 by Norman Krämer
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "csutil/scf.h"
+
+/**
+ * A common interface to be implemented by the platform specific opengl canvases.
+ * Currently covers only aspects that are different for opengl on different platforms
+ * like the "GetProcAddress"
+ */
+
+SCF_VERSION (iOpenGLInterface, 0, 0, 1);
+
+struct iOpenGLInterface : public iBase
+{
+  /**
+   * Retrieve the address of the function <funcname>.
+   * Return NULL if not available.
+   */
+  virtual void *GetProcAddress (const char *funcname) = 0;
+};
+
+#endif
