232c5d7e781644d55ca250d1b334c40d2d608bdc
Christopher Nelson <paradox@bbhc.org>
2001-10-15 18:59:12 +0000

    Christopher:
    	+ Added the snprintf.h header file and modified it a bit to keep it from thinking that it wanted to specify certain things depending on certain defines.
    
    	+ Renamed all the xxsnprintfxx functions to cs_xxsnprintfxx
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@8438 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/csutil/snprintf.h
@@ -0,0 +1,17 @@
+#ifndef _PORTABLE_SNPRINTF_H_
+#define _PORTABLE_SNPRINTF_H_
+
+#define PORTABLE_SNPRINTF_VERSION_MAJOR 2
+#define PORTABLE_SNPRINTF_VERSION_MINOR 2
+
+//  This file was modified slighty by Christopher Nelson to change the name of these functions for
+// Crystal Space.  questions: mailto://paradox@bbhc.org
+
+extern int cs_snprintf(char *, size_t, const char *, /*args*/ ...);
+extern int cs_vsnprintf(char *, size_t, const char *, va_list);
+extern int cs_asprintf  (char **ptr, const char *fmt, /*args*/ ...);
+extern int cs_vasprintf (char **ptr, const char *fmt, va_list ap);
+extern int cs_asnprintf (char **ptr, size_t str_m, const char *fmt, /*args*/ ...);
+extern int cs_vasnprintf(char **ptr, size_t str_m, const char *fmt, va_list ap);
+
+#endif
