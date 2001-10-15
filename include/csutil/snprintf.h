6ec995957406392560c19795117f3fbef50d50ec
Christopher Nelson <paradox@bbhc.org>
2001-10-15 19:07:39 +0000

    Commented out vasprintf and vasnprintf
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@8441 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/snprintf.h
+++ include/csutil/snprintf.h
@@ -10,8 +10,8 @@
 extern int cs_snprintf(char *, size_t, const char *, /*args*/ ...);
 extern int cs_vsnprintf(char *, size_t, const char *, va_list);
 extern int cs_asprintf  (char **ptr, const char *fmt, /*args*/ ...);
-extern int cs_vasprintf (char **ptr, const char *fmt, va_list ap);
+//extern int cs_vasprintf (char **ptr, const char *fmt, va_list ap);
 extern int cs_asnprintf (char **ptr, size_t str_m, const char *fmt, /*args*/ ...);
-extern int cs_vasnprintf(char **ptr, size_t str_m, const char *fmt, va_list ap);
+//extern int cs_vasnprintf(char **ptr, size_t str_m, const char *fmt, va_list ap);
 
 #endif
