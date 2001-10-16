59daa0ebd437a5e8b4791a84a46ecc283c51f41e
Eric Sunshine <sunshine@sunshineco.com>
2001-10-16 15:57:48 +0000

    snprintf.h now includes stdarg.h to ensure that the va_list type is declared.  Also now only applies `extern "C"' if __cplusplus is defined.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@8471 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/snprintf.h
+++ include/csutil/snprintf.h
@@ -1,11 +1,17 @@
 #ifndef _CRYSTAL_SNPRINTF_H_
 #define _CRYSTAL_SNPRINTF_H_
 
+#include <stdarg.h>
+
+#if defined(__cplusplus)
 extern "C" {
+#endif
 
 extern int cs_snprintf(char *, size_t, const char *, /*args*/ ...);
 extern int cs_vsnprintf(char *, size_t, const char *, va_list);
 
+#if defined(__cplusplus)
 }
+#endif
 
 #endif
