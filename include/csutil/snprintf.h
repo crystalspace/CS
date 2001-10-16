623f179bea6a57580e5ae8581af5b7335f320ccc
Christopher Nelson <paradox@bbhc.org>
2001-10-16 14:52:37 +0000

    Christopher:
    	+ Added a new version of snprintf that supports floating point from the Apache project.
    
    	+ Added a macroized header cersion of ctype that is required by the apache snprintf implementation.
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@8466 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/snprintf.h
+++ include/csutil/snprintf.h
@@ -1,19 +1,11 @@
-#ifndef _PORTABLE_SNPRINTF_H_
-#define _PORTABLE_SNPRINTF_H_
+#ifndef _CRYSTAL_SNPRINTF_H_
+#define _CRYSTAL_SNPRINTF_H_
 
-#include <stdarg.h>
+extern "C" {
 
-#define PORTABLE_SNPRINTF_VERSION_MAJOR 2
-#define PORTABLE_SNPRINTF_VERSION_MINOR 2
+extern int cs_snprintf(char *, size_t, const char *, /*args*/ ...);
+extern int cs_vsnprintf(char *, size_t, const char *, va_list);
 
-//  This file was modified slighty by Christopher Nelson to change the name of
-// these functions for Crystal Space.  Questions? mailto:paradox@bbhc.org
-
-extern "C" int cs_snprintf(char *, size_t, const char *, /*args*/ ...);
-extern "C" int cs_vsnprintf(char *, size_t, const char *, va_list);
-extern "C" int cs_asprintf  (char **ptr, const char *fmt, /*args*/ ...);
-//extern "C" int cs_vasprintf (char **ptr, const char *fmt, va_list ap);
-extern "C" int cs_asnprintf (char **ptr, size_t str_m, const char *fmt, /*args*/ ...);
-//extern "C" int cs_vasnprintf(char **ptr, size_t str_m, const char *fmt, va_list ap);
+}
 
 #endif
