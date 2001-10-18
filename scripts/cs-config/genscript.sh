835eabc7c6c4be83d2b68c026fd99c76f8d84ebc
Eric Sunshine <sunshine@sunshineco.com>
2001-10-18 17:42:02 +0000

    Eric Sunshine made the following changes to the cs-config generation facility:
    -*- Now works with older /bin/test programs which do not understand
        the -e test.
    
    -*- The package version information is now gleaned automatically from
        CS/include/csver.h rather than being hardcoded into the
        cs-config/genscript.sh script.
    
    Unfortunately, cs-config still has a number of shortcomings on some
    platforms, and will require some more work before it can be useful on
    those platforms.
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@8521 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- scripts/cs-config/genscript.sh
+++ scripts/cs-config/genscript.sh
@@ -1,15 +1,20 @@
 #! /bin/sh
 # This generates the cs-config script
 
+verfile=include/csver.h
+vmajor=`sed -e '/#define[ 	][ 	]*CS_VERSION_MAJOR/!d' -e '/#define[ 	][ 	]*CS_VERSION_MAJOR/s/\(#define[ 	][ 	]*CS_VERSION_MAJOR[ 	][ 	]*"\)\(.*\)"\(.*\)/\2/' < ${verfile}`
+vminor=`sed -e '/#define[ 	][ 	]*CS_VERSION_MINOR/!d' -e '/#define[ 	][ 	]*CS_VERSION_MINOR/s/\(#define[ 	][ 	]*CS_VERSION_MINOR[ 	][ 	]*"\)\(.*\)"\(.*\)/\2/' < ${verfile}`
+rdate=`sed -e '/#define[ 	][ 	]*CS_RELEASE_DATE/!d' -e '/#define[ 	][ 	]*CS_RELEASE_DATE/s/\(#define[ 	][ 	]*CS_RELEASE_DATE[ 	][ 	]*"\)\(.*\)"\(.*\)/\2/' < ${verfile}`
+
 if test -x cs-config; then
 	rm cs-config
 fi
 cat $5/cs-config.temppre	> cs-config
-echo "#	WARNING: This script is automatic generated! " >> cs-config
+echo "#	WARNING: This script is generated automatically! " >> cs-config
 echo				>> cs-config
 echo "prefix=\${CRYSTAL-$1}"	>> cs-config
 echo "exec_prefix=\${prefix}"	>> cs-config
-echo "version=0.19"		>> cs-config
+echo "version='${vmajor}.${vminor} (${rdate})'"		>> cs-config
 echo "includedir=\${prefix}/include"	>> cs-config
 echo "libdir=\${prefix}/lib"	>> cs-config
 echo "syslibs=\"$4\""		>> cs-config
