6d8af610eabf672bb6d6c2c2690b54ccf039ed72
Jorge Acereda <al004046@anubis.uji.es>
2001-11-03 18:24:52 +0000

    Corrected an unaccuracy in csGetTicks() and changed it to use sysconf() correctly.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@8752 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/cssys/unix/utiming.cpp
+++ libs/cssys/unix/utiming.cpp
@@ -16,28 +16,25 @@
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#include <time.h>
+#include <unistd.h>
 #include <sys/times.h>
 #include "cssysdef.h"
 #include "cssys/sysfunc.h"
 
-#if !defined(COMP_GCC3)
-#define cs_clk_tck() (1000 / CLK_TCK)
-#else
-extern "C" long int __sysconf(int);
-inline long int cs_clk_tck()
-{
-  static long int cache = 0;
-  if (cache == 0)
-    cache = 1000 / ((__clock_t)__sysconf(2));
-  return cache;
-}
-#endif
 
 // This function should return milliseconds since some specific time
 csTicks csGetTicks ()
 {
+  // We shouldn't cache 1000/CLK_TCK, because we loose accuracy
+  // on platforms where CLK_TCK is 60. (16 != 16.66666...)
+#ifdef _SC_CLK_TCK
+  static clock_t clktck = 0;
+  if (clktck == 0)
+    clktck = sysconf(_SC_CLK_TCK);  
+#else
+#define clktck CLK_TCK
+#endif
   // NOTE: times() can return -1 on errors.  Yikes!
   struct tms buf;
-  return times (&buf) * cs_clk_tck();
+  return (1000 * times (&buf)) / clktck;
 }
