0f39da784c24965c06a84168488a863a218b0a3a
Christopher Nelson <paradox@bbhc.org>
2001-11-05 15:34:46 +0000

    Christopher:
    	+ Removed aws directory
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@8774 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/aws/iawsdefs.h
+++
@@ -1,9 +0,0 @@
-#ifndef __AWS_DEFINITIONS__
-#define __AWS_DEFINITIONS__
-
-const unsigned int AWSF_CMP_HIDDEN =      8;  // set if component is hidden
-const unsigned int AWSF_CMP_TRANSPARENT = 2;  // set if component is transparent
-const unsigned int AWSF_CMP_ALWAYSERASE = 4;  // set if component always needs erased by parent first
-
-
-#endif
