0e6752c805e28c3f08c56519d4d1ac14a648fb09
Matze Braun <matze@braunis.de>
2001-11-07 08:46:05 +0000

    fixed config scripts to remove there temp files on cygwin too
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@8817 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- bin/chktools.sh
+++ bin/chktools.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 # Checks for an installed nasm
 
-# Create a dummy NASM program
-echo "%xdefine TEST" >conftest.asm
-
 # Check if NASM is installed and if it has the right version
 NASMBIN=`which nasm 2>&1 | grep -v "[Nn]o"`
 
 if [ -n "${NASMBIN}" ]; then
+  #create a dummy NASM program
+  echo "%xdefine TEST" >conftest.asm
   # Well, we really should check here for obj format...
   # but we'll use ELF as it really doesn't matter
   ${NASMBIN} -f elf conftest.asm -o conftest.o 2>/dev/null && echo "NASM.INSTALLED = yes"
--- bin/endtest.sh
+++ bin/endtest.sh
@@ -8,15 +8,15 @@
 # information needed for building Crystal Space.  It is pipied to stdout,
 # and errors are piped to stderr.
 
-echo "int main() { long x = 0x12; return *(unsigned char *)&x == 0x12; }" > endtest.cpp
+echo "int main() { long x = 0x12; return *(unsigned char *)&x == 0x12; }" > comptest.cpp
 
-${CXX} -o endtest endtest.cpp 2>/dev/null || echo "endtest.sh: cannot compile testcase" >&2
-if test -f ./endtest; then
-    if ./endtest; then
+${CXX} -o comptest endtest.cpp 2>/dev/null || echo "endtest.sh: cannot compile testcase" >&2
+if test -f ./comptest; then
+    if ./comptest; then
 	echo "CS_BIG_ENDIAN = 1"
     else
 	echo "CS_LITTLE_ENDIAN = 1"
     fi
 fi
 
-rm -f endtest.cpp endtest
+rm -f comptest.cpp comptest comptest.exe
