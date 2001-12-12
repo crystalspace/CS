32ea0b574752905b683f202219e1b6c1f6bdc067
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2001-12-12 05:44:01 +0000

    Bumped up version for pending 0.92 release.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9012 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csver.h
+++ include/csver.h
@@ -19,9 +19,9 @@
 #ifndef __CS_CSVER_H__
 #define __CS_CSVER_H__
 
-#define CS_VERSION_MAJOR "0.91"	/* NOTE: Update docs/texinfo/version.txi  */
-#define CS_VERSION_MINOR "dev"	/* whenever updating these two values.    */
-#define CS_RELEASE_DATE "Wed, 24-Oct-2001"
+#define CS_VERSION_MAJOR "0.92"	/* NOTE: Update docs/texinfo/version.txi  */
+#define CS_VERSION_MINOR "001"	/* whenever updating these two values.    */
+#define CS_RELEASE_DATE "Wed, 12-Dec-2001"
 
 #if !defined(CS_PLATFORM_NAME)
 #  define CS_PLATFORM_NAME "MysteryPlatform"
