826ae93fa1dee895d3509eb1d62b68fa621fddde
Christopher Nelson <paradox@bbhc.org>
2001-12-24 18:39:03 +0000

    Added scripting helpers.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9102 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ plugins/aws/awsscr.cpp
@@ -0,0 +1,34 @@
+#include "cssysdef.h"
+#include "iaws/aws.h"
+#include "awsscr.h"
+#include "awsfparm.h"
+#include "awsadler.h"
+
+#include <string.h>
+
+void
+awsActionDispatcher::Register(char *name, void (Action)(void *owner, iAwsParmList &parmlist))
+{
+  awsActionMap *map = new awsActionMap();
+
+  map->name=aws_adler32(aws_adler32(0, NULL, 0), (unsigned char *)name, strlen(name));
+  map->Action = Action;
+
+  actions.Push(map);
+}
+
+void
+awsActionDispatcher::Execute(char *action, void *owner, iAwsParmList &parmlist)
+{
+  unsigned long name = aws_adler32(aws_adler32(0, NULL, 0), (unsigned char *)action, strlen(action));
+
+  int i;
+  for(i=0; i<actions.Length(); ++i)
+  {
+    awsActionMap *map = (awsActionMap *)actions[i];
+
+    if (name==map->name)
+      map->Action(owner,parmlist);
+  }
+}
+
---
+++ plugins/aws/awsscr.h
@@ -0,0 +1,28 @@
+#ifndef __AWS_SCRIPT_UTILITY__
+#define __AWS_SCRIPT_UTILITY__
+
+#include "csutil/csvector.h"
+
+struct awsActionMap
+{
+  /// The name of the action reduced to a long
+  unsigned long name;
+
+  /// The action to execute
+  void (*Action)(void *owner, iAwsParmList &parmlist);
+};
+
+class awsActionDispatcher
+{
+  /// List of actions to execute
+  csBasicVector actions;
+
+public:
+  /// Register an action.
+  void Register(char *name, void (Action)(void *owner, iAwsParmList &parmlist));
+
+  /// Execute the corresponding action
+  void Execute(char *action, void *owner, iAwsParmList &parmlist);
+};
+  
+#endif
