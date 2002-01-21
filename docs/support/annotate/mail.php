b8311d466e716cd88f432d761370709aa0426b25
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2002-01-21 12:06:38 +0000

    Added email.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9288 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- docs/support/annotate/mail.php
+++ docs/support/annotate/mail.php
@@ -2,5 +2,6 @@
  // Users to which notification will be sent when annotation is made.
  $mail[]="matze@braunis.de";
  $mail[]="jorrit.tyberghein@pandora.be";
+ $mail[]="jorrit.tyberghein@uz.kuleuven.ac.be";
  $mail[]="sunshine@sunshineco.com";
 ?>
