ff1d363b85d5b345b462caddad67cc98852ff919
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2002-01-22 09:25:25 +0000

    Created a crystal-docs mailing list. The doc annotation script will now send mail to that mailing list.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9303 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- docs/support/annotate/mail.php
+++ docs/support/annotate/mail.php
@@ -1,7 +1,4 @@
 <?php
  // Users to which notification will be sent when annotation is made.
- $mail[]="matze@braunis.de";
- $mail[]="jorrit.tyberghein@pandora.be";
- $mail[]="jorrit.tyberghein@uz.kuleuven.ac.be";
- $mail[]="sunshine@sunshineco.com";
+ $mail[]="crystal-docs@lists.sourceforge.net";
 ?>
