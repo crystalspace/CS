06f476c81450c7d16898284e38264d4a7e6544aa
Christopher Nelson <paradox@bbhc.org>
2002-01-25 18:38:40 +0000

    Modified implementation of mmapio.h
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9341 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csutil/mmapio.cpp
+++ libs/csutil/mmapio.cpp
@@ -1,133 +1,15 @@
 #include "cssysdef.h"
 #include "csutil/mmapio.h"
 
-#ifdef _LINUX_
-
-#include <unistd.h>
-#include <sys/mman.h>
-
-#endif
-
 csMemoryMappedIO::csMemoryMappedIO(unsigned _block_size, char *filename):
   block_size(_block_size)
 {
-
-#ifdef  _HAS_MEMORY_MAPPED_IO_
-
-#if defined WIN32
-
-  // Have Windows map this file in for use
-  if ((platform->hMappedFile = OpenFileMapping(FILE_MAP_READ, false, filename)) == NULL || 
-      (file_size=GetFileSize(platform->hMappedFile, NULL) == 0xFFFFFFFF                 || 
-      (platform->data = MapViewofFile(platform->hMappedFile, FILE_MAP_READ, 0, 0, file_size))==NULL
-     )
-  {
-    valid_mmio_object=false;
-    return;
-  }
-  else
-    valid_mmio_object=true;
-
-#elif defined _LINUX_ || defined _MACOSX_
-
-  struct stat statInfo;
-  
-  // Have [*]nix map this file in for use
-  if ((platform->hMappedFile = open(filename, O_RDONLY) == -1   ||
-      (fstat(platform->hMappedFile, &statInfo ) == -1           ||
-      (platform->data = mmap(0, statInfo.st_size, PROT_READ, 0, platform->hMappedFile)) == -1
-     )
-  {
-    valid_mmio_object=false;
-    return;
-  }
-  else
-  {
-    valid_mmio_object=true;
-    file_size=statInfo.st_size;
-  }
-
-
-#endif // platform specific loading
-
-
-#else
-
-  if ((mapped_file=fopen(filename, "rb")) != NULL)
-  {
-    valid_mmio_object=false;
-    return;
-  }
-  else
-  {
-    // Create a page map with one bit per cache_block_size * block_size bytes.
-    page_map = new csBitArray(file_size/(cache_block_size * block_size));
-
-    // Clear the cache
-    memset(&cache, 0, sizeof(cache));
-
-    // Set that we're good to go.
-    valid_mmio_object=true;
-  }
-
-
-#endif
-
+  valid_mmio_object = MemoryMapFile(&platform, filename);
 }
 
 csMemoryMappedIO::~csMemoryMappedIO()
 {
-
-#ifdef  _HAS_MEMORY_MAPPED_IO_ //////////////////////////////////////////////////////////////////////
-
-#if defined WIN32
-    
-  if (platform.data!=NULL)
-    UnmapViewofFile(platform.data);
-
-  if (platform.hMappedFile!=NULL)
-    CloseHandle(platform.hMappedFile);
-
-#elif defined _LINUX_
-    
-  if (platform.data != -1)
-    munmap(platform.data, file_size);
-
-  if (platform.hMappedFile != -1)
-    close(platform.hMappedFile);
-
-#endif // platform specific loading
-
-
-#else ////////////////////////////////////////////////////////////////////////////////////////////////
-
-  if (mapped_file)
-    fclose(mapped_file);
-
-  if (page_map)
-    delete page_map;
-
-  unsigned int i;
-
-  // Free all allocated memory.
-  for(i=0; i<csmmioDefaultHashSize; ++i)
-  {
-    CacheBlock *cp, *np;
-
-    cp=cache[i];
-    while(cp)
-    {
-     np=cp->next;
-     delete [] cp->data;
-     delete cp;
-
-     cp=np;
-    }
-  }
-
-#endif
-
-
+  UnMemoryMapFile(&platform);
 }
 
 void *
@@ -219,3 +101,58 @@ csMemoryMappedIO::CachePage(unsigned int page)
   fseek(mapped_file, page*cache_block_size*block_size, SEEK_SET);
   fread(cp->data, block_size, cache_block_size, mapped_file);
 }
+
+
+/////////////////////////////////////////// Software Support for Memory Mapping ///////////////////////////////////
+
+#ifndef CS_HAS_MEMORY_MAPPED_IO
+
+bool
+csMemoryMappedIO::MemoryMapFile(mmioInfo *_platform, char *filename)
+{
+  if ((_platform->hMappedFile=fopen(filename, "rb")) != NULL)
+  {
+    return false;
+  }
+  else
+  {
+    // Create a page map with one bit per cache block.
+    page_map = new csBitArray(_platform->file_size/cache_block_size);
+
+    // Clear the cache
+    memset(&cache, 0, sizeof(cache));
+
+    // Set that we're good to go.
+    return true;
+  }
+}
+
+void
+csMemoryMappedIO::UnMemoryMapFile(mmioInfo *_platform)
+{
+  if (_platform->hMappedFile)
+    fclose(_platform->hMappedFile);
+
+  if (page_map)
+    delete page_map;
+
+  unsigned int i;
+  CacheBlock *cp, *np;
+
+  // Free all allocated memory.
+  for(i=0; i<csmmioDefaultHashSize; ++i)
+  {    
+    cp=cache[i];
+    while(cp)
+    {
+     np=cp->next;
+     delete [] cp->data;
+     delete cp;
+
+     cp=np;
+    }
+  }
+}
+
+
+#endif
