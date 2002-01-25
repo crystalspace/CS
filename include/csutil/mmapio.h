e6580fead80eafef5db7f92741103c94e78458a8
Christopher Nelson <paradox@bbhc.org>
2002-01-25 05:08:50 +0000

    Update
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9322 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/mmapio.h
+++ include/csutil/mmapio.h
@@ -3,6 +3,38 @@
 
 #include "csutil/bitarray.h"
 
+/*******************************************************************
+
+  Design notes:
+
+   1. Although the offset and page in the cache block can both be 
+   calculated from either value, i chose to precalculate and store
+   BOTH items.  The reason for this is that it avoids one 
+   multiplication on each access, and one division when paging data
+   in to cache.  For the default values data storage per cache is
+   8192 bytes with 20 bytes additional overhead.  This means that
+   0.2% of the data is overhead.  That's worth it to avoid a heavy
+   op like multiplication
+
+   2. Usage of the singly-linked list for storage was chosen over a
+   static array of cache blocks because there's no simple way to
+   provide a direct index into the cache list from the index or page.
+   Since we can probably guarantee that all blocks are NOT going to
+   be in memory at once, we cannot index into the array based on a
+   page index, etc.  A sorted list would probably provide the fastest
+   lookup times, but would it be worth the overhead?  A hash table
+   provides a number of short lists, which in this case are unsorted.
+   Using QuickSort, lookup on a sorted table of 2048 entries would take
+   about log2(2048) operations. With the default hash table size, each 
+   list is about 1.5 entries long if spread uniformly.  This means that
+   most lookups on a cache of this size should take between two and 
+   four operations per (counting the modulus.)  While the hash table 
+   does consume about 4k more memory, I think the slight memory usage 
+   is worth it, considering the massive speedup.
+    
+ *******************************************************************/
+ 
+
 /// Default cache block size
 const unsigned csmmioDefaultCacheBlockSize = 256;
 
@@ -51,9 +83,12 @@ class csMemoryMappedIO
     /// The age, used to decide when to release a cache block
     unsigned age;
 
-    /// The starting offset of all the block in this cache block.
+    /// The starting offset for this page
     unsigned offset;
 
+    /// The page number of this block
+    unsigned page;
+    
     /// The next item in our block list.
     CacheBlock *next;
 
