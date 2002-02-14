12f132a825998e32a4b31c988709db4e6c1cd630
Christopher Nelson <paradox@bbhc.org>
2002-02-14 22:45:38 +0000

    The updated test app that uses layouts.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9520 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/tests/awstest/awstest.cpp
+++ apps/tests/awstest/awstest.cpp
@@ -398,11 +398,13 @@ awsTest::Initialize(int argc, const char* const argv[], const char *iConfigName)
   iAwsWindow *test = aws->CreateWindowFrom("Splash");
   iAwsWindow *test2 = aws->CreateWindowFrom("Another");
   iAwsWindow *test3 = aws->CreateWindowFrom("Engine View");
+  iAwsWindow *test4 = aws->CreateWindowFrom("Layout Test");
 
   test3->SetEngineView(wview);
 
   if (test)  test->Show();
   if (test3) test3->Show();
+  if (test4) test4->Show();
 
   //if (test2) test2->Show();
   s->SetTestWin(test2);
