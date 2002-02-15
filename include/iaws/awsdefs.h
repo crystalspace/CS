137cfcdc31218c3c6282d270df555ca503f8ffd4
Norman Krämer <norman@users.sourceforge.net>
2002-02-15 21:20:43 +0000

    AWSF_CMP_INVISIBLE was missing
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9554 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/iaws/awsdefs.h
+++ include/iaws/awsdefs.h
@@ -21,5 +21,6 @@
 const unsigned int AWSF_CMP_HIDDEN      = 8; // set if component is hidden
 const unsigned int AWSF_CMP_TRANSPARENT = 2; // set if component is transparent
 const unsigned int AWSF_CMP_ALWAYSERASE = 4; // set if component always needs erased by parent first
+const unsigned int AWSF_CMP_INVISIBLE   = 16; // set if component is so small it gets invisible
 
 #endif
