37cef0ef5290a68a38b827af37820c037bd3257b
Frank Richter <res@crystalspace3d.org>
2002-02-21 20:04:18 +0000

    disabled mipmap creation for proc textures
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9596 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/renderer/common/txtmgr.cpp
+++ plugins/video/renderer/common/txtmgr.cpp
@@ -122,7 +122,7 @@ void csTextureHandle::CreateMipmaps ()
   DG_LINK (this, tex[0]);
 
   // 2D textures uses just the top-level mipmap
-  if ((flags & (CS_TEXTURE_3D | CS_TEXTURE_NOMIPMAPS)) == CS_TEXTURE_3D)
+  if ((flags & (CS_TEXTURE_3D | CS_TEXTURE_NOMIPMAPS | CS_TEXTURE_PROC)) == CS_TEXTURE_3D)
   {
     // Create each new level by creating a level 2 mipmap from previous level
     iImage *i1 = image->MipMap (1, tc);
