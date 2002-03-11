a573da70255d44c3854baba335f28d30eefd139e
Philip Wyett <philswyett@gmx.co.uk>
2002-03-11 01:42:28 +0000

    Removed an uneeded line.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9738 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/video/video.cpp
+++ apps/video/video.cpp
@@ -397,7 +397,6 @@ iMeshWrapper *wallmesh = engine->CreateSectorWallsMesh (room, "walls");
 	  iGraphics2D* g2d = myG3D->GetDriver2D ();
 	  int x = (g2d->GetWidth ()  - desc.width) / 2;
 	  int y = (g2d->GetHeight ()  - desc.height) / 2;
-	  //pVStream->SetRect (x, y, desc.width, desc.height);
 	  pVStream->SetRect (x, y, desc.width, desc.height);
  	}
 	else
