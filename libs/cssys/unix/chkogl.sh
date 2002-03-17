8ffb6f4cc1cecd1e248e176f1d806499c06b06c2
Norman Krämer <norman@users.sourceforge.net>
2002-03-17 17:24:46 +0000

    added script to detect existance of glXGetProcAddressARB
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@9815 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ libs/cssys/unix/chkogl.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Test for OpemGL headers and features
+
+if [ -d /usr/local/include/GL ]; then
+  INC_OPENGL_PATH="/usr/local/include"
+elif [ -d /usr/include/GL ]; then
+  INC_OPENGL_PATH="/usr/include"
+elif [ -d /usr/openwin/include/GL ]; then
+  INC_OPENGL_PATH="/usr/openwin/include"
+else
+  echo "$0: Cannot find the OpenGL include directory!" >&2
+fi
+
+cat << EOF > gltest.cpp
+#define GLX_GLXEXT_PROTOTYPES
+#include <GL/glx.h>
+int main()
+{
+ (void*)glXGetProcAddressARB((const GLubyte *)"YoMama");
+ return 0;
+}
+EOF
+${CXX} -c gltest.cpp -I${INC_OPENGL_PATH} 2>/dev/null || echo "CSGL_EXT_STATIC_ASSERTION=yes"
+
+rm -f gltest.cpp gltest.o
