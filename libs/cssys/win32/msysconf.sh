223b1ce515318b3697ae5d762bca8a18c4f552f8
Philip Wyett <philswyett@gmx.co.uk>
2002-05-22 23:13:09 +0000

    - Added support for MSYS from the MinGW project.
      To build with MSYS do "make msys MODE=debug" in place of your old method.
    
    - Fixed mkdir command in CS so msys can deal with directory creation.
    
    Note: This is very hacked and early support and is subject to change.
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@10277 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ libs/cssys/win32/msysconf.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+#
+# This script file is used to autodetect some parameters
+# needed for building Crystal Space on various Unix flavours.
+#
+# Arguments: $1 is user-preferred install dir, argument can be left out
+#
+# The configuration (a makefile fragment) is piped to stdout
+# and errors are piped to stderr.
+#
+
+INSTALL_DIR=$1
+
+BIN_DIR=bin
+
+# find out architecture
+. ${BIN_DIR}/arch.sh
+
+# Check for compiler
+#. ${BIN_DIR}/comptest.sh
+
+# Check for tools
+. ${BIN_DIR}/chktools.sh
+
+# Analyse system headers
+. ${BIN_DIR}/chkheadr.sh
+
+# test for endianess
+. ${BIN_DIR}/endtest.sh
+
+# Check for python
+. ${BIN_DIR}/haspythn.sh
+
+# test for phyton
+# Phyton doesn't work yet
+#. ${BIN_DIR}/haspythn.sh
+
+# find install dir
+[ -z "${INSTALL_DIR}" ] && INSTALL_DIR=/usr/local/crystal
+echo "INSTALL_DIR = ${INSTALL_DIR}"
+
+exit 0
