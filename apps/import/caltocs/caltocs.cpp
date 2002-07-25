2ebea2a8c6e1a0e534c94e744c6b6ca608764264
Philip Wyett <philswyett@gmx.co.uk>
2002-07-25 09:10:22 +0000

    Moved #include "cssysdef.h" to allow it to cull warnings under MSVC 6.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@10684 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/import/caltocs/caltocs.cpp
+++ apps/import/caltocs/caltocs.cpp
@@ -16,8 +16,8 @@
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#include "cssysdef.h"
 #include "cal3d/cal3d.h"
+#include "cssysdef.h"
 #include <stdio.h>
 #include <stdarg.h>
 
