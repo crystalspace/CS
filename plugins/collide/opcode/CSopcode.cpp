ea64df341eda2237f7ca110153f9c90c2206df8b
Charles Al' Quarra <charsquarra@hotmail.com>
2002-08-29 12:28:42 +0000

    license and authoring stuff
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@10998 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/collide/opcode/CSopcode.cpp
+++ plugins/collide/opcode/CSopcode.cpp
@@ -1,6 +1,32 @@
+/*
+*
+*
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+-------------------------------------------------------------------------
+*           OPCODE collision detection plugin for CrystalSpace
+*
+*           OPCODE library was written by Pierre Terdiman
+*                  ported to CS by Charles Quarra
+*
+*
+-------------------------------------------------------------------------
+*/
+
+
 #include "cssysdef.h"
 #include "CSopcode.h"
-//#include "CSopcodecollider.h"
 #include "igeom/polymesh.h"
 #include "csgeom/transfrm.h"
 #include "csutil/scfstr.h"
@@ -54,6 +80,7 @@ csOPCODECollideSystem::csOPCODECollideSystem (iBase *pParent)
 
 csOPCODECollideSystem::~csOPCODECollideSystem ()
 {
+ delete[](pairs);
 }
  bool csOPCODECollideSystem::Initialize (iObjectRegistry* iobject_reg) {
  object_reg = iobject_reg;
