195019170e1b1ab44ab34a3e09a176c75ed1800f
Charles Al' Quarra <charsquarra@hotmail.com>
2002-08-29 12:29:08 +0000

    license and authoring stuff
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@10999 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/collide/opcode/CSopcode.h
+++ plugins/collide/opcode/CSopcode.h
@@ -1,3 +1,29 @@
+/*
+*      
+*
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+-------------------------------------------------------------------------
+*           OPCODE collision detection plugin for CrystalSpace
+* 
+*           OPCODE library was written by Pierre Terdiman 
+*                  ported to CS by Charles Quarra
+*
+*
+-------------------------------------------------------------------------
+*/
+
 #ifndef __OPCODE_PLUGIN_H__
 #define __OPCODE_PLUGIN_H__
 
@@ -9,12 +35,7 @@
 #include "Opcode.h"
 
 struct iObjectRegistry;
-//struct AABBTreeCollider;
-//struct BVTCache;
-/**
-* This is the implementation for our API and
-* also the implementation of the plugin.
-*/
+
 class csOPCODECollideSystem : public iCollideSystem
 {
 
