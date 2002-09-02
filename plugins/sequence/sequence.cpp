95361a00bd040c67d1d3482f3f77b5d54a23e657
Matze Braun <matze@braunis.de>
2002-09-02 13:41:44 +0000

    ups... introduced a stupid error with my last commit and fixed that now
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@11038 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/sequence/sequence.cpp
+++ plugins/sequence/sequence.cpp
@@ -15,8 +15,9 @@
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
-
 #include <string.h>
+
+#include "cssysdef.h"
 #include "cssys/sysfunc.h"
 #include "sequence.h"
 #include "csutil/scf.h"
