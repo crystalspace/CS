729d841964771c57e274e704d3e0bfadc82c7836
Daniel Duhprey <duhprey@yahoo.com>
2002-09-02 18:34:29 +0000

    Tutorial/test program for terrbig
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@11042 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ apps/tutorial/tbtut/tbtut.cpp
@@ -0,0 +1,356 @@
+/*
+    Copyright (C) 2002 by Jorrit Tyberghein, Daniel Duhprey
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <cssysdef.h>
+#include <cssys/sysfunc.h>
+#include <csver.h>
+
+#include <csutil/csobject.h>
+#include <csutil/cmdhelp.h>
+#include <csutil/cspmeter.h>
+#include <csutil/cscolor.h>
+#include <cstool/initapp.h>
+#include <cstool/csview.h>
+
+#include <iutil/objreg.h>
+#include <iutil/vfs.h>
+#include <iutil/virtclk.h>
+#include <iutil/csinput.h>
+#include <iutil/eventq.h>
+#include <iutil/event.h>
+#include <iutil/plugin.h>
+#include <iutil/cfgmgr.h>
+
+#include <iengine/engine.h>
+#include <iengine/sector.h>
+#include <iengine/camera.h>
+#include <iengine/campos.h>
+#include <iengine/mesh.h>
+#include <iengine/material.h>
+#include <iengine/light.h>
+#include <iengine/statlght.h>
+
+#include <imesh/object.h>
+#include <imesh/thing/thing.h>
+#include <imesh/thing/polygon.h>
+#include <imesh/ball.h>
+
+#include <ivideo/graph3d.h>
+#include <ivideo/graph2d.h>
+#include <ivideo/txtmgr.h>
+#include <ivideo/fontserv.h>
+
+#include <igraphic/imageio.h>
+
+#include <isound/loader.h>
+#include <isound/handle.h>
+#include <isound/source.h>
+#include <isound/listener.h>
+#include <isound/wrapper.h>
+#include <isound/renderer.h>
+
+#include <ivaria/reporter.h>
+#include <ivaria/stdrep.h>
+#include <ivaria/conout.h>
+#include <ivaria/perfstat.h>
+#include <ivaria/dynamics.h>
+
+#include <imap/parser.h>
+
+#include <imesh/terrbig.h>
+
+#include "tbtut.h"
+
+CS_IMPLEMENT_APPLICATION
+
+TerrBigTut *Sys = NULL;
+
+TerrBigTut::TerrBigTut (iObjectRegistry* o) 
+{
+  object_reg = o;
+}
+
+TerrBigTut::~TerrBigTut ()
+{
+}
+
+void TerrBigTut::Report (int s, const char *m)
+{
+  csReport (object_reg, s, "TerrBigTut", m);
+}
+
+bool TerrBigTut::Initialize () 
+{
+  if (!csInitializer::RequestPlugins (object_reg,
+    CS_REQUEST_VFS,
+    CS_REQUEST_SOFTWARE3D,
+    CS_REQUEST_ENGINE,
+    CS_REQUEST_FONTSERVER,
+	CS_REQUEST_IMAGELOADER,
+    CS_REQUEST_LEVELLOADER,
+    CS_REQUEST_REPORTER,
+    CS_REQUEST_REPORTERLISTENER,
+    CS_REQUEST_END)) 
+  {
+    Report (CS_REPORTER_SEVERITY_ERROR, 
+      "Failed to initialize plugins"); 
+    return false;
+  }
+
+  if (!csInitializer::SetupEventHandler (object_reg, SimpleEventHandler)) {
+    Report (CS_REPORTER_SEVERITY_ERROR,
+      "Unable to create event handler");  
+    return false;
+  }
+
+  if (csCommandLineHelper::CheckHelp (object_reg)) {
+    csCommandLineHelper::Help (object_reg);
+    return false;
+  }
+
+  if ((vc = CS_QUERY_REGISTRY (object_reg, iVirtualClock)) == NULL) {
+    Report (CS_REPORTER_SEVERITY_ERROR,
+      "Unable to initialize virtual clock");
+    return false;
+  }
+
+  if ((engine = CS_QUERY_REGISTRY (object_reg, iEngine)) == NULL) {
+    Report (CS_REPORTER_SEVERITY_ERROR,
+      "Unable to initialize engine");
+    return false;
+  }
+
+  if ((loader = CS_QUERY_REGISTRY (object_reg, iLoader)) == NULL) {
+    Report (CS_REPORTER_SEVERITY_ERROR,
+      "Unable to initialize map loader");
+    return false;
+  }
+
+  if ((g3d = CS_QUERY_REGISTRY (object_reg, iGraphics3D)) == NULL) {
+    Report (CS_REPORTER_SEVERITY_ERROR,
+      "Unable to initialize graphics");
+    return false;
+  }
+
+  if ((kbd = CS_QUERY_REGISTRY (object_reg, iKeyboardDriver)) == NULL) {
+    Report (CS_REPORTER_SEVERITY_ERROR,
+      "Unable to initialize keyboard");
+    return false;
+  }
+
+  if (!csInitializer::OpenApplication (object_reg)) {
+    Report (CS_REPORTER_SEVERITY_ERROR,
+      "Unable to start the application");
+    return false;
+  }
+
+  engine->SetLightingCacheMode (0); 
+
+  if (!loader->LoadTexture ("stone", "/lib/std/stone4.gif")) {
+    Report (CS_REPORTER_SEVERITY_ERROR,
+      "Error loading 'stone4' texture!\n");
+    return false;
+  }
+  iMaterialWrapper *tm = engine->GetMaterialList()->FindByName ("stone");
+
+  room = engine->CreateSector ("room");
+  csRef<iMeshWrapper> walls (csPtr<iMeshWrapper> (
+    engine->CreateSectorWallsMesh (room, "walls")));
+
+  csRef<iThingState> walls_state (
+  	SCF_QUERY_INTERFACE (walls->GetMeshObject (), iThingState));
+  iPolygon3D* p;
+  p = walls_state->CreatePolygon ();
+  p->SetMaterial (tm);
+  p->CreateVertex (csVector3 (-100, 0, 100));
+  p->CreateVertex (csVector3 (100, 0, 100));
+  p->CreateVertex (csVector3 (100, 0, -100));
+  p->CreateVertex (csVector3 (-100, 0, -100));
+  p->SetTextureSpace (p->GetVertex (0), p->GetVertex (1), 3);
+
+  p = walls_state->CreatePolygon ();
+  p->SetMaterial (tm);
+  p->CreateVertex (csVector3 (-100, 100, -100));
+  p->CreateVertex (csVector3 (100, 100, -100));
+  p->CreateVertex (csVector3 (100, 100, 100));
+  p->CreateVertex (csVector3 (-100, 100, 100));
+  p->SetTextureSpace (p->GetVertex (0), p->GetVertex (1), 3);
+
+  p = walls_state->CreatePolygon ();
+  p->SetMaterial (tm);
+  p->CreateVertex (csVector3 (-100, 100, 100));
+  p->CreateVertex (csVector3 (100, 100, 100));
+  p->CreateVertex (csVector3 (100, 0, 100));
+  p->CreateVertex (csVector3 (-100, 0, 100));
+  p->SetTextureSpace (p->GetVertex (0), p->GetVertex (1), 3);
+
+  p = walls_state->CreatePolygon ();
+  p->SetMaterial (tm);
+  p->CreateVertex (csVector3 (100, 100, 100));
+  p->CreateVertex (csVector3 (100, 100, -100));
+  p->CreateVertex (csVector3 (100, 0, -100));
+  p->CreateVertex (csVector3 (100, 0, 100));
+  p->SetTextureSpace (p->GetVertex (0), p->GetVertex (1), 3);
+
+  p = walls_state->CreatePolygon ();
+  p->SetMaterial (tm);
+  p->CreateVertex (csVector3 (-100, 100, -100));
+  p->CreateVertex (csVector3 (-100, 100, 100));
+  p->CreateVertex (csVector3 (-100, 0, 100));
+  p->CreateVertex (csVector3 (-100, 0, -100));
+  p->SetTextureSpace (p->GetVertex (0), p->GetVertex (1), 3);
+
+  p = walls_state->CreatePolygon ();
+  p->SetMaterial (tm);
+  p->CreateVertex (csVector3 (100, 100, -100));
+  p->CreateVertex (csVector3 (-100, 100, -100));
+  p->CreateVertex (csVector3 (-100, 0, -100));
+  p->CreateVertex (csVector3 (100, 0, -100));
+  p->SetTextureSpace (p->GetVertex (0), p->GetVertex (1), 3);
+
+  csRef<iMeshFactoryWrapper> factory;
+  factory = engine->CreateMeshFactory ("crystalspace.mesh.object.terrbig",
+    "terrain_factory");
+  if (!factory) {
+    Report (CS_REPORTER_SEVERITY_ERROR, "Can't create terrbig mesh factory!");
+    return false;
+  }
+  csRef<iMeshWrapper> mesh;
+  mesh = engine->CreateMeshWrapper (factory, "terrain", room, csVector3 (0,0,0));
+  csRef<iTerrBigState> terrbigstate;
+  terrbigstate = SCF_QUERY_INTERFACE (mesh->GetMeshObject(), iTerrBigState);
+
+  if (!loader->LoadTexture ("wood", "/lib/stdtex/andrew_wood.jpg")) {
+    Report (CS_REPORTER_SEVERITY_ERROR,
+      "Error loading 'wood' texture!\n");
+    return false;
+  }
+  iMaterialWrapper *mat = engine->GetMaterialList()->FindByName ("wood");
+  iMaterialHandle *mathandle = mat->GetMaterialHandle ();
+  terrbigstate->SetMaterialsList (&mathandle, 1);
+  terrbigstate->LoadHeightMapFile ("./data/terrain/test.map");
+
+  mesh->SetRenderPriority (engine->GetRenderPriority ("object"));
+  mesh->SetZBufMode (CS_ZBUF_USE);
+  mesh->DeferUpdateLighting (CS_NLIGHT_STATIC|CS_NLIGHT_DYNAMIC, 10);
+
+  csRef<iStatLight> light;
+  iLightList* ll = room->GetLights ();
+
+  light = csPtr<iStatLight> (
+  	engine->CreateLight (NULL, csVector3 (-3, 5, 0), 10,
+  	csColor (1, 0, 0), false));
+  ll->Add (light->QueryLight ());
+
+  light = csPtr<iStatLight> (
+  	engine->CreateLight (NULL, csVector3 (3, 5,  0), 10,
+  	csColor (0, 0, 1), false));
+  ll->Add (light->QueryLight ());
+
+  light = csPtr<iStatLight> (
+  	engine->CreateLight (NULL, csVector3 (0, 5, -3), 10,
+  	csColor (0, 1, 0), false));
+  ll->Add (light->QueryLight ());
+
+  engine->Prepare ();
+
+  view = csPtr<iView> (new csView (engine, g3d));
+  view->GetCamera ()->SetSector (room);
+  view->GetCamera ()->GetTransform ().SetOrigin (csVector3 (0, 5, -3));
+  iGraphics2D* g2d = g3d->GetDriver2D ();
+  view->SetRectangle (0, 0, g2d->GetWidth (), g2d->GetHeight ());
+  view->GetCamera()->Move (csVector3 (0, 20, 0));
+
+  iTextureManager* txtmgr = g3d->GetTextureManager ();
+  txtmgr->SetPalette ();
+  return true;
+}
+
+void TerrBigTut::MainLoop ()
+{
+  csDefaultRunLoop (object_reg);
+}
+
+bool TerrBigTut::SimpleEventHandler (iEvent &e)
+{
+  return Sys->HandleEvent (e);
+}
+
+bool TerrBigTut::HandleEvent (iEvent &e)
+{
+  if (e.Type == csevBroadcast && e.Command.Code == cscmdProcess) {
+    SetupFrame ();
+	return true;
+  }
+  if (e.Type == csevBroadcast && e.Command.Code == cscmdFinalProcess) {
+    FinishFrame ();
+	return true;
+  }
+  if (e.Type == csevKeyDown && e.Key.Code == CSKEY_ESC) {
+    csRef<iEventQueue> q (CS_QUERY_REGISTRY (object_reg, iEventQueue));
+    if (q) q->GetEventOutlet()->Broadcast (cscmdQuit);
+    return true;
+  };
+
+  return false;
+}
+
+void TerrBigTut::SetupFrame () 
+{
+  float elapsed_time = ((float)vc->GetElapsedTicks ())/1000.0;
+  float speed = elapsed_time * 0.6;
+
+  iCamera *c = view->GetCamera ();
+  if (kbd->GetKeyState (CSKEY_RIGHT))
+    c->GetTransform ().RotateThis (CS_VEC_ROT_RIGHT, speed);
+  if (kbd->GetKeyState (CSKEY_LEFT))
+    c->GetTransform ().RotateThis (CS_VEC_ROT_LEFT, speed);
+  if (kbd->GetKeyState (CSKEY_PGUP))
+    c->GetTransform ().RotateThis (CS_VEC_TILT_UP, speed);
+  if (kbd->GetKeyState (CSKEY_PGDN))
+    c->GetTransform ().RotateThis (CS_VEC_TILT_DOWN, speed);
+  if (kbd->GetKeyState (CSKEY_UP))
+    c->Move (CS_VEC_FORWARD * 4 * elapsed_time);
+  if (kbd->GetKeyState (CSKEY_DOWN))
+    c->Move (CS_VEC_FORWARD * -4 * elapsed_time);
+
+  if (g3d->BeginDraw (engine->GetBeginDrawFlags () | CSDRAW_3DGRAPHICS)) {
+    view->Draw (); 
+  }
+}
+
+void TerrBigTut::FinishFrame ()
+{
+  g3d->FinishDraw ();
+  g3d->Print (NULL);
+}
+
+int main (int argc, char *argv[])
+{
+  iObjectRegistry* object_reg = csInitializer::CreateEnvironment (argc, argv);
+
+  Sys = new TerrBigTut(object_reg);
+  if (Sys->Initialize ()) {
+    Sys->MainLoop ();
+  }
+  delete Sys;
+
+  csInitializer::DestroyApplication (object_reg);
+  return 0;
+}
+
---
+++ apps/tutorial/tbtut/tbtut.mak
@@ -0,0 +1,69 @@
+# Application description
+DESCRIPTION.tbtut = Crystal Space Terrbig Tutorial
+
+#------------------------------------------------------------- rootdefines ---#
+ifeq ($(MAKESECTION),rootdefines)
+
+# Application-specific help commands
+APPHELP += \
+  $(NEWLINE)echo $"  make tbtut        Make the $(DESCRIPTION.tbtut)$"
+
+endif # ifeq ($(MAKESECTION),rootdefines)
+
+#------------------------------------------------------------- roottargets ---#
+ifeq ($(MAKESECTION),roottargets)
+
+.PHONY: tbtut tbtutclean
+
+all apps: tbtut
+tbtut:
+	$(MAKE_APP)
+tbtutclean:
+	$(MAKE_CLEAN)
+
+endif # ifeq ($(MAKESECTION),roottargets)
+
+#------------------------------------------------------------- postdefines ---#
+ifeq ($(MAKESECTION),postdefines)
+
+vpath %.cpp apps/tutorial/tbtut
+
+TBTUT.EXE = tbtut$(EXE)
+INC.TBTUT = $(wildcard apps/tutorial/tbtut/*.h)
+SRC.TBTUT = $(wildcard apps/tutorial/tbtut/*.cpp)
+OBJ.TBTUT = $(addprefix $(OUT)/,$(notdir $(SRC.TBTUT:.cpp=$O)))
+DEP.TBTUT = CSTOOL CSGFX CSUTIL CSSYS CSGEOM CSUTIL CSSYS
+LIB.TBTUT = $(foreach d,$(DEP.TBTUT),$($d.LIB))
+
+#TO_INSTALL.EXE += $(TBTUT.EXE)
+
+MSVC.DSP += TBTUT
+DSP.TBTUT.NAME = tbtut
+DSP.TBTUT.TYPE = appcon
+
+endif # ifeq ($(MAKESECTION),postdefines)
+
+#----------------------------------------------------------------- targets ---#
+ifeq ($(MAKESECTION),targets)
+
+.PHONY: build.tbtut tbtutclean
+
+all: $(TBTUT.EXE)
+build.tbtut: $(OUTDIRS) $(TBTUT.EXE)
+clean: tbtutclean
+
+$(TBTUT.EXE): $(DEP.EXE) $(OBJ.TBTUT) $(LIB.TBTUT)
+	$(DO.LINK.EXE)
+
+tbtutclean:
+	-$(RM) $(TBTUT.EXE) $(OBJ.TBTUT)
+
+ifdef DO_DEPEND
+dep: $(OUTOS)/tbtut.dep
+$(OUTOS)/tbtut.dep: $(SRC.TBTUT)
+	$(DO.DEP)
+else
+-include $(OUTOS)/tbtut.dep
+endif
+
+endif # ifeq ($(MAKESECTION),targets)
