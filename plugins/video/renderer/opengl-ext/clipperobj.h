500ecea4c139511a3af67ee159dea7c6dc72ce16
Philipp Aumayr <phueppl@gmx.at>
2002-09-07 01:19:36 +0000

    forgot LGPL License header
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@11144 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/renderer/opengl-ext/clipperobj.h
+++ plugins/video/renderer/opengl-ext/clipperobj.h
@@ -1,6 +1,37 @@
 #ifndef CLIPPEROBJ_H_INCLUDED
 #define CLIPPEROBJ_H_INCLUDED
-
+/* ========== HTTP://WWW.SOURCEFORGE.NET/PROJECTS/CRYSTAL ==========
+ *
+ * FILE: gl_clipperobj.h
+ *
+ * DESCRIPTION:
+ *  an Interface definition for diffrent OpenGL clippers
+ *
+ * LICENSE:
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * AUTHOR:
+ *    Philipp R. Aumayr
+ *
+ * CVS/RCS ID:
+ *    $Id:
+ *
+ * === COPYRIGHT (c)2002 ============== PROJECT CRYSTAL SPACE 3D === */
+/* -----------------------------------------------------------------
+ * Preprocessor Includes
+ * ----------------------------------------------------------------- */
 
 struct iGraphics3d;
 struct iClipper2D;
--- plugins/video/renderer/opengl-ext/gl_clipperobj_ext.cpp
+++ plugins/video/renderer/opengl-ext/gl_clipperobj_ext.cpp
@@ -1,7 +1,41 @@
+/* ========== HTTP://WWW.SOURCEFORGE.NET/PROJECTS/CRYSTAL ==========
+ *
+ * FILE: gl_clipperobj_ext.cpp
+ *
+ * DESCRIPTION:
+ *  Implements a Vertex Buffer suitable for forming primitive types
+ *
+ * LICENSE:
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * AUTHOR:
+ *    Philipp R. Aumayr
+ *
+ * CVS/RCS ID:
+ *    $Id:
+ *
+ * === COPYRIGHT (c)2002 ============== PROJECT CRYSTAL SPACE 3D === */
+/* -----------------------------------------------------------------
+ * Preprocessor Includes
+ * ----------------------------------------------------------------- */
+
 #include "cssysdef.h"
 #include "gl_clipperobj_ext.h"
 #include "ivideo/graph3d.h"
 #include "igeom/clip2d.h"
+
 
 
 #if defined(CS_OPENGL_PATH)
