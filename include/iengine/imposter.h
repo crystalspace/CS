7faa5cb47c30efbf6172574a0269aeefe72e4c1b
Keith Fulton <keith@paqrat.com>
2002-10-16 07:10:35 +0000

    Added support for iSharedVariables and iImposter.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@11667 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/iengine/imposter.h
@@ -0,0 +1,74 @@
+/*
+    Copyright (C) 2002 by Keith Fulton
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __IENGINE_IMPOSTER_H__
+#define __IENGINE_IMPOSTER_H__
+
+
+/**\file
+ */
+/**
+ * \addtogroup engine3d
+ * @{ */
+
+#include <iengine/sharevar.h>
+#include "csutil/scf.h"
+
+SCF_VERSION (iImposter, 0, 0, 1);
+
+/**
+ * iImposter defines the interface a mesh (or other) class must
+ * implement to be used as imposter mesh by the engine.
+ */
+struct iImposter : public iBase
+{
+  /// Self explanatory
+  virtual void SetImposterActive(bool flag)=0;
+  virtual bool GetImposterActive() const =0;
+
+  /**
+   * Minimum Imposter Distance is the distance from camera 
+   * beyond which imposter is used. Imposter gets a 
+   * ptr here because value is a shared variable 
+   * which can be changed at runtime for many objects.
+   */
+  virtual void SetMinDistance(iSharedVariable* dist) = 0;
+
+  /** 
+   * Rotation Tolerance is the maximum allowable 
+   * angle difference between when the imposter was 
+   * created and the current position of the camera.
+   * Angle greater than this triggers a re-render of
+   * the imposter.
+   */
+  virtual void SetRotationTolerance(iSharedVariable* angle) = 0;
+
+  /**
+   * Tells the object to create its proctex and polygon
+   * for use by main render process later.
+   */
+  virtual void CreateImposter(csReversibleTransform& pov) = 0;
+
+  /// Draw imposter on screen.
+  /// private: virtual void Draw(iRenderView* rview) = 0;
+
+  /// Determine if imposter or true rendering will be used
+  virtual bool WouldUseImposter(csReversibleTransform& pov) const = 0;
+};
+
+#endif
---
+++ include/iengine/sharevar.h
@@ -0,0 +1,80 @@
+/*
+    Copyright (C) 2002 by Keith Fulton
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __IENGINE_SHAREVAR_H__
+#define __IENGINE_SHAREVAR_H__
+
+/**\file
+ */
+/**
+ * \addtogroup engine3d
+ * @{ */
+
+#include "cstypes.h"
+#include "csutil/scf.h"
+
+SCF_VERSION (iSharedVariable, 0, 0, 1);
+
+/**
+ * iSharedVariable implements a refcounted float value which can
+ * be shared across many objects and updated efficiently.
+ */
+struct iSharedVariable : public iBase
+{
+  virtual void Set(float val) = 0;
+  virtual float Get() const = 0;
+  virtual void SetName (const char *iName) = 0;
+  virtual const char *GetName () const = 0;
+};
+
+SCF_VERSION (iSharedVariableList, 0, 0, 2);
+
+/**
+ * A list of shared variables.
+ */
+struct iSharedVariableList : public iBase
+{
+  /// Return the number of Shared Variables in this list.
+  virtual int GetCount () const = 0;
+
+  /// Return a SharedVariable by index.
+  virtual iSharedVariable *Get (int n) const = 0;
+
+  /// Add a SharedVariable.
+  virtual int Add (iSharedVariable *obj) = 0;
+
+  /// Remove a SharedVariable.
+  virtual bool Remove (iSharedVariable *obj) = 0;
+
+  /// Remove the nth SharedVariable.
+  virtual bool Remove (int n) = 0;
+
+  /// Remove all SharedVariables.
+  virtual void RemoveAll () = 0;
+
+  /// Find a SharedVariable and return its index.
+  virtual int Find (iSharedVariable *obj) const = 0;
+
+  /// Find a SharedVariable by name.
+  virtual iSharedVariable *FindByName (const char *Name) const = 0;
+
+  /// iSharedVariable Factory method.  This does not add the new var to the list.
+  virtual iSharedVariable *New() const = 0;
+};
+
+#endif
