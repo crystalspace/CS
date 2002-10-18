1cd2be1648f3ec2ad26f95bd70109a476ececb45
Marten Svanfeldt <developer@svanfeldt.com>
2002-10-18 17:43:21 +0000

    First addition of render3d - opengl renderer - files - thebolt
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@11696 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ plugins/video/render3d/opengl/gl_render3d.cpp
@@ -0,0 +1,173 @@
+/*
+    Copyright (C) 2002 by Mårten Svanfeldt
+                          Anders Stenberg
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+#include "ivideo/render3d.h"
+
+#include "render3d.h"
+
+SCF_IMPLEMENT_IBASE(csGLRender3D)
+  SCF_IMPLEMENTS_INTERFACE(iRender3D)
+  SCF_IMPLEMENTS_EMBEDDED_INTERFACE(iComponent)
+  SCF_IMPLEMENTS_EMBEDDED_INTERFACE(iEffectClient)
+SCF_IMPLEMENT_IBASE_END
+
+SCF_IMPLEMENT_EMBEDDED_IBASE (csGLRender3D::eiComponent)
+  SCF_IMPLEMENTS_INTERFACE (iComponent)
+SCF_IMPLEMENT_EMBEDDED_IBASE_END
+
+SCF_IMPLEMENT_EMBEDDED_IBASE (csGLRender3D::eiEffectClient)
+  SCF_IMPLEMENTS_INTERFACE (iEffectClient)
+SCF_IMPLEMENT_EMBEDDED_IBASE_END
+
+SCF_IMPLEMENT_IBASE (csGLRender3D::EventHandler)
+  SCF_IMPLEMENTS_INTERFACE (iEventHandler)
+SCF_IMPLEMENT_IBASE_END
+
+
+csGLRender3D::csGLRender3D (iBase *parent)
+{
+  SCF_CONSTRUCT_IBASE (parent);
+  SCF_CONSTRUCT_EMBEDDED_IBASE(scfiComponent);
+  SCF_CONSTRUCT_EMBEDDED_IBASE(scfiEffectClient);
+}
+
+
+////////////////////////////////////////////////////////////////////
+//                         iRender3D
+////////////////////////////////////////////////////////////////////
+
+
+
+
+bool csGLRender3D::Open ()
+{
+  if (!g2d->Open ())
+  {
+    Report (CS_REPORTER_SEVERITY_ERROR, "Error opening Graphics2D context.");
+    return false;
+  }
+
+	effectserver = CS_QUERY_REGISTRY(object_reg, iEffectServer);
+	if( !effectserver )
+  {
+    csRef<iPluginManager> plugin_mgr (
+      CS_QUERY_REGISTRY (object_reg, iPluginManager));
+    effectserver = CS_LOAD_PLUGIN (plugin_mgr,
+      "crystalspace.video.effects.stdserver", iEffectServer);
+    object_reg->Register (effectserver, "iEffectServer");
+  }
+}
+
+void csGLRender3D::Close ()
+{
+  if (g2d)
+    g2d->Close ();
+}
+
+bool csGLRender3D::BeginDraw (int drawflags)
+{
+	return false;
+}
+
+void csGLRender3D::FinishDraw ()
+{
+}
+
+void csGLRender3D::Print (csRect* area)
+{
+	G2D->Print (area);
+}
+
+
+
+////////////////////////////////////////////////////////////////////
+//                         iEffectClient
+////////////////////////////////////////////////////////////////////
+
+
+
+
+bool csGLRender3D::Validate (iEffectDefinition* effect, iEffectTechnique* technique);
+{
+	return false;
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////
+//                          iComponent
+////////////////////////////////////////////////////////////////////
+
+
+
+
+bool csGLRender3D::Initialize (iObjectRegistry* p)
+{
+  object_reg = p;
+  
+	if (!scfiEventHandler)
+    scfiEventHandler = new EventHandler (this);
+  
+	csRef<iEventQueue> q (CS_QUERY_REGISTRY(object_reg, iEventQueue));
+  if (q)
+    q->RegisterListener (scfiEventHandler, CSMASK_Broadcast);
+
+
+  csRef<iPluginManager> plugin_mgr (
+    CS_QUERY_REGISTRY (object_reg, iPluginManager));
+
+	// @@@ Should check what canvas to load
+  g2d = CS_LOAD_PLUGIN (plugin_mgr, 
+		"crystalspace.graphics2d.glwin32", iGraphics2D);
+  if (!g2d)
+    return false;
+
+	return true;
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////
+//                         iEventHandler
+////////////////////////////////////////////////////////////////////
+
+
+
+
+bool csGLRender3D::HandleEvent (iEvent& Event)
+{
+  if (Event.Type == csevBroadcast)
+    switch (Event.Command.Code)
+    {
+      case cscmdSystemOpen:
+      {
+        Open ();
+        return true;
+      }
+      case cscmdSystemClose:
+      {
+        Close ();
+        return true;
+      }
+    }
+  return false;
+}
---
+++ plugins/video/render3d/opengl/gl_render3d.h
@@ -0,0 +1,166 @@
+/*
+    Copyright (C) 2002 by Mårten Svanfeldt
+                          Anders Stenberg
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __GL_RENDER3D_H__
+#define __GL_RENDER3D_H__
+
+struct iObjectRegistry;
+struct iGraphics2D;
+struct iTextureManager;
+struct iRenderBufferManager;
+struct iLightingManager;
+struct iEffectDefinition;
+struct iEffectTechnique;
+class csRender3dCaps;
+
+class csGLRender3D : public iRender3D
+{
+private:
+	csRef<iObjectRegistry> object_reg;
+	csRef<iGraphics2D> g2d;
+	csRef<iTextureManager> texturemgr;
+	csRef<iRenderBufferManager> buffermgr;
+	csRef<iLightingManager> lightmgr;
+	csRef<iEffectServer> effectserver;
+
+	float fov;
+	int viewwidth, viewheight;
+
+	csRender3dCaps rendercaps;
+
+	csStringHash strings;
+	
+public:
+	SCF_DECLARE_IBASE;
+
+	csGLRender3D (iBase *parent);
+	~csGLRender3D ();
+
+	////////////////////////////////////////////////////////////////////
+	//                            iRender3d
+	////////////////////////////////////////////////////////////////////
+
+  /// Open 3d renderer.
+  bool Open();
+
+  /// Close renderer and release all resources used
+  void Close();
+
+  /// Get a pointer to our 2d canvas driver. NOTE: It's not increfed,
+  /// and therefore it shouldn't be decref-ed by caller.
+	iGraphics2d* Get2DDriver() 
+		{ return g2d; }
+
+  /// Get a pointer to our texture manager
+	iTextureManager* GetTextureManager() 
+		{ return texturemgr; }
+
+  /// Get a pointer to the VB-manager
+  /// Always use the manager referenced here to get VBs
+	iRenderBufferManager* GetBufferManager() 
+		{ return buffermgr; }
+
+  /// Get a pointer to lighting manager
+	iLightingManager* GetLightingManager() 
+		{ return lightmgr; }
+
+  /// Dimensions of window
+  void SetDimension(int width, int height);
+	void GetDimension(int &width, int &height) 
+		{ width = viewwidth; height = viewheight; }
+
+  /// Capabilities of the driver
+	csRender3dCaps* GetCaps() 
+		{ return &rendercaps; }
+
+  /// Field of view
+  void SetFOV(float fov);
+	float GetFOV() 
+		{ return fov; }
+
+  /// Set world to view transform
+  void SetWVMatrix(csReversibleTransform* wvmatrix);
+  csReversibleTransform* GetWVMatrix();
+
+  /// Begin drawing in the renderer
+  bool BeginDraw(int drawflags);
+
+  /// Indicate that drawing is finished
+  void FinishDraw();
+
+  /// Do backbuffer printing
+  void Print(csRect* area);
+
+  /// Drawroutine. Only way to draw stuff
+  void DrawMesh(csRenderMesh* mymesh);
+
+  /// Get a stringhash to be used by our streamsources etc.
+	csStringHash *GetStringContainer() 
+		{ return strings; }
+
+	////////////////////////////////////////////////////////////////////
+	//                         iEffectClient
+	////////////////////////////////////////////////////////////////////
+
+	bool Validate (iEffectDefinition* effect, iEffectTechnique* technique);
+
+	struct eiEffectClient : public iEffectClient
+	{
+		SCF_DECLARE_EMBEDDED_IBASE(csGLRender3D);
+		virtual bool Validate (
+			iEffectDefinition* effect, iEffectTechnique* technique)
+			{ return scfParent->Validate (effect, technique); }
+	} scfiEffectClient;
+
+	////////////////////////////////////////////////////////////////////
+	//                          iComponent
+	////////////////////////////////////////////////////////////////////
+
+	bool Initialize (iObjectRegistry reg);
+
+	struct eiComponent : public iComponent
+	{
+		SCF_DECLARE_EMBEDDED_IBASE(csGLRender3D);
+		virtual bool Initialize (iObjectRegistry* reg)
+			{ return scfParent->Initialize (reg); }
+	} scfiComponent;
+	
+	////////////////////////////////////////////////////////////////////
+	//                         iEventHandler
+	////////////////////////////////////////////////////////////////////
+	
+	bool HandleEvent (iEvent& Event);
+
+	struct EventHandler : public iEventHandler
+	{
+	private:
+		csGLRender3D* parent;
+	public:
+		EventHandler (csGLRender3D* parent)
+		{
+			SCF_CONSTRUCT_IBASE (NULL);
+			EventHandler::parent = parent;
+		}
+		SCF_DECLARE_IBASE;
+		virtual bool HandleEvent (iEvent& ev) 
+			{ return parent->HandleEvent (ev); }
+	} * scfiEventHandler;
+};
+
+#endif // __GL_RENDER3D_H__
