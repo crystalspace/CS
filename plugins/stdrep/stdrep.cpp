e1778289dece0617cf689be2ebaa713f14c752d0
Anders Stenberg <dentoid@users.sourceforge.net>
2002-11-11 00:42:23 +0000

    Small fix for compiling with new renderer
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@11846 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/stdrep/stdrep.cpp
+++ plugins/stdrep/stdrep.cpp
@@ -220,7 +220,11 @@ void csReporterListener::SetDefaults ()
 {
   console = CS_QUERY_REGISTRY (object_reg, iConsoleOutput);
   nativewm = NULL;
+#ifndef CS_USE_NEW_RENDERER
   csRef<iGraphics3D> g3d (CS_QUERY_REGISTRY (object_reg, iGraphics3D));
+#else
+  csRef<iGraphics3D> g3d (CS_QUERY_REGISTRY (object_reg, iRender3D));
+#endif // CS_USE_NEW_RENDERER
   if (g3d)
   {
     iGraphics2D* g2d = g3d->GetDriver2D ();
