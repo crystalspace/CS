159c3dbabefab9a794d3672845b85a1a79df5a5b
Charles Al' Quarra <charsquarra@hotmail.com>
2002-11-25 03:16:47 +0000

    i suspect that i fixed some bugs on InsertChunk and DeleteChunk pointed out by sunshineco
      im not liable by any bugs introduced in this fix =)
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@11916 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csutil/csvector.cpp
+++ libs/csutil/csvector.cpp
@@ -96,26 +96,28 @@ bool csBasicVector::Delete (int n)
 }
 
 
-bool csBasicVector::InsertChunk (int n, int size , csSome Item)
+bool csBasicVector::InsertChunk (int n, int size , csSome* Item)
 { // no run-time checks here, just in debug mode
   CS_ASSERT( n <= count );
+  CS_ASSERT( size>0 );
   SetLength (count + size); // Increments 'count' as a side-effect
   const int nmove = (count - (n + size));
-  CS_ASSERT(nmove > 0);
-  memmove ( &root [ n + size ] , &root [ n ] , nmove * sizeof (csSome) );
-  memcpy  ( &root [ n ] , Item , size * sizeof (csSome) );
+  if (nmove > 0)
+    memmove ( &root [ n + size ] , &root [ n ] , nmove * sizeof (csSome) );
+    
+   memcpy  ( &root [ n ] , Item , size * sizeof (csSome) );    
   return true;  
 }
 
 bool csBasicVector::DeleteChunk (int n, int size)
 { // no run-time checks here, just in debug mode
   CS_ASSERT( (n >= 0) && (n < count) );
-  CS_ASSERT( (size >= 0) && ( n + size < count ) );
-  const int ncount = count - size;
-  const int nmove  = ncount - n;
-  CS_ASSERT( nmove > 0 );
-  memmove (&root [ n ] , &root [ n + size ] , nmove * sizeof (csSome) );
-  SetLength (ncount);
+  CS_ASSERT( (size > 0) && ( n + size <= count ) );
+  const int nmove  = count - n;
+  if ( nmove > 0 )
+    memmove (&root [ n ] , &root [ n + size ] , nmove * sizeof (csSome) );
+  
+  SetLength ( count - size );
   return true;
 }
 
