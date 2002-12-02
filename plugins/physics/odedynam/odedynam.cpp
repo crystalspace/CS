903dc16fc8bdf325c72f2c0cb30b9bd50cbaeefc
Daniel Duhprey <duhprey@yahoo.com>
2002-12-02 20:08:16 +0000

    Fix for SetPosition/GetPosition
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@11975 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/physics/odedynam/odedynam.cpp
+++ plugins/physics/odedynam/odedynam.cpp
@@ -975,13 +975,13 @@ bool csODERigidBody::AttachColliderPlane (const csPlane3& plane,
 
 void csODERigidBody::SetPosition (const csVector3& pos)
 {
-    dGeomSetPosition (groupID, pos.x, pos.y, pos.z);
+    dBodySetPosition (bodyID, pos.x, pos.y, pos.z);
     if (statjoint != 0) dJointSetFixed(statjoint);
 }
 
 const csVector3 csODERigidBody::GetPosition () const
 {
-  const dReal* pos = dGeomGetPosition (bodyID);
+  const dReal* pos = dBodyGetPosition (bodyID);
   return csVector3 (pos[0], pos[1], pos[2]);
 }
 
