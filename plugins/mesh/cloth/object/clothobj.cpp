763d1f7b364f200ec569d46ea54fccbe71be184c
Charles Al' Quarra <charsquarra@hotmail.com>
2003-01-10 15:15:13 +0000

    I (\/) to indent          \/
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@12267 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/cloth/object/clothobj.cpp
+++ plugins/mesh/cloth/object/clothobj.cpp
@@ -64,119 +64,121 @@ SCF_IMPLEMENT_EMBEDDED_IBASE (csStuffObject::csClothMeshState)
 SCF_IMPLEMENT_EMBEDDED_IBASE_END
 
 void csStuffObject::SetupVertexBuffer ()
-{  int count=0;
- if (!vbuf)
- {
-	 printf(" SETUPVERTEX BUFFER %u \n",count++);
-	 csRef<iGraphics3D> g3d (CS_QUERY_REGISTRY (Obj_Reg, iGraphics3D));
-	 // @@@ priority should be a parameter.
-	 printf(" %u \n",count++);
-	 
-	 vbufmgr = g3d->GetVertexBufferManager ();
-	 vbuf = vbufmgr->CreateBuffer (0);
-	 //  vbuf1 = vbufmgr->CreateBuffer (1);
-	 printf(" %u \n",count++);
-	 
-	 vbufmgr->AddClient (&scfiVertexBufferManagerClient);
-	 mesh.buffers[0] = vbuf;
-	 // mesh.buffers[1] = vbuf1;
-	 
- }
+{
+  int count=0;
+  if (!vbuf)
+  {
+    printf(" SETUPVERTEX BUFFER %u \n",count++);
+    csRef<iGraphics3D> g3d (CS_QUERY_REGISTRY (Obj_Reg, iGraphics3D));
+    // @@@ priority should be a parameter.
+    printf(" %u \n",count++);
+    
+    vbufmgr = g3d->GetVertexBufferManager ();
+    vbuf = vbufmgr->CreateBuffer (0);
+    //  vbuf1 = vbufmgr->CreateBuffer (1);
+    printf(" %u \n",count++);
+    
+    vbufmgr->AddClient (&scfiVertexBufferManagerClient);
+    mesh.buffers[0] = vbuf;
+    // mesh.buffers[1] = vbuf1;	 
+  }
  printf(" EXITING SETUPVERTEX BUFFER %u \n",count++);
  
 }
 
 bool csStuffObject::Initialize(iObjectRegistry *iO_R)
 {
-	printf("initializtion \n");
-	if (iO_R==NULL) { 	printf(" NO OBJECT_REG! \n"); return false; };
-	Obj_Reg=iO_R;
-	shift.Set(0,0,0);
-	max_radius.Set(5.0,5.0,0.0);
-	vis_cb=NULL;
-	vertices=NULL;
-	material=NULL;
-	texels=NULL;
-	colors=NULL;
-	mesh.triangles=NULL;
-	mesh.vertex_fog=NULL;
-	setup=false;
-	return true;
+  printf("initializtion \n");
+  if (iO_R==NULL) { 	printf(" NO OBJECT_REG! \n"); return false; };
+  Obj_Reg=iO_R;
+  shift.Set(0,0,0);
+  max_radius.Set(5.0,5.0,0.0);
+  vis_cb=NULL;
+  vertices=NULL;
+  material=NULL;
+  texels=NULL;
+  colors=NULL;
+  mesh.triangles=NULL;
+  mesh.vertex_fog=NULL;
+  setup=false;
+  return true;
 };
 
 void csStuffObject::SetupMesh()
 {
-	if (setup) return;
-		
-	printf( " setting up the mesh... \n");	
-	setup      = true;
-	fact_state = SCF_QUERY_INTERFACE ( factory , iClothFactoryState );
-	Fabric     = new Cloth ( fact_state , shift , object_bbox , csVector3( 0.03, 0.01, 0.01 ) );
+  if (setup) return;
+
+  printf( " setting up the mesh... \n");	
+  setup      = true;
+  fact_state = SCF_QUERY_INTERFACE ( factory , iClothFactoryState );
+  Fabric     = new Cloth ( fact_state , shift , object_bbox , csVector3( 0.0, -0.15 , 0.0 ) );
+  
+  // after initializing a Cloth, all triangulation and eventually refinement is already done,
+  // and i want to copy that to factory just once (and maybe save/cache it)
+  //memcpy ( factory_mesh->GetTriangles() , Fabric->triangles , sizeof(csTriangle)*num_triangles );
+  printf( " setting up the mesh2... \n");	
+  Dynamics      = new Integrator ( Fabric );
+  num_vertices  = Fabric -> nverts;
+  vertices      = Fabric -> vertices;
+  texels        = fact_state-> GetTexels(); 
+  colors        = fact_state-> GetColors(); 
+  material      = fact_state->GetMaterialWrapper();
+  
+  SetupVertexBuffer();
+  mesh.num_triangles = Fabric -> ntris;  
+  mesh.triangles     = Fabric -> triangles;  
+  mesh.vertex_fog = new G3DFogInfo[num_vertices];
+  
+  //memcpy (mesh.triangles, triangles, sizeof(csTriangle)*num_triangles);
+  mesh.morph_factor = 0.0f;
+  mesh.num_vertices_pool = 1;
+  mesh.do_morph_texels = false;
+  mesh.do_morph_colors = false;
+  mesh.vertex_mode = G3DTriangleMesh::VM_WORLDSPACE;
+  
+  //Adams-Moulton integrator needs 4 euler steps before being able to take over
+  Dynamics->ComputeInitial();
+  Dynamics->ComputeInitial();
+  Dynamics->ComputeInitial();
+  Dynamics->ComputeInitial();  
 	
-	// after initializing a Cloth, all triangulation and eventually refinement is already done,
-	// and i want to copy that to factory just once (and maybe save/cache it)
-	//memcpy ( factory_mesh->GetTriangles() , Fabric->triangles , sizeof(csTriangle)*num_triangles );
-	printf( " setting up the mesh2... \n");	
-	Dynamics      = new Integrator ( Fabric );
-	num_vertices  = Fabric -> nverts;
-	vertices      = Fabric -> vertices;
-	texels        = fact_state-> GetTexels(); 
-	colors        = fact_state-> GetColors(); 
-	material      = fact_state->GetMaterialWrapper();
-	
-	SetupVertexBuffer();
-	mesh.num_triangles = Fabric -> ntris;
-	if ( mesh.triangles )  { delete[](mesh.triangles);  };
-	mesh.triangles     = Fabric -> triangles;  
-	mesh.vertex_fog = new G3DFogInfo[num_vertices];
-	//memcpy (mesh.triangles, triangles, sizeof(csTriangle)*num_triangles);
-	mesh.morph_factor = 0.0f;
-	mesh.num_vertices_pool = 1;
-	mesh.do_morph_texels = false;
-	mesh.do_morph_colors = false;
-	mesh.vertex_mode = G3DTriangleMesh::VM_WORLDSPACE;
-    
-    Dynamics->ComputeInitial();
-    Dynamics->ComputeInitial();
-    Dynamics->ComputeInitial();
-    Dynamics->ComputeInitial();  
-	
-	printf( " finished setting up the mesh... \n");	
-	
+  printf( " finished setting up the mesh... \n");	
+  
+  //printf( " summing diff %u + %u = %u \n ",(int)15 , (short)28 , ((int)15) + ((short)28) );
 };
 
 
 //-------------------MeshObject implementation
 //
 
-csStuffObject::csStuffObject(iMeshObjectFactory *fact) {
-printf("creator \n");
-factory = fact;
-vbufmgr=NULL;
-
-SCF_CONSTRUCT_IBASE(fact);
-SCF_CONSTRUCT_EMBEDDED_IBASE(scfiVertexBufferManagerClient);
-SCF_CONSTRUCT_EMBEDDED_IBASE(scfiObjectModel);
-SCF_CONSTRUCT_EMBEDDED_IBASE(scfiClothMeshState);
+csStuffObject::csStuffObject(iMeshObjectFactory *fact) 
+{
+  printf("creator \n");
+  factory = fact;
+  vbufmgr=NULL;
+  
+  SCF_CONSTRUCT_IBASE(fact);
+  SCF_CONSTRUCT_EMBEDDED_IBASE(scfiVertexBufferManagerClient);
+  SCF_CONSTRUCT_EMBEDDED_IBASE(scfiObjectModel);
+  SCF_CONSTRUCT_EMBEDDED_IBASE(scfiClothMeshState);
 };
 
 csStuffObject::~csStuffObject() 
 {
-	delete [] mesh.vertex_fog;
-	delete    Fabric;
-	delete    Dynamics;
+  delete [] mesh.vertex_fog;
+  delete    Fabric;
+  delete    Dynamics;
 };
 
-
-bool csStuffObject::DrawTest(iRenderView *rview, iMovable *movable) {
- //printf(" draw test \n");
-// SetupMesh();
-
+bool csStuffObject::DrawTest(iRenderView *rview, iMovable *movable) 
+{
+  //printf(" draw test \n");
+  // SetupMesh();
+  
   iCamera* camera = rview->GetCamera ();
   csReversibleTransform tr_o2c = camera->GetTransform ();
   if (!movable->IsFullTransformIdentity ())
     tr_o2c /= movable->GetFullTransform ();
-
   csVector3 radius;
   csSphere sphere;
   GetRadius (radius, sphere.GetCenter ());
@@ -185,21 +187,19 @@ bool csStuffObject::DrawTest(iRenderView *rview, iMovable *movable) {
   if (max_radius < radius.z) max_radius = radius.z;
   sphere.SetRadius (max_radius);
   
-   int clip_portal, clip_plane, clip_z_plane;
+  int clip_portal, clip_plane, clip_z_plane;
   if (rview->ClipBSphere (tr_o2c, sphere, clip_portal, clip_plane,
   	clip_z_plane) == false)
      {   return false; };
-
-
-
- iGraphics3D* g3d = rview->GetGraphics3D ();
+     
+  iGraphics3D* g3d = rview->GetGraphics3D ();
   g3d->SetObjectToCamera (&tr_o2c);
   mesh.clip_portal = clip_portal;
   mesh.clip_plane = clip_plane;
   mesh.clip_z_plane = clip_z_plane;
   mesh.do_mirror = camera->IsMirrored ();
-
-return true;
+     
+  return true;
 };
 
 void csStuffObject::GetRadius (csVector3& rad, csVector3& cent)
@@ -209,26 +209,27 @@ void csStuffObject::GetRadius (csVector3& rad, csVector3& cent)
 }
 
 
-bool csStuffObject::Draw(iRenderView *rview, iMovable*, csZBufMode zbufmode){
- //printf("       DRAW\n");
-	iGraphics3D* g3d = rview->GetGraphics3D ();
- if (!material)
-  {   csRef<iEngine> e = CS_QUERY_REGISTRY(Obj_Reg, iEngine);
-	  //csRef<iComponent> plugin ( CS_REQUEST_PLUGIN( CS_REQUEST_IMAGELOADER ) );
-	  csRef<iLoader> loader = CS_QUERY_REGISTRY ( Obj_Reg , iLoader  );
-	  iTextureManager* txtmgr = g3d->GetTextureManager ();
+bool csStuffObject::Draw(iRenderView *rview, iMovable*, csZBufMode zbufmode)
+{
+  //printf("       DRAW\n");
+  iGraphics3D* g3d = rview->GetGraphics3D ();
+  if (!material)
+  {
+    csRef<iEngine> e = CS_QUERY_REGISTRY(Obj_Reg, iEngine);
+    //csRef<iComponent> plugin ( CS_REQUEST_PLUGIN( CS_REQUEST_IMAGELOADER ) );
+    csRef<iLoader> loader = CS_QUERY_REGISTRY ( Obj_Reg , iLoader  );
+    iTextureManager* txtmgr = g3d->GetTextureManager ();
 
     printf ("INTERNAL ERROR: cloth used without material!\n");
-	  iTextureWrapper* txt = loader->LoadTexture ("spark",
+    iTextureWrapper* txt = loader->LoadTexture ("spark",    
   	"/lib/std/spark.png", CS_TEXTURE_3D, txtmgr, true);
-  if (txt == NULL)
-  {
-    printf( " no texture for u!! \n" );
-    return false;
-  }
-  material = e->GetMaterialList()->FindByName("spark");
-  
-  
+    if (txt == NULL)
+    {
+      printf( " no texture for u!! \n" );
+      return false;
+    }
+    material = e->GetMaterialList()->FindByName("spark");
+    
     return false;
   }
   iMaterialHandle* mat = material->GetMaterialHandle ();
@@ -237,7 +238,7 @@ bool csStuffObject::Draw(iRenderView *rview, iMovable*, csZBufMode zbufmode){
     printf ("INTERNAL ERROR: cloth used without valid material handle!\n");
     return false;
   }
-
+  
   if (vis_cb) if (!vis_cb->BeforeDrawing (this, rview)) return false;
   
   // Prepare for rendering.
@@ -249,35 +250,31 @@ bool csStuffObject::Draw(iRenderView *rview, iMovable*, csZBufMode zbufmode){
   mesh.use_vertex_color =true;
   mesh.mixmode = MixMode | CS_FX_GOURAUD;
   CS_ASSERT (!vbuf->IsLocked ());
-  vbufmgr->LockBuffer (vbuf,
-  	vertices, texels, colors, num_vertices, 0);
+  vbufmgr->LockBuffer (vbuf,vertices, texels, colors, num_vertices, 0);
   rview->CalculateFogMesh (g3d->GetObjectToCamera (), mesh);
   g3d->DrawTriangleMesh (mesh);
   vbufmgr->UnlockBuffer (vbuf);
-
-return true;
+  return true;
 };
 
 void csStuffObject::GetObjectBoundingBox (csBox3& bbox, int /*type*/)
-{
-  //SetupMesh ( NULL );
-	 SetupMesh();
-  bbox = object_bbox;
-  	printf("  exiting GOBB  \n");
+{  
+  SetupMesh();
+  bbox = object_bbox;  
 }
 
 
  iMeshObjectFactory* csStuffObject::GetFactory () const
  { return factory; };
- void csStuffObject::UpdateLighting (iLight **,int, iMovable *) {
-  SetupMesh();
-  	//printf(" exiting UpdateLighting  \n");
+ void csStuffObject::UpdateLighting (iLight **,int, iMovable *) 
+ {
+   SetupMesh();
  };
  void csStuffObject::SetVisibleCallback(iMeshObjectDrawCallback *cb) { vis_cb=cb; };
  iMeshObjectDrawCallback* csStuffObject::GetVisibleCallback () const { return vis_cb; };
  void csStuffObject::NextFrame (unsigned int ticks, const csVector3& /*pos*/)
  {
-     Dynamics->Update(ticks);
+   Dynamics->Update(ticks);
  };
  bool csStuffObject::WantToDie () const { return false; };
  void csStuffObject::HardTransform (const csReversibleTransform &) {};
@@ -285,46 +282,45 @@ void csStuffObject::GetObjectBoundingBox (csBox3& bbox, int /*type*/)
  bool csStuffObject::HitBeamOutline (const csVector3& start,
   const csVector3& end, csVector3& isect, float* pr)
     { 
-  csSegment3 seg (start, end);
-  int i, max = mesh.num_triangles;
-  csTriangle *tr = mesh.triangles;
-  csVector3 *vrt = vertices;
-  for (i = 0 ; i < max ; i++)
-  {
-    if (csIntersect3::IntersectTriangle (vrt[tr[i].a], vrt[tr[i].b],
+      csSegment3 seg (start, end);
+      int i, max = mesh.num_triangles;
+      csTriangle *tr = mesh.triangles;
+      csVector3 *vrt = vertices;
+      for (i = 0 ; i < max ; i++)
+      {
+        if (csIntersect3::IntersectTriangle (vrt[tr[i].a], vrt[tr[i].b],
     	vrt[tr[i].c], seg, isect))
-    {
-      if (pr) *pr = qsqrt (csSquaredDist::PointPoint (start, isect) /
+        {
+          if (pr) *pr = qsqrt (csSquaredDist::PointPoint (start, isect) /
 		csSquaredDist::PointPoint (start, end));
-
-      return true;
-    }
-  }
-  return false;
-	};
+          return true;
+        }
+      }
+      return false;
+    };
 	
  bool csStuffObject::HitBeamObject (const csVector3& start,
   const csVector3& end, csVector3& isect, float* pr)
   {
-	  // This is now closer to an outline hitting method. It will
-  // return as soon as it touches any triangle in the mesh, and
-  // will be a bit faster than its more accurate cousin (below).
-  
-  csSegment3 seg (start, end);
-  int i, max = mesh.num_triangles;
-  csTriangle *tr = mesh.triangles;
-  csVector3 *vrt = vertices;
-  for (i = 0 ; i < max ; i++)
-  {
-    if (csIntersect3::IntersectTriangle (vrt[tr[i].a], vrt[tr[i].b],
-    	vrt[tr[i].c], seg, isect))
+    // This is now closer to an outline hitting method. It will
+    // return as soon as it touches any triangle in the mesh, and
+    // will be a bit faster than its more accurate cousin (below).
+    
+    csSegment3 seg (start, end);
+    int i, max = mesh.num_triangles;
+    csTriangle *tr = mesh.triangles;
+    csVector3 *vrt = vertices;
+    for (i = 0 ; i < max ; i++)
     {
-      if (pr) *pr = qsqrt (csSquaredDist::PointPoint (start, isect) /
-		csSquaredDist::PointPoint (start, end));
-      return true;
+      if (csIntersect3::IntersectTriangle (vrt[tr[i].a], vrt[tr[i].b],
+    	vrt[tr[i].c], seg, isect))
+      {
+        if (pr) *pr = qsqrt (csSquaredDist::PointPoint (start, isect) /
+	csSquaredDist::PointPoint (start, end));
+        return true;
+      }
     }
-  }
-  return false;
+    return false;
   };
   
  void csStuffObject::SetLogicalParent (iBase *) {};
@@ -347,24 +343,24 @@ SCF_IMPLEMENT_EMBEDDED_IBASE(StuffFactory::csClothFactoryState)
   SCF_IMPLEMENTS_INTERFACE (iClothFactoryState)
 SCF_IMPLEMENT_EMBEDDED_IBASE_END
  
-StuffFactory::StuffFactory(iBase* parent) {
-SCF_CONSTRUCT_IBASE( parent )
-SCF_CONSTRUCT_EMBEDDED_IBASE(scfiClothFactoryState)	
-object_reg       = NULL;
-material         = NULL;		
-factory_vertices = NULL; 
-factory_texels   = NULL;
-factory_colors   = NULL;
-factory_triangles= NULL;
-};
+StuffFactory::StuffFactory(iBase* parent) 
+ {
+   SCF_CONSTRUCT_IBASE( parent )
+   SCF_CONSTRUCT_EMBEDDED_IBASE(scfiClothFactoryState)	
+   object_reg       = NULL;
+   material         = NULL;		
+   factory_vertices = NULL; 
+   factory_texels   = NULL;
+   factory_colors   = NULL;
+   factory_triangles= NULL;
+  };
 
 StuffFactory::~StuffFactory() 
 {
-	if (factory_vertices)  { delete [] factory_vertices; };
-	if (factory_texels)    { delete [] factory_texels;   };
-	if (factory_colors)    { delete [] factory_colors;   };
-	if (factory_triangles) { delete [] factory_triangles; };
-	
+  if (factory_vertices)  { delete [] factory_vertices; };
+  if (factory_texels)    { delete [] factory_texels;   };
+  if (factory_colors)    { delete [] factory_colors;   };
+  if (factory_triangles) { delete [] factory_triangles; };	
 }
 
 bool StuffFactory::Initialize(iObjectRegistry* iO_R)
