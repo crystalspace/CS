b5276624fdb42186996c28fcefd026d51805a7bc
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2003-02-01 14:07:17 +0000

    Second related fix to emit.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@12627 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/emit/object/emit.cpp
+++ plugins/mesh/emit/object/emit.cpp
@@ -764,6 +764,8 @@ void csEmitMeshObject::MoveAgeParticle (int i, int elapsed, float delta_t)
     col = between*belowage->color + invbet*aboveage->color;
   }
   // adjust the particle
+  // Jorrit: @@@ This fix is needed or we sometimes get a division by zero!
+  if (ABS (oldscale) < .0001) oldscale = 1;
   GetParticle(i)->ScaleBy(newscale / oldscale);
   GetParticle(i)->Rotate(rotspeed * delta_t);
   if(MixMode & CS_FX_ADD)
