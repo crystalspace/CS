ccb4bfec943dfd22400c3f74d1aabfc46a53e786
Eric Sunshine <sunshine@sunshineco.com>
2003-02-04 10:08:46 +0000

    Updated version number to 0.96.002.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/migrated/branches/R0_96@12694 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csver.h
+++ include/csver.h
@@ -30,9 +30,9 @@
 /// Major version
 #define CS_VERSION_MAJOR CS_VER_QUOTE(0.96)
 /// Minor version (release, or "dev" for CVS version)
-#define CS_VERSION_MINOR CS_VER_QUOTE(1)
+#define CS_VERSION_MINOR CS_VER_QUOTE(2)
 /// Date of release
-#define CS_RELEASE_DATE  CS_VER_QUOTE(Wed 29-Jan-2003)
+#define CS_RELEASE_DATE  CS_VER_QUOTE(Tue 4-Feb-2003)
 
 #define CS_VER_QUOTE(X) #X
 
