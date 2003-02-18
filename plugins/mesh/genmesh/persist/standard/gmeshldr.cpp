85891ba8ccf44d0b669d520312a6bd724a5d4995
Daniel Duhprey <duhprey@yahoo.com>
2003-02-18 06:50:36 +0000

    Removed itoa in trade for sprintf in gmeshldr
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@12954 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/genmesh/persist/standard/gmeshldr.cpp
+++ plugins/mesh/genmesh/persist/standard/gmeshldr.cpp
@@ -184,7 +184,7 @@ bool csGeneralFactoryLoader::ParseStream(iDocumentNode *node, iGeneralFactorySta
           char attribname[12];
           attribname[0] = 'f';
           attribname[1] = '\0';
-          itoa(i, &attribname[1], 10);
+          sprintf (&attribname[1], "%10d", i);
 
           floatarray[vertexindex * compcount + i] = child->GetAttributeValueAsFloat(attribname);
         }
@@ -212,7 +212,7 @@ bool csGeneralFactoryLoader::ParseStream(iDocumentNode *node, iGeneralFactorySta
           char attribname[12];
           attribname[0] = 'i';
           attribname[1] = '\0';
-          itoa(i, &attribname[1], 10);
+          sprintf(&attribname[1], "%10d", i);
 
           intarray[vertexindex * compcount + i] = child->GetAttributeValueAsInt(attribname);
         }
@@ -242,7 +242,7 @@ bool csGeneralFactoryLoader::ParseStream(iDocumentNode *node, iGeneralFactorySta
           char attribname[12];
           attribname[0] = 's';
           attribname[1] = '\0';
-          itoa(i, &attribname[1], 10);
+          sprintf (&attribname[1], "%10d", i);
 
           shortarray[vertexindex * compcount + i] = child->GetAttributeValueAsInt(attribname);
         }
@@ -272,7 +272,7 @@ bool csGeneralFactoryLoader::ParseStream(iDocumentNode *node, iGeneralFactorySta
           char attribname[12];
           attribname[0] = 'b';
           attribname[1] = '\0';
-          itoa(i, &attribname[1], 10);
+          sprintf (&attribname[1], "%10d", i);
 
           bytearray[vertexindex * compcount + i] = child->GetAttributeValueAsInt(attribname);
         }
