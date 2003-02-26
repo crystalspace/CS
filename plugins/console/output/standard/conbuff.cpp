f213579ac82ed73b74b4e200126f2142c0165ec1
Eric Sunshine <sunshine@sunshineco.com>
2003-02-26 07:24:02 +0000

    Eric Sunshine made the following changes to csConsoleBuffer:
    -*- Fixed bug: SetLength() was accessing uninitialized memory.
    
    -*- Fixed bugs: Several methods potentially were deleting the csString
        referenced by the `empty' member and other "empty" lines, thus
        allowing those other references to access a deleted object.
    
    -*- Fixed bug: SetCurLine() had bogus logic.
    
    -*- Fixed bugs: A significant number of boundary (one-off) errors were
        present.
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/migrated/branches/R0_96@13046 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/console/output/standard/conbuff.cpp
+++ plugins/console/output/standard/conbuff.cpp
@@ -25,10 +25,9 @@ csConsoleBuffer::csConsoleBuffer (int length, int size)
   buffer = NULL;
   SetLength (length);
   SetPageSize (size);
-  empty = new csString (" ");
-
-  // Initialize the state variables
-  current_line = display_top = 0;
+  empty = new csString ("");
+  current_line = 0;
+  display_top = 0;
   display_bottom = page_size;
 }
 
@@ -55,55 +54,39 @@ void csConsoleBuffer::NewLine (bool snap)
   if (current_line >= len)
   {
     // Delete the topmost line
-    delete buffer [0];
+    if (buffer [0] != empty)
+      delete buffer [0];
     // Shift all the lines up by one
-    memmove (buffer, &buffer [1], (len - 1) * sizeof (csString *));
+    memmove (buffer, &buffer + 1, (len - 1) * sizeof (csString *));
+    buffer [len - 1] = NULL;
     current_line = len - 1;
-    // We don't want the now second to last string deleted!
-    buffer [current_line] = NULL;
 
     // Invalidate all the lines now visible to scroll properly
-    int i;
-    for (i = display_top; i <= display_bottom; i++)
+    for (int i = display_top; i < display_bottom; i++)
       dirty [i] = true;
   }
 
   // Clear the new current line and mark it dirty
-  if (buffer [current_line] != NULL)
-  {
+  if (buffer [current_line] != empty)
     delete buffer [current_line];
-    buffer [current_line] = NULL;
-    dirty [current_line] = true;
-  }
+  buffer [current_line] = NULL;
+  dirty [current_line] = true;
 
   if (snap
    && ((current_line >= display_bottom)
-    || (current_line <= display_top)))
+    || (current_line < display_top)))
   {
-    display_top++;
-    display_bottom++;
+    display_bottom = current_line + 1;
+    display_top = display_bottom - page_size;
 
-    /*
-    display_top = current_line - page_size + 1;
-    if (display_top < 0)
-    {
-      display_top = 0;
-      display_bottom = page_size;
-    }
-    else
-      display_bottom = current_line + 1;
-    */
     // Invalidate all the lines now visible to scroll properly
-    int i;
-    for (i = display_top; i <= display_bottom; i++)
+    for (int i = display_top; i < display_bottom; i++)
       dirty [i] = true;
   }
-
 }
 
 csString *csConsoleBuffer::WriteLine ()
 {
-  // Prevent any NULL pointer ugliness
   if (buffer [current_line] == NULL
    || buffer [current_line] == empty)
     buffer [current_line] = new csString ();
@@ -113,56 +96,41 @@ csString *csConsoleBuffer::WriteLine ()
 
 const csString *csConsoleBuffer::GetLine (int line, bool *dirty_line)
 {
-  // Tsk tsk...
   if (line > display_bottom)
+  {
+    if (dirty_line) *dirty_line = false;
     return NULL;
-
-  // Calculate the position of the line
+  }
   int pos = display_top + line;
-
-  // Return the current dirty flag
   if (dirty_line)
     *dirty_line = dirty [pos];
-
-  // Clear the dirty flag
   dirty [pos] = false;
-
-  // Return the csString
   return buffer [pos];
 }
 
 void csConsoleBuffer::SetLength (int length)
 {
-  int i;
-
   if (buffer)
   {
-    // Clear out the current buffer and delete it
     Clear ();
     delete buffer;
     delete dirty;
   }
 
-  // Set up the new buffer
   len = length;
   buffer = new csString * [length];
   dirty = new bool [length];
 
-  // Clear out any stray values in the new array
-  for (i = 0; i < len; i++)
-    if (buffer [i])
-    {
-      buffer [i] = NULL;
-      dirty [i] = false;
-    }
+  for (int i = 0; i < len; i++)
+  {
+    buffer [i] = 0;
+    dirty [i] = false;
+  }
 }
 
 void csConsoleBuffer::Clear ()
 {
-  int i;
-
-  // Go through each line and delete it's csString
-  for (i = 0; i < len; i++)
+  for (int i = 0; i < len; i++)
     if (buffer [i])
     {
       if (buffer [i] != empty)
@@ -171,7 +139,6 @@ void csConsoleBuffer::Clear ()
       dirty [i] = true;
     }
 
-  // Reset the state variables
   current_line = display_top = 0;
   display_bottom = page_size;
 }
@@ -182,7 +149,7 @@ void csConsoleBuffer::SetPageSize (int size)
   display_bottom = display_top + size;
   if (current_line > display_bottom)
   {
-    display_bottom = current_line;
+    display_bottom = current_line + 1;
     display_top = display_bottom - size;
     if (display_top < 0)
     {
@@ -191,16 +158,6 @@ void csConsoleBuffer::SetPageSize (int size)
     }
   }
   page_size = size;
-  /*
-  display_top = current_line - size + 1;
-  if (display_top < 0)
-  {
-    display_top = 0;
-    display_bottom = page_size;
-  }
-  else
-    display_bottom = current_line + 1;
-  */
 }
 
 void csConsoleBuffer::SetTopLine (int line)
@@ -230,9 +187,8 @@ void csConsoleBuffer::SetTopLine (int line)
 
 void csConsoleBuffer::SetCurLine (int line)
 {
-  // Make sure we don't go past the end of the buffer
-  if (line > len)
-    line = len;
+  if (line >= len)
+    current_line = len - 1;
   else
     current_line = line;
 }
@@ -240,17 +196,14 @@ void csConsoleBuffer::SetCurLine (int line)
 void csConsoleBuffer::DeleteLine(int line)
 {
   int pos;
-
-  // Mix for disaster?
-  if (line <= display_bottom)
+  if (line < display_bottom)
     pos = display_top + line;
+  else if (line >= len)
+    pos = len - 1;
   else
     pos = line;
-
-  // Delete the given line
-  delete buffer [pos];
+  if (buffer [pos] != empty)
+    delete buffer [pos];
   buffer [pos] = NULL;
-
-  // Set the dirty flag
   dirty [pos] = true;
 }
