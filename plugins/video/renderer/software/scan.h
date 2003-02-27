5ba3310451a6d012d155f69fb95267cb029a49f2
Frank Richter <res@crystalspace3d.org>
2003-02-27 00:21:53 +0000

    - fixed the "shifted alpha map" bug in the software renderer.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@13066 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/renderer/software/scan.h
+++ plugins/video/renderer/software/scan.h
@@ -117,6 +117,10 @@ struct csScanSetup
   int tw2fp;
   /// Texture height in 16:16 fixed-point format - 1
   int th2fp;
+  /// the difference of U-coordinates between cached texture and alpha map
+  int amap_uofs;
+  /// the difference of V-coordinates between cached texture and alpha map
+  int amap_vofs;
 
   /// The unlighted texture bitmap.
   unsigned char *bitmap;
