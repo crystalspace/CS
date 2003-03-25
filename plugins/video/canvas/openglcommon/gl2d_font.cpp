6cf211b5904c6228e534a608e2b43b01e0f1d5a2
Eric Sunshine <sunshine@sunshineco.com>
2003-03-25 05:07:51 +0000

    Eric Sunshine applied a patch from Jochen Moench <jochen.moench@de.thalesgroup.com> to GLFontCache::Write() in gl2d_font.cpp to allow it to output the full range of 8-bit characters (by using unsigned rather than signed character indexes).  Also important, this patch prevents negative-indexed accesses of the glyphs[] array.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/migrated/branches/R0_96@13331 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/canvas/openglcommon/gl2d_font.cpp
+++ plugins/video/canvas/openglcommon/gl2d_font.cpp
@@ -355,14 +355,14 @@ void GLFontCache::Write (iFont *font, int x, int y, const char *text)
   float texheight = gs->texheight;
 
   GLGlyph *glyphs = gs->glyphs;
-  GLuint hLastTexture = glyphs [*text].hTexture - 1;
+  GLuint hLastTexture = glyphs [(unsigned char)(*text)].hTexture - 1;
 
   float x1 = 0.0;
 
   glBegin (GL_QUADS);
   for (; *text; ++text)
   {
-    GLGlyph &glyph = glyphs [*text];
+    GLGlyph &glyph = glyphs [(unsigned char)(*text)];
 
     float x_right, x2 = x_right = x1 + glyph.width;
     float tx1, tx2, ty1, ty2, y1, y2;
