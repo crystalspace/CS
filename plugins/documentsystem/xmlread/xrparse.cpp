308788024561eea13c341204273dcff84d860268
Jonathan Tarbox <jtarbox@sbcglobal.net>
2003-04-05 18:11:25 +0000

    adding Rhad's fix for xmlread asserts in MSVC
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@13482 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/documentsystem/xmlread/xrparse.cpp
+++ plugins/documentsystem/xmlread/xrparse.cpp
@@ -42,7 +42,7 @@ char* TrXmlBase::SkipWhiteSpace( char* p )
   {
     return 0;
   }
-  while ( isspace (*p)) p++;
+  while ( isspace ((unsigned char)*p)) p++;
   return p;
 }
 
@@ -52,7 +52,7 @@ const char* TrXmlBase::SkipWhiteSpace( const char* p )
   {
     return 0;
   }
-  while ( isspace (*p)) p++;
+  while ( isspace ((unsigned char)*p)) p++;
   return p;
 }
 
