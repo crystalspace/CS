3006607fd32380424d834c937b357eca2f839ab0
Matze Braun <matze@braunis.de>
2003-05-07 17:49:13 +0000

    - Matze did several changes to the jam project generation (the script             that creates a basic jambuildsystem for your external projects):             - Added distclean and maintainerclean rules after the latest               changes in CS.             - Made the script interactive instead of requiring lots of               commandline arguments. Now the script outputs detailed               desciptions too.             - Creates a basic README with some building and installation               notes now.             - Rewrote some parts of the script.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@13914 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- scripts/jamtemplate/createproject.sh
+++ scripts/jamtemplate/createproject.sh
@@ -1,16 +1,125 @@
 #!/bin/sh
 
+# Helper function to read variables
+function ReadValue()
+{
+    TEXT="$2"
+    VARNAME="$1"
+    echo -n "$TEXT"
+    read READVAL
+
+    export $VARNAME=$READVAL
+}
+
+function ReadValueStrict()
+{
+    TEXT="$2"
+    VARNAME="$1"
+    READVAL=""
+    
+    while test -z "$READVAL"; do
+	echo -n "$TEXT"
+	read READVAL
+	if test -z "$READVAL"; then
+	    echo "Please enter a value!"
+	fi
+    done
+
+    export $VARNAME="$READVAL"
+}
+
+function CheckYesNo()
+{
+    VARNAME="$1"
+    TEXT="$2"
+    DEFAULT="$3"
+    READOK=no
+    
+    while test "$READOK" = "no"; do
+        echo -n "$TEXT"
+	read READVAL       
+	
+	if test -z "$READVAL"; then
+	    export $VARNAME=$DEFAULT
+	    READOK=yes
+	fi
+	if test "$READVAL" = "yes"; then
+	    export $VARNAME=yes
+	    READOK=yes
+	fi
+	if test "$READVAL" = "no"; then
+	    export $VARNAME=no
+	    READOK=yes
+	fi
+	
+	if test "$READOK" = "no"; then
+	    echo 'Please enter "yes" or "no"!'
+	fi
+    done
+}
+
+# Part 1: Interaktive - Gather informations
+cat << __EOF__
+Crystal Space external Project Template
+
+This script will generate a basic Crystal Space project for you, complete
+with configuration(autoconf) and build system(jam). The project will be created
+in the current directory. If you don't want that press CTRL+C now!
+
+__EOF__
+
+cat << "__EOF__"
+**** Project
+You should choose a projectname now. We need a shortform of the name which
+will be used in directory and filenames. The shortname should be lowercase not
+must not contain any special chars or spaces. The long name will be present in
+helptexts and README files. So If you project is called "Space Fighters -
+Revenge for $$$" then you should probably choose shortname spacefighters and
+"Space Fighters - Revenge for $$$" as longname.
+You may provide a Homepage address for the project. (Please specify a complete
+URI. Example: "http://www.spacefighter.org")
+
+__EOF__
+ReadValueStrict PROJECTNAME "Shortname: "
+ReadValueStrict LONGNAME "Longname: "
+ReadValue HOMEPAGE "Homepage: "
+
+cat << __EOF__
+
+**** Contact
+The informations about the author are mentioned in the README file and in the
+configuration scripts so that people see a support address when typing
+"configure --help".
+
+__EOF__
+ReadValueStrict AUTHOR "Author: "
+ReadValueStrict EMAIL "E-Mail: "
+
+cat << __EOF__
+
+**** Fine Tuning
+ok.
+__EOF__
+#CheckYesNo KDEVELOP "Do you want me to generate a kdevelop projectfile?[no] " "no"
+
+echo ""
+echo "*Creating $PROJECTNAME Project."
+
 TEMPLATEDIR=`dirname $0`
 CSDIR="$TEMPLATEDIR/../.."
-PROJECTNAME="$1"
-EMAIL="$2"
-if [ -z "$EMAIL" ]; then
-  echo "Please invoke this script with a name for your project and an email"
-  echo "address of the author."
-  exit 1
-fi
 
-echo Creating package
+function SetupFromTemplate
+{
+    SOURCE="$1"
+    TARGET="$2"
+
+# XXX We should somehow quote here...
+    cat "$1" | sed -e "s^#PROJECTNAME#^$PROJECTNAME^g" \
+	     | sed -e "s^#EMAIL#^$EMAIL^g" \
+	     | sed -e "s^#HOMEPAGE#^$HOMEPAGE^g" \
+	     | sed -e "s^#LONGNAME#^$LONGNAME^g" \
+	     | sed -e "s^#AUTHOR#^$AUTHOR^g" > "$2"
+}
 
 # create directories
 mkdir "$PROJECTNAME" || exit 2
@@ -25,7 +134,8 @@ cp -pr "$CSDIR/mk/autoconf" "$PROJECTNAME/mk"
 # copy templatefiles
 cp -p "$TEMPLATEDIR/autogen.template" "$PROJECTNAME/autogen.sh"
 chmod +x "$PROJECTNAME/autogen.sh"
-cat "$TEMPLATEDIR/configure.template" | sed -e "s/#PROJECTNAME#/$PROJECTNAME/g" -e "s/#EMAIL#/$EMAIL/g" > "$PROJECTNAME/configure.ac"
+SetupFromTemplate "$TEMPLATEDIR/configure.template" "$PROJECTNAME/configure.ac"
+SetupFromTemplate "$TEMPLATEDIR/README.template" "$PROJECTNAME/README"
 cp -p "$TEMPLATEDIR/Jamfile.template" "$PROJECTNAME/Jamfile"
 cp -p "$TEMPLATEDIR/Jamrules.template" "$PROJECTNAME/Jamrules"
 cp -p "$TEMPLATEDIR/initjamfile.m4" "$PROJECTNAME/mk/autoconf"
@@ -60,3 +170,13 @@ echo Running autoconf
 cd "$PROJECTNAME"
 ./autogen.sh
 
+# Display notice
+cat << "__EOF__"
+
+*Generation successfull.
+
+You should now look at your project and try to customize it at your needs. Esp.
+the README file only contains some generic blabla now.
+
+Have fun with Crystal Space.
+__EOF__
