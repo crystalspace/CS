3336bcd6d80d79f167e1e3ed11d94688c2e973d1
Frank Richter <res@crystalspace3d.org>
2003-05-14 22:59:38 +0000

    - added a base class for texture factories.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@13991 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ libs/cstool/basetexfact.cpp
@@ -0,0 +1,51 @@
+/*
+    Copyright (C) 2003 by Jorrit Tyberghein
+	      (C) 2003 by Frank Richter
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+
+#include "cstool/basetexfact.h"
+
+SCF_IMPLEMENT_IBASE (csBaseTextureFactory)
+  SCF_IMPLEMENTS_INTERFACE (iTextureFactory)
+SCF_IMPLEMENT_IBASE_END
+
+csBaseTextureFactory::csBaseTextureFactory (iBase* parent, 
+					    iObjectRegistry* object_reg)
+{
+  SCF_CONSTRUCT_IBASE (parent);
+  
+  csBaseTextureFactory::object_reg = object_reg;
+  
+  width = 128;
+  height = 128;
+}
+
+csBaseTextureFactory::~csBaseTextureFactory ()
+{
+}
+
+void csBaseTextureFactory::SetSize (int w, int h)
+{
+  width = w; height = h;
+}
+
+void csBaseTextureFactory::GetSize (int& w, int& h)
+{
+  w = width; h = height;
+}
