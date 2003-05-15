4b6284270d9fcada8e090df0a907f3e07dc1db8d
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2003-05-15 13:26:27 +0000

    Jorrit fixed a bug he introduced in his last commit with the iso map loader.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@14002 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/isoldr/isoload.cpp
+++ plugins/isoldr/isoload.cpp
@@ -150,6 +150,8 @@ public:
   virtual iMeshWrapper* FindMeshObject (const char* name);
   virtual iTextureWrapper* FindTexture (const char* name);
   virtual iLight *FindLight (const char* name);
+  virtual bool CheckDupes () const { return false; }
+  virtual bool CurrentRegionOnly () const { return false; }
 };
 
 SCF_IMPLEMENT_IBASE(StdIsoLoaderContext);
