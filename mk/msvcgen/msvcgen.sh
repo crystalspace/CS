871ff2d479e58fa08ad40d3bf3976772dfc39828
Matze Braun <matze@braunis.de>
2003-05-22 16:53:04 +0000

    improved msvcgen.sh script
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@14052 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- mk/msvcgen/msvcgen.sh
+++ mk/msvcgen/msvcgen.sh
@@ -1,6 +1,31 @@
 #!/bin/sh
 # Use this script to generate msvc project files from the Jamfiles
 
-jam $@ -sJAMCONFIG=mk/msvcgen/Jamconfig -sMSVC_VERSION=7 msvcgen
-jam $@ -sJAMCONFIG=mk/msvcgen/Jamconfig -sMSVC_VERSION=6 msvcgen
+if test "$1" = "--help"; then
+    echo "  mk/msvcgen/msvcgen.sh [6|7]"
+    echo "Generates msvc projectfiles. You may specify version 6 or 7 if you"
+    echo "only want to generate for a specific version (default is both)"
+    exit 0
+fi
 
+DO_MSVC6=no
+DO_MSVC7=no
+if test "$1" = 7; then
+    DO_MSVC7=yes
+    shift
+fi
+if test "$1" = 6; then
+    DO_MSVC6=yes
+    shift
+fi
+if test "$DO_MSVC6" = no -a "$DO_MSVC7" = no; then
+    DO_MSVC6=yes
+    DO_MSVC7=yes
+fi
+
+if test "$DO_MSVC6" = yes; then
+    jam $@ -sJAMCONFIG=mk/msvcgen/Jamconfig -sMSVC_VERSION=6 msvcgen
+fi
+if test "$DO_MSVC7" = yes; then
+    jam $@ -sJAMCONFIG=mk/msvcgen/Jamconfig -sMSVC_VERSION=7 msvcgen
+fi
