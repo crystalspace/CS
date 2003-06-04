29e322d2800e44fd351f89379b0d1fc22f92f23b
Frank Richter <res@crystalspace3d.org>
2003-06-04 12:23:10 +0000

    - updated some render loop interfaces
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@14231 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/iengine/renderloop.h
+++ include/iengine/renderloop.h
@@ -22,6 +22,7 @@
 #define __CS_IENGINE_RENDERLOOP_H__
 
 /**\file
+ * Render loop interfaces.
  */
 /**
  * \addtogroup engine3d
@@ -34,6 +35,11 @@ struct iClipper2D;
 struct iSector;
 class csRenderView;
 
+/**
+ * Name of the default render loop created by the engine.
+ */
+#define CS_DEFAULT_RENDERLOOP_NAME	"*default"
+
 SCF_VERSION (iRenderStep, 0, 0, 1);
 
 struct iRenderStep : public iBase
@@ -42,11 +48,58 @@ struct iRenderStep : public iBase
   virtual void Perform (csRenderView* rview, iSector* sector) = 0;
 };
  
-SCF_VERSION (iRenderLoop, 0, 0, 1);
+SCF_VERSION (iRenderLoop, 0, 0, 2);
 
 struct iRenderLoop : public iBase
 {
   virtual void Draw (iCamera* c, iClipper2D* clipper) = 0;
+};
+
+SCF_VERSION (iRenderLoopManager, 0, 0, 1);
+
+/**
+ * Render loop manager.
+ * Use to create new loops and manage loop names.
+ * \remark It's not recommended to unregister the loop with the name of
+ * #CS_DEFAULT_RENDERLOOP_NAME.
+ */
+struct iRenderLoopManager : public iBase
+{
+  /**
+   * Create a new render loop.
+   * \remark This render loop is "unnamed". To name it, use Register().
+   */
+  virtual iRenderLoop* Create () = 0;
+  
+  /**
+   * Associate a name with a renderloop.
+   * One name is associated with one render loop. If you try to register
+   * a loop with a name that is already used, Register() will fail.
+   * \param name Name the render loop is registered with.
+   * \param loop The render loop.
+   * \return Whether the loop could be registered with the name. Fails if
+   *  either the name is already used or the loop is already registered.
+   */
+  virtual bool Register (const char* name, iRenderLoop* loop) = 0;
+  /**
+   * Get the render loop associated with the name.
+   * \param name Name for which the renderloop is to be retrieved.
+   * \return The render loop associated with the name, or 0 when no loop
+   *  is registered with that name.
+   */
+  virtual iRenderLoop* Retrieve (const char* name) = 0;
+  /**
+   * Get the name asociated to the render loop.
+   * \param loop Render loop which associated name is to be retrieved.
+   * \return Name of the loop. 0 if the loop isn't registered.
+   */
+  virtual const char* GetName (iRenderLoop* loop) = 0;
+  /**
+   * Remove an association between a name and a render loop.
+   * \param loop Render loop which associated name should be removed.
+   * \return Whether the association was successfully removed.
+   */
+  virtual bool Unregister (iRenderLoop* loop) = 0;
 };
  
 /**@}*/
