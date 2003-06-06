d6a1c4651810cfbf7b1920e10dec388ef72792c2
Eric Sunshine <sunshine@sunshineco.com>
2003-06-06 00:22:23 +0000

    Added implmentation note to comments in win32.jam and cssys/win32/mkdef.sh.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@14275 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/cssys/win32/mkdef.sh
+++ libs/cssys/win32/mkdef.sh
@@ -7,6 +7,11 @@
 infile=$1
 outfile=$2
 
+# Implementation note: Do not change the layout/formatting of the sed
+# invocation.  It is purposely split over multiple lines (using backslash +
+# newline) in order to ensure that the entries in the generated .def file are
+# each placed on a separate line.
+
 echo "EXPORTS" > $outfile
 echo "  plugin_compiler" >> $outfile
 sed '/<implementation>/!d;s:[ 	]*<implementation>\(..*\)</implementation>:  \1_scfInitialize\
