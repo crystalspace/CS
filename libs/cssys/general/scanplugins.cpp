071a5d5f32a5d1f7827c9f5066e8069b7e5e5ca4
Frank Richter <res@crystalspace3d.org>
2003-06-14 23:32:42 +0000

    Explanation for ugly fopen() hack.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@14371 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/cssys/general/scanplugins.cpp
+++ libs/cssys/general/scanplugins.cpp
@@ -49,6 +49,9 @@ csRef<iStrVector> csScanPluginsDir (const char* dir,
   plugins.AttachNew (new scfStrVector ());
   metadata.DeleteAll ();
     
+  csRef<iDocumentSystem> docsys (
+    csPtr<iDocumentSystem> (new csTinyDocumentSystem ()));
+  
   struct dirent* de;
   DIR* dh = opendir(dir);
   if (dh != 0)
@@ -66,7 +69,32 @@ csRef<iStrVector> csScanPluginsDir (const char* dir,
 
           FILE* file = fopen(scffilepath , "rb");          
 
-          // fopen HACK: in Lin
+          // fopen HACK:
+	  /*
+	     [res] 
+	      One user (Charlls) got weird linking errors. This hack is here
+	      to have the stuff working at least, until the problem is 
+	      properly sorted out.
+		g++ -L/usr/local/lib -lpthread  -Lout/UNIX/X86/optimize  -o 
+		  csdemo out/UNIX/X86/optimize/apps/demo/demo.o 
+		  out/UNIX/X86/optimize/apps/demo/demoldr.o 
+		  out/UNIX/X86/optimize/apps/demo/demoop.o 
+		  out/UNIX/X86/optimize/apps/demo/demoseq.o -lcsgfx -lcsutil 
+		  -lcstool -lcssys -lcsgeom -lcsutil -lcssys -ldl -lm -lnsl
+		out/UNIX/X86/optimize/libcssys.a(scanplugins.o)(.text+0x231): 
+		  In function `csScanPluginsDir(char const*, 
+		  csRef<iStrVector>&, csRefArray<iDocument>&)':
+		: undefined reference to 
+		  `csPhysicalFile::csPhysicalFile[in-charge](char const*, 
+		  char const*)'
+		out/UNIX/X86/optimize/libcssys.a(scanplugins.o)(.text+0x310): 
+		  In function `csScanPluginsDir(char const*, 
+		  csRef<iStrVector>&, csRefArray<iDocument>&)':
+		  : undefined reference to `csPhysicalFile::~csPhysicalFile 
+		  [in-charge]()'
+		collect2: ld returned 1 exit status
+		make[1]: *** [csdemo] Error 1
+	   */
 
           fseek( file , 0 , SEEK_END );
           int i = ftell( file );
@@ -75,14 +103,16 @@ csRef<iStrVector> csScanPluginsDir (const char* dir,
           char*  Buffer = new char [ i+1 ];
           Buffer[ i ]='\0';
           fread( Buffer , 1 , i , file );                
-
+	  
+	  fclose (file);
+	  
           // End of fopen HACK          
-
-	  //csTinyDocumentSystem docsys;
-          csRef<iDocumentSystem> docsys (csPtr<iDocumentSystem> (new csTinyDocumentSystem ()));
 
 	  csRef<iDocument> doc = docsys->CreateDocument();
 	  char const* errmsg = doc->Parse( Buffer );
+	  
+	  delete[] Buffer;
+	  
 	  if (errmsg == 0)
 	  {
 	    metadata.Push (doc);
