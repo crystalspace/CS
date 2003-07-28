e4412848f9f2d916d65eae7e85bd2a775cf3a657
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2003-07-28 20:02:57 +0000

    - Jorrit added iStringArray. This is an SCF interface for 	  csStringArray. csStringArray does not directly implement this 	  interface though. Instead there is scfStringArray which embeds 	  an csStringArray and implements iStringArray. This is similar 	  to iStrVector and the idea is that iStringArray will completely 	  replace iStrVector.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@14821 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/csutil/scfstringarray.h
@@ -0,0 +1,104 @@
+/*
+    Crystal Space String Array SCF interface
+    Copyright (C) 2003 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_SCFSTRINGARRAY_H__
+#define __CS_SCFSTRINGARRAY_H__
+
+#include "iutil/stringarray.h"
+#include "csutil/stringarray.h"
+
+/// This class is a thin wrapper around csStringArray with SCF capability
+class scfStringArray : public iStringArray
+{
+  csStringArray v;
+
+public:
+  SCF_DECLARE_IBASE;
+
+  /// Create a iStrVector from scratch.
+  scfStringArray (int iLimit = 16, int iDelta = 16) : v (iLimit, iDelta)
+  { SCF_CONSTRUCT_IBASE (0); }
+
+  /// Destructor - nothing to do.
+  virtual ~scfStringArray ()
+  { }
+
+  /// Query array length.
+  virtual int Length () const
+  {
+    return v.Length ();
+  }
+
+  /// Push a string onto the stack.
+  virtual void Push (char const *value)
+  {
+    v.Push (value);
+  }
+
+  /// Pop a string from the top of stack.
+  virtual char *Pop ()
+  {
+    return v.Pop ();
+  }
+
+  /// Get Nth string in vector.
+  virtual char const *Get (int n) const
+  {
+    return v.Get (n);
+  }
+
+  /// Find index of given string.
+  virtual int Find (const char *value) const
+  {
+    return v.Find (value);
+  }
+
+  /// Find index of a string in a pre-sorted string array.
+  virtual int FindSortedKey (void *key) const
+  {
+    return v.FindSortedKey (key);
+  }
+
+  /// Sort the string array.
+  virtual void Sort ()
+  {
+    v.Sort ();
+  }
+
+  /// Delete Nth string in the array.
+  virtual bool Delete (int n)
+  {
+    return v.Delete (n);
+  }
+
+  /// Insert a string before Nth string in the array.
+  virtual bool Insert (int n, char const *value)
+  {
+    return v.Insert (n, value);
+  }
+
+  /// Delete all strings in array.
+  virtual void DeleteAll ()
+  {
+    v.DeleteAll ();
+  }
+};
+
+#endif // __CS_SCFSTRINGARRAY_H__
+
---
+++ include/iutil/stringarray.h
@@ -0,0 +1,69 @@
+/*
+    Crystal Space String Array SCF interface
+    Copyright (C) 2003 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_IUTIL_STRINGARRAY_H__
+#define __CS_IUTIL_STRINGARRAY_H__
+
+#include "csutil/scf.h"
+
+/**\file
+ * String Array interface
+ */
+/**\addtogroup util
+ * @{ */
+SCF_VERSION (iStringArray, 0, 0, 1);
+
+/// This is an SCF-compatible interface for csStringArray.
+struct iStringArray : public iBase
+{
+  /// Query array length.
+  virtual int Length () const = 0;
+
+  /// Push a string onto the stack.
+  virtual void Push (const char *value) = 0;
+
+  /// Pop a string from the top of stack.
+  virtual char *Pop () = 0;
+
+  /// Get Nth string in vector.
+  virtual char const *Get (int n) const = 0;
+
+  /// Find index of given string.
+  virtual int Find (const char *value) const = 0;
+
+  /// Find index of a string in a pre-sorted string array.
+  virtual int FindSortedKey (void *key) const = 0;
+
+  /// Sort the string array.
+  virtual void Sort () = 0;
+
+  /// Delete Nth string in the array.
+  virtual bool Delete (int n) = 0;
+
+  /// Insert a string before Nth string in the array.
+  virtual bool Insert (int n, char const *value) = 0;
+
+  /// Delete all strings in array.
+  virtual void DeleteAll () = 0;
+};
+
+/** @} */
+
+#endif // __CS_IUTIL_STRINGARRAY_H__
+
