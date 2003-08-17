0becd9b8e344ee74f88ccba8398d0a5b62e0a29d
Eric Sunshine <sunshine@sunshineco.com>
2003-08-17 06:07:13 +0000

    Eric Sunshine moved an MSVC6 specialization (added by Boyan) out of csutil/array.h and into cssys/win32/csosdefs.h where it belongs.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@15069 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/cssys/win32/csosdefs.h
+++ include/cssys/win32/csosdefs.h
@@ -21,37 +21,38 @@
 #define __CS_CSOSDEFS_H__
 
 #if defined(COMP_VC)
-  #pragma warning(disable:4291)   // no matching operator delete found
-  #pragma warning(disable:4244)   // conversion from 'double' to 'float'
-  #pragma warning(disable:4305)   // conversion from 'const double' to 'float'
-  #pragma warning(disable:4018)   // Signed unsigned warnings
-  #pragma warning(disable:4245)   // Signed unsigned warnings
-  #pragma warning(disable:4805)   // unsafe mix of bool and int.
-  #pragma warning(disable:4800)   // Forcing value to bool
-  #pragma warning(disable:4514)   // Removal of unreferenced inline function
-  #pragma warning(disable:4097)   // use of xxx as synonym for a classname
-  #pragma warning(disable:4127)   // conditional expression is constant
-  #pragma warning(disable:4189)   // local variable is intialized but not referenced
-  #pragma warning(disable:4706)   // Assignment in conditional expression
-  #pragma warning(disable:4611)   // interaction between _setjmp and C++ destructors not portable
-  #pragma warning(disable:4710)   // function not inlined
-  #pragma warning(disable:4201)   // structure/ union without name. (Only relevant on MSVC 5.0)
-  #pragma warning(disable:4702)   // Unreachable Code
-  #pragma warning(disable:4512)   // Could not generate assignment operator
-  #pragma warning(disable:4100)   // Use of void* as a formal function parameter
-  #pragma warning(disable:4390)   // Empty control statement
-  #pragma warning(disable:4514)   // Removed unreferenced inlines. Only MSVC 6 relevant.
-  #pragma warning(disable:4505)   // 'function' : unreferenced local function has been removed
-  #pragma warning(disable:4146)   // unary minus operator applied to unsigned type, result still unsigned
-  #pragma warning(disable:4701)	  // local variable 'name' may be used without having been initialized
-  #pragma warning(disable:4355)   // 'this' : used in base member initializer list
-  #pragma warning(disable:4102)   // 'label' : unreferenced label
-  #pragma warning(disable:4005)   // 'identifier' : macro redefinition
-  #pragma warning(disable:4130)   // 'operator' : logical operation on address of string constant
-  #pragma warning(disable:4711)   // function 'function' selected for inline expansion
-  #pragma warning(disable:4312)	  // 'variable' : conversion from 'type' to 'type' of greater size
 
+  #pragma warning(disable:4018)   // Signed unsigned warnings
+  #pragma warning(disable:4097)   // use of xxx as synonym for a classname
+  #pragma warning(disable:4100)   // Use of void* as a formal function parameter
+  #pragma warning(disable:4102)   // 'label' : unreferenced label
+  #pragma warning(disable:4127)   // conditional expression is constant
+  #pragma warning(disable:4130)   // 'operator' : logical operation on address of string constant
+  #pragma warning(disable:4146)   // unary minus operator applied to unsigned type, result still unsigned
+  #pragma warning(disable:4189)   // local variable is intialized but not referenced
+  #pragma warning(disable:4201)   // structure/ union without name. (Only relevant on MSVC 5.0)
+  #pragma warning(disable:4244)   // conversion from 'double' to 'float'
+  #pragma warning(disable:4245)   // Signed unsigned warnings
   #pragma warning(default:4265)   // class has virtual functions, but destructor is not virtual
+  #pragma warning(disable:4005)   // 'identifier' : macro redefinition
+  #pragma warning(disable:4291)   // no matching operator delete found
+  #pragma warning(disable:4305)   // conversion from 'const double' to 'float'
+  #pragma warning(disable:4312)	  // 'variable' : conversion from 'type' to 'type' of greater size
+  #pragma warning(disable:4355)   // 'this' : used in base member initializer list
+  #pragma warning(disable:4390)   // Empty control statement
+  #pragma warning(disable:4505)   // 'function' : unreferenced local function has been removed
+  #pragma warning(disable:4512)   // Could not generate assignment operator
+  #pragma warning(disable:4514)   // Removal of unreferenced inline function
+  #pragma warning(disable:4514)   // Removed unreferenced inlines. Only MSVC 6 relevant.
+  #pragma warning(disable:4611)   // interaction between _setjmp and C++ destructors not portable
+  #pragma warning(disable:4701)	  // local variable 'name' may be used without having been initialized
+  #pragma warning(disable:4702)   // Unreachable Code
+  #pragma warning(disable:4706)   // Assignment in conditional expression
+  #pragma warning(disable:4710)   // function not inlined
+  #pragma warning(disable:4711)   // function 'function' selected for inline expansion
+  #pragma warning(disable:4786)   // identifier was truncated to '255' characters in the browser information (VC6)
+  #pragma warning(disable:4800)   // Forcing value to bool
+  #pragma warning(disable:4805)   // unsafe mix of bool and int.
 
   #pragma inline_depth (255)
   #pragma inline_recursion (on)
