1c4e855d985d747feb18085e02abca0ffd34fbe6
Mathew Sutcliffe <oktal@users.sourceforge.net>
2003-08-24 22:12:37 +0000

    Added stuff to Jamfiles and code to cssys to enable .csplugin meta data to be stored in the headers of the plugin files themselves, on GNU systems.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@15124 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ libs/cssys/general/bfdplugins.cpp
@@ -0,0 +1,222 @@
+/*
+    Load plugin meta data from object file headers.
+    Copyright (C) 2003 by Jorrit Tyberghein
+	      (C) 2003 by Eric Sunshine
+	      (C) 2003 by Frank Richter
+	      (C) 2003 by Mat Sutcliffe
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#define CS_SYSDEF_PROVIDE_DIR
+#include "cssysdef.h"
+#include "cssys/csshlib.h"
+#include "cssys/sysfunc.h"
+#include "csutil/csstring.h"
+#include "csutil/scfstringarray.h"
+#include "csutil/physfile.h"
+#include "csutil/scfstr.h"
+#include "csutil/util.h"
+#include "csutil/xmltiny.h"
+#include "iutil/document.h"
+
+#include <string.h>
+#include <bfd.h>
+
+static void AppendStrVecString (iStringArray*& strings, const char* str)
+{
+  if (!strings)
+  {
+    strings = new scfStringArray ();
+  }
+  strings->Push (csStrNew (str));
+}
+
+csRef<iString> csGetPluginMetadata (const char* fullPath, 
+				    csRef<iDocument>& metadata)
+{
+  iString* result = 0;
+
+  if (!metadata)
+  {
+    /*
+      TinyXML documents hold references to the document system.
+      So we have to create a new csTinyDocumentSystem (). Using just
+      'csTinyDocumentSystem docsys' would result in a crash when the
+      documents try to DecRef() to already destructed document system.
+     */
+    csRef<iDocumentSystem> docsys = csPtr<iDocumentSystem>
+      (new csTinyDocumentSystem ());
+    metadata = docsys->CreateDocument ();
+  }
+  char const* errmsg;
+
+  int len = strlen (fullPath);
+  if (len >= 3 && strcasecmp (fullPath + len - 3, ".so") == 0)
+  {
+    static bool init = true;
+    if (init)
+    {
+      bfd_init ();
+      init = false;
+    }
+
+    bfd *abfd = bfd_openr (fullPath, 0);
+    if (! abfd) errmsg = "libbfd can't open file";
+    else
+    {
+      if (! bfd_check_format (abfd, bfd_object))
+        errmsg = "libbfd can't parse file";
+      else
+      {
+        asection *sect = bfd_get_section_by_name (abfd, ".crystal");
+        if (! sect) errmsg = "libbfd can't find '.crystal' section";
+        else
+        {
+          int size = bfd_section_size (abfd, sect);
+          char *buf = (char *) malloc (size + 1);
+          if (! buf) errmsg = "can't allocate memory for metadata";
+          else if (! bfd_get_section_contents (abfd, sect, buf, 0, size))
+            errmsg = "libbfd can't get section contents";
+          else
+          {
+            buf[size] = 0;
+            errmsg = metadata->Parse (buf);
+            free (buf);
+          }
+        }
+      }
+      bfd_close (abfd);
+    }
+  }
+  else
+  {
+    csPhysicalFile file (fullPath, "rb");
+    errmsg = metadata->Parse (&file);
+  }
+  if (errmsg != 0)
+  {
+    metadata = 0;
+
+    csString errstr;
+    errstr.Format ("Error parsing metadata from %s: %s",
+      fullPath, errmsg);
+
+    result = new scfString (errstr);
+  }
+
+  return csPtr<iString> (result);
+}
+  
+// Scan a directory for .csplugin files
+void InternalScanPluginDir (iStringArray*& messages,
+			    const char* dir, 
+			    csRef<iStringArray>& plugins,
+			    bool recursive)
+{
+  struct dirent* de;
+  DIR* dh = opendir(dir);
+  if (dh != 0)
+  {
+    while ((de = readdir(dh)) != 0)
+    {
+      if (!isdir(dir, de))
+      {
+        int const n = strlen(de->d_name);
+        if ((n >= 9 && strcasecmp(de->d_name + n - 9, ".csplugin") == 0)
+         || (n >= 3 && strcasecmp(de->d_name + n - 3, ".so") == 0))
+        {
+	  csString scffilepath;
+	  scffilepath << dir << PATH_SEPARATOR << de->d_name;
+	  
+	  plugins->Push (csStrNew (scffilepath));
+        }
+        else
+	{
+	  if (recursive && (strcmp (de->d_name, ".") != 0)
+	    && (strcmp (de->d_name, "..") != 0))
+	  {
+	    iStringArray* subdirMessages = 0;
+	    
+	    csString scffilepath;
+	    scffilepath << dir << PATH_SEPARATOR << de->d_name;
+	  
+	    InternalScanPluginDir (subdirMessages, scffilepath,
+	      plugins, recursive);
+  
+	    if (subdirMessages != 0)
+	    {
+	      for (int i = 0; i < subdirMessages->Length(); i++)
+	      {
+		AppendStrVecString (messages, subdirMessages->Get (i));
+	      }
+	      subdirMessages->DecRef();
+	    }
+	  }
+	}
+      }
+    }
+    closedir(dh);
+  }
+}
+
+csRef<iStringArray> csScanPluginDir (const char* dir, 
+				   csRef<iStringArray>& plugins,
+				   bool recursive)
+{
+  iStringArray* messages = 0;
+
+  if (!plugins)
+    plugins.AttachNew (new scfStringArray ());
+
+  InternalScanPluginDir (messages, dir, plugins, 
+    recursive);
+	 
+  return csPtr<iStringArray> (messages);
+}
+
+csRef<iStringArray> csScanPluginDirs (csPluginPaths* dirs, 
+				    csRef<iStringArray>& plugins)
+{
+  iStringArray* messages = 0;
+
+  if (!plugins)
+    plugins.AttachNew (new scfStringArray ());
+
+  for (int i = 0; i < dirs->GetCount (); i++)
+  {
+    iStringArray* dirMessages = 0;
+    InternalScanPluginDir (dirMessages, (*dirs)[i].path, plugins, 
+      (*dirs)[i].scanRecursive);
+    
+    if (dirMessages != 0)
+    {
+      csString tmp;
+      tmp.Format ("The following error(s) occured while scanning '%s':",
+	(*dirs)[i].path);
+
+      AppendStrVecString (messages, tmp);
+
+      for (int i = 0; i < dirMessages->Length(); i++)
+      {
+	tmp.Format (" %s", dirMessages->Get (i));
+	AppendStrVecString (messages, tmp);
+      }
+      dirMessages->DecRef();
+    }
+  }
+	 
+  return csPtr<iStringArray> (messages);
+}
