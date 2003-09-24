5701c6667084f932d56dcf3e8133fa2c2d8eb01a
Eric Sunshine <sunshine@sunshineco.com>
2003-09-24 15:03:18 +0000

    Eric Sunshine eliminated a slew of compilation warnings from rgbpixel.h reported by MSVC regarding implicit conversion from int to unsigned char. Also eliminated extremely dangerous code which assumed 32-bit architecture.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@15495 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csgfx/rgbpixel.h
+++ include/csgfx/rgbpixel.h
@@ -82,7 +82,10 @@ struct csRGBcolor
   { return !operator == (c); }
   /// add two csRGBcolors
   csRGBcolor operator + (const csRGBcolor& c) const
-  { return csRGBcolor (c.red + red, c.green + green, c.blue + blue); }
+  { return csRGBcolor (
+    (unsigned char)c.red + red,
+    (unsigned char)c.green + green,
+    (unsigned char)c.blue + blue); }
 } CS_STRUCT_ALIGN_4BYTE_END;
 
 // As an optimization, we sometimes handle R/G/B values simultaneously.
@@ -103,25 +106,25 @@ struct csRGBpixel
   /// The red, green, blue and alpha components
   unsigned char red, green, blue, alpha;
   /// Constructor (initialize to zero, alpha to 255)
-  csRGBpixel () /* : red(0), green(0), blue(0), alpha(255) {} */
-  { *(uint32 *)this = (uint32)~RGB_MASK; }
+  csRGBpixel () : red(0), green(0), blue(0), alpha(255) {}
   /// Copy constructor
-  csRGBpixel (const csRGBpixel& p)
-  /* : red (p.red), green (p.green), blue (p.blue), alpha (p.alpha) {} */
-  { *(uint32*)this = *(uint32*)&p; }
+  csRGBpixel (const csRGBpixel& p) :
+    red (p.red), green (p.green), blue (p.blue), alpha (p.alpha) {}
   /// Yet another copy constructor
   csRGBpixel (const csRGBcolor& c) :
     red (c.red), green (c.green), blue (c.blue), alpha (255) {}
   /// Initialize the pixel with some R/G/B value
   csRGBpixel (int r, int g, int b) :
-    red (r), green (g), blue (b), alpha (255) {}
+    red ((unsigned char)r),
+    green ((unsigned char)g),
+    blue ((unsigned char)b),
+    alpha ((unsigned char)255) {}
   /// Compare with an csRGBcolor
   bool operator == (const csRGBcolor& c) const
   { return (c.red == red) && (c.green == green) && (c.blue == blue); }
   /// Compare with an csRGBpixel (including alpha value)
   bool operator == (const csRGBpixel& p) const
-  /* { return (p.red == red) && (p.green == green) && (p.blue == blue); } */
-  { return *(uint32*)this == *(uint32*)&p; }
+  { return (p.red == red) && (p.green == green) && (p.blue == blue); }
   /// Check if the csRGBpixel is not equal to an csRGBcolor
   bool operator != (const csRGBcolor& c) const
   { return !operator == (c); }
@@ -136,48 +139,63 @@ struct csRGBpixel
   { return csRGBcolor (red, green, blue); }
   /// Compare with another csRGBpixel, but don't take alpha into account
   bool eq (const csRGBpixel& p) const
-  { return ((*(uint32*)this) & RGB_MASK) == ((*(uint32*)&p) & RGB_MASK); }
+  { return operator==(p); }
   /// Get the pixel intensity
   int Intensity ()
   { return (red + green + blue) / 3; }
   /// Get the pixel luminance
   unsigned char Luminance ()
-  { return (((int)red)*30 + ((int)green)*59 + ((int)blue)*11)/100; }
+  {
+    return (unsigned char)(((int)red*30 + (int)green*59 + (int)blue*11) / 100);
+  }
   /// Assign given red/green/blue values to this pixel
   void Set (const int r, const int g, const int b)
-  { red = r; green = g; blue = b; alpha = 255; }
+  {
+    red = (unsigned char)r;
+    green = (unsigned char)g;
+    blue = (unsigned char)b;
+    alpha = (unsigned char)255;
+  }
   /// Assign given red/green/blue/alpha values to this pixel
   void Set (const int r, const int g, const int b, const int a)
-  { red = r; green = g; blue = b; alpha = a; }
+  {
+    red = (unsigned char)r;
+    green = (unsigned char)g;
+    blue = (unsigned char)b;
+    alpha = (unsigned char)a;
+  }
   /// Assign another csRGBpixel
   void Set (const csRGBpixel& p)
-  /* : red (p.red), green (p.green), blue (p.blue), alpha (p.alpha) {} */
-  { *(uint32*)this = *(uint32*)&p; }
+  { red = p.red; green = p.green; blue = p.blue; alpha = p.alpha; }
   /// Add a csRGBcolor
   void operator += (const csRGBcolor& c)
   {
-      red   += c.red;
-      green += c.green;
-      blue  += c.blue;
+      red   = (unsigned char)(red   + c.red  );
+      green = (unsigned char)(green + c.green);
+      blue  = (unsigned char)(blue  + c.blue );
   }
   /**
    * Use this only if you know there is no overflow. Also take
    * care to set alpha to 0 in 'c'!
    */
-  void UnsafeAdd (const csRGBpixel&c)
-  { *(uint32*)this += *(uint32*)&c; }
+  void UnsafeAdd (const csRGBpixel& c)
+  {
+    red   = (unsigned char)(red   + c.red  );
+    green = (unsigned char)(green + c.green);
+    blue  = (unsigned char)(blue  + c.blue );
+  }
   /**
    * Use this in the general case. This version test for overflow.
    * Note that alpha is ignored by this routine.
    */
-  void SafeAdd (const csRGBpixel&c)
+  void SafeAdd (const csRGBpixel& c)
   {
-    int color = red+c.red;
-    red = (color > 255) ? 255 : color;
-    color = green+c.green;
-    green = (color > 255) ? 255 : color;
-    color = blue+c.blue;
-    blue = (color > 255) ? 255 : color;
+    int color = red + c.red;
+    red   = (unsigned char)(color > 255 ? 255 : color);
+    color = green + c.green;
+    green = (unsigned char)(color > 255 ? 255 : color);
+    color = blue + c.blue;
+    blue  = (unsigned char)(color > 255 ? 255 : color);
   }
 } CS_STRUCT_ALIGN_4BYTE_END;
 
