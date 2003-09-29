c930554eb711785a8a784027c6bd514d82a0792c
Eric Sunshine <sunshine@sunshineco.com>
2003-09-29 15:01:24 +0000

    Eric Sunshine eliminated compilation warning from portalcontainer.cpp.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@15540 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csengine/objects/portalcontainer.cpp
+++ libs/csengine/objects/portalcontainer.cpp
@@ -768,7 +768,7 @@ void csPortalContainer::DrawOnePortal (
   // If we have fog or the portal is z-filled we need to keep the
   // camera plane because recursive rendering may cause it to change.
   csPlane3 keep_plane;
-  float keep_camera_z;	// Also keep z-coordinate of vertex 0.
+  float keep_camera_z = 0;	// Also keep z-coordinate of vertex 0.
   if (is_this_fog || po->flags.Check (CS_PORTAL_ZFILL))
   {
     keep_plane = camera_plane;
