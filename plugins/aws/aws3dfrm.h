e3032206af9fcd4b36b7240cd62459d94639b059
Philip Wyett <philswyett@gmx.co.uk>
2003-12-25 15:44:32 +0000

    - Philip Wyett begins a general cleanup of CS code.
      Note: 'aws3dfrm.h' had no LGPL info and thus no copyright to the original
            author. 'neverjade' should insert his real name _or_ Jorrits
            (to give copyright to the project as a whole and leave no ambiguity)
            in place of question marks.
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@16224 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/aws/aws3dfrm.h
+++ plugins/aws/aws3dfrm.h
@@ -1,61 +1,75 @@
-#ifndef __CS_AWS_3D_FRAME_DRAWER__
-#define __CS_AWS_3D_FRAME_DRAWER__
+/*
+    Copyright (C) ???
 
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
 
-# include "csgeom/csrectrg.h"
-# include "ivideo/texture.h"
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_AWS_AWS3DFRM_H__
+#define __CS_AWS_AWS3DFRM_H__
+
+#include "csgeom/csrectrg.h"
+#include "ivideo/texture.h"
+
 struct iGraphics2D;
 struct iGraphics3D;
-
 struct iAws;
 struct iAwsComponent;
 
+/**
+ * This class draws several different 3d frame types to avoid code duplication.
+ */
 
-/// This class draws several different 3d frame types to avoid code duplication.
 class aws3DFrame
 {
 protected:
-
-  // a ref to the current 2d drawing context
+  /// A ref to the current 2d drawing context
   iGraphics2D* g2d;
 
-  // a ref to the current 3d drawing context
+  /// A ref to the current 3d drawing context
   iGraphics3D* g3d;
 
-  // the basic colors
-  int hi, hi2, lo, lo2, fill, dfill, black,
-	  bfill;
+  /// The basic colors
+  int hi, hi2, lo, lo2, fill, dfill, black, bfill;
 
-
-  // background/overlay textures
+  /// Background/Overlay textures
   iTextureHandle *bkg, *ovl;
 
-  // background/overlay alpha levels
+  /// Background/Overlay alpha levels
   int bkg_alpha, ovl_alpha;
 
+  void DrawTexturedBackground (
+    csRectRegion* todraw,
+    iTextureHandle* txt,
+    int alpha_level,
+    csRect txt_align);
 
-  void DrawTexturedBackground(csRectRegion* todraw,
-                              iTextureHandle* txt,
-                              int alpha_level,
-							                csRect txt_align);
+  void DrawFlatBackground (csRectRegion* todraw, int color);
 
-  void DrawFlatBackground(csRectRegion* todraw,
-                          int color);
+  void DrawRaisedFrame (csRect frame);
 
-  void DrawRaisedFrame(csRect frame);
+  void DrawSmallRaisedFrame (csRect frame);
 
-  void DrawSmallRaisedFrame(csRect frame);
+  void DrawSunkenFrame (csRect frame);
 
-  void DrawSunkenFrame(csRect frame);
+  void DrawSmallSunkenFrame (csRect frame);
 
-  void DrawSmallSunkenFrame(csRect frame);
+  void DrawBumpFrame (csRect frame);
 
-  void DrawBumpFrame(csRect frame);
+  void DrawBevelFrame (csRect frame);
 
-  void DrawBevelFrame(csRect frame);
-
-  void DrawThickFrame(csRect frame);
-
+  void DrawThickFrame (csRect frame);
 public:
   static const int fsBump;
   static const int fsSimple;
@@ -70,59 +84,47 @@ public:
   static const int fsSmallSunken;
   static const int fsMask;
 
-public:
   /// Creates a frame drawer
   aws3DFrame ();
 
   /// Destroys a frame drawer
   ~aws3DFrame ();
 
-  // Retrieves the default pallete along with
-  // textures and alpha levels
-  void Setup(iAws *wmgr, 
-             iTextureHandle* bkg = 0,
-             int bkg_alpha = 0,
-             iTextureHandle* ovl = 0,
-             int ovl_alpha = 0);
+  /**
+   * Retrieve the default pallete along with textures and alpha levels
+   */
+  void Setup (
+    iAws *wmgr,
+    iTextureHandle* bkg = 0,
+    int bkg_alpha = 0,
+    iTextureHandle* ovl = 0,
+    int ovl_alpha = 0);
 
-  /// Draws a frame of the given type
-  /// Three different versions are presented
-  /// depending on whether you need offsets 
-  /// on the background/overlay textures
+  /**
+   * Draws a frame of the given type. Three different versions are presented
+   * depending on whether you need offsets on the background/overlay textures
+   */
+  void Draw (csRect frame, int frame_style, csRectRegion* rgn = 0);
 
-  void Draw (
-        csRect frame,
-        int frame_style, 
-		    csRectRegion* rgn = 0);
-
-  void Draw(
-        csRect frame,
-        int frame_style,
-        csRect bkg_align,
-        csRectRegion* rgn = 0);
+  void Draw (csRect frame, int frame_style, csRect bkg_align,
+    csRectRegion* rgn = 0);
   
-  void Draw(
-        csRect frame,
-        int frame_style,
-        csRect bkg_align,
-        csRect ovl_align,
-        csRectRegion* rgn = 0);
+  void Draw (csRect frame, int frame_style, csRect bkg_align,
+    csRect ovl_align, csRectRegion* rgn = 0);
 
+  csRect SubRectToAlign (csRect comp_frame, csRect txt_sub_rect);
 
-  csRect SubRectToAlign(csRect comp_frame, csRect txt_sub_rect);
-
-  void SetBackgroundTexture(iTextureHandle* bkg);
-  void SetOverlayTexture(iTextureHandle* ovl);
-  void SetBackgroundAlpha(int bkg_alpha);
-  void SetOverlayAlpha(int ovl_alpha);
-  void SetBackgroundColor(int color);
-
-  csRect GetInsets(int style);
-
+  void SetBackgroundTexture (iTextureHandle* bkg);
+  void SetOverlayTexture (iTextureHandle* ovl);
+  void SetBackgroundAlpha (int bkg_alpha);
+  void SetOverlayAlpha (int ovl_alpha);
+  void SetBackgroundColor (int color);
+  csRect GetInsets (int style);
 };
 
-// These are the frame styles actually support by the 3d frame class, and thus globally
-
+/**
+ * These are the frame styles supported by the 3d frame class and thus globally
+ */
 const int _3dfsBump = 0;
 const int _3dfsSimple = 1;
 const int _3dfsRaised = 2;
@@ -136,4 +138,4 @@ const int _3dfsSmallRaised = 9;
 const int _3dfsSmallSunken = 10;
 const int _3dfsMask  = 0x1f;
 
-#endif // __CS_AWS_3D_FRAME_DRAWER__
+#endif // __CS_AWS_AWS3DFRM_H__
