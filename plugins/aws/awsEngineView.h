be9f58b8a3d979a6082255213dbbb1e0d0f9b07d
Philip Wyett <philswyett@gmx.co.uk>
2003-12-25 16:12:45 +0000

    - Philip Wyett begins a general cleanup of CS code.
      Note: 'awsEngineView.h' had no LGPL info and thus no copyright to the original
            author. 'neverjade' should insert his real name _or_ Jorrits
            (to give copyright to the project as a whole and leave no ambiguity)
            in place of question marks.
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@16226 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/aws/awsEngineView.h
+++ plugins/aws/awsEngineView.h
@@ -1,8 +1,23 @@
-// awsEngineView.h: interface for the awsEngineView class.
-//
-//////////////////////////////////////////////////////////////////////
-#ifndef __CS_AWS_ENGINEVIEW_H__
-#define __CS_AWS_ENGINEVIEW_H__
+/*
+    Copyright (C) ???
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_AWS_AWSENGINEVIEW_H__
+#define __CS_AWS_AWSENGINEVIEW_H__
 
 #include "awscomp.h"
 
@@ -11,10 +26,10 @@ struct iView;
 class awsEngineView : public awsComponent  
 {
 public:
-  awsEngineView();
-  virtual ~awsEngineView();
+  awsEngineView ();
+  virtual ~awsEngineView ();
 
-  virtual void OnDraw(csRect clip);
+  virtual void OnDraw (csRect clip);
 
   /// Gets properties
   bool GetProperty (const char *name, void **parm);
@@ -24,7 +39,6 @@ public:
 
   /// Returns the named TYPE of the component, like "Radio Button", etc.
   virtual const char *Type ();
-
 private:
   iView* view;
 };
@@ -32,11 +46,10 @@ private:
 class awsEngineViewFactory : public awsComponentFactory
 {
 public:
-  awsEngineViewFactory(iAws* mgr);
-  ~awsEngineViewFactory();
+  awsEngineViewFactory (iAws* mgr);
+  ~awsEngineViewFactory ();
 
-  iAwsComponent* Create();
+  iAwsComponent* Create ();
 };
 
-#endif // __CS_AWS_ENGINEVIEW_H__
-
+#endif // __CS_AWS_AWSENGINEVIEW_H__
