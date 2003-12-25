62976e638c37315a9386e48c1cb0a4f26bfecfdc
Philip Wyett <philswyett@gmx.co.uk>
2003-12-25 16:03:09 +0000

    - Philip Wyett begins a general cleanup of CS code.
      Note: 'awsControlBar.h' had no LGPL info and thus no copyright to the original
            author. 'neverjade' should insert his real name _or_ Jorrits
            (to give copyright to the project as a whole and leave no ambiguity)
            in place of question marks.
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@16225 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/aws/awsControlBar.h
+++ plugins/aws/awsControlBar.h
@@ -1,43 +1,70 @@
-// awsControlBar.h: interface for the awsControlBar class.
-//
-//////////////////////////////////////////////////////////////////////
-#ifndef __CS_AWS_CONTROLBAR_H__
-#define __CS_AWS_CONTROLBAR_H__
+/*
+    Copyright (C) ???
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_AWS_AWSCONTROLBAR_H__
+#define __CS_AWS_AWSCONTROLBAR_H__
 
 #include "awscomp.h"
 #include "awsPanel.h"
 
-/** This is the base class for a number of different component types. A
+/**
+ * This is the base class for a number of different component types. A
  * control bar is just a simple component that has any number of any type
  * of component in it, layed out in a row.
  */
+ 
 class awsControlBar : public awsPanel
 {
 private:
-  /// the components in the bar
+  /// The components in the bar
   awsComponentVector comps;
 
-  /// spacing desired
+  /// Spacing desired
   int hGap, vGap;
 
-  /// alignment
+  /// Vertical alignment
   int vert_align;
 
-  /// true if the control bar should automatically resize its width
-  /// to fit all components
+  /**
+   * Returns true if the control bar should automatically resize its
+   * width to fit all components.
+   */
   bool size_to_fit_horz;
 
-  /// true if the control bar should automatically resize its height
-  /// to fit all components
+  /**
+   * Returns true if the control bar should automatically resize its
+   * height to fit all components.
+   */
   bool size_to_fit_vert;
 
-  // true if the components are to be layed out in a column rather than row
+  /**
+   * Returns true if the components are to be layed out in a column
+   * rather than row.
+   */
   bool vertical;
 
-  // true if items should be stretched perpendicular to the row direction
+  /**
+   * Returns true if items should be stretched perpendicular to the
+   * row direction.
+   */
   bool stretch_items;
 
-  // layout the child components and resize this component if necessary
+  /// Layout the child components and resize this component if necessary.
   void DoLayout ();
 
 public:
@@ -45,52 +72,51 @@ public:
   virtual ~awsControlBar ();
 
   virtual bool Setup (iAws *_wmgr, iAwsComponentNode *settings);
-  virtual const char *Type ()
-  {return "Control Bar";}
+  virtual const char *Type () { return "Control Bar"; }
 
-  bool Execute(const char* action, iAwsParmList *parmlist);
+  bool Execute (const char* action, iAwsParmList *parmlist);
 
   /// Adds a component to the bar
-  void AddChild(iAwsComponent *comp);
+  void AddChild (iAwsComponent *comp);
 
   /// This will remove the component from the bar
-  void RemoveChild(iAwsComponent *comp);
+  void RemoveChild (iAwsComponent *comp);
 
   /// Returns true if the component resizes width to fit the controls when they change
-  bool GetSizeToFitHorz();
+  bool GetSizeToFitHorz ();
 
   /// Returns true if the component resizes height to fit the controls when they change
-  bool GetSizeToFitVert();
+  bool GetSizeToFitVert ();
 
   /// Sets whether the component resizes width to fit the controls when they change
-  void SetSizeToFitHorz(bool b);
+  void SetSizeToFitHorz (bool b);
  
   /// Sets whether the component resizes height to fit the controls when they change
-  void SetSizeToFitVert(bool b);
+  void SetSizeToFitVert (bool b);
 
   /// Resizes the component to fit the controls snuggly
-  void SizeToFit();
+  void SizeToFit ();
 
   /// Resizes the component to fit the width of the controls snuggly
-  void SizeToFitHorz();
+  void SizeToFitHorz ();
 
   /// Resizes the component to fit the height of the controls snuggly
-  void SizeToFitVert();
+  void SizeToFitVert ();
 
-  bool GetVertical();
-  void SetVertical(bool vertical);
+  bool GetVertical ();
+  void SetVertical (bool vertical);
 
-  int GetAlignment();
-  void SetAlignment(int alignment);
+  int GetAlignment ();
+  void SetAlignment (int alignment);
 
-  int GetHorzGap();
-  void SetHorzGap(int gap);
+  int GetHorzGap ();
+  void SetHorzGap (int gap);
 
-  int GetVertGap();
-  void SetVertGap(int gap);
+  int GetVertGap ();
+  void SetVertGap (int gap);
 
-  bool GetStretchComponents();
-  void SetStretchComponents(bool stretch);
+  bool GetStretchComponents ();
+  void SetStretchComponents (bool stretch);
 
   static const int alignTop;
   static const int alignBottom;
@@ -100,10 +126,9 @@ public:
 class awsControlBarFactory : public awsComponentFactory
 {
 public:
-  awsControlBarFactory(iAws* _wmgr);
+  awsControlBarFactory (iAws* _wmgr);
   
-  iAwsComponent* Create();
+  iAwsComponent* Create ();
 };
 
-#endif // __CS_AWS_CONTROLBAR_H__
-
+#endif // __CS_AWS_AWSCONTROLBAR_H__
