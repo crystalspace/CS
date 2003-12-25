1099ee2e104ef3ee3f85935f1596b83cdc3a89a5
Philip Wyett <philswyett@gmx.co.uk>
2003-12-25 18:23:39 +0000

    - Philip Wyett continues a general cleanup of CS code.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@16231 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/aws/awsTabCtrl.h
+++ plugins/aws/awsTabCtrl.h
@@ -1,6 +1,21 @@
-// awsTabCtrl.h: interface for the awsTabCtrl class.
-//
-//////////////////////////////////////////////////////////////////////
+/*
+    Copyright (C) ???
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #ifndef __CS_AWS_TABCTRL_H__
 #define __CS_AWS_TABCTRL_H__
 
@@ -8,45 +23,46 @@
 #include "awsscrbr.h"
 #include "csutil/parray.h"
 
-/** This class implements a basic tab button on a tab control. */
+/**
+ * This class implements a basic tab button on a tab control.
+ */
+ 
 class awsTab : public awsComponent
 {
- protected:
-
-  // texture background, overlay and icon
+protected:
+  /// Texture background, overlay and icon.
   iTextureHandle *tex[3];
 
-  // does this button depict the active tab ?
+  /// Does this button depict the active tab?
   bool is_active;
 
-  // is this the first button that is drawn ? important for color selection
+  /// Is this the first button that is drawn? Important for color selection.
   bool is_first;
 
-  // does this bottom reside on the top of the notebook or below
+  /// Does this bottom reside on the top of the notebook or below.
   bool is_top;
 
-  // caption
+  /// Caption
   iString *caption;
 
-  // is the mouse currently held doen ?
+  /// Is the mouse currently held doen 
   bool captured;
 
-  // if theres an icon, how is it align
+  /// If theres an icon, how is it align.
   int icon_align;
 
-  // alpha level for overlay texture if any
+  /// Alpha level for overlay texture if any.
   int alpha_level;
 
-  // A parameter that the user can store with this tab
-  // Often its helpful as an identifier for which tab was pressed
-  // or what to do about it
+  /**
+   * A parameter that the user can store with this tab. Often its helpful
+   * as an identifier for which tab was pressed or what to do about it.
+   */
   void* user_param;
 
   // trigger event if needed
   bool HandleClick (int x, int y);
-
- public:
-
+public:
   awsTab ();
   virtual ~awsTab ();
 
@@ -61,12 +77,11 @@ class awsTab : public awsComponent
   bool OnMouseDoubleClick (int, int x, int y);
   virtual csRect getMinimumSize ();
 
-  virtual const char *Type ()
-  {return "Tab Button";}
+  virtual const char *Type () { return "Tab Button"; }
 
   void SetActive (bool what);
-  void SetFirst (bool what){is_first=what;}
-  void SetTop (bool what){is_top=what;}
+  void SetFirst (bool what) { is_first=what; }
+  void SetTop (bool what) { is_top=what; }
 
   static const int signalActivateTab;
   static const int signalDeactivateTab;
@@ -76,23 +91,24 @@ class awsTab : public awsComponent
   static const int iconBottom;
 };
 
-/* This class implements a simple tab control. 
-
-   Basic usage is to insert tabs using the Add action, supplying a caption and optional parameter.
-   Add will return an iAwsSource* which you would then connect however you like.
-   When a tab is selcted/deselected a sActivateTab/sDeactivateTab signal will be sent
-   through the source you received. Also from the source's component you can query
-   the "User Param" property to retrieve the parameter you passed in Add. Either the
-   index, the source, or the parameter can be used to identify tabs in other method calls.
-   However indexes can change by adding/removing tabs, and it is your responsibilty
-   to ensure the user_param is unique if you desire to use it.
-*/
+/**
+ * This class implements a simple tab control.
+ *<p>
+ * Basic usage is to insert tabs using the Add action, supplying a caption
+ * and optional parameter. Add will return an iAwsSource* which you would
+ * then connect however you like. When a tab is selcted/deselected a
+ * sActivateTab/sDeactivateTab signal will be sent through the source you
+ * received. Also from the source's component you can query the "User Param"
+ * property to retrieve the parameter you passed in Add. Either the index,
+ * the source, or the parameter can be used to identify tabs in other
+ * method calls. However indexes can change by adding/removing tabs, and it
+ * is your responsibilty to ensure the user_param is unique if you desire
+ * to use it.
+ */
 
 class awsTabCtrl : public awsComponent  
 {
 protected:
-
-
   class TabVector : public csPDelArray<awsTab>
   {
   public:
@@ -106,91 +122,98 @@ protected:
 
   TabVector vTabs;
 
-  // first visible button
+  /// First visible button
   int first;
-  // the active tab
+  
+  /// The active tab
   int active;
-  // button bar on top of notebook or below ?
+  
+  /// Button bar on top of notebook or below ?
   bool is_top;
 
-  // true if the client area should be shrunk because of the scroll buttons
+  /// True if the client area should be shrunk because of the scroll buttons.
   bool clip_to_scroll;
 
-  // our kitchen sink
+  /// Our kitchen sink
   awsSink *sink;
 
-  // max height of buttons in bar
+  /// Max height of buttons in bar
   int maxheight;
 
-  // the two "next/prev" buttons
+  /// The two "next/prev" buttons
   awsSliderButton next, prev;
 
-  // slots for the next/prev buttons
+  /// Slots for the next/prev buttons
   awsSlot slot_activate, slot_next, slot_prev;
 
-  // images for next/prev buttons
+  /// Images for next/prev buttons
   iTextureHandle *nextimg, *previmg;
 
-  // layout the buttons, hide or show them, align the next/prev handles
+  /// Layout the buttons, hide or show them, align the next/prev handles
   void DoLayout ();
 
-  // Finds the index of the first tab which uses this param
-  int FindTab(void* user_param);
-
- public:
-
-
-  awsTabCtrl();
+  /// Finds the index of the first tab which uses this param
+  int FindTab (void* user_param);
+public:
+  awsTabCtrl ();
   virtual ~awsTabCtrl ();
 
-  virtual void OnDraw(csRect clip);
+  virtual void OnDraw (csRect clip);
   virtual bool Setup (iAws *_wmgr, iAwsComponentNode *settings);
-  virtual const char *Type (){return "Simple Tab Control";}
+  virtual const char *Type () { return "Simple Tab Control"; }
 
-  virtual csRect getInsets();
-  virtual csRect getPreferredSize();
+  virtual csRect getInsets ();
+  virtual csRect getPreferredSize ();
 
-  // This will create a button based on the caption
-  // This source will fire sActivateTab and sDeactivateTab
-  // The parameter can also be retrieved from the source component
-  // by querying it's "User Param" property
-  // If this component is the first that has been added it becomes the active one.
+  /**
+   * This will create a button based on the caption. This source will fire
+   * sActivateTab and sDeactivateTab. The parameter can also be retrieved
+   * from the source component by querying it's "User Param" property. If
+   * this component is the first that has been added becomes the active one.
+   */
   iAwsSource* AddTab (iString* caption, void* user_param = 0);
 
-  // This will remove the tab at index
-  // The next tab will become active (or the prev if no next exist) if this was the active one.
+  /**
+   * This will remove the tab at index. The next tab will become active
+   * (or the prev if no next exist) if this was the active one.
+   */
   void RemoveTab (int index);
-  // Remove the tab that uses this source
-  // The next tab will become active (or the prev if no next exist) if this was the active one.
+
+  /**
+   * Remove the tab that uses this source. The next tab will become active
+   * (or the prev if no next exist) if this was the active one.
+   */
   void RemoveTab (iAwsSource* src);
-  // Remove the tab that has this user param
-  // The next tab will become active (or the prev if no next exist) if this was the active one.
+
+  /**
+   * Remove the tab that has this user param. The next tab will become
+   * active (or the prev if no next exist) if this was the active one.
+   */
   void RemoveTab (void* user_param);
 
-  // Activate the <idx>-th tab
+  /// Activate the <idx>-th tab
   void ActivateTab (int idx);
-  // Activate the tab that uses this src
+  /// Activate the tab that uses this src
   void ActivateTab (iAwsSource* src);
-  // Activate the tab that uses this user_param
+  /// Activate the tab that uses this user_param
   void ActivateTab (void* param);
 
-  // Returns the source for the currently active tab
-  iAwsSource* GetActiveTab();
-  // Returns the index of the currently active tab. 
-  int GetActiveTabIndex();
-  // Returns the user_param of the currently active tab.
-  void* GetActiveTabParam(); 
+  /// Returns the source for the currently active tab
+  iAwsSource* GetActiveTab ();
+  /// Returns the index of the currently active tab. 
+  int GetActiveTabIndex ();
+  /// Returns the user_param of the currently active tab.
+  void* GetActiveTabParam (); 
 
-  // Scroll list of button left
+  /// Scroll list of button left.
   void ScrollLeft ();
-
-  // Scroll list of button right
+  /// Scroll list of button right.
   void ScrollRight ();
 
-  // scroll buttons until the <idx>-th becomes visible
+  /// Scroll buttons until the <idx>-th becomes visible.
   void MakeVisible (int idx);
 
-  virtual void OnResized();
+  virtual void OnResized ();
 
   static void ActivateTabCallback (void *sk, iAwsSource *source);
 
@@ -199,9 +222,8 @@ protected:
 
   static const int HandleSize;
 
-  // show buttonbar at top or bottom
+  /// Show buttonbar at top or bottom.
   void SetTopBottom (bool to_top);
 };
 
 #endif // __CS_AWS_TABCTRL_H__
-
