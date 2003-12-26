4d57008bded6fbb62e914c87fef7a8434682ba47
Philip Wyett <philswyett@gmx.co.uk>
2003-12-26 14:50:19 +0000

    - Philip Wyett continues a general cleanup of CS code.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@16237 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/aws/awscscr.h
+++ plugins/aws/awscscr.h
@@ -1,9 +1,5 @@
-#ifndef __CS_AWS_CSCR_H__
-#define __CS_AWS_CSCR_H__
-
-/**************************************************************************
+/*
     Copyright (C) 2000-2001 by Christopher Nelson
-
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -18,49 +14,54 @@
     You should have received a copy of the GNU Library General Public
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*****************************************************************************/
-# include "cstool/proctex.h"
-# include "ivideo/graph2d.h"
-# include "ivideo/graph3d.h"
-# include "csgeom/transfrm.h"
-# include "av3dtxt.h"
+*/
 
-class awsScreenCanvas :
-  public iAwsCanvas
+#ifndef __CS_AWS_CSCR_H__
+#define __CS_AWS_CSCR_H__
+
+#include "cstool/proctex.h"
+#include "ivideo/graph2d.h"
+#include "ivideo/graph3d.h"
+#include "csgeom/transfrm.h"
+#include "av3dtxt.h"
+
+class awsScreenCanvas : public iAwsCanvas
 {
 private:
-  /// Context to the screen's g2d
+  /// Context to the screen's g2d.
   iGraphics2D *rG2D;
 
-  /// Context to the screen's g3d
+  /// Context to the screen's g3d.
   iGraphics3D *rG3D;
 public:
   SCF_DECLARE_IBASE;
 
-  /// Initialize this canvas with custom pointers to a valid g2d and g3d
+  /// Initialize this canvas with custom pointers to a valid g2d and g3d.
   awsScreenCanvas (iGraphics2D *g2d, iGraphics3D *g3d) :
-  rG2D(g2d),
-  rG3D(g3d)
+    rG2D(g2d),
+    rG3D(g3d)
   {
     SCF_CONSTRUCT_IBASE (0);
     g2d->IncRef ();
     g3d->IncRef ();
   }
 
-  /// Destruct, release references
+  /// Destruct, release references.
   virtual ~awsScreenCanvas ()
   {
     rG2D->DecRef ();
     rG3D->DecRef ();
-    SCF_DESTRUCT_IBASE();
+    SCF_DESTRUCT_IBASE ();
   }
+
   virtual void Animate (csTicks current_time)
   {
-    (void)current_time; /* do nothing */
+    (void)current_time; // Do nothing
   }
 
   iGraphics2D *G2D () { return rG2D; }
   iGraphics3D *G3D () { return rG3D; }
+
   virtual void Show (
     csRect *area = 0,
     iGraphics3D *g3d = 0,
@@ -68,7 +69,7 @@ public:
   {
     (void)area;
     (void)g3d;
-    (void)Alpha;        /* do nothing */
+    (void)Alpha;        // Do nothing
   }
 };
 
