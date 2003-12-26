264ab6824c44163dec62001ebe95cb75c2c7ff2f
Philip Wyett <philswyett@gmx.co.uk>
2003-12-26 20:26:14 +0000

    - Philip Wyett continues a general cleanup of CS code.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@16249 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/aws/awsscr.h
+++ plugins/aws/awsscr.h
@@ -1,29 +1,49 @@
-#ifndef __CS_AWS_SCRIPT_UTILITY_H__
-#define __CS_AWS_SCRIPT_UTILITY_H__
+/*
+    Copyright (C) ???
+  
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
-# include "csutil/array.h"
+#ifndef __CS_AWS_SCR_H__
+#define __CS_AWS_SCR_H__
+
+#include "csutil/array.h"
 
 struct awsActionMap
 {
-  /// The name of the action reduced to a long
+  /// The name of the action reduced to a long.
   unsigned long name;
 
-  /// The action to execute
+  /// The action to execute.
   void (*Action) (void *owner, iAwsParmList* parmlist);
 };
 
 class awsActionDispatcher
 {
-  /// List of actions to execute
+private:
+  /// List of actions to execute.
   csArray<awsActionMap*> actions;
 public:
   /// Register an action.
-  void Register (const char *name,
-        void (Action) (void *owner, iAwsParmList* parmlist));
+  void Register (
+    const char *name,
+    void (Action) (void *owner,
+    iAwsParmList* parmlist));
 
-  /// Execute the corresponding action
+  /// Execute the corresponding action.
   void Execute (const char *action, void *owner, iAwsParmList* parmlist);
 };
 
-#endif // __CS_AWS_SCRIPT_UTILITY_H__
-
+#endif // __CS_AWS_SCR_H__
