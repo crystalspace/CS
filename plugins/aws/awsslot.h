7ebcc0971b2928c6b8b999ad484f970ec9b783eb
Philip Wyett <philswyett@gmx.co.uk>
2003-12-26 20:57:00 +0000

    - Philip Wyett continues a general cleanup of CS code.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@16251 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/aws/awsslot.h
+++ plugins/aws/awsslot.h
@@ -1,46 +1,44 @@
-#ifndef __CS_AWS_SLOT_H__
-#define __CS_AWS_SLOT_H__
-
-/**************************************************************************
+/*
     Copyright (C) 2000-2001 by Christopher Nelson
-
+  
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
-
+  
     This library is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Library General Public License for more details.
-
+  
     You should have received a copy of the GNU Library General Public
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*****************************************************************************/
-# include "iaws/aws.h"
-# include "iutil/comp.h"
-# include "csutil/parray.h"
+*/
 
-/*********************************************************************************************************************
-*                                                                                                                    *
-*      This implements the signal source architecture.  A signal source is an object which knows how to register and *
-*    unregister slots.  A signal source always defines and emits signals.  A signal source does not care if someone  *
-*    is listening or not.                                                                                            *
-*                                                                                                                    *
-*********************************************************************************************************************/
+#ifndef __CS_AWS_SLOT_H__
+#define __CS_AWS_SLOT_H__
 
-/// This is the sink manager, which controls all registered sinks.
+#include "iaws/aws.h"
+#include "iutil/comp.h"
+#include "csutil/parray.h"
+
+/**                                                                                                                   *
+ * This implements the signal source architecture.  A signal source is
+ * an object which knows how to register and unregister slots. A signal
+ * source always defines and emits signals. A signal source does not
+ * care if someone is listening or not.                                                                                            *
+ */
+
 class awsSinkManager : public iAwsSinkManager
 {
+private:
   struct SinkMap
   {
     unsigned long name;
     csRef<iAwsSink> sink;
 
-    SinkMap (unsigned long n, iAwsSink *s) : name(n), sink(s)
-    {
-    };
+    SinkMap (unsigned long n, iAwsSink *s) : name (n), sink (s) { };
   };
 
   csPDelArray<SinkMap> sinks;
@@ -51,26 +49,29 @@ public:
   virtual ~awsSinkManager ();
 
   bool Initialize (iObjectRegistry *sys);
-public:
+
   /// Registers a sink by name for lookup.
   virtual void RegisterSink (const char *name, iAwsSink *sink);
 
-  /// Removes a sink
+  /// Removes a sink.
   virtual bool RemoveSink (iAwsSink* sink);
 
   /// Finds a sink by name for connection.
   virtual iAwsSink *FindSink (const char *name);
 
-  /// Create a new embeddable sink, with parm as the void * passed into the triggers.
+  /**
+   * Create a new embeddable sink, with parm as the void * passed
+   * into the triggers.
+   */
   virtual iAwsSink *CreateSink (void *parm);
 
   /// Create a new embeddable slot
   virtual iAwsSlot *CreateSlot ();
-public:
-  // Implement iComponent interface.
+
+  /// Implement iComponent interface.
   struct eiComponent : public iComponent
   {
-    SCF_DECLARE_EMBEDDED_IBASE(awsSinkManager);
+    SCF_DECLARE_EMBEDDED_IBASE (awsSinkManager);
     virtual bool Initialize (iObjectRegistry *p)
     {
       return scfParent->Initialize (p);
@@ -79,73 +80,76 @@ public:
   scfiComponent;
 };
 
-/// This is the implementation of a signal sink.  It handles trigger registration and dispatch.
-class awsSink :
-  public iAwsSink
+/**
+ * This class is the implementation of a signal sink.  It handles trigger
+ * registration and dispatch.
+ */
+class awsSink : public iAwsSink
 {
+private:
   struct TriggerMap
   {
     unsigned long name;
     void (*trigger) (void *, iAwsSource *);
 
     TriggerMap (unsigned long n, void (*t) (void *, iAwsSource *))
-    	: name(n), trigger(t)
-    {
-    };
+    	: name(n), trigger(t) { };
   };
 
   /// List of triggers registered.
   csPDelArray<TriggerMap> triggers;
 
-  /// Parameter to pass to triggers
+  /// Parameter to pass to triggers.
   void *parm;
 
-  /// Last error code
+  /// Last error code.
   unsigned int sink_err;
-
 public:
   SCF_DECLARE_IBASE;
 
-  /// P is the parm that is passed to triggers
+  /// P is the parm that is passed to triggers.
   awsSink ();
   virtual ~awsSink ();
 
-  void* GetParm() { return parm; }
+  void* GetParm () { return parm; }
   void SetParm (void* p) { parm = p; }
 
-  /// Maps a trigger name to a trigger id
+  /// Maps a trigger name to a trigger id.
   virtual unsigned long GetTriggerID (const char *name);
 
-  /// Handles trigger events
+  /// Handles trigger events.
   virtual void HandleTrigger (int trigger, iAwsSource *source);
 
-  /// A sink should call this to register trigger events
-  virtual void RegisterTrigger (const char *name,
-                void (*Trigger) (void *, iAwsSource *));
+  /// A sink should call this to register trigger events.
+  virtual void RegisterTrigger (
+    const char *name,
+    void (*Trigger) (void *, iAwsSource *));
 
-  /// Gets the last error code
-  virtual unsigned int GetError() { return sink_err; }
+  /// Gets the last error code.
+  virtual unsigned int GetError () { return sink_err; }
 };
 
-/// This is the signal source implementation, subclassed by all components.
-class awsSource :
-  public iAwsSource
+/**
+ * This class is the signal source implementation, subclassed by all
+ * other components.
+ */
+class awsSource : public iAwsSource
 {
-  /// Owner
+private:
+  /// Owner.
   iAwsComponent *owner;
 
   struct SlotSignalMap
   {
-    /// The slot that's registered
+    /// The slot that's registered.
     csRef<iAwsSlot> slot;
 
-    /// The signal it's registered for
+    /// The signal it's registered for.
     unsigned long signal;
   };
 
-  /// contains a list of all slots that we have registered
+  /// contains a list of all slots that we have registered.
   csPDelArray<SlotSignalMap> slots;
-
 public:
   SCF_DECLARE_IBASE;
 
@@ -153,13 +157,16 @@ public:
   awsSource ();
   void SetOwner (iAwsComponent *_owner) { owner = _owner; }
 
-  /// Does nothing
+  /// Does nothing.
   virtual ~awsSource ();
 
-  /// Gets the component owner for this (sources are embedded)
+  /// Gets the component owner for this (sources are embedded).
   virtual iAwsComponent *GetComponent ();
 
-  /// Registers a slot for any one of the signals defined by a source.  Each sources's signals exist in it's own namespace
+  /**
+   * Registers a slot for any one of the signals defined by a source.
+   * Each sources's signals exist in it's own namespace.
+   */
   virtual bool RegisterSlot (iAwsSlot *slot, unsigned long signal);
 
   /// Unregisters a slot for a signal.
@@ -169,24 +176,28 @@ public:
   virtual void Broadcast (unsigned long signal);
 };
 
-/**********************************************************************************************************************
-*                                                                                                                     *
-*   This implements the slot architecture.  Slots are conduits for signals.  Signals always have a source.  Slots may *
-*  be a conduit for multiple signals from multiple sources.  It is up to the user to determine how to use his slots.  *
-*  Slots do not care, other than they route based on signal, NOT source.  Therefore, if a slot is hooked up to two    *
-*  different sources that emit the same signal, then any trigger registered for that signal will recieve the signal   *
-*  from EVERY source that emits it.                                                                                   *
-*                                                                                                                     *
-**********************************************************************************************************************/
-class awsSlot :
-  public iAwsSlot
+/**                                                                                                                    *
+ * This implements the slot architecture.  Slots are conduits for signals.
+ * Signals always have a source.  Slots may be a conduit for multiple
+ * signals from multiple sources. It is up to the user to determine how
+ * to use his slots. Slots do not care, other than they route based on
+ * signal, NOT source.  Therefore, if a slot is hooked up to two different
+ * sources that emit the same signal, then any trigger registered for
+ * that signal will recieve the signal from EVERY source that emits it.                                                                                   *
+ */
+class awsSlot : public iAwsSlot
 {
+private:
   /// The sink that this slot manages.
 
-  /** A mapping between signals and triggers.  One signal may map to multiple triggers, or
-    * vice versa.  The mapping list is traversed everytime there is a signal emitted.  All
-    * mappings are evaluated and if they qualify, are activated. Note that a slot may be
-    * connected to multiple sources, but that it routes based on signal, NOT source. */
+  /**
+   * A mapping between signals and triggers.  One signal may map to
+   * multiple triggers, or vice versa. The mapping list is traversed
+   * everytime there is a signal emitted.  All mappings are evaluated
+   * and if they qualify, are activated. Note that a slot may be
+   * connected to multiple sources, but that it routes based on
+   * signal, NOT source.
+   */
   struct SignalTriggerMap
   {
     unsigned long signal;
@@ -194,38 +205,46 @@ class awsSlot :
     csRef<iAwsSink> sink;
     unsigned long refs;
 
-    SignalTriggerMap (unsigned long s, iAwsSink *sk,
-    	unsigned long t, unsigned long r)
-	: signal(s), trigger(t), sink(sk), refs(r)
-    {
-    };
+    SignalTriggerMap (
+      unsigned long s,
+      iAwsSink *sk,
+    	unsigned long t,
+      unsigned long r)
+    : signal (s), trigger (t), sink (sk), refs (r)
+    { };
   };
 
   csPDelArray<SignalTriggerMap> stmap;
 public:
   SCF_DECLARE_IBASE;
 
-  /// Does nothing
+  /// Does nothing.
   awsSlot ();
 
-  /// Also does nothing
+  /// Also does nothing.
   virtual ~awsSlot ();
 
-  /// Creates a connection from the source:signal to the sink:trigger specified.
+  /**
+   * Creates a connection from the source:signal to the sink:trigger specified.
+   */
   virtual void Connect (
-                iAwsSource *source,
-                unsigned long signal,
-                iAwsSink *sink,
-                unsigned long trigger);
+    iAwsSource *source,
+    unsigned long signal,
+    iAwsSink *sink,
+    unsigned long trigger);
 
-  /// Disconnects the slot from a signal source, also properly unmaps a signal::trigger binding.
+  /**
+   * Disconnects the slot from a signal source, also properly unmaps
+   * a signal::trigger binding.
+   */
   virtual void Disconnect (
-                iAwsSource *source,
-                unsigned long signal,
-                iAwsSink *sink,
-                unsigned long trigger);
+    iAwsSource *source,
+    unsigned long signal,
+    iAwsSink *sink,
+    unsigned long trigger);
 
   /// Emit a signal to activate all necessary triggers.
   virtual void Emit (iAwsSource &source, unsigned long signal);
 };
+
 #endif // __CS_AWS_SLOT_H__
