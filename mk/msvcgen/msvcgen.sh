47f9c00c31c0a0b287be96dd54ed49176ff001f4
Eric Sunshine <sunshine@sunshineco.com>
2004-01-20 01:32:51 +0000

    Expanded help message.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@16512 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- mk/msvcgen/msvcgen.sh
+++ mk/msvcgen/msvcgen.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-# Use this script to generate msvc project files from the Jamfiles
+# Use this script to generate MSVC project files from the Jamfiles
 
 if test "$1" = "--help"; then
-    echo "  mk/msvcgen/msvcgen.sh [6|7]"
-    echo "Generates msvc projectfiles. You may specify version 6 or 7 if you"
-    echo "only want to generate for a specific version (default is both)"
+    echo "  mk/msvcgen/msvcgen.sh [6|7] [-sVAR=VALUE ...]"
+    echo "Generates MSVC project files. You may specify version 6 or 7 if you"
+    echo "only want to generate for a specific version (default is both)."
+    echo "You can also make Jam variable assignments using Jam's -s option."
+    echo "In fact, additional arguments are passed directly to Jam."
     exit 0
 fi
 
@@ -23,9 +25,9 @@ if test "$DO_MSVC6" = no -a "$DO_MSVC7" = no; then
     DO_MSVC7=yes
 fi
 
-if test "$DO_MSVC6" = yes; then
+if test $DO_MSVC6 = yes; then
     jam $@ -sJAMCONFIG=mk/msvcgen/Jamconfig -sMSVC_VERSION=6 msvcgen
 fi
-if test "$DO_MSVC7" = yes; then
+if test $DO_MSVC7 = yes; then
     jam $@ -sJAMCONFIG=mk/msvcgen/Jamconfig -sMSVC_VERSION=7 msvcgen
 fi
