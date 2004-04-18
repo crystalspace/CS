c235a4fb61b5883779635490e3edc9ba8f2e26d3
Eric Sunshine <sunshine@sunshineco.com>
2004-04-18 05:05:25 +0000

    Eric Sunshine fixed CS/bin/autogen.sh so that it returns the exit status of 'autoconf' as its own exit status. This allows invokers of autogen.sh to determine if Autoconf failed.  Also removed out-of-date comment in autogen.sh.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@17436 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- bin/autogen.sh
+++ bin/autogen.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
 
-# Correct working directory?
 if test ! -f configure.ac ; then
   echo "*** Please invoke this script from directory containing configure.ac."
   exit 1
@@ -9,10 +8,12 @@ fi
 MACROFILE=aclocal.m4
 MACRODIR=mk/autoconf
 
-# Creating normal configure file
 rm -f $MACROFILE
 for i in $MACRODIR/*.m4 ; do
   cat $i >> $MACROFILE
 done
+
 autoconf $@
+rc = $?
 rm -f $MACROFILE
+exit $rc
