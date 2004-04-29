7ead96a29edd3fe1642efe92b7b98ea353d9de1a
Eric Sunshine <sunshine@sunshineco.com>
2004-04-29 21:35:33 +0000

    Eric Sunshine rewrote gobs of unportable shell programming in the jamtemplate/createproject.sh script.  Also generally cleaned up and unified the rest of the jamtemplate resources.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@17664 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- scripts/jamtemplate/createproject.sh
+++ scripts/jamtemplate/createproject.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-# Helper function to read variables
-function ReadValue()
+# Helper to read variables
+ReadValue()
 {
     TEXT="$2"
     VARNAME="$1"
     echo -n "$TEXT"
     read READVAL
-
-    export $VARNAME=$READVAL
+    eval "$VARNAME=\"$READVAL\" ; export $VARNAME"
 }
 
-function ReadValueStrict()
+ReadValueStrict()
 {
     TEXT="$2"
     VARNAME="$1"
@@ -21,14 +20,14 @@ function ReadValueStrict()
 	echo -n "$TEXT"
 	read READVAL
 	if test -z "$READVAL"; then
-	    echo "Please enter a value!"
+	    echo "Please enter a value."
 	fi
     done
 
-    export $VARNAME="$READVAL"
+    eval "$VARNAME=\"$READVAL\" ; export $VARNAME"
 }
 
-function CheckYesNo()
+CheckYesNo()
 {
     VARNAME="$1"
     TEXT="$2"
@@ -40,91 +39,88 @@ function CheckYesNo()
 	read READVAL       
 	
 	if test -z "$READVAL"; then
-	    export $VARNAME=$DEFAULT
+	    eval "$VARNAME=\"$DEFAULT\" ; export $VARNAME"
 	    READOK=yes
-	fi
-	if test "$READVAL" = "yes"; then
-	    export $VARNAME=yes
+	elif test "$READVAL" = "yes"; then
+	    eval "$VARNAME=yes ; export $VARNAME"
 	    READOK=yes
-	fi
-	if test "$READVAL" = "no"; then
-	    export $VARNAME=no
+	elif test "$READVAL" = "no"; then
+	    eval "$VARNAME=no ; export $VARNAME"
 	    READOK=yes
 	fi
 	
 	if test "$READOK" = "no"; then
-	    echo 'Please enter "yes" or "no"!'
+	    echo 'Please enter "yes" or "no".'
 	fi
     done
 }
 
-# Part 1: Interaktive - Gather informations
+# Poor man's (portable) 'dirname' command.
+dirpart() {
+    expr "$1" : '\(..*\)[/\\].*'
+}
+
+# Part 1: Interactive - Gather information
 cat << __EOF__
 Crystal Space external Project Template
 
-This script will generate a basic Crystal Space project for you, complete
-with configuration(autoconf) and build system(jam). The project will be created
-in a subdirectory of the current directory. You can always abort the script with
-CTRL+C.
+This script will generate a basic Crystal Space project for you, complete with
+configuration (Autoconf) and build system (Jam).  The project will be created
+in a subdirectory of the current directory.  You can abort the script at any
+time by typing Ctrl-C.
 
 __EOF__
 
 cat << "__EOF__"
 **** Project
-You should choose a projectname now. We need a shortform of the name which
-will be used in directory and filenames. The shortname should be lowercase not
-must not contain any special chars or spaces. The long name will be present in
-helptexts and README files. So If you project is called "Space Fighters -
-Revenge for $$$" then you should probably choose shortname spacefighters and
-"Space Fighters - Revenge for $$$" as longname.
-You may provide a Homepage address for the project. (Please specify a complete
-URI. Example: "http://www.spacefighter.org")
+Choose a project name.  We need a short form of the name for use in directory
+and file names.  The short name should be lowercase (though not strictly
+required) and must not contain any special characters or spaces.  We also need
+a long name for use in help messages and the README file.  For example, if you
+project is called "Space Fighters - Revenge for Ducky", then you may want to
+choose a short name "spacefighters", and a long name "Space Fighters - Revenge
+for Ducky".  You may also provide an address for the project's home page.
+Please specify a complete URI.  For example: http://www.spacefighter.org/
 
 __EOF__
-ReadValueStrict PROJECTNAME "Shortname: "
-ReadValueStrict LONGNAME "Longname: "
+ReadValueStrict PROJECTNAME "Short name: "
+ReadValueStrict LONGNAME "Long name: "
 ReadValueStrict VERSION "Version: "
-ReadValue HOMEPAGE "Homepage: "
+ReadValue HOMEPAGE "Home page URI: "
 
 cat << __EOF__
 
 **** Contact
-The informations about the author are mentioned in the README file and in the
-configuration scripts so that people see a support address when typing
-"configure --help".
+The information about the author is mentioned in the README file and in the
+configuration script so that people see a support address when they invoke
+"./configure --help".
 
 __EOF__
 ReadValueStrict AUTHOR "Author: "
-ReadValueStrict EMAIL "E-Mail: "
+ReadValueStrict EMAIL "Email: "
 ReadValue COPYRIGHT "Copyright: "
 
-cat << __EOF__
-
-**** Fine Tuning
-ok.
-__EOF__
-#CheckYesNo KDEVELOP "Do you want me to generate a kdevelop projectfile?[no] " "no"
-
 echo ""
-echo "*Creating $PROJECTNAME Project."
+echo "*Creating project: $PROJECTNAME"
 
-TEMPLATEDIR=`dirname $0`
+TEMPLATEDIR=`dirpart $0`
 CSDIR="$TEMPLATEDIR/../.."
 
-function SetupFromTemplate
+Instantiate()
 {
     SOURCE="$1"
     TARGET="$2"
 
-# XXX We should somehow quote here...
-    cat "$1" | sed -e "s^#PROJECTNAME#^$PROJECTNAME^g" \
-	     | sed -e "s^#EMAIL#^$EMAIL^g" \
-	     | sed -e "s^#HOMEPAGE#^$HOMEPAGE^g" \
-	     | sed -e "s^#LONGNAME#^$LONGNAME^g" \
-	     | sed -e "s^#AUTHOR#^$AUTHOR^g" \
-	     | sed -e "s^#VERSION#^$VERSION^g" \
-	     | sed -e "s^#COPYRIGHT#^$COPYRIGHT^g" \
-	       > "$2"
+    # XXX We should somehow quote here...
+    cat "$SOURCE" | sed -e " \
+	s^#PROJECTNAME#^$PROJECTNAME^g;
+	s^#EMAIL#^$EMAIL^g; \
+	s^#HOMEPAGE#^$HOMEPAGE^g; \
+	s^#LONGNAME#^$LONGNAME^g; \
+	s^#AUTHOR#^$AUTHOR^g; \
+	s^#VERSION#^$VERSION^g; \
+	s^#COPYRIGHT#^$COPYRIGHT^g; \
+	" > "$TARGET"
 }
 
 # create directories
@@ -132,58 +128,34 @@ mkdir "$PROJECTNAME" || exit 2
 mkdir "$PROJECTNAME/mk" || exit 3
 mkdir "$PROJECTNAME/src" || exit 4
 
-# copy jamrules
-cp -pr "$CSDIR/mk/jam" "$PROJECTNAME/mk"
-# copy autoconf macros
-cp -pr "$CSDIR/mk/autoconf" "$PROJECTNAME/mk"
+# copy autoconf macros and jamrules
+cp -pr "$CSDIR/mk/autoconf" "$CSDIR/mk/jam" "$PROJECTNAME/mk"
+rm -rf "$PROJECTNAME/mk/autoconf/CVS" "$PROJECTNAME/mk/jam/CVS"
 
 # copy templatefiles
 cp -p "$TEMPLATEDIR/autogen.template" "$PROJECTNAME/autogen.sh"
 chmod +x "$PROJECTNAME/autogen.sh"
-SetupFromTemplate "$TEMPLATEDIR/configure.template" "$PROJECTNAME/configure.ac"
-SetupFromTemplate "$TEMPLATEDIR/README.template" "$PROJECTNAME/README"
+Instantiate "$TEMPLATEDIR/configure.template" "$PROJECTNAME/configure.ac"
+Instantiate "$TEMPLATEDIR/README.template" "$PROJECTNAME/README"
+Instantiate "$TEMPLATEDIR/Jamfile-src.template" "$PROJECTNAME/src/Jamfile"
+Instantiate "$TEMPLATEDIR/main.template" "$PROJECTNAME/src/main.cpp"
 cp -p "$TEMPLATEDIR/Jamfile.template" "$PROJECTNAME/Jamfile"
 cp -p "$TEMPLATEDIR/Jamrules.template" "$PROJECTNAME/Jamrules"
 cp -p "$TEMPLATEDIR/initjamfile.m4" "$PROJECTNAME/mk/autoconf"
 cp -p "$TEMPLATEDIR/cs_check_host.m4" "$PROJECTNAME/mk/autoconf"
-cp -p "$CSDIR/mk/autoconf/crystal.m4" "$PROJECTNAME/mk/autoconf"
 
-# create some simple Jamfile and dummy file in src
-cat > "$PROJECTNAME/src/Jamfile" << __END__
-SubDir TOP src ;
-
-Application $PROJECTNAME
-	: [ Wildcard *.cpp *.h ]
-;
-ExternalLibs $PROJECTNAME : CRYSTAL ;
-Help $PROJECTNAME : "$LONGNAME" ;
-__END__
-
-cat > "$PROJECTNAME/src/main.cpp" << __END__
-#include <config.h>
-
-CS_IMPLEMENT_APPLICATION
-
-int main(int argc, char** argv)
-{
-  (void) argc;
-  (void) argv;
-  printf ("This is the insanely great $PROJECTNAME project.\n");
-  return 0;
-}
-__END__
-
-echo Running autoconf
+echo "*Running: autoheader autoconf"
 cd "$PROJECTNAME"
 ./autogen.sh
+rm -rf autom4te.cache
 
 # Display notice
 cat << "__EOF__"
 
-*Generation successfull.
+*Generation successful.
 
-You should now look at your project and try to customize it at your needs. Esp.
-the README file only contains some generic blabla now.
+You should now examine the generated project and customize it as needed.  In
+particular, the files README and main.cpp will require modification.
 
 Have fun with Crystal Space.
 __EOF__
