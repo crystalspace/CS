e98e345ad6957791e7e7965be51b6ca6242238e5
Eric Sunshine <sunshine@sunshineco.com>
2004-06-06 15:07:49 +0000

    Removed debugging goop.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@18109 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/walktest/walktest.cpp
+++ apps/walktest/walktest.cpp
@@ -668,7 +668,6 @@ void WalkTest::DrawFrame3D (int drawflags, csTicks /*current_time*/)
   //------------
   if (!do_covtree_dump)
   {
-printf ("================================\n");
     if (split == -1)
       view->Draw ();
     else 
