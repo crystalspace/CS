3145295833153abae0c72b6a231fd5c1c1ae409a
Eric Sunshine <sunshine@sunshineco.com>
2004-07-28 20:53:43 +0000

    Eric Sunshine fixed bug in csArray::DeleteRange() where it failed to destroy (invoke the destructor of) the last object in the range.  Reported by Bedrich Zaba <frogbride@seznam.cz>.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@18653 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/array.h
+++ include/csutil/array.h
@@ -874,12 +874,12 @@ public:
     if (start >= count) return;
     if (end < 0) return;
     if (start < 0) start = 0;
-    if (end >= count) end = count-1;
+    if (end >= count) end = count - 1;
     size_t i;
-    for (i = start ; i < end ; i++)
+    for (i = start ; i <= end ; i++)
       ElementHandler::Destroy (root + i);
 
-    size_t const range_size = end-start+1;
+    size_t const range_size = end - start + 1;
     size_t const ncount = count - range_size;
     size_t const nmove = count - end - 1;
     if (nmove > 0)
