69069958451bfc491282d69de76a4e09f9ea66cb
Eric Sunshine <sunshine@sunshineco.com>
2004-08-11 04:28:52 +0000

    Eliminated compilation warning on Linux PPC.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@18735 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/thing/object/lghtmap.cpp
+++ plugins/mesh/thing/object/lghtmap.cpp
@@ -307,7 +307,7 @@ const char* csLightMap::ReadFromCache (
     if (file->Read ((char*)lm_ptr, 3) != 3)
       return "File too short while reading static lightmap data!";
     lm_ptr += 3;
-    *(lm_ptr++) = -127;
+    *(lm_ptr++) = (char)-127;
   }
 
   //-------------------------------
