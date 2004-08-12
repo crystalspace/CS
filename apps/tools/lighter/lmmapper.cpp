f0ad4be42cba35fc18b9bc5b4817dbf40d457891
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2004-08-12 12:05:19 +0000

    - Jorrit added litLightMapMapper classes to the lighter application. 	  This class computes the mapping between a polygon and the lightmap 	  coordinates. Currently not used.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@18756 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ apps/tools/lighter/lmmapper.cpp
@@ -0,0 +1,89 @@
+/*
+    Copyright (C) 2004 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+#include "lmmapper.h"
+
+
+litLightMapMapper::litLightMapMapper ()
+{
+}
+
+void litLightMapMapper::InitializeMapping (const csArray<csVector3>& poly,
+  	const csPlane3& plane, float density)
+{
+  litLightMapMapper::density = density;
+
+  csVector3 abs_normal (fabs (plane.A ()),
+			fabs (plane.B ()),
+			fabs (plane.C ()));
+
+  // Find the axis from where we can see most of the polygon. i.e. the
+  // axis that maximizes the visible lightmap space.
+  if (abs_normal.x >= abs_normal.y && abs_normal.x >= abs_normal.z)
+    axis = LIT_AXIS_X;
+  else if (abs_normal.y >= abs_normal.x && abs_normal.y >= abs_normal.z)
+    axis = LIT_AXIS_Y;
+  else
+    axis = LIT_AXIS_Z;
+
+  // Set the offset to 0,0 to start with. That way we can already use
+  // PolyToLM() function.
+  offset.Set (0, 0);
+  // Now find the minimum u and v.
+  csVector2 min_uv;
+  PolyToLM (poly[0], min_uv);
+  size_t i;
+  for (i = 1 ; i < poly.Length () ; i++)
+  {
+    csVector2 uv;
+    PolyToLM (poly[i], uv);
+    if (uv.x < min_uv.x) uv.x = min_uv.x;
+    if (uv.y < min_uv.y) uv.y = min_uv.y;
+  }
+
+  // Make sure we have a properly aligned uv-mapping (aligning with
+  // adjacent polygons for example):
+  offset.x = floor (min_uv.x);
+  offset.y = floor (min_uv.y);
+}
+
+void litLightMapMapper::PolyToLM (const csVector3& vpoly, csVector2& vlm)
+{
+  switch (axis)
+  {
+    case LIT_AXIS_X:
+      vlm.x = vpoly.z * density + offset.x;
+      vlm.y = -vpoly.y * density + offset.y;
+      break;
+    case LIT_AXIS_Y:
+      vlm.x = vpoly.x * density + offset.x;
+      vlm.y = -vpoly.z * density + offset.y;
+      break;
+    case LIT_AXIS_Z:
+      vlm.x = vpoly.x * density + offset.x;
+      vlm.y = -vpoly.y * density + offset.y;
+      break;
+  }
+}
+
+void litLightMapMapper::LMToPoly (const csVector2& vlm, csVector3& vpoly)
+{
+  // @@@ TODO
+}
+
---
+++ apps/tools/lighter/lmmapper.h
@@ -0,0 +1,76 @@
+/*
+    Copyright (C) 2004 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __LMMAPPER_H__
+#define __LMMAPPER_H__
+
+#include <stdarg.h>
+#include <csutil/array.h>
+#include <csgeom/vector2.h>
+#include <csgeom/vector3.h>
+#include <csgeom/plane3.h>
+
+#define LIT_AXIS_X 0
+#define LIT_AXIS_Y 1
+#define LIT_AXIS_Z 2
+
+/**
+ * This class controls the mapping from polygon 3D coordinates
+ * to lightmap coordinates and vice versa.
+ */
+class litLightMapMapper
+{
+private:
+  // Axis that we selected to map this lightmap. We will always try
+  // to find the axis from which side you can view most of the lightmap.
+  // One of LIT_AXIS_...
+  int axis;
+
+  // Density of this mapping.
+  float density;
+
+  // Offset to get to the correct mapping.
+  csVector2 offset;
+
+public:
+  litLightMapMapper ();
+
+  /**
+   * Initialize this mapper for a given polygon.
+   * \param poly is the polygon for which we will calculate the mapping.
+   * \param plane is the plane of the polygon. The normal of this plane
+   *        must be normalized.
+   * \param density is the number of lightmap units in a world unit.
+   */
+  void InitializeMapping (const csArray<csVector3>& poly,
+  	const csPlane3& plane, float density);
+
+  /**
+   * Transform a point that must be on the polygon plane to a
+   * lightmap coordinate.
+   */
+  void PolyToLM (const csVector3& vpoly, csVector2& vlm);
+
+  /**
+   * Transform a lightmap coordinate to a point on the polygon.
+   */
+  void LMToPoly (const csVector2& vlm, csVector3& vpoly);
+};
+
+#endif // __LMMAPPER_H__
+
