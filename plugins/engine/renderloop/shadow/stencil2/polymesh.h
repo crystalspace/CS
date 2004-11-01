528a89df122638b38026aa7ab5622ed8ee7e0ace
Boyan Hristov <boyan_hristov@newmail.ru>
2004-11-01 12:01:54 +0000

    *** empty log message ***
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@19417 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ plugins/engine/renderloop/shadow/stencil2/polymesh.h
@@ -0,0 +1,157 @@
+/* 
+    Copyright (C) 2003 by Jorrit Tyberghein
+	      (C) 2003 by Frank Richter
+          (C) Hristo Hristov, Boyan Hristov
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_STENCIL2_POLYMESH_H__
+#define __CS_STENCIL2_POLYMESH_H__
+
+#include "csgeom/polymesh.h"
+#include "csgeom/pmtools.h"
+
+class csStencil2PolygonMesh : public csPolygonMesh
+{
+private:
+  csDirtyAccessArray<csVector3> verts;
+  csDirtyAccessArray<csVector3> face_normals;
+  csDirtyAccessArray<csMeshedPolygon> polys;
+  csDirtyAccessArray<int> vertidx;
+  int* secondary_vertidx;
+  csTriangle* triangles;
+  int tri_count;
+
+  void Triangulate ()
+  {
+    if (triangles) return;
+    csPolygonMeshTools::Triangulate (this, triangles, tri_count);
+  }
+
+  void CalcFaceNormals()
+  {
+	Triangulate();
+	face_normals.SetLength(0);
+	int i;
+    for (i = 0; i < tri_count; i ++) 
+	{ 
+		csVector3 face_normal = 
+			(verts[triangles[i].b] - verts[triangles[i].a]) % (verts[triangles[i].c] - verts[triangles[i].a]);
+		face_normal.Normalize();
+		face_normals.Push(face_normal);
+    }
+  }
+
+public:
+  csStencil2PolygonMesh ()
+  {
+    triangles = 0;
+    secondary_vertidx = 0;
+  }
+
+  virtual ~csStencil2PolygonMesh ()
+  {
+    delete[] secondary_vertidx;
+  }
+
+  /// AddPolys will take over ownership of vertidx.
+  void AddPolys (const csArray<csMeshedPolygon>& polysToAdd,
+  	int* vertidx)
+  {
+    CS_ASSERT (secondary_vertidx == 0);
+    secondary_vertidx = vertidx;
+    size_t i;
+    for (i = 0; i < polysToAdd.Length (); i++)
+    {
+      polys.Push (polysToAdd[i]);
+    }
+	
+	triangles = 0;
+	CalcFaceNormals();
+	ShapeChanged ();
+  }
+
+void CopyFrom (iPolygonMesh* polyMesh)
+{
+	delete[] secondary_vertidx;
+	secondary_vertidx = 0;
+
+	int numVerts = polyMesh->GetVertexCount ();
+	csVector3* oldVerts = polyMesh->GetVertices ();
+	verts.SetLength (numVerts);
+	memcpy (verts.GetArray (), oldVerts, sizeof (csVector3) * numVerts);
+
+	int numPolys = polyMesh->GetPolygonCount ();
+	csMeshedPolygon* oldPolys = polyMesh->GetPolygons ();
+	polys.SetLength (numPolys);
+
+	int i;
+	// First count how many vertex indices we need.
+	int totvertidx = 0;
+	for (i = 0 ; i < numPolys ; i++)
+		totvertidx += oldPolys[i].num_vertices;
+	vertidx.SetLength (totvertidx);
+	int* vertidx_p = vertidx.GetArray ();
+
+	for (i = 0; i < numPolys; i++)
+	{
+		csMeshedPolygon& poly = polys[i];
+		poly.num_vertices = oldPolys[i].num_vertices;
+		poly.vertices = vertidx_p;
+		memcpy (poly.vertices, oldPolys[i].vertices, 
+		poly.num_vertices * sizeof (int));
+		vertidx_p += poly.num_vertices;
+	}
+
+	CalcFaceNormals();
+
+	ShapeChanged ();
+}
+
+  virtual int GetVertexCount () 
+  { 
+    return verts.Length (); 
+  }
+  virtual csVector3* GetVertices () 
+  { 
+    return verts.GetArray (); 
+  }
+  virtual int GetPolygonCount () 
+  { 
+    return polys.Length (); 
+  }
+  virtual csMeshedPolygon* GetPolygons () 
+  { 
+    return polys.GetArray ();
+  }
+  virtual int GetTriangleCount ()
+  {
+    Triangulate ();
+    return tri_count;
+  }
+  virtual csTriangle* GetTriangles ()
+  {
+    Triangulate ();
+    return triangles;
+  }
+  csVector3* GetFaceNormals()
+  {
+    //CalcFaceNormals();
+    return face_normals.GetArray();
+  }
+};
+
+#endif // __CS_STENCIL2_POLYMESH_H__
---
+++ plugins/engine/renderloop/shadow/stencil2/stencil2.h
@@ -0,0 +1,230 @@
+/* 
+    Copyright (C) 2003 by Jorrit Tyberghein, Daniel Duhprey
+              (C) Hristo Hristov, Boyan Hristov
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_STENCIL2_H
+#define __CS_STENCIL2_H
+
+#include "csutil/ref.h"
+
+#include "iutil/objreg.h"
+#include "iutil/comp.h"
+#include "ivideo/graph3d.h"
+#include "ivideo/rndbuf.h"
+#include "ivideo/shader/shader.h"
+#include "iengine/rendersteps/irenderstep.h"
+#include "iengine/rendersteps/icontainer.h"
+#include "iengine/rendersteps/ilightiter.h"
+#include "iengine/viscull.h"
+#include "igeom/objmodel.h"
+#include "igeom/polymesh.h"
+#include "csutil/hash.h"
+#include "csutil/csstring.h"
+#include "csutil/strhash.h"
+#include "csutil/dirtyaccessarray.h"
+#include "csutil/weakref.h"
+#include "csgfx/shadervarcontext.h"
+#include "../../common/basesteptype.h"
+#include "../../common/basesteploader.h"
+#include "../../common/parserenderstep.h"
+
+class csStencil2ShadowStep;
+class csStencil2ShadowType;
+
+class csStencil2ShadowCacheEntry : public iObjectModelListener
+{
+private:
+	csStencil2ShadowStep* parent;
+	iObjectModel* model;
+	iMeshWrapper* meshWrapper;
+
+	struct csLightCacheEntry 
+	{
+		iLight* light;
+		csVector3 meshLightPos;
+		csRef<iRenderBuffer> shadow_index_buffer;
+		int edge_start, index_range;
+	};
+
+	csHash<csLightCacheEntry*, iLight*> lightcache;
+
+	csRef<iRenderBuffer> shadow_vertex_buffer;
+	csRef<iRenderBuffer> shadow_normal_buffer;
+	csRef<iRenderBuffer> active_index_buffer;
+
+	struct Edge
+	{
+		int v1, v2;
+		int face_1, face_2;
+	};
+
+	csHash<Edge *> edges_hash;
+	csArray<Edge *> edges;
+	csArray<int> silhouette_edges;
+
+	int vertex_count, triangle_count;
+	int edge_count;
+	csArray<csVector3> face_normals;
+	csDirtyAccessArray<int> edge_indices;
+	csArray<csVector3> edge_midpoints;
+	csArray<csVector3> edge_normals;
+
+	csStencil2PolygonMesh* closedMesh;
+	bool enable_caps;
+	bool meshShadows;
+public:
+	SCF_DECLARE_IBASE;
+	csShaderVariableContext *svcontext;
+
+	csStencil2ShadowCacheEntry (csStencil2ShadowStep* parent, 
+	iMeshWrapper* mesh);
+	virtual ~csStencil2ShadowCacheEntry ();
+
+	virtual void ObjectModelChanged (iObjectModel* model);
+	virtual iRenderBuffer *GetRenderBuffer (csStringID name);
+	void EnableShadowCaps () { enable_caps = true; }
+	void DisableShadowCaps () { enable_caps = false; }
+	bool ShadowCaps () { return enable_caps; }
+
+	bool MeshCastsShadow () { return meshShadows; }
+
+	void UpdateBuffers() ;
+
+	bool CalculateEdges();
+	void AddEdge(int index_v1, int index_v2, int face_index);
+	bool GetShadow(csVector3 &light_pos, float shadow_length, bool front_cap, bool extrusion,  bool back_cap,
+					csArray<csVector3> & shadow_vertices, csArray<int> & shadow_indeces);
+
+	void UpdateRenderBuffers(csArray<csVector3> & shadow_vertices, csArray<int> & shadow_indeces);
+};
+
+class csStencil2ShadowStep : public iRenderStep,
+			public iLightRenderStep,
+			public iRenderStepContainer
+{
+private:
+	friend class csStencil2ShadowCacheEntry;
+
+	iObjectRegistry* object_reg;
+	csWeakRef<iGraphics3D> g3d;
+	csWeakRef<iShaderManager> shmgr;
+	csRef<csStencil2ShadowType> type;
+
+	static csStringID shadow_vertex_name;
+	static csStringID shadow_normal_name; 
+	static csStringID shadow_index_name;
+
+	bool enableShadows;
+	csRefArray<iLightRenderStep> steps;
+
+	csArray<iMeshWrapper*> shadowMeshes;
+	csHash< csRef<csStencil2ShadowCacheEntry>, iMeshWrapper*> shadowcache;
+
+	void Report (int severity, const char* msg, ...);
+
+	void ModelInFrustum(csVector3 &light_pos, float shadow_length, csPlane3* frustum_planes, 
+						uint32& frustum_mask, csBox3 &model_bounding_box,
+						bool & front_cap_in_frustum, 
+						bool & extrusion_in_frustum,
+						bool & back_cap_in_frustum);
+
+	int CalculateShadowMethod(iRenderView *rview, csVector3 &light_pos,
+								const csReversibleTransform &t, csBox3 &model_bounding_box);
+
+	void DrawShadow(iRenderView *rview, int method, csStencil2ShadowCacheEntry * cache_entry, 
+				iMeshWrapper *mesh, csArray<csVector3> & shadow_vertices, csArray<int> & shadow_indeces, 
+				iShader* shader, size_t shaderTicket, size_t pass);
+
+public:
+	SCF_DECLARE_IBASE;
+
+	csStencil2ShadowStep (csStencil2ShadowType* type);
+	virtual ~csStencil2ShadowStep ();
+
+	bool Initialize (iObjectRegistry* objreg);
+
+	void Perform (iRenderView* rview, iSector* sector,
+	csShaderVarStack &stacks);
+	void Perform (iRenderView* rview, iSector* sector, iLight* light,
+	csShaderVarStack &stacks);
+
+	virtual int AddStep (iRenderStep* step);
+	virtual int GetStepCount ();
+
+	struct ShadowDrawVisCallback : public iVisibilityCullerListener
+	{
+	csStencil2ShadowStep* parent;
+
+	SCF_DECLARE_IBASE;
+	ShadowDrawVisCallback ();
+	virtual ~ShadowDrawVisCallback ();
+
+	virtual void ObjectVisible (iVisibilityObject *visobject, 
+		iMeshWrapper *mesh, uint32 frustum_mask);
+	} shadowDrawVisCallback;
+	friend struct ShadowDrawVisCallback;
+};
+
+class csStencil2ShadowFactory : public iRenderStepFactory
+{
+	iObjectRegistry* object_reg;
+	csRef<csStencil2ShadowType> type;
+public:
+	SCF_DECLARE_IBASE;
+
+	csStencil2ShadowFactory (iObjectRegistry* object_reg,
+		csStencil2ShadowType* type);
+	virtual ~csStencil2ShadowFactory ();
+
+	virtual csPtr<iRenderStep> Create ();
+};
+
+class csStencil2ShadowType : public csBaseRenderStepType
+{
+	csRef<iShader> shadow;
+	bool shadowLoaded;
+
+	void Report (int severity, const char* msg, ...);
+public:
+	csStencil2ShadowType (iBase* p);
+	virtual ~csStencil2ShadowType ();
+
+	virtual csPtr<iRenderStepFactory> NewFactory ();
+
+	iShader* GetShadow ();
+};
+
+class csStencil2ShadowLoader : public csBaseRenderStepLoader
+{
+	csRenderStepParser rsp;
+	csStringHash tokens; 
+
+#define CS_TOKEN_ITEM_FILE "plugins/engine/renderloop/shadow/stencil2/stencil2.tok"
+#include "cstool/tokenlist.h"
+
+public:
+	csStencil2ShadowLoader (iBase *p);
+	virtual ~csStencil2ShadowLoader ();
+
+	virtual bool Initialize (iObjectRegistry* object_reg);
+
+	virtual csPtr<iBase> Parse (iDocumentNode* node,
+		iLoaderContext* ldr_context, iBase* context);
+};
+
+#endif // __CS_STENCILEN_H
