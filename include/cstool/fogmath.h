736667083cd4ecb03fd8abe8e4a5c05cf2476e53
Anders Stenberg <dentoid@users.sourceforge.net>
2004-11-27 23:33:28 +0000

    Anders did the following changes: - Moved key/value reading & writing to syntax services, and added key/value loading to xml shaders. - Updated bugplug scene info dump to write child meshes too (and changed the method of indentation a bit) - Made DrawSimpleMesh actually care about the "dyndomain" member of the mesh - Added fog to NR. There are still issues though:     - Fog looks crappy when looking perpendicular to a portal. This is actually due to properties of z-based fog, but maybe could be tweaked to look better.     - All shaders aren't updated to support fog, only the standard or_lighting, best technique.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@19602 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/cstool/fogmath.h
@@ -0,0 +1,32 @@
+/*
+    Copyright (C) 2004 by Anders Stenberg
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_FOGCALC_H__
+#define __CS_FOGCALC_H__
+
+/**
+ * Calculates fog density for a certain depth in the range [0..1]
+ */
+inline float FogRamp (float depth)
+{
+  return 1.0 - exp (-depth*7);
+  //return depth;
+}
+
+#endif // __CS_FOGCALC_H__
+
