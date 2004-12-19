f1eec04a807d43d22dde361e5a75b8e85bb76821
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2004-12-19 11:10:22 +0000

    - Jorrit added a new PVS calculation tool. In the future this tool 	  will load a map, take all static geometry and then calculate the 	  PVS data for it which can be used by PVSvis. Currently this tool 	  is only a copy of simpmap tutorial.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@19800 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ apps/tools/pvscalc/pvscalc.cpp
@@ -0,0 +1,248 @@
+/*
+    Copyright (C) 2004 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "pvscalc.h"
+
+CS_IMPLEMENT_APPLICATION
+
+//-----------------------------------------------------------------------------
+
+PVSCalc::PVSCalc ()
+{
+  SetApplicationName ("CrystalSpace.PVSCalcMap");
+}
+
+PVSCalc::~PVSCalc ()
+{
+}
+
+void PVSCalc::ProcessFrame ()
+{
+  // First get elapsed time from the virtual clock.
+  csTicks elapsed_time = vc->GetElapsedTicks ();
+  // Now rotate the camera according to keyboard state
+  float speed = (elapsed_time / 1000.0) * (0.06 * 20);
+
+  iCamera* c = view->GetCamera();
+
+  if (kbd->GetKeyState (CSKEY_SHIFT))
+  {
+    // If the user is holding down shift, the arrow keys will cause
+    // the camera to strafe up, down, left or right from it's
+    // current position.
+    if (kbd->GetKeyState (CSKEY_RIGHT))
+      c->Move (CS_VEC_RIGHT * 4 * speed);
+    if (kbd->GetKeyState (CSKEY_LEFT))
+      c->Move (CS_VEC_LEFT * 4 * speed);
+    if (kbd->GetKeyState (CSKEY_UP))
+      c->Move (CS_VEC_UP * 4 * speed);
+    if (kbd->GetKeyState (CSKEY_DOWN))
+      c->Move (CS_VEC_DOWN * 4 * speed);
+  }
+  else
+  {
+    // left and right cause the camera to rotate on the global Y
+    // axis; page up and page down cause the camera to rotate on the
+    // _camera's_ X axis (more on this in a second) and up and down
+    // arrows cause the camera to go forwards and backwards.
+    if (kbd->GetKeyState (CSKEY_RIGHT))
+      rotY += speed;
+    if (kbd->GetKeyState (CSKEY_LEFT))
+      rotY -= speed;
+    if (kbd->GetKeyState (CSKEY_PGUP))
+      rotX += speed;
+    if (kbd->GetKeyState (CSKEY_PGDN))
+      rotX -= speed;
+    if (kbd->GetKeyState (CSKEY_UP))
+      c->Move (CS_VEC_FORWARD * 4 * speed);
+    if (kbd->GetKeyState (CSKEY_DOWN))
+      c->Move (CS_VEC_BACKWARD * 4 * speed);
+  }
+
+  // We now assign a new rotation transformation to the camera.  You
+  // can think of the rotation this way: starting from the zero
+  // position, you first rotate "rotY" radians on your Y axis to get
+  // the first rotation.  From there you rotate "rotX" radians on the
+  // your X axis to get the final rotation.  We multiply the
+  // individual rotations on each axis together to get a single
+  // rotation matrix.  The rotations are applied in right to left
+  // order .
+  csMatrix3 rot = csXRotMatrix3 (rotX) * csYRotMatrix3 (rotY);
+  csOrthoTransform ot (rot, c->GetTransform().GetOrigin ());
+  c->SetTransform (ot);
+
+  // Tell 3D driver we're going to display 3D things.
+  if (!g3d->BeginDraw (engine->GetBeginDrawFlags () | CSDRAW_3DGRAPHICS))
+    return;
+
+  // Tell the camera to render into the frame buffer.
+  view->Draw ();
+}
+
+void PVSCalc::FinishFrame ()
+{
+  // Just tell the 3D renderer that everything has been rendered.
+  g3d->FinishDraw ();
+  g3d->Print (0);
+}
+
+bool PVSCalc::OnKeyboard(iEvent& ev)
+{
+  // We got a keyboard event.
+  csKeyEventType eventtype = csKeyEventHelper::GetEventType(&ev);
+  if (eventtype == csKeyEventTypeDown)
+  {
+    // The user pressed a key (as opposed to releasing it).
+    utf32_char code = csKeyEventHelper::GetCookedCode(&ev);
+    if (code == CSKEY_ESC)
+    {
+      // The user pressed escape to exit the application.
+      // The proper way to quit a Crystal Space application
+      // is by broadcasting a cscmdQuit event. That will cause the
+      // main runloop to stop. To do that we get the event queue from
+      // the object registry and then post the event.
+      csRef<iEventQueue> q = 
+        CS_QUERY_REGISTRY(GetObjectRegistry(), iEventQueue);
+      if (q.IsValid()) q->GetEventOutlet()->Broadcast(cscmdQuit);
+    }
+  }
+  return false;
+}
+
+bool PVSCalc::OnInitialize(int argc, char* argv[])
+{
+  // RequestPlugins() will load all plugins we specify. In addition
+  // it will also check if there are plugins that need to be loaded
+  // from the config system (both the application config and CS or
+  // global configs). In addition it also supports specifying plugins
+  // on the commandline.
+  if (!csInitializer::RequestPlugins(GetObjectRegistry(),
+    CS_REQUEST_VFS,
+    CS_REQUEST_OPENGL3D,
+    CS_REQUEST_ENGINE,
+    CS_REQUEST_FONTSERVER,
+    CS_REQUEST_IMAGELOADER,
+    CS_REQUEST_LEVELLOADER,
+    CS_REQUEST_REPORTER,
+    CS_REQUEST_REPORTERLISTENER,
+    CS_REQUEST_END))
+    return ReportError("Failed to initialize plugins!");
+
+  // Now we need to setup an event handler for our application.
+  // Crystal Space is fully event-driven. Everything (except for this
+  // initialization) happens in an event.
+  if (!RegisterQueue(GetObjectRegistry()))
+    return ReportError("Failed to set up event handler!");
+
+  return true;
+}
+
+void PVSCalc::OnExit()
+{
+}
+
+bool PVSCalc::Application()
+{
+  // Open the main system. This will open all the previously loaded plug-ins.
+  // i.e. all windows will be opened.
+  if (!OpenApplication(GetObjectRegistry()))
+    return ReportError("Error opening system!");
+
+  // Now get the pointer to various modules we need. We fetch them
+  // from the object registry. The RequestPlugins() call we did earlier
+  // registered all loaded plugins with the object registry.
+  // The virtual clock.
+  g3d = CS_QUERY_REGISTRY(GetObjectRegistry(), iGraphics3D);
+  if (!g3d) return ReportError("Failed to locate 3D renderer!");
+
+  engine = CS_QUERY_REGISTRY(GetObjectRegistry(), iEngine);
+  if (!engine) return ReportError("Failed to locate 3D engine!");
+
+  vc = CS_QUERY_REGISTRY(GetObjectRegistry(), iVirtualClock);
+  if (!vc) return ReportError("Failed to locate Virtual Clock!");
+
+  kbd = CS_QUERY_REGISTRY(GetObjectRegistry(), iKeyboardDriver);
+  if (!kbd) return ReportError("Failed to locate Keyboard Driver!");
+
+  loader = CS_QUERY_REGISTRY(GetObjectRegistry(), iLoader);
+  if (!loader) return ReportError("Failed to locate Loader!");
+
+  // We need a View to the virtual world.
+  view.AttachNew(new csView (engine, g3d));
+  iGraphics2D* g2d = g3d->GetDriver2D ();
+  // We use the full window to draw the world.
+  view->SetRectangle (0, 0, g2d->GetWidth (), g2d->GetHeight ());
+
+  // Here we load our world from a map file.
+  if (!LoadMap ()) return false;
+
+  // Let the engine prepare all lightmaps for use and also free all images 
+  // that were loaded for the texture manager.
+  engine->Prepare ();
+
+  // these are used store the current orientation of the camera
+  rotY = rotX = 0;
+
+  // Now we need to position the camera in our world.
+  view->GetCamera ()->SetSector (room);
+  view->GetCamera ()->GetTransform ().SetOrigin (pos);
+
+  // This calls the default runloop. This will basically just keep
+  // broadcasting process events to keep the game going.
+  Run();
+
+  return true;
+}
+
+bool PVSCalc::LoadMap ()
+{
+  // Set VFS current directory to the level we want to load.
+  csRef<iVFS> VFS (CS_QUERY_REGISTRY (object_reg, iVFS));
+  VFS->ChDir ("/lev/partsys");
+  // Load the level file which is called 'world'.
+  if (!loader->LoadMapFile ("world"))
+    ReportError("Error couldn't load level!");
+
+  // Find the starting position in this level.
+  if (engine->GetCameraPositions ()->GetCount () > 0)
+  {
+    // There is a valid starting position defined in the level file.
+    iCameraPosition* campos = engine->GetCameraPositions ()->Get (0);
+    room = engine->GetSectors ()->FindByName (campos->GetSector ());
+    pos = campos->GetPosition ();
+  }
+  else
+  {
+    // We didn't find a valid starting position. So we default
+    // to going to room called 'room' at position (0,0,0).
+    room = engine->GetSectors ()->FindByName ("room");
+    pos = csVector3 (0, 0, 0);
+  }
+  if (!room)
+    ReportError("Can't find a valid starting position!");
+
+  return true;
+}
+
+/*---------------------------------------------------------------------*
+ * Main function
+ *---------------------------------------------------------------------*/
+int main (int argc, char* argv[])
+{
+  return PVSCalc().Main(argc, argv);
+}
---
+++ apps/tools/pvscalc/pvscalc.h
@@ -0,0 +1,111 @@
+/*
+    Copyright (C) 2004 by Jorrit Tyberghein
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __PVSCALC_H__
+#define __PVSCALC_H__
+
+#include <crystalspace.h>
+
+/**
+ * PVS calculator application. This is for the PVS visibility culler.
+ */
+class PVSCalc : public csApplicationFramework, public csBaseEventHandler
+{
+private:
+  /// A pointer to the 3D engine.
+  csRef<iEngine> engine;
+
+  /// A pointer to the map loader plugin.
+  csRef<iLoader> loader;
+
+  /// A pointer to the 3D renderer plugin.
+  csRef<iGraphics3D> g3d;
+
+  /// A pointer to the keyboard driver.
+  csRef<iKeyboardDriver> kbd;
+
+  /// A pointer to the virtual clock.
+  csRef<iVirtualClock> vc;
+
+  /// A pointer to the view which contains the camera.
+  csRef<iView> view;
+
+  /// A pointer to the sector the camera will be in.
+  iSector* room;
+
+  /// Position of camera in map.
+  csVector3 pos;
+
+  /// Current camera orientation.
+  float rotX, rotY;
+
+  /**
+   * Handle keyboard events - ie key presses and releases.
+   * This routine is called from the event handler in response to a 
+   * csevKeyboard event.
+   */
+  bool OnKeyboard (iEvent&);
+
+  /**
+   * Setup everything that needs to be rendered on screen. This routine
+   * is called from the event handler in response to a cscmdProcess
+   * broadcast message.
+   */
+  void ProcessFrame ();
+
+  /**
+   * Finally render the screen. This routine is called from the event
+   * handler in response to a cscmdFinalProcess broadcast message.
+   */
+  void FinishFrame ();
+
+  /// Here we will load our world from a map file.
+  bool LoadMap ();
+
+public:
+
+  /// Construct our game. This will just set the application ID for now.
+  PVSCalc ();
+
+  /// Destructor.
+  ~PVSCalc ();
+
+  /// Final cleanup.
+  void OnExit ();
+
+  /**
+   * Main initialization routine. This routine will set up some basic stuff
+   * (like load all needed plugins, setup the event handler, ...).
+   * In case of failure this routine will return false. You can assume
+   * that the error message has been reported to the user.
+   */
+  bool OnInitialize (int argc, char* argv[]);
+
+  /**
+   * Run the application.
+   * First, there are some more initialization (everything that is needed 
+   * by PVSCalc1 to use Crystal Space), then this routine fires up the main
+   * event loop. This is where everything starts. This loop will  basically
+   * start firing events which actually causes Crystal Space to function.
+   * Only when the program exits this function will return.
+   */
+  bool Application ();
+
+};
+
+#endif // __PVSCALC_H__
