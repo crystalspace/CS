d383e4ab28a7911c7c6a8e003119b8c85232bb40
Eric Sunshine <sunshine@sunshineco.com>
2005-03-27 21:05:11 +0000

    Eric Sunshine removed the hand-coded x86 assembly versions of csQsqrt() and csQisqrt(). In the best cases, the platform-supplied sqrt() function was 17-19 times faster than the assembly versions; in the worst cases, 2-4 times faster than the assembly versions. (The PowerPC assembly version of csQsqrt() written by Matt Reda, on the other hand, is 4-5 times faster than the MacOS/X-supplied sqrt(), so this assembly version remains.)
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@20686 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csqsqrt.h
+++ include/csqsqrt.h
@@ -1,7 +1,6 @@
 /*
-    Copyright (C) 2000 by Andrew Zabolotny (Intel version)
-    Copyright (C) 2002 by Matthew Reda <reda@mac.com> (PowerPC version)
     Fast computation of sqrt(x) and 1/sqrt(x)
+    Copyright (C) 2002 by Matthew Reda <reda@mac.com> (PowerPC version)
   
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -19,15 +18,16 @@
 */
 
 /**\file
- * Fast computation of sqrt(x) and 1/sqrt(x).<p>
- * Define CS_NO_QSQRT if you experience mysterious problems with CS which 
- * you think are related to this version of sqrt not behaving properly. If 
- * you find something like that I'd like to be notified of this so we can 
- * make sure this really is the problem.
+ * Fast computation of sqrt(x) and 1/sqrt(x).<p> Define CS_NO_QSQRT if you
+ * experience mysterious problems with CS which you think are related to the
+ * specially optimized csQsqrt() not behaving properly.
  */
 
 #ifndef __CS_CSQSQRT_H__
 #define __CS_CSQSQRT_H__
+
+#include "cssysdef.h"
+#include <math.h>
 
 /**
  * \addtogroup floating_point
@@ -49,117 +49,10 @@ static inline float csQisqrt (float x);
 
 /** @} */
 
-#if (!defined (CS_NO_QSQRT)) && defined (CS_PROCESSOR_X86) && defined (CS_COMPILER_GCC)
+#if !defined(CS_NO_QSQRT) && \
+    defined(CS_PROCESSOR_POWERPC) && defined(CS_COMPILER_GCC)
 
-/** \page SingleFloatFormat: Single-precision floating-point format (32 bits)
-    \internal 	
-	SEEEEEEE.EMMMMMMM.MMMMMMMM.MMMMMMMM
-	S: Sign (0 - positive, 1 - negative)
-	E: Exponent (plus 127, 8 bits)
-	M: Mantissa (23 bits)
-*/
-
-/**
- * This is a relatively small inline routine which will compute sqrt(x)
- * very quick, taking a square root is now just a little longer than
- * doing a division. The function is inline so that it runs at maximal
- * possible speed. It does eight multiplications but multiplication is
- * cheap on P5+ processors (3 clocks). On a Celeron CPU it takes
- * approximatively 50 clocks, while a division is 42 clocks.
- */
- 
-static inline float csQsqrt (float x)
-{
-  float ret;
-  static float c1=0.5f;
-  static float c2=1.5f;
-
-
-// Original C++ formulae:
-// float tmp = x;
-// *((unsigned *)&tmp) = (0xbe6f0000 - *((unsigned *)&tmp)) >> 1;
-// double h = x * 0.5;
-// double a = tmp;
-// a *= 1.5 - a * a * h;
-// a *= 1.5 - a * a * h;
-// return a * x;
-
-  // Use __volatile__ so that the compiler will not mess with this
-  // code. Under some versions of gcc in combination with -O2 optimize
-  // mode not using __volatile__ can cause errors.
-  __asm__ __volatile__ (
-		"flds	%1\n"			// x
-		"movl	$0xbe6f0000,%%eax\n"
-		"subl	%1,%%eax\n"
-		"shrl	$1,%%eax\n"
-		"movl	%%eax,%1\n"
-		"flds	%2\n"			// x 0.5
-		"fmul	%%st(1)\n"		// x h
-		"flds	%3\n"			// x h 1.5
-		"flds	%1\n"			// x h 1.5 a
-		"fld	%%st\n"			// x h 1.5 a a
-		"fmul	%%st\n"			// x h 1.5 a a*a
-		"fmul	%%st(3)\n"		// x h 1.5 a a*a*h
-		"fsubr	%%st(2)\n"		// x h 1.5 a 1.5-a*a*h
-		"fmulp	%%st(1)\n"		// x h 1.5 a
-		"fld	%%st\n"			// x h 1.5 a a
-		"fmul	%%st\n"			// x h 1.5 a a*a
-		"fmulp	%%st(3)\n"		// x a*a*h 1.5 a
-		"fxch\n"			// x a*a*h a 1.5
-		"fsubp  %%st,%%st(2)\n"		// x 1.5-a*a*h a
-		"fmulp	%%st(1)\n"		// x a
-		"fmulp	%%st(1)\n"		// a
-	: "=&t" (ret), "+m" (x) : "m" (c1), "m" (c2)
-	: "eax", "st(1)", "st(2)", "st(3)", "st(4)", "st(5)", "st(6)", "st(7)"
-  );
-  return ret;
-}
-
-/**
- * This routine is basically equivalent to csQsqrt() except that it returns
- * 1/sqrt(x) rather than the proper square root. It should be used anywhere
- * you need the inverse root (in 3D graphics it is a common situation),
- * because the routine is a little faster than csQsqrt() and also you avoid
- * a division (which gives you an overall 2X speedup).
- */
-static inline float csQisqrt (float x)
-{
-  float ret;
-  static float c1=0.5f;
-  static float c2=1.5f;
-  // Use __volatile__ so that the compiler will not mess with this
-  // code. Under some versions of gcc in combination with -O2 optimize
-  // mode not using __volatile__ can cause errors.
-  __asm__ __volatile__ (
-		"flds	%1\n"			// x
-		"movl	$0xbe6f0000,%%eax\n"
-		"subl	%1,%%eax\n"
-		"shrl	$1,%%eax\n"
-		"movl	%%eax,%1\n"
-		"flds	%2\n"			// x 0.5
-		"fmulp	%%st(1)\n"		// h
-		"flds	%3\n"			// h 1.5
-		"flds	%1\n"			// h 1.5 a
-		"fld	%%st\n"			// h 1.5 a a
-		"fmul	%%st\n"			// h 1.5 a a*a
-		"fmul	%%st(3)\n"		// h 1.5 a a*a*h
-		"fsubr	%%st(2)\n"		// h 1.5 a 1.5-a*a*h
-		"fmulp	%%st(1)\n"		// h 1.5 a
-		"fld	%%st\n"			// h 1.5 a a
-		"fmul	%%st\n"			// h 1.5 a a*a
-		"fmulp	%%st(3)\n"		// a*a*h 1.5 a
-		"fxch\n"			// a*a*h a 1.5
-		"fsubp  %%st,%%st(2)\n"		// 1.5-a*a*h a
-		"fmulp	%%st(1)\n"		// a
-	: "=t" (ret), "+m" (x) : "m" (c1), "m" (c2)
-	: "eax", "st(1)", "st(2)", "st(3)", "st(4)", "st(5)", "st(6)", "st(7)"
-  );
-  return ret;
-}
-
-#elif (!defined (CS_NO_QSQRT)) && defined (CS_PROCESSOR_POWERPC) && defined (CS_COMPILER_GCC)
-
-/**
+/*
  * Use the PowerPC fsqrte to get an estimate of 1/sqrt(x) Then apply two
  * Newton-Rhaphson refinement steps to get a more accurate response Finally
  * multiply by x to get x/sqrt(x) = sqrt(x).  Add additional refinement steps
@@ -183,7 +76,7 @@ static inline float csQsqrt(float x)
   return y0;
 };
 
-/**
+/*
  * Similar to csQsqrt() above, except we do not multiply by x at the end, and
  * return 1/sqrt(x).
  */
@@ -199,17 +92,10 @@ static inline float csQisqrt(float x)
   return y0;
 };
 
-#elif (!defined (CS_NO_QSQRT)) && defined (CS_PROCESSOR_X86) && defined (CS_COMPILER_MSVC)
-
-#include <math.h>
-static inline float csQsqrt (float x) { return sqrtf(x); }
-static inline float csQisqrt(float x) { return 1.0f / sqrtf(x); }
-
 #else
 
-#include <math.h>
-static inline float csQsqrt (float x) { return (float)sqrt(x); }
-static inline float csQisqrt(float x) { return (float)(1.0 / sqrt(x)); }
+static inline float csQsqrt (float x) { return sqrtf(x); }
+static inline float csQisqrt(float x) { return 1.0f / sqrtf(x); }
 
 #endif
 
