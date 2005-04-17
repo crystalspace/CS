e168125700fca6c8dae0b6cd420ad660143b7015
Frank Richter <res@crystalspace3d.org>
2005-04-17 12:54:42 +0000

    res fixed csShaderVariable::operator=() to correctly handle   accessors. res updated the API mod docs for the iMaterialHandle removal.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@20932 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csgfx/shadervar.cpp
+++ libs/csgfx/shadervar.cpp
@@ -23,65 +23,74 @@
 //CS_LEAKGUARD_IMPLEMENT (csShaderVariable);
 
 csShaderVariable::csShaderVariable (csStringID name) :
-  csRefCount (), TextureHandValue (0), TextureWrapValue (0), VectorValue (0),
-  MatrixValuePtr(0), TransformPtr (0), accessor (0), Name (name)
+  csRefCount (), VectorValue (0), MatrixValuePtr(0), TransformPtr (0), 
+  Name (name)
 {
 }
 
 csShaderVariable& csShaderVariable::operator= (csShaderVariable& copyFrom)
 {
-  switch (copyFrom.Type)
+  Name = copyFrom.Name;
+  if (copyFrom.accessor != 0)
   {
-    case INT:
-      {
-        int val;
-        copyFrom.GetValue (val); 
-        SetValue (val);
-      }
-      break;
-    case TEXTURE:
-      {
-	if (copyFrom.TextureWrapValue != 0)
-        {
-          iTextureWrapper *val;
-          copyFrom.GetValue (val);  SetValue (val);
-        }
-	else
-        {
-          iTextureHandle* val;
+    Type = copyFrom.Type;
+    accessor = copyFrom.accessor;
+  }
+  else
+  {
+    switch (copyFrom.Type)
+    {
+      case INT:
+	{
+	  int val;
+	  copyFrom.GetValue (val); 
+	  SetValue (val);
+	}
+	break;
+      case TEXTURE:
+	{
+	  if (copyFrom.TextureWrapValue != 0)
+	  {
+	    iTextureWrapper *val;
+	    copyFrom.GetValue (val);  SetValue (val);
+	  }
+	  else
+	  {
+	    iTextureHandle* val;
+	    copyFrom.GetValue (val);  SetValue (val);
+	  }
+	}
+	break;
+      case RENDERBUFFER:
+	{
+	  iRenderBuffer* val;
 	  copyFrom.GetValue (val);  SetValue (val);
-        }
-      }
-      break;
-    case RENDERBUFFER:
-      {
-        iRenderBuffer* val;
-	copyFrom.GetValue (val);  SetValue (val);
-      }
-      break;
-    case FLOAT: // Stored in VectorValue.
-    case COLOR: // Ditto.
-    case VECTOR2:
-    case VECTOR3:
-    case VECTOR4:
-      {
-	csVector4 v; 
-	copyFrom.GetValue (v); SetValue (v);
-	Type = copyFrom.Type;
-      }
-      break;
-    case MATRIX:
-      {
-	csMatrix3 v;
-	copyFrom.GetValue(v); SetValue(v);
-      }
-      break;
-    case TRANSFORM:
-      {
-	csReversibleTransform v;
-	copyFrom.GetValue(v); SetValue(v);
-      }
-      break;
+	}
+	break;
+      case FLOAT: // Stored in VectorValue.
+      case COLOR: // Ditto.
+      case VECTOR2:
+      case VECTOR3:
+      case VECTOR4:
+	{
+	  csVector4 v; 
+	  copyFrom.GetValue (v); SetValue (v);
+	  Type = copyFrom.Type;
+	}
+	break;
+      case MATRIX:
+	{
+	  csMatrix3 v;
+	  copyFrom.GetValue(v); SetValue(v);
+	}
+	break;
+      case TRANSFORM:
+	{
+	  csReversibleTransform v;
+	  copyFrom.GetValue(v); SetValue(v);
+	}
+	break;
+    }
   }
   return *this;
 }
