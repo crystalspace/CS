dc2f0f42089515f8fe2f18112431dcc2c73659f2
Eric Sunshine <sunshine@sunshineco.com>
2005-07-21 09:22:57 +0000

    Syntax correction.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@21815 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csgfx/imagecubemapmaker.h
+++ include/csgfx/imagecubemapmaker.h
@@ -39,7 +39,7 @@
 class CS_CRYSTALSPACE_EXPORT csImageCubeMapMaker : public csImageBase
 {
 protected:
-  enum { NUM_FACES = 6; };
+  enum { NUM_FACES = 6 };
   /// The cube face images
   csRef<iImage> cubeImages[NUM_FACES];
   /**
