6abd028e92a8aaad25ec2b72d576b1b1e494b293
Eric Sunshine <sunshine@sunshineco.com>
2005-07-24 09:39:40 +0000

    Fixed less-than-useful comments.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@21834 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/iengine/camera.h
+++ include/iengine/camera.h
@@ -233,13 +233,13 @@ struct iCamera : public iBase
 
   /**
    * Calculate perspective corrected point for this camera.
-   * \deprecated Use Perspective() instead.
+   * \deprecated Use Perspective(const csVector3&) instead.
    */
   CS_DEPRECATED_METHOD virtual void Perspective (const csVector3& v,
   	csVector2& p) const = 0;
   /**
    * Calculate inverse perspective corrected point for this camera.
-   * \deprecated Use Perspective() instead.
+   * \deprecated Use InvPerspective(const csVector2&) instead.
    */
   CS_DEPRECATED_METHOD virtual void InvPerspective (const csVector2& p, float z,
   	csVector3& v) const = 0;
