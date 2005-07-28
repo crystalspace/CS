2e2fc1c94e871d081d1359b6784439c36d570126
Frank Richter <res@crystalspace3d.org>
2005-07-28 15:01:28 +0000

    res worked around namespace clashes of the Opcode in CS with the   Opcode in ODE. (Corrected issues GCC didn't catch)
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@21869 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/collide/opcode/Ice/IcePreprocessor.h
+++ plugins/collide/opcode/Ice/IcePreprocessor.h
@@ -108,15 +108,6 @@
 		#pragma intrinsic(labs)
 	#endif
 
-	// ANSI compliance
-	#ifdef  _DEBUG
-		// Remove painful warning in debug
-		inline_ bool __False__(){ return false; }
-		#define for if(__False__()){}	else for
-	#else
-		#define for if(0){}	else for
-	#endif
-
 	// Silly Cygwin defines _X in ctypes.h
 	#ifdef _X
 	#undef _X
