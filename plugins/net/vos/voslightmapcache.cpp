5707f7e3765a7e737d7fc4c11bb9563218b96b78
Peter Amstutz <tetron@student.umass.edu>
2005-08-13 21:23:41 +0000

    forgot to add these files
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@21997 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ plugins/net/vos/voslightmapcache.cpp
@@ -0,0 +1,68 @@
+#include "cssysdef.h"
+#include "voslightmapcache.h"
+
+SCF_IMPLEMENT_IBASE(LightmapCache)
+  SCF_IMPLEMENTS_INTERFACE(iCacheManager)
+SCF_IMPLEMENT_IBASE_END
+
+using namespace VUtil;
+using namespace VOS;
+
+LightmapCache::LightmapCache(Property* target)
+    : property(target, true)
+{
+}
+
+void LightmapCache::SetCurrentType (const char* type)
+{
+    LOG("LightmapCache", 3, "SetCurrentType " << type);
+}
+
+
+const char* LightmapCache::GetCurrentType () const
+{
+    return "";
+}
+
+void LightmapCache::SetCurrentScope (const char* scope)
+{
+    if(scope) { LOG("LightmapCache", 3, "SetCurrentScope " << scope); }
+    else { LOG("LightmapCache", 3, "SetCurrentScope NULL"); }
+}
+
+const char* LightmapCache::GetCurrentScope () const
+{
+    return "";
+}
+
+bool LightmapCache::CacheData (const void* data, unsigned long size, const char* type,
+                               const char* scope, unsigned int id)
+{
+    LOG("lightmapcache", 3, "caching " << size << " bytes");
+    property->replace(std::string((char*)data, size));
+    return true;
+}
+
+csPtr<iDataBuffer> LightmapCache::ReadCache (const char* type,
+                                             const char* scope,
+                                             uint32 id)
+{
+    std::string s = property->read();
+    char* m = new char[s.size()];
+    memcpy(m, s.c_str(), s.size());
+
+    LOG("lightmapcache", 3, "reading cache " << s.size() << " bytes");
+
+    return csPtr<iDataBuffer>(new csDataBuffer(m, s.size()));
+}
+
+bool LightmapCache::ClearCache (const char* type, const char* scope,
+                                const uint32* id)
+{
+    return true;
+}
+
+void LightmapCache::Flush()
+{
+}
+
---
+++ plugins/net/vos/voslightmapcache.h
@@ -0,0 +1,39 @@
+#ifndef _LIGHTMAPCACHE_H_
+#define _LIGHTMAPCACHE_H_
+
+#include <vos/metaobjects/property/property.hh>
+#include <iutil/cache.h>
+#include <imesh/lighting.h>
+#include <csutil/databuf.h>
+
+class LightmapCache : public iCacheManager
+{
+private:
+    VUtil::vRef<VOS::Property> property;
+
+public:
+    SCF_DECLARE_IBASE;
+
+    LightmapCache(VOS::Property* target);
+    virtual ~LightmapCache() { };
+    virtual void SetCurrentType (const char* type);
+
+    virtual const char* GetCurrentType () const;
+
+    virtual void SetCurrentScope (const char* scope);
+
+    virtual const char* GetCurrentScope () const;
+
+    virtual bool CacheData (const void* data, unsigned long size,
+                            const char* type, const char* scope, unsigned int id);
+
+    virtual csPtr<iDataBuffer> ReadCache (
+        const char* type, const char* scope, uint32 id);
+
+    virtual bool ClearCache (const char* type = 0, const char* scope = 0,
+                             const uint32* id = 0);
+
+    virtual void Flush();
+};
+
+#endif
