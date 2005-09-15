710c6fb0038b5c4f17115ae5625655500ed8053e
Frank Richter <res@crystalspace3d.org>
2005-09-15 18:15:32 +0000

    res added a "Search" field to the online API docs.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@22317 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- docs/support/annotate/transform.sh
+++ docs/support/annotate/transform.sh
@@ -13,6 +13,10 @@ for d in $DIRS; do
       -e "s/href *= *\"(?!http\:\/\/)([^\"]*)\.html([^\"]*)\"/href=\"\$1.php\$2\"/gi" \
       -e "s:<title>(.*)</title>:<title>\$1</title><?php \\\$theme=\"\$1\"; ?>:i" \
       -e "s:</body>:<?php require(\"annotate.php\"); ?></body>:i" \
+      -e "s!(<a class=\"qindex\" href=\"pages.php\">Related&nbsp;Pages</a>)!\
+\$1 | Search: <form class=\"search\" action=\"/cgi-bin/htsearch\" method=\"get\">\
+<input class=\"search\" type=\"text\" name=\"words\" value=\"\" size=\"20\"/>\
+</form>!i" \
       $d
     cp docs/support/annotate/*.php $d
   fi
