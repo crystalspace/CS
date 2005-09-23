de1ac0137c118c214693ba54460b4d215bb49bac
Eric Sunshine <sunshine@sunshineco.com>
2005-09-23 12:40:24 +0000

    jobber.pl isn't smart enough to 'cvs add' isndsys.h automatically, so do so manually.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@22411 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/isndsys.h
@@ -0,0 +1,36 @@
+/* isndsys.h -- Generated automatically; do not edit. */
+#ifndef __ISNDSYS_H__
+#define __ISNDSYS_H__
+/*
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+  
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+  
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+/**\file 
+ * Directory master header. This header file includes all headers in a
+ * subdirectory of the top CrystalSpace include directory.
+ */
+#include "cssysdef.h"
+#include "isndsys/ss_data.h"
+#include "isndsys/ss_driver.h"
+#include "isndsys/ss_filter.h"
+#include "isndsys/ss_listener.h"
+#include "isndsys/ss_loader.h"
+#include "isndsys/ss_manager.h"
+#include "isndsys/ss_renderer.h"
+#include "isndsys/ss_source.h"
+#include "isndsys/ss_stream.h"
+#include "isndsys/ss_structs.h"
+#endif /* __ISNDSYS_H__ */
+
