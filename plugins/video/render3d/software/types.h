506d51fd7c364fcf0c4cf09a6233b89ad232e560
Frank Richter <res@crystalspace3d.org>
2005-10-01 17:37:27 +0000

    Interpolation fix
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@22587 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/render3d/software/types.h
+++ plugins/video/render3d/software/types.h
@@ -111,7 +111,9 @@ namespace cspluginSoft3d
   };
   struct InterpolateScanlinePerspCommon
   {
+    /// 1/z and per-pixel delta
     csFixed24 Iz, dIzdx;
+    /// 1/f - float for better accuracy, delta is for 1 interpolation span
     float Iz_f, dIzdx_f;
   };
 
@@ -120,12 +122,16 @@ namespace cspluginSoft3d
   {
     struct PerFloat1
     {
+      /// "Direct" component
       csFixed16 c;
+      /// "Direct" component delta
       csFixed16 dcdx;
     };
     struct PerFloat2
     {
+      /// 1/component
       float Ic;
+      /// 1/component delta
       float dIcdx;
     };
     PerFloat1 floats[maxFloats];
@@ -147,13 +153,14 @@ namespace cspluginSoft3d
       Iz = Iz_f = L.Iz;
       dIzdx = dIzdx_f = (R.Iz - L.Iz) * inv_l;
       dIzdx_f *= ipf;
+      Iz_f += dIzdx_f;
+      float z2 = 1.0f / Iz_f;
       for (size_t f = 0; f < maxFloats; f++)
       {
 	const float cL = L.Floats[f].c;
 	const float IcL = L.Floats[f].Ic;
 	const float IcR = R.Floats[f].Ic;
 	floats[f].c = cL;
-	float z2 = 1.0f/(Iz_f + dIzdx_f);
 	floats_f[f].dIcdx = (IcR - IcL) * fact;
 	floats_f[f].Ic = IcL + floats_f[f].dIcdx;
 	floats[f].dcdx = (floats_f[f].Ic*z2 - floats[f].c) >> InterpolShift;
@@ -171,9 +178,12 @@ namespace cspluginSoft3d
       }
       else
       {
+	/* Note: Iz_f is "ahead" one interpolation span, ie when the
+	 * deltas are set up for the next, it has the value from the
+	 * beginning if that span. */
 	const float z = 1.0f/Iz_f;
-	Iz_f += dIzdx_f;
 	Iz = Iz_f;
+	Iz_f += dIzdx_f;
 	const float z2 = 1.0f / Iz_f;
 	ipx = InterpolStep;
 	for (size_t f = 0; f < maxFloats; f++)
