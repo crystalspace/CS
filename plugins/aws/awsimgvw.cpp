531ce3b25dad67230e0863ddd5eaac34f91c7314
Eric Sunshine <sunshine@sunshineco.com>
2005-10-02 02:09:41 +0000

    Eric Sunshine fixed bug in awsImageView::Setup() where its invocation of iAwsPrefManager::GetSring("Image") would fail every time, unconditionally. Reported by manolopm@cip.es.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@22592 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/aws/awsimgvw.cpp
+++ plugins/aws/awsimgvw.cpp
@@ -60,18 +60,13 @@ bool awsImageView::Setup (iAws *_wmgr, iAwsComponentNode *settings)
   pm->GetInt (settings, "Style", frame_style);
   pm->GetInt (settings, "Alpha", alpha_level); // Local overrides, if present.
 
-  iString *file = 0;
-  pm->GetString (settings, "Image", file);
-  if (file) 
-  {
-    unsigned char r=0,g=0,b=0;
-    pm->GetRGB (settings, "KeyColor", r, g, b);
-    img1 = pm->GetTexture (file->GetData (), file->GetData ());
-  }
-
+  unsigned char r=0,g=0,b=0;
+  scfString file;
+  pm->GetString (settings, "Image", &file);
+  pm->GetRGB (settings, "KeyColor", r, g, b);
+  img1 = pm->GetTexture (file.GetData (), file.GetData ());
   img2 = pm->GetTexture ("Texture");
 
-  unsigned char r,g,b;
   if (pm->GetRGB (settings, "Color", r, g, b))
   {
     draw_color = true;
