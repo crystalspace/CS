846e58bfb9303441ecdca735d467ae918a67745d
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2005-11-15 05:46:02 +0000

    Jorrit fixed a bug in HardTransform() of genmesh. It didn't transform the normals which would give bad results on lighting.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@22882 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/genmesh/object/genmesh.cpp
+++ plugins/mesh/genmesh/object/genmesh.cpp
@@ -2447,6 +2447,8 @@ void csGenmeshMeshObjectFactory::HardTransform (
   int i;
   for (i = 0 ; i < num_mesh_vertices ; i++)
     mesh_vertices[i] = t.This2Other (mesh_vertices[i]);
+  for (i = 0 ; i < num_mesh_vertices ; i++)
+    mesh_normals[i] = t.This2OtherRelative (mesh_normals[i]);
 
   mesh_vertices_dirty_flag = true;
 
