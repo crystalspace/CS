72ef1b52b948682fd5a24f58133dc3aeb05ebb3f
Frank Richter <res@crystalspace3d.org>
2005-11-16 22:23:05 +0000

    Make more sense
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@22902 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/render3d/opengl/gl_r2t_framebuf.cpp
+++ plugins/video/render3d/opengl/gl_r2t_framebuf.cpp
@@ -162,7 +162,8 @@ void csGLRender2TextureFramebuf::FinishDraw ()
 	}
       }
       if (needSubImage)
-	glCopyTexSubImage2D (textarget, 0, 0, 0, 0, 0, txt_w, txt_h);
+	glCopyTexSubImage2D (textarget, 0, 0, 0, 0, 0, 
+	  G3D->viewwidth, G3D->viewheight);
       else
 	glCopyTexImage2D (textarget, 0, GL_RGBA, 0, 0, txt_w, txt_h, 0);
       tex_mm->SetNeedMips (true);
