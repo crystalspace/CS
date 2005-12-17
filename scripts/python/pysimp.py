3e3552c0599c0877fcf509c885b791a0e30a4abc
Pablo Martin <caedesv@users.sourceforge.net>
2005-12-17 19:49:27 +0000

    uploaded the fixes to pysimp (forgot to checkin the corrections).
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@23094 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- scripts/python/pysimp.py
+++ scripts/python/pysimp.py
@@ -24,9 +24,9 @@ def CreateRoom(matname):
     engine = CS_QUERY_REGISTRY(object_reg, iEngine)
     room = engine.GetSectors().FindByName("room")
     walls = engine.CreateSectorWallsMesh(room,"walls")
-    thingstate = SCF_QUERY_INTERFACE(walls.GetMeshObject(), iThingState)
-    material=engine.GetMaterialList().FindByName(matname)
-    walls_state = thingstate.GetFactory()
+    walls_factory = walls.GetMeshObject().GetFactory()
+    material = engine.GetMaterialList().FindByName(matname)
+    walls_state = SCF_QUERY_INTERFACE(walls_factory, iThingFactoryState)
     walls_state.AddInsideBox (csVector3 (-5, 0, -5), csVector3 (5, 20, 5))
     walls_state.SetPolygonMaterial (CS_POLYRANGE_LAST, material);
     walls_state.SetPolygonTextureMapping (CS_POLYRANGE_LAST, 3);
