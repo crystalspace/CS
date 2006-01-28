90cd2b3ad50c01b2cee0ad9a2028ade54003ec66
CS management scripts <crystal-manage@users.sourceforge.net>
2006-01-28 01:34:05 +0000

    Automated master header file repair.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@23436 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csgfx.h
+++ include/csgfx.h
@@ -44,6 +44,7 @@
 #include "csgfx/shadervarcontext.h"
 #include "csgfx/shadervarframeholder.h"
 #include "csgfx/vertexlight.h"
+#include "csgfx/vertexlistwalker.h"
 #include "csgfx/xorpat.h"
 #endif /* __CSGFX_H__ */
 
