b265704328b11463df08841bb1ad4a682f5e31cd
Christopher Nelson <paradox@bbhc.org>
2006-03-06 03:37:16 +0000

    Updated the titlebar. Much prettier now.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@23704 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- data/aws/widgets/Styles.js
+++ data/aws/widgets/Styles.js
@@ -272,41 +272,36 @@ Style3D =
 {
 	TitleBar : function(pen)
 	{
-		prefs = Skin.current;
-		
+		var prefs = Skin.current;
+				
 		var w = this.width, h = this.height;
-		var cw = w * prefs.TitleBarRoundness, aw=cw*2.0;		
+		var aw = prefs.TitleBarHeight, cw=aw>>1;		
+		var tbw = prefs.WindowMin.w + prefs.WindowZoom.w + prefs.WindowClose.w + 6;
 		
 		pen.Clear();
 		
 		// Draw the frame.
 		pen.SetColor(prefs.FillColor);		
-		pen.DrawArc(0,0,aw,h, Math.PI, Math.PI*1.6, true);
-		pen.DrawArc(w-aw,0,w,h, Math.PI*1.5, Math.PI*2.1, true);
-		pen.DrawRect(cw,0,w-cw,h, true);
-		pen.DrawRect(0,h>>1, cw,h, true);
-		pen.DrawRect(w-cw,h>>1, w,h, true);		
+		pen.DrawArc(0,0,aw,h, Math.PI*0.5, Math.PI*1.5, true);
+		pen.DrawRect(cw,0,w,h, true);
 		
+		// Background color
+		pen.SetColor(prefs.ActiveTitleBarColor1);
+		pen.DrawRect(cw+5,5, w-tbw, h-5, true);
 		
-		pen.SetColor(0,0,0,1);
-		pen.DrawArc(0,0,aw,h, Math.PI, Math.PI*1.6, false);
-		pen.DrawLine(0,h>>1,0,h);
-		pen.DrawLine(cw,0,w-cw,0);
-		
-		pen.DrawArc(w-aw,0,w,h, Math.PI*1.5, Math.PI*2.1, false);
-		pen.DrawLine(w,h>>1,w,h);
-		pen.DrawLine(0,h,w,h);
-		
-		pen.SetColor(prefs.HighlightColor);		
-		pen.DrawArc(1,1,aw,h, Math.PI, Math.PI*1.6, false);
-		pen.DrawLine(1,h>>1,1,h);
-		pen.DrawLine(cw,1,w-cw,1);
-		
+		// Setup pen
 		pen.SetColor(prefs.ShadowColor);
-		pen.DrawArc(w-aw,1,w,h, Math.PI*1.5, Math.PI*2.1, false);
-		pen.DrawLine(w-1,h>>1,w-1,h);
-		pen.DrawLine(0,h-1,w,h-1);
+		pen.SwapColors();
+		pen.SetColor(prefs.HighlightColor);
 		
+		// Make the highlights
+		pen.DrawArc(0,0,aw,h, Math.PI*0.5, Math.PI*1.5, false);
+		pen.DrawLine(cw,0, w,0);
+		pen.SwapColors();
+		pen.DrawLine(w,0, w,h);
+		pen.DrawLine(cw,h,w,h);
+				
+		pen.DrawRect(cw+5,5, w-tbw, h-5, false, true);		
 	},
 	
 	ScrollBar : function(pen)
