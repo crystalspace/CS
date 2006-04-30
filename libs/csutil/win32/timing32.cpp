b1d048809c09cd9468acc2662820e4ab7e7f8514
Andrew Mann <amann@tccgi.com>
2006-04-30 19:46:46 +0000

    - Changed the calculation logic into whole and fractional second parts to avoid overflow. This fixes a problem where time would appear to move backwards after about an hour on a 2.4 ghz CPU. (shorter on faster CPUs, longer on slower CPUs) - Added an assertion when the time reported by the OS moves backwards. This can happen with some AMD X2 processors and requires OS level workarounds.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@24167 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csutil/win32/timing32.cpp
+++ libs/csutil/win32/timing32.cpp
@@ -54,10 +54,12 @@ csTicks csGetTicks ()
 // Based on code by Thomas Hieber,  03/20/1999
 int64 csGetMicroTicks()
 {
-  // Storage for the initial Get*Ticks() invocation call
-  static int64 FirstCount = 0;
+  // Storage for the base count from which the delta is calculated.
+  static int64 BaseCount = 0;
   // Storage for the performance timer frequency
   static int64 Freq      = 0;
+  // Storage for the number of whole seconds that have elapsed since we've started
+  static int64 WholeSecs = 0;
 
   //Freq was set to -1, if the current Hardware does not support
   //high resolution timers. We will use GetTickCount instead then.
@@ -75,14 +77,43 @@ int64 csGetMicroTicks()
       return GetTickCount() * 1000;
     }
     // Start counting from first time this function is called.
-    QueryPerformanceCounter((LARGE_INTEGER*)&FirstCount);
+    QueryPerformanceCounter((LARGE_INTEGER*)&BaseCount);
   }
 
   //retrieve current count
   int64 Count = 0;
   QueryPerformanceCounter((LARGE_INTEGER*)&Count);
 
-  return 1000000*(Count-FirstCount)/Freq;
+  // The number of elapsed counts from the base
+  int64 ElapsedRaw=(Count-BaseCount);
+
+  // 2006/04/30 - There are some AMD X2 processors out now that incorrectly halt the instruction counter when
+  //              one of the cores is put to sleep.  This can cause time to appear to jump around (forward and back)
+  //              as the process is run on one core or another.  Workarounds are documented on various web sites.
+  CS_ASSERT_MSG("Timing error! Your system has reported a backwards-moving time value!", ElapsedRaw>=0);
+
+  // There's not much that can be done about this.  Let's presume time hasn't moved and hope we get scheduled
+  //  on the same logical processor we started on sometime soon.
+  if (ElapsedRaw < 0)
+    ElapsedRaw = 0;
+
+  // If we've passed a whole second, move it into the whole seconds accumulator
+  if (ElapsedRaw >= Freq)
+  {
+    int64 ElapsedSecs = ElapsedRaw / Freq;
+    // Add whole seconds to the appropriate accumulator
+    WholeSecs += ElapsedSecs;
+    // Add the elapsed whole seconds to the base count in the form of raw count
+    BaseCount += (ElapsedSecs * Freq);
+    // Remove from the elapsed count in the form of raw count
+    ElapsedRaw -= (ElapsedSecs * Freq);
+  }
+
+  // Counters on fast systems may count at the clock rate of the CPU.  A CPU running at
+  //  2.4 ghz will cause the calculation of (1000000 * ElapsedCycles) to overflow a 64
+  //  bit signed integer at about an hour of runtime. By breaking the calculation up
+  //  in this manner, we extend runtime before overflow to 1,000,000 hours on such a system
+  return (1000000 * WholeSecs) + ((1000000 * ElapsedRaw) / Freq);
 }
 
 void csSleep (int SleepTime)
