3dfac16fb213af873590459dacdc63508b643da5
Frank Richter <res@crystalspace3d.org>
2006-05-24 14:58:38 +0000

    Make that typename_qualifier.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@24348 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/hash.h
+++ include/csutil/hash.h
@@ -356,7 +356,8 @@ public:
   /// Get all the elements with the given key, or empty if there are none.
   csArray<T> GetAll (const K& key) const
   {
-    return GetAll<typename csArray<T>::ElementHandlerType, typename csArray<T>::AllocatorType> (key);
+    return GetAll<typename_qualifier csArray<T>::ElementHandlerType, 
+      typename_qualifier csArray<T>::AllocatorType> (key);
   }
 
   /// Get all the elements with the given key, or empty if there are none.
