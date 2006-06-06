1967be42d0763b2976c2d4f7a148caaf594df177
Eric Sunshine <sunshine@sunshineco.com>
2006-06-06 21:21:16 +0000

    Added EXGTENSIVE_MEMDEBUG / MEMORY_TRACKER goop to radixsort.h to work around incompatiblities with #include <new>.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@24474 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/radixsort.h
+++ include/csutil/radixsort.h
@@ -25,6 +25,11 @@
  */
 
 #include "csextern.h"
+
+// Hack: Work around problems caused by #defining 'new'.
+#if defined(CS_EXTENSIVE_MEMDEBUG) || defined(CS_MEMORY_TRACKER)
+# undef new
+#endif
 #include <new>
 
 /**
@@ -145,5 +150,9 @@ private:
 
   bool ranksValid;
 };
+
+#if defined(CS_EXTENSIVE_MEMDEBUG) || defined(CS_MEMORY_TRACKER)
+# define new CS_EXTENSIVE_MEMDEBUG_NEW
+#endif
 
 #endif
