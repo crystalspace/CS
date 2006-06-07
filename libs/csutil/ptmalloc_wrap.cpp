efa68ebacac58bf667456de9f245e74b16d5d397
Frank Richter <res@crystalspace3d.org>
2006-06-07 01:48:22 +0000

    res augmented the ptmalloc wrapper to do some error detection in   debug mode.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/feature/ptmalloc@24479 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csutil/ptmalloc_wrap.cpp
+++ libs/csutil/ptmalloc_wrap.cpp
@@ -17,6 +17,7 @@
 */
 
 #include "cssysdef.h"
+#include "csutil/csendian.h"
 
 // ptmalloc functions
 extern "C"
@@ -32,16 +33,113 @@ namespace CS
 {
   namespace Memory
   {
+  #ifdef CS_DEBUG
+    /* In debug mode, a small "cookie" is placed in front and after the memory
+     * returned by the allocators in order to detect corruption, and, since
+     * the cookie value is different per module, freeing memory across 
+     * modules. */
+    typedef time_t CookieType;
+    static CookieType cookie;
+    static CookieType GetCookie (void* p)
+    {
+      while (cookie == 0)
+      {
+        // Semi-random
+        time (&cookie);
+        // Make somewhat unique
+        cookie = cookie ^ (CookieType)&cookie;
+      }
+      return csSwapBytes::Swap (cookie) ^ CookieType (p);
+    }
+  #endif
+
     void* ptmalloc (size_t n)
-    { return ::ptmalloc (n); }
-    void ptfree (void* p)
-    { ::ptfree (p); }
-    void* ptrealloc (void* p, size_t n)
-    { return ::ptrealloc (p, n); }
+    { 
+    #ifdef CS_DEBUG
+      uint8* p = 
+        (uint8*)::ptmalloc (n + sizeof (size_t) + 2*sizeof (CookieType)); 
+      const CookieType startCookie = GetCookie (p);
+      const CookieType endCookie = csSwapBytes::Swap (startCookie);
+      // Write allocated size(needed for checks in free()) and cookies.
+      *((size_t*)p) = n;
+      p += sizeof (size_t);
+      *((CookieType*)p) = startCookie;
+      p += sizeof (cookie);
+      *((CookieType*)(p + n)) = endCookie;
+      // Pepper.
+      memset (p, 0xca, n);
+      return p;
+    #else
+      return ::ptmalloc (n); 
+    #endif
+    }
+    void ptfree (void* P)
+    { 
+    #ifdef CS_DEBUG
+      if (P == 0) return;
+      // Compute original allocated address
+      uint8* p = (uint8*)P;
+      p -= sizeof(CookieType);
+      const CookieType startCookie = GetCookie (p - sizeof(size_t));
+      const CookieType endCookie = csSwapBytes::Swap (startCookie);
+      // Verify cookies
+      CS_ASSERT(*(CookieType*)p == startCookie);
+      p -= sizeof(size_t);
+      size_t n = *((size_t*)p);
+      CS_ASSERT(*(CookieType*)((uint8*)P + n) == endCookie);
+      // Salt.
+      memset (p, 0xcf, n + sizeof (size_t) + 2*sizeof (CookieType));
+      ::ptfree (p); 
+    #else
+      ::ptfree (p); 
+    #endif
+    }
+    void* ptrealloc (void* P, size_t n)
+    { 
+    #ifdef CS_DEBUG
+      if (P == 0) return ptmalloc (n);
+      // Compute original allocated address
+      uint8* p = (uint8*)P;
+      p -= sizeof(CookieType);
+      // Verify cookies
+      const CookieType startCookie = GetCookie (p - sizeof(size_t));
+      const CookieType endCookie = csSwapBytes::Swap (startCookie);
+      CS_ASSERT(*(CookieType*)p == startCookie);
+      p -= sizeof(size_t);
+      size_t nOld = *((size_t*)p);
+      CS_ASSERT(*(CookieType*)((uint8*)P + nOld) == endCookie);
+
+      uint8* np = 
+        (uint8*)::ptrealloc (p, n + sizeof (size_t) + 2*sizeof (CookieType)); 
+      // Cookie may have changed since the memory address may have changed,
+      // update
+      const CookieType newStartCookie = GetCookie (np);
+      const CookieType newEndCookie = csSwapBytes::Swap (newStartCookie);
+      *((size_t*)np) = n;
+      np += sizeof (size_t);
+      *((CookieType*)np) = newStartCookie;
+      np += sizeof (cookie);
+      *((CookieType*)(np + n)) = newEndCookie;
+      // Spice the enlarged area
+      if (n > nOld)
+        memset (np + nOld, 0xca, n-nOld);
+      return np;
+    #else
+      return ::ptrealloc (p, n); 
+    #endif
+    }
     void* ptmemalign (size_t a, size_t n)
-    { return ::ptmemalign (a, n); }
+    { 
+      return ::ptmemalign (a, n); 
+    }
     void* ptcalloc (size_t n, size_t s)
-    { return ::ptcalloc (n, s); }
+    { 
+    #ifdef CS_DEBUG
+      return ::ptmalloc (n * s); 
+    #else
+      return ::ptcalloc (n, s); 
+    #endif
+    }
   } // namespace Memory
 } // namespace CS
 
