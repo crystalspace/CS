27f822e60c9ec2eb150be33cd18525144dcc7eb4
Frank Richter <res@crystalspace3d.org>
2006-09-16 14:13:56 +0000

    res declared the weak event listener functions as exported and put   them into the CS namespace.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@25360 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/genmesh/skelanim/gmeshskelanim.cpp
+++ plugins/mesh/genmesh/skelanim/gmeshskelanim.cpp
@@ -1671,7 +1671,7 @@ csGenmeshSkelAnimationControlTypeOld::~csGenmeshSkelAnimationControlTypeOld ()
   {
     csRef<iEventQueue> q = CS_QUERY_REGISTRY (object_reg, iEventQueue);
     if (q)
-      RemoveWeakListener (q, weakEventHandler);
+      CS::RemoveWeakListener (q, weakEventHandler);
   }
 }
 
@@ -1682,7 +1682,7 @@ bool csGenmeshSkelAnimationControlTypeOld::Initialize (iObjectRegistry* object_r
   csRef<iEventQueue> q = CS_QUERY_REGISTRY (object_reg, iEventQueue);
   vc = CS_QUERY_REGISTRY (object_reg, iVirtualClock);
   if (q != 0)
-    RegisterWeakListener (q, this, PreProcess, weakEventHandler);
+    CS::RegisterWeakListener (q, this, PreProcess, weakEventHandler);
   return true;
 }
 
