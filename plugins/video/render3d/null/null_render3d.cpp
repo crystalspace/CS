a1ae78e543c6218e7c7f3f85a6faea61c2eed719
Frank Richter <res@crystalspace3d.org>
2006-10-18 15:51:32 +0000

    res merged r25517 into 1.0 (remove pointless null3d message).
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/release/V1.0@25636 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/render3d/null/null_render3d.cpp
+++ plugins/video/render3d/null/null_render3d.cpp
@@ -157,14 +157,9 @@ bool csNullGraphics3D::Open ()
     w = h = -1;
     return false;
   }
-  bool fs = G2D->GetFullScreen ();
 
   pfmt = *G2D->GetPixelFormat ();
   SetDimensions (G2D->GetWidth (), G2D->GetHeight());
-
-  csReport (object_reg, CS_REPORTER_SEVERITY_NOTIFY,
-    "crystalspace.render3d.null", "Using %s mode %dx%d.",
-    fs ? "full screen" : "windowed", w, h);
 
   SetPerspectiveAspect (G2D->GetHeight ());
   SetPerspectiveCenter (G2D->GetWidth ()/2, G2D->GetHeight ()/2);
