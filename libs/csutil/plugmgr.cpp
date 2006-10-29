da8672403e389d2cdd0b5378073e6e92be16330f
Frank Richter <res@crystalspace3d.org>
2006-10-29 14:25:54 +0000

    res changed the plugin manager to use csRef<>s instead of explicit   reference counting for retaining iComponent references. Also   simplified some code in LoadPlugin().
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@25734 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csutil/plugmgr.cpp
+++ libs/csutil/plugmgr.cpp
@@ -45,7 +45,6 @@ csPluginManager::csPlugin::~csPlugin ()
 {
   //csPrintf ("DecRef %08p/'%s' ref=%d\n", Plugin, ClassID, Plugin->GetRefCount ()); fflush (stdout);
   delete [] ClassID;
-  Plugin->DecRef ();
 }
 
 //------------------------------------------------------------------------
@@ -172,15 +171,7 @@ void csPluginManager::QueryOptions (iComponent *obj)
 
 iBase *csPluginManager::LoadPlugin (const char *classID, bool init)
 {
-  iComponent *p = 0;
-  { 
-    // The reference must be held beyond the scope of this block.
-    csRef<iComponent> dummy (SCF_CREATE_INSTANCE (classID, iComponent));
-    if (dummy) {
-      p = dummy;
-      p->IncRef ();
-    }
-  }
+  csRef<iComponent> p (scfCreateInstance<iComponent> (classID));
   
   if (!p)
   {
@@ -191,7 +182,7 @@ iBase *csPluginManager::LoadPlugin (const char *classID, bool init)
   else
   {
     csScopedMutexLock lock (mutex);
-    size_t index = (size_t)-1;
+    size_t index = csArrayItemNotFound;
     // See if the plugin is already in our plugin list.
     for (size_t i = 0 ; i < Plugins.Length () ; i++)
     {
@@ -204,46 +195,23 @@ iBase *csPluginManager::LoadPlugin (const char *classID, bool init)
 	}
     }
 
-    bool added_here = false;
-    if (index == (size_t)-1)
+    if (index == csArrayItemNotFound)
     {
       // The plugin wasn't in our plugin list yet. Add it here.
       index = Plugins.Push (new csPlugin (p, classID));
-      added_here = true;
     }
 
     if ((!init) || p->Initialize (object_reg))
     {
       p->IncRef();
-      if (p)
-      {
-        if (!added_here)
-	{
-	  // If we didn't add the plugin (i.e. this is not the first time
-	  // we called LoadPlugin() for this plugin) then we need to
-	  // DecRef() the component to avoid memory leaks.
-	  p->DecRef ();
-	}
-
-        if (init) QueryOptions (p);
-        return p;
-      }
-      else
-      {
-        if (!added_here)
-	{
-	  // If we didn't add the plugin (i.e. this is not the first time
-	  // we called LoadPlugin() for this plugin) then we need to
-	  // DecRef() the component to avoid memory leaks.
-	  p->DecRef ();
-	}
-      }
+      if (init) QueryOptions (p);
+      return p;
     }
     csReport (object_reg, CS_REPORTER_SEVERITY_WARNING,
     	"crystalspace.pluginmgr.loadplugin",
     	"failed to initialize plugin '%s'", classID);
     // If we added this plugin in this call then we remove it here as well.
-    if (added_here)
+    if (index != csArrayItemNotFound)
       Plugins.DeleteIndex (index);
   }
   return 0;
@@ -257,7 +225,6 @@ bool csPluginManager::RegisterPlugin (const char *classID,
   if (obj->Initialize (object_reg))
   {
     QueryOptions (obj);
-    obj->IncRef ();
     return true;
   }
   else
