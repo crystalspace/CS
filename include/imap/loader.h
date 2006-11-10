d405e8eb08746bb46411e863e12d0a4993288a6a
Jorrit Tyberghein <jorrit.tyberghein@gmail.com>
2006-11-10 14:15:23 +0000

    Added \deprecated and bumped up iLoader SCF version.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@25858 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/imap/loader.h
+++ include/imap/loader.h
@@ -133,7 +133,7 @@ struct iMissingLoaderData : public virtual iBase
  */
 struct iLoader : public virtual iBase
 {
-  SCF_INTERFACE (iLoader, 1, 0, 0);
+  SCF_INTERFACE (iLoader, 2, 0, 0);
 
   /**
    * Load an image file. The image will be loaded in the format requested by
@@ -256,6 +256,8 @@ struct iLoader : public virtual iBase
   /**
    * New Sound System: Load a sound file, create a stream and create a
    * wrapper object for it.
+   * \deprecated Use LoadSoundStream() without mode3d parameter. The mode
+   * should be given at the time the stream is created later.
    * \param name of the sound.
    * \param fname is the VFS filename.
    * \param mode3d is one of CS_SND3D_DISABLE, CS_SND3D_RELATIVE, or
