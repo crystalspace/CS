c3f66f19923a821b96f92f8acd2bf9ca4c62d1ab
Frank Richter <res@crystalspace3d.org>
2006-11-21 00:31:09 +0000

    API docs tweaks
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@25902 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/threading/win32_mutex.h
+++ include/csutil/threading/win32_mutex.h
@@ -24,7 +24,7 @@
 
 #if !defined(CS_PLATFORM_WIN32)
 #error "This file is only for Windows and requires you to include csysdefs.h before"
-#endif
+#else
 
 namespace CS
 {
@@ -194,4 +194,6 @@ namespace Implementation
 } // namespace Threading
 } // namespace CS
 
-#endif
+#endif // !defined(CS_PLATFORM_WIN32)
+
+#endif // __CS_CSUTIL_THREADING_WIN32_MUTEX_H__
