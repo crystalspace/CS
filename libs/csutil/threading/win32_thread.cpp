500a6212b8ec5f7529b4dd2cf812dd9c20f77ace
Marten Svanfeldt <developer@svanfeldt.com>
2006-11-23 22:31:13 +0000

    - Marten rewrote the ThreadedJobQueue to not have any internal deadlocks and in the process rewrote it to use the new threading subsystem.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@25929 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/csutil/threading/win32_thread.cpp
+++ libs/csutil/threading/win32_thread.cpp
@@ -75,7 +75,7 @@ namespace Implementation
     class ThreadStartParams
     {
     public:
-      ThreadStartParams (Runnable* runner, int32& isRunningFlag)
+      ThreadStartParams (Runnable* runner, int32* isRunningFlag)
         : runnable (runner), isRunning (isRunningFlag)
       {
       }
@@ -84,20 +84,15 @@ namespace Implementation
       void Wait ()
       {
         ScopedLock<Mutex> lock (mutex);
-        while (!isRunning)
+        while (!(*isRunning))
           startCondition.Wait (mutex);
       }
 
       void Started ()
       {
         ScopedLock<Mutex> lock (mutex);
-        AtomicOperations::Set (&isRunning, 1);
+        AtomicOperations::Set (isRunning, 1);
         startCondition.NotifyOne ();
-      }
-
-      void Stopped ()
-      {
-        AtomicOperations::Set (&isRunning, 0);
       }
 
     
@@ -105,18 +100,20 @@ namespace Implementation
       Condition startCondition;
 
       Runnable* runnable;
-      int32& isRunning;
+      int32* isRunning;
     };
 
     unsigned int __stdcall proxyFunc (void* param)
     {
       ThreadStartParams* tp = static_cast<ThreadStartParams*> (param);
 
+      int32* isRunning = tp->isRunning;
+
       tp->Started ();
 
       tp->runnable->Run ();
 
-      tp->Stopped ();
+      AtomicOperations::Set (isRunning, 0);
 
       return 0;
     }
@@ -129,11 +126,16 @@ namespace Implementation
   {
   }
 
+  ThreadBase::~ThreadBase ()
+  {
+
+  }
+
   void ThreadBase::Start ()
   {
     if (!threadHandle)
     {
-      ThreadStartParams param (runnable, isRunning);
+      ThreadStartParams param (runnable, &isRunning);
 
       // _beginthreadex does not always return a void*,
       // on some versions of MSVC it gives uintptr_t
