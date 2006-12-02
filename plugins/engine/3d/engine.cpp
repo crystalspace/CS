crystalspace/CS

commit abc3cfa27b8bddd49e3f949a7a1f19eff0039896

res fixed a couple of VC deprecation warnings (collection and scene   node array related).
git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@25992 8cc4aa7f-3514-0410-904f-f2cc9021211c

plugins/engine/3d/engine.cpp
@@ -244,6 +244,9 @@ iCameraPosition *csCameraPositionList::FindByName (
 }
 
 //---------------------------------------------------------------------------
+
+#include "csutil/win32/msvc_deprecated_warn_off.h"
+
 csCollectionList::csCollectionList ()
   : scfImplementationType (this)
 {
@@ -302,6 +305,8 @@ iCollection *csCollectionList::FindByName (
 {
   return collections.FindByName (Name);
 }
+
+#include "csutil/win32/msvc_deprecated_warn_on.h"
 
 //---------------------------------------------------------------------------
 
@@ -912,9 +917,8 @@ void csEngine::ResetWorldSpecificSettings()
 {
   SetClearZBuf (defaultClearZBuf);
   SetClearScreen (defaultClearScreen);
-  csRef<iThingEnvironment> te (SCF_QUERY_INTERFACE (
-          GetThingType (),
-          iThingEnvironment));
+  csRef<iThingEnvironment> te (scfQueryInterface<iThingEnvironment> (
+          GetThingType ()));
   te->SetLightmapCellSize (16);
   SetMaxLightmapSize (defaultMaxLightmapWidth, defaultMaxLightmapHeight);
   SetAmbientLight (csColor (
@@ -1076,12 +1080,12 @@ void csEngine::AddMeshAndChildren (iMeshWrapper* mesh)
 {
   meshes.Add (mesh);
   // @@@ Consider no longer putting child meshes on main engine list???
-  const csRefArray<iSceneNode>& children = mesh->QuerySceneNode ()
-  	->GetChildren ();
+  const csRef<iSceneNodeArray> children = mesh->QuerySceneNode ()
+    ->GetChildrenArray ();
   size_t i;
-  for (i = 0 ; i < children.Length () ; i++)
+  for (i = 0 ; i < children->GetSize() ; i++)
   {
-    iMeshWrapper* mesh = children[i]->QueryMesh ();
+    iMeshWrapper* mesh = children->Get (i)->QueryMesh ();
     if (mesh)
       AddMeshAndChildren (mesh);
   }
@@ -1374,11 +1378,12 @@ void csEngine::PrecacheMesh (iMeshWrapper* s, iRenderView* rview)
   if (s->GetMeshObject ())
     s->GetMeshObject ()->GetRenderMeshes (num, rview,
       s->GetMovable (), 0xf);
-  const csRefArray<iSceneNode>& children = s->QuerySceneNode ()->GetChildren ();
+  const csRef<iSceneNodeArray> children = s->QuerySceneNode ()
+    ->GetChildrenArray ();
   size_t i;
-  for (i = 0 ; i < children.Length () ; i++)
+  for (i = 0 ; i < children->GetSize () ; i++)
   {
-    iMeshWrapper* mesh = children[i]->QueryMesh ();
+    iMeshWrapper* mesh = children->Get (i)->QueryMesh ();
     if (mesh)
       PrecacheMesh (mesh, rview);
   }
@@ -1812,6 +1817,8 @@ iCameraPosition* csEngine::FindCameraPosition (const char* name,
   return campos;
 }
 
+#include "csutil/win32/msvc_deprecated_warn_off.h"
+
 iCollection* csEngine::FindCollection (const char* name,
 	iRegion* reg)
 {
@@ -1829,6 +1836,8 @@ iCollection* csEngine::FindCollection (const char* name,
     col = GetCollections ()->FindByName (n);
   return col;
 }
+
+#include "csutil/win32/msvc_deprecated_warn_on.h"
 
 void csEngine::ReadConfig (iConfigFile *Config)
 {
@@ -3140,13 +3149,13 @@ csPtr<iMeshWrapper> csEngine::CreatePortal (
   csRef<iPortalContainer> pc;
   if (name)
   {
-    const csRefArray<iSceneNode>& children = parentMesh->QuerySceneNode ()->
-    	GetChildren ();
+    const csRef<iSceneNodeArray> children = mesh->QuerySceneNode ()
+      ->GetChildrenArray ();
     size_t i;
-    for (i = 0 ; i < children.Length () ; i++)
+    for (i = 0 ; i < children->GetSize(); i++)
     {
       // @@@ Not efficient.
-      iMeshWrapper* mesh = children[i]->QueryMesh ();
+      iMeshWrapper* mesh = children->Get (i)->QueryMesh ();
       if (mesh)
       {
         if (!strcmp (name, mesh->QueryObject ()->GetName ()))

include/csutil/nobjvec.h
@@ -34,6 +34,13 @@
  * Named Object Vector class
  */
 
+/* Workaround for deprecation warnings caused by types used as the template 
+ * parameter */
+#include "csutil/win32/msvc_deprecated_warn_off.h"
+
+/**\addtogroup util_containers
+ * @{ */
+
 /**
  * This class implements a typed array that correctly keeps track
  * of reference count and also is able to find by name. Assumes
@@ -72,5 +79,8 @@ public:
   }
 };
 
+/** @} */
+
+#include "csutil/win32/msvc_deprecated_warn_on.h"
 
 #endif // __CS_NOBJVEC_H__

include/csutil/scf_impl.h
@@ -118,6 +118,13 @@
 #define SCF_IMPL_CAT_I(a, b) SCF_IMPL_CAT_II(a ## b)
 #define SCF_IMPL_CAT_II(res) res
 
+/* The deprecation turn-off may seem odd here - but MSVC emits
+ * deprecation warnings when using the destructor of a deprecated
+ * class. Since interfaces may be deprecated this may cause 
+ * deprecation warnings emitted in ~SCF_IMPL_NAME, which is kinda silly.
+ */
+#include "csutil/win32/msvc_deprecated_warn_off.h"
+
 template<class Class SCF_IMPL_TYPES>
 class CS_CRYSTALSPACE_EXPORT SCF_IMPL_NAME :
   public SCF_IMPL_SUPER
@@ -342,6 +349,8 @@ private:
 #endif
   }
 };
+
+#include "csutil/win32/msvc_deprecated_warn_on.h"
 
 #undef SCF_IMPL_NAME
 #undef SCF_IMPL_SUPER

include/iengine/collectn.h
@@ -68,6 +68,8 @@ struct CS_DEPRECATED_TYPE_MSG ("Collections are obsolete. Don't use then")
   virtual iObject* GetObject (int i) const = 0;
 };
 
+#include "csutil/win32/msvc_deprecated_warn_off.h"
+
 /**
  * iCollection list.
  *
@@ -78,7 +80,8 @@ struct CS_DEPRECATED_TYPE_MSG ("Collections are obsolete. Don't use then")
  *   - Engine stores them.
  *   - Application uses them.
  */
-struct iCollectionList : public virtual iBase
+struct CS_DEPRECATED_TYPE_MSG ("Collections are obsolete. Don't use then")
+  iCollectionList : public virtual iBase
 {
   SCF_INTERFACE(iCollectionList,2,0,0);
   /// Create a new collection.
@@ -108,6 +111,8 @@ struct iCollectionList : public virtual iBase
   /// Find a collection by name.
   virtual iCollection *FindByName (const char *Name) const = 0;
 };
+
+#include "csutil/win32/msvc_deprecated_warn_on.h"
 
 #endif // __CS_IENGINE_COLLECTN_H__
 

include/iengine/engine.h
@@ -1261,6 +1261,7 @@ struct iEngine : public virtual iBase
     /// Get the list of all shared variables.
   virtual iSharedVariableList* GetVariableList () const = 0;
 
+#include "csutil/win32/msvc_deprecated_warn_off.h"
   /**
    * Get the list of collections.
    * \deprecated Collections are obsolete.
@@ -1284,6 +1285,7 @@ struct iEngine : public virtual iBase
   CS_DEPRECATED_METHOD_MSG("Collections are obsolete")
   virtual iCollection* FindCollection (const char* name,
   	iRegion* region = 0) = 0;
+#include "csutil/win32/msvc_deprecated_warn_on.h"
 
   /**
    * Convenience function to 'remove' a CS object from the engine.

include/iengine/region.h
@@ -38,7 +38,6 @@ struct iObject;
 struct iSector;
 struct iTextureWrapper;
 
-
 /**
  * A region. A region is basically a collection of objects in the
  * 3D engine that can be treated as a unit.
@@ -106,8 +105,10 @@ struct iRegion : public virtual iBase
   virtual iMaterialWrapper *FindMaterial (const char *iName) = 0;
   /// Find a camera position in this region by name
   virtual iCameraPosition *FindCameraPosition (const char *iName) = 0;
+#include "csutil/win32/msvc_deprecated_warn_off.h"
   /// Find a collection in this region by name
   virtual iCollection *FindCollection (const char *iName) = 0;
+#include "csutil/win32/msvc_deprecated_warn_on.h"
 
   /**
    * Check if some object is in this region.

plugins/engine/3d/cscoll.h
@@ -30,6 +30,8 @@ class csCollectionList;
 class csEngine;
 class csSector;
 
+#include "csutil/win32/msvc_deprecated_warn_off.h"
+
 /**
  * A collection object is for convenience of the script language.
  * It simply groups objects which are related in some way.
@@ -84,5 +86,7 @@ private:
 
   csCollectionList* collections;
 };
+
+#include "csutil/win32/msvc_deprecated_warn_on.h"
 
 #endif // __CS_CSCOLL_H__

plugins/engine/3d/engine.h
@@ -125,6 +125,8 @@ private:
   iLight* FetchNext ();
 };
 
+#include "csutil/win32/msvc_deprecated_warn_off.h"
+
 /**
  * List of collections for the engine. This class implements iCollectionList.
  */
@@ -150,6 +152,8 @@ public:
 private:
   csRefArrayObject<iCollection> collections;
 };
+
+#include "csutil/win32/msvc_deprecated_warn_on.h"
 
 struct csSectorPos
 {
@@ -201,6 +205,8 @@ public:
   virtual void FreeMesh (iMeshWrapper*);
 };
 
+
+#include "csutil/win32/msvc_deprecated_warn_off.h"
 
 /**
  * The 3D engine.
@@ -991,5 +997,7 @@ private:
   csEventID CanvasClose;
   csRef<iEventHandler> weakEventHandler;
 };
+
+#include "csutil/win32/msvc_deprecated_warn_on.h"
 
 #endif // __CS_ENGINE_H__

commit 20c3deb97ac07d6bbd404ea4c9999561c8491dd7

res fixed a crash in csEngine::CreatePortal() introduced in r25992.
git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@26017 8cc4aa7f-3514-0410-904f-f2cc9021211c

plugins/engine/3d/engine.cpp
@@ -3149,7 +3149,7 @@ csPtr<iMeshWrapper> csEngine::CreatePortal (
   csRef<iPortalContainer> pc;
   if (name)
   {
-    const csRef<iSceneNodeArray> children = mesh->QuerySceneNode ()
+    const csRef<iSceneNodeArray> children = parentMesh->QuerySceneNode ()
       ->GetChildrenArray ();
     size_t i;
     for (i = 0 ; i < children->GetSize(); i++)