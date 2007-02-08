ff79dc5689c5404901a0081ddeaa92cff84b0941
Frank Richter <res@crystalspace3d.org>
2007-02-08 15:10:04 +0000

    res split csutil/threading/atomicops.h up into multiple files (one   per implementation).
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@26356 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/threading/atomicops.h
+++ include/csutil/threading/atomicops.h
@@ -116,302 +116,47 @@ namespace Threading
       return Impl::CompareAndSet (target, (void*)0, (void*)0);
     }
   };
+}
+}
 
 #if defined (CS_PLATFORM_WIN32) && \
-    defined (CS_COMPILER_MSVC)      //Windows and MSVC, use intrinsics
+    defined (CS_COMPILER_MSVC)
 
-#pragma warning(push)
-#pragma warning(disable: 4311)
+  #include "csutil/threading/atomicops_msvc.h"
 
-  class AtomicOperationsMSVC
-  {
-  public:
-    inline static int32 Set (int32* target, int32 value)
-    {
-      return (int32)_InterlockedExchange ((long*)target, value);
-    }
-
-    inline static void* Set (void** target, void* value)
-    {
-#if CS_PROCESSOR_SIZE == 64
-      return (void*)_InterlockedExchange64 ((__int64*)target, (__int64)value);
-#else
-      return (void*)_InterlockedExchange ((long*)target, (long)value);
-#endif
-    }
-
-    inline static int32 CompareAndSet (int32* target, int32 value,
-                                       int32 comparand)
-    {
-      return (int32)_InterlockedCompareExchange ((long*)target,
-        (long)value, (long)comparand);
-    }
-
-    inline static void* CompareAndSet (void** target, void* value,
-      void* comparand)
-    {
-#if CS_PROCESSOR_SIZE == 64
-      return (void*)_InterlockedCompareExchange64 ((__int64*)target, 
-        (__int64)value, (__int64)comparand);
-#else
-      return (void*)_InterlockedCompareExchange ((long*)target, 
-        (long)value, (long)comparand);
-#endif
-    }
-
-    inline static int32 Increment (int32* target)
-    {
-      return (int32)_InterlockedIncrement ((long*)target);
-    }
-
-    inline static int32 Decrement (int32* target)
-    {
-      return (int32)_InterlockedDecrement ((long*)target);
-    }
-  };
-  
-  typedef AtomicOperationsBase<AtomicOperationsMSVC> AtomicOperations;
-#pragma warning(pop)
+  namespace CS { namespace Threading {
+    typedef AtomicOperationsBase<AtomicOperationsMSVC> AtomicOperations;
+  } }
 
 #elif defined(CS_COMPILER_GCC) && \
       defined(CS_PROCESSOR_X86)
+  
+  #include "csutil/threading/atomicops_gcc_x86.h"
 
-  class AtomicOperationsX86GCC
-  {
-  public:
-    inline static int32 Set (int32* target, int32 value)
-    {
-      __asm__ __volatile__
-      (
-      "xchgl %0, %1"
-      : "=r" (value)
-      : "m" (*target), "0" (value)
-      : "memory"
-      );
-      return value;
-    }
-
-    inline static void* Set (void** target, void* value)
-    {
-#if CS_PROCESSOR_SIZE == 32
-      return (void*)Set ((int32*)target, (int32)value);
-#elif CS_PROCESSOR_SIZE == 64
-      __asm__ __volatile__
-      (
-      "xchgq %0, %1"
-      : "=r" (value)
-      : "m" (*target), "0" (value)
-      : "memory"
-      );
-      return value;
-#endif
-    }
-
-    inline static int32 CompareAndSet (int32* target, int32 value,
-      int32 comparand)
-    {
-      int32 prev;
-      __asm__ __volatile__
-      (
-      "lock; cmpxchgl %1, %2"
-      : "=a" (prev)
-      : "r" (value), "m" (*target), "0" (comparand)
-      : "memory"
-      );
-      return prev;
-    }
-
-    inline static void* CompareAndSet (void** target, void* value,
-      void* comparand)
-    {
-#if CS_PROCESSOR_SIZE == 32
-      return (void*)CompareAndSet ((int32*)target, (int32)value, 
-        (int32)comparand);
-#elif CS_PROCESSOR_SIZE == 64
-      void* prev;
-      __asm__ __volatile__
-      (
-      "lock; cmpxchgq %1, %2"
-      : "=a" (prev)
-      : "r" (value), "m" (*target), "0" (comparand)
-      : "memory"
-      );
-      return prev;
-#endif
-    }
-
-    inline static int32 Increment (int32* target, int32 incr = 1)
-    {
-      int32 result;
-      __asm__ __volatile__
-      (
-      "lock; xaddl %0, %1"
-      : "=r" (result), "=m" (*target)
-      : "0" (incr), "m" (*target)
-      : "memory"
-      );
-      return result + incr;
-    }
-
-    inline static int32 Decrement (int32* target)
-    {
-      return (int32)Increment (target, -1);
-    }
-  };
-
-  typedef AtomicOperationsBase<AtomicOperationsX86GCC> AtomicOperations;
+  namespace CS { namespace Threading {
+    typedef AtomicOperationsBase<AtomicOperationsX86GCC> AtomicOperations;
+  } }
 
 #elif defined(CS_COMPILER_GCC) && \
       defined(CS_PROCESSOR_POWERPC)
+  
+  #include "csutil/threading/atomicops_gcc_ppc.h"
 
-  class AtomicOperationsPPCGCC
-  {
-  public:
-    inline static int32 Set (int32* target, int32 value)
-    {
-      __asm__ __volatile__
-        (
-        "       lwsync \n"
-        "1:     lwarx   %0,0,%2 \n"
-        "       dcbt     0,%2 \n"
-        "       stwcx.  %3,0,%2 \n"
-        "       bne-    1b\n"
-        "       isync \n"
-        : "=&r" (value), "=m" (*(unsigned int *)target)
-        : "r" (target), "r" (value), "m" (*(unsigned int *)target)
-        : "cc", "memory"
-        );
-      return value;
-    }
-
-    inline static void* Set (void** target, void* value)
-    {
-      return (void*)Set ((int32*)target, (int32)value);
-    }
-
-    inline static int32 CompareAndSet (int32* target, int32 value,
-      int32 comparand)
-    {
-      int32 prev;
-
-      __asm__ __volatile__ (
-      "       lwsync \n"
-      "1:     lwarx   %0,0,%2\n"
-      "       cmpw    0,%0,%3\n"
-      "       bne-    2f\n"
-      "       dcbt     0,%2 \n"
-      "       stwcx.  %4,0,%2\n"
-      "       bne-    1b\n"
-      "       isync     \n"
-      "2:"
-      : "=&r" (prev), "=m" (*target)
-      : "r" (target), "r" (comparand), "r" (value), "m" (*target)
-      : "cc", "memory");
-      return prev;
-    }
-
-    inline static void* CompareAndSet (void** target, void* value,
-      void* comparand)
-    {
-      return (void*)CompareAndSet ((int32*)target, (int32)value, 
-        (int32)comparand);
-    }
-
-    inline static int32 Increment (int32* target, int32 incr = 1)
-    {
-      //@@Potentially dangerous code, needs to be revisited
-      int32 prevValue, currValue, nextValue;
-      do 
-      {
-        currValue = *target;
-        nextValue = currValue + incr;
-        prevValue = CompareAndSet (target, nextValue, currValue);
-      } while(prevValue == currValue);
-      return nextValue;
-    }
-
-    inline static int32 Decrement (int32* target)
-    {
-      return (int32)Increment (target, -1);
-    }
-  };
-
-  typedef AtomicOperationsBase<AtomicOperationsPPCGCC> AtomicOperations;
+  namespace CS { namespace Threading {
+    typedef AtomicOperationsBase<AtomicOperationsPPCGCC> AtomicOperations;
+  } }
 
 #elif defined(CS_COMPILER_GCC) && \
       defined(CS_PROCESSOR_SPARC)
 
-  class AtomicOperationsPPCGCC
-  {
-  public:
-    inline static int32 Set (int32* target, int32 value)
-    {
-      *const_cast<volatile int32*> (target) = value;
-      __asm__ __volatile__
-        (
-        "membar #StoreStore | #StoreLoad"
-        : : : "memory"
-        );
-      return value;
-    }
+  #include "csutil/threading/atomicops_sparc.h"
 
-    inline static void* Set (void** target, void* value)
-    {
-      return (void*)Set ((int32*)target, (int32)value);
-    }
-
-    inline static int32 CompareAndSet (int32* target, int32 value,
-      int32 comparand)
-    {
-      int32 prev;
-
-      __asm__ __volatile__ 
-        (
-        "cas [%1],%2,%0"
-        : "+r" (value)
-        : "r" (target), "r" (comparand)
-        );
-
-      return prev;
-    }
-
-    inline static void* CompareAndSet (void** target, void* value,
-      void* comparand)
-    {
-      return (void*)CompareAndSet ((int32*)target, (int32)value, 
-        (int32)comparand);
-    }
-
-    inline static int32 Increment (int32* target, int32 incr = 1)
-    {
-      //@@Potentially dangerous code, needs to be revisited
-      int32 prevValue, currValue, nextValue;
-      do 
-      {
-        __asm__ __volatile__
-          (
-          "membar #StoreLoad | #LoadLoad"
-          : : : "memory"
-          );
-
-        currValue = *target;
-        nextValue = currValue + incr;
-        prevValue = CompareAndSet (target, nextValue, currValue);
-      } while(prevValue == currValue);
-      return nextValue;
-    }
-
-    inline static int32 Decrement (int32* target)
-    {
-      return (int32)Increment (target, -1);
-    }
-  };
+  namespace CS { namespace Threading {
+    typedef AtomicOperationsBase<AtomicOperationsSparc> AtomicOperations;
+  } }
 
 #else
 #error "No atomic operations defined for your platform!"
 #endif
-
-}
-}
 
 #endif
