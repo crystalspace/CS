a18b60ba600c44cd5bf6463657790e30de5ccb4b
Mike Gist <xordan@gmail.com>
2007-06-03 13:41:32 +0000

    - More work on linux x86 runtime detection.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2007/platformopt@27017 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/processorspecdetection_nonwin_gcc_x86.h
+++ include/csutil/processorspecdetection_nonwin_gcc_x86.h
@@ -22,44 +22,102 @@ namespace CS
     {
         namespace Implementation
         {
-            /* On 64-bit x86 cpu's we know cpuid is supported.
-            * Also, normal gcc asm isn't supported, but we can use
-            * this macro to make use of cpuid.
-            */
-
-#if (CS_PROCESSOR_SIZE == 64)
-#define cpuid(func, eax, ebx, ecx, edx)\
-    __asm__ __volatile__ ("cpuid":\
-    "=a" (eax), "=b" (ebx), "=c" (ecx), "=d" (edx) : "a" (func));
-#endif
-
             class DetectInstructionsNonWinGCCx86
             {
             public:
                 static bool CheckSupportedInstruction(int iSet)
                 {
-                    int eax, ebx, ecx, edx;
+                    // Data from asm.
+                    int CPUnum = 0;
+                    int have_cpuid = 0;
+                    int maxEax = 0;
+                    int ecxCapFlags = 0;
+                    int edxCapFlags = 0;
                     // 64-bit x86
-                    if(CS_PROCESSOR_SIZE == 64)
-                        cpuid(0x1, eax, ebx, ecx, edx);
-
+#if (CS_PROCESSOR_SIZE == 64)
+                    // Function for cpuid to return
+                    const int function = 0x1;
+                    // Data from asm.
+                    int eaxCapFlags = 0;
+                    int ebxCapFlags = 0;
+// On 64-bit x86 cpu's we know cpuid is supported.
+__asm__(
+    "  cpuid                            \n"
+    : "=a" (eaxCapFlags), "=b" (ebxCapFlags), "=c" (ecxCapFlags), "=d" (edxCapFlags)
+    : "a" (function));
+#else
+char procName[16];
+maxEax = 0;
+__asm__(
+    //detect 386/486
+    "  pushfl                           \n"
+    "  popl         %%eax               \n"      //get EFLAGS
+    "  movl         %%eax, %%ebx        \n"      //save original EFLAGS
+    "  xorl         $0x40000, %%eax     \n"      //toggle AC bit
+    "  pushl        %%eax               \n"      //copy to stack
+    "  popfl                            \n"      //copy to EFLAGS
+    "  pushfl                           \n"
+    "  popl         %%eax               \n"      //get EFLAGS again
+    "  xorl         %%ebx, %%eax        \n"      //check AC bit
+    "  andl         $0x40000, %%eax     \n"
+    "  movl         $386,%0             \n"      //386
+    "  je           1f                  \n"      //is a 386, stop detection
+    "  pushl        %%ebx               \n"      //restore EFLAGS
+    "  popfl                            \n"
+    //detect 486/pentium+
+    "  pushfl                           \n"      //get EFLAGS
+    "  popl         %%eax               \n"
+    "  movl         %%eax, %%ecx        \n"
+    "  xorl         $0x200000,%%eax     \n"      //toggle ID bit in EFLAGS
+    "  pushl        %%eax               \n"      //save new EFLAGS value on stack
+    "  popfl                            \n"      //replace current EFLAGS value
+    "  pushfl                           \n"      //get new EFLAGS
+    "  popl         %%eax               \n"      //store new EFLAGS in EAX
+    "  xorl         %%eax, %%ecx        \n"      //can not toggle ID bit,
+    "  movl         $486,%0             \n"
+    "  jz           1f                  \n"      //processor=80486
+    "  movl         $586,%0             \n"      //586+
+    "  movl         $1,%1               \n"      //we have cpuid
+    //check number of cpuid instructions
+    "  xorl         %%eax,%%eax         \n"      // thebolt: this was a movl $0,%eax
+    "  cpuid                            \n"
+    "  movl         %%eax,%2            \n"      //save the maximum eax for cpuid
+    //save MFT string
+    "  movl         %5,%%esi            \n"
+    "  movl         %%ebx,0(%%esi)      \n"
+    "  movl         %%edx,4(%%esi)      \n"
+    "  movl         %%ecx,8(%%esi)      \n"
+    "  movl         $0,12(%%esi)        \n"
+    "  cmp          $1,%2               \n"
+    "  jb           1f                  \n"
+    //get flagstring
+    "  movl         $1,%%eax            \n"
+    "  cpuid                            \n"
+    "  movl         %%ecx,%3            \n"
+    "  movl         %%edx,%4            \n"
+    "1:                                 \n"
+    : "=g" (CPUnum), "=g" (have_cpuid), "=g" (maxEax), "=g" (ecxCapFlags), "=g" (edxCapFlags)
+    : "g" (procName), "2" (maxEax)
+    : "eax", "ebx", "ecx", "edx", "esi");
+#endif
+ 
                     switch(iSet)
                     {
                     case 0:
                         {
-                            return ((edx & (1<<23)) != 0);
+                            return ((edxCapFlags & (1<<23)) != 0);
                         }
                     case 1:
                         {
-                            return ((edx & (1<<25)) != 0);
+                            return ((edxCapFlags & (1<<25)) != 0);
                         }
                     case 2:
                         {
-                            return ((edx & (1<<26)) != 0);
+                            return ((edxCapFlags & (1<<26)) != 0);
                         }
                     case 3:
                         {
-                            return ((ecx & 1) != 0);
+                            return ((ecxCapFlags & 1) != 0);
                         }
                     default:
                         {
