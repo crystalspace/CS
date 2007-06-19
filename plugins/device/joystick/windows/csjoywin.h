dddb84b66d7ae621ceb922739b7a66cc1e9f9f84
Frank Richter <res@crystalspace3d.org>
2007-06-19 13:10:15 +0000

    res committed a patch from Joerg Fischer (with minor stylistic   changes) to properly support joysticks on Windows whose axes do not   correspond to the order found in the DIJOYSTATE2 structure (which is   probably about every joystick). See trac #343.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@27090 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/device/joystick/windows/csjoywin.h
+++ plugins/device/joystick/windows/csjoywin.h
@@ -62,10 +62,13 @@ private:
     uint nAxes;			 // number of axis
     joystate state[2];		 // joystick current & last state
     int nstate;			 // this is current state
+	csDirtyAccessArray<int32> axesMapping;
+
     joydata() : number(0), device(0), nButtons(0), nAxes(0), nstate(0) {}
     joydata(joydata const& j) : number(j.number), device(j.device),
       nButtons(j.nButtons), nAxes(j.nAxes), nstate(j.nstate)
-    {
+	{
+	  axesMapping = j.axesMapping;
       for (int i = 0; i < 2; i++)
         state[i] = j.state[i];
     }
@@ -83,7 +86,7 @@ private:
   bool Init ();
   bool Close ();
   void Report (int severity, const char* msg, ...);
-  void LoadAxes(joystate&);
+  void LoadAxes(joystate& j, const joydata& jdata);
 
   void ReportDXError (HRESULT hr, const char* msg, ...);
  public:
