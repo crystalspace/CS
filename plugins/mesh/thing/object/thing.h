4e1282ef7239646ca686610a4f7ca88109984895
Frank Richter <res@crystalspace3d.org>
2007-08-28 12:54:51 +0000

    res added a missing "msvc warning off" include to thing.h, fixing   some MSVC #pragma warnings.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@27502 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/thing/object/thing.h
+++ plugins/mesh/thing/object/thing.h
@@ -87,6 +87,8 @@ struct RepMaterial
   	old_mat (o), new_mat (n) { }
 };
 
+#include "csutil/win32/msvc_deprecated_warn_off.h"
+
 /**
  * A helper class for iTriangleMesh implementations used by csThing.
  */
