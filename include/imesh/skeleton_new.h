78fbd72391b77bbe859a3023168d66df0c9895f0
Amir Taaki <genjix@users.sourceforge.net>
2007-09-12 21:25:47 +0000

    - Genjix added new node based interface proposal.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/feature/skeletonrefactor@27631 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/imesh/skeleton_new.h
+++ include/imesh/skeleton_new.h
@@ -22,72 +22,130 @@
 #include "csgeom/quaternion.h"
 #include "csutil/scf.h"
 
-struct SkeletonAnimationParams
+struct iAmirSkeletonFactory;
+
+struct iAmirSkeletonAnimationNode : public iBase
 {
-  SkeletonAnimationParams () : feather_in (0), feather_out (0), blend (1.0f),
-    repeat (1), speed (1.0f), delay (0) { }
+  SCF_INTERFACE (iAmirSkeletonAnimationNode, 0, 0, 0);
 
   // The blend is linearly increased from 0 to 1 (after feather_in), and then
   // starts decreasing back to 0 when feather_out ticks is left in the animation.
-  csTicks feather_in, feather_out;
+  virtual csTicks GetFeatherIn () = 0;
+  virtual csTicks SetFeatherIn (csTicks featherin) = 0;
+  virtual csTicks GetFeatherOut () = 0;
+  virtual csTicks SetFeatherOut (csTicks featherout) = 0;
   // If in the blending layer then this is the mix blend factor.
   // Otherwise its unused. Clipped to 0 or above.
-  float blend;
+  virtual float GetBlend () = 0;
+  virtual float SetBlend () = 0;
+};
+
+// an instance of an animation
+struct iAmirSkeletonAnimation : public iAmirSkeletonAnimationNode
+{
+  SCF_INTERFACE (iAmirSkeletonAnimation, 0, 0, 0);
+
+  // gotten from hidden factory interface
+  virtual const char* GetName () = 0;
+
   // how many times this animation should repeat.
   // Is decremented each time a loop is finished.
-  int repeat;
+  virtual int GetRepeat () = 0;
+  virtual void SetRepeat (float repeat) = 0;
   // play speed
-  float speed;
+  virtual float GetSpeed () = 0;
+  virtual void SetSpeed (float speed) = 0;
   // The name of the animation to sync with. Will just use the first
-  // found animation if multiple matches are found.
-  csString sync;
+  // found animation if multiple matches are found. (stored as size_t id)
+  virtual const char* GetSyncAnimation () = 0;
+  virtual void SetSyncAnimation (const char* animname) = 0;
+
+  // tsk tsk, i mean headstart
   // delay before the animation starts taking effect
-  csTicks delay;
+  virtual csTicks GetDelay () = 0;
+  virtual void SetDelay (csTicks delay) = 0;
 };
 
-struct SkeletonAnimationKeyFrame
+// an instance of an animation
+struct iAmirSkeletonAnimationFactory : public iBase
 {
-  struct BoneDescription
+  SCF_INTERFACE (iAmirSkeletonAnimationFactory, 0, 0, 0);
+
+  // a name and an array of these make up an animation factory
+  struct KeyFrame
   {
-    csString bone_name;
-    csQuaternion rot;
-    csVector3 pos;
+  public:
+    void SetTimePosition (csTicks t) { time = t; }
+    void AddKeyFrameToBone (size_t boneid, const csQuaternion &rot,
+      const csVector3 &pos)
+    {
+      BoneDescription d;
+      d.rot = rot;
+      d.pos = pos;
+      keyed_bones.Push (d);
+    }
+  private:
+    struct BoneDescription
+    {
+      size_t boneid;
+      csQuaternion rot;
+      csVector3 pos;
+    };
+    csArray<BoneDescription> keyed_bones;
+    csTicks time;
   };
-  csArray<BoneDescription> keyed_bones;
-  csTicks time;
+  // add keyframe
+  virtual KeyFrame* CreateKeyFrame () = 0;
+
+  virtual const char* GetName () = 0;
+  virtual void SetName () = 0;
 };
 
-struct iNewSkeleton : public virtual iBase
+struct iAmirSkeletonAnimationMixerNode : public iAmirSkeletonAnimationNode
 {
-  SCF_INTERFACE (iNewSkeleton, 0, 0, 0);
+  SCF_INTERFACE (iAmirSkeletonAnimationMixerNode, 0, 0, 0);
 
-  enum MixLayerType
+  enum MixType
   {
-    BLEND,
-    OVERWRITE,
+    BLEND = 0,
+    OVERWRITE
+    //ADD eventually...
   };
 
-  virtual bool PlayAnimation (const char* anim, SkeletonAnimationParams params,
-      MixLayerType mixtype) = 0;
+  virtual MixType GetMixType () = 0;
+  virtual MixType SetMixType (MixType mixtype) = 0;
 
-  virtual csReversibleTransform GetBoneTransformOffset (size_t boneid) = 0;
-
-  virtual void CreateAnimation (const char* name,
-      const csArray<SkeletonAnimationKeyFrame> &keyframes) = 0;
+  virtual csArray<iBase*> &GetAnimations ();
 };
 
-struct iNewSkeletonFactory : public virtual iBase
+struct iAmirSkeleton : public virtual iBase
 {
-  SCF_INTERFACE (iNewSkeletonFactory, 0, 0, 0);
+  SCF_INTERFACE (iAmirSkeleton, 0, 0, 0);
 
+  // get root animation node. if its just a normal animation then it plays
+  virtual iBase* GetAnimation ();
+
+  virtual iAmirSkeletonFactory* GetFactory() = 0;
+};
+
+struct iAmirSkeletonFactory : public virtual iBase
+{
+  SCF_INTERFACE (iAmirSkeletonFactory, 0, 0, 0);
+
+  // name is discarded in non-debug build
   virtual void CreateBone (const char *name) = 0;
+
+  virtual iAmirSkeletonAnimation* FindAnimationByName (const char *name) = 0;
+
+  virtual iAmirSkeleton* CreateSkeleton () = 0;
 };
 
-struct iNewSkeletonGraveyard : public virtual iBase
+struct iAmirSkeletonGraveyard : public virtual iBase
 {
-  SCF_INTERFACE (iNewSkeletonGraveyard, 0, 0, 0);
+  SCF_INTERFACE (iAmirSkeletonGraveyard, 0, 0, 0);
 
-  virtual iNewSkeletonFactory* CreateSkeletonFactory () = 0;
+  virtual iAmirSkeletonFactory* CreateFactory () = 0;
+  virtual iSkeletonFactory *FindFactory(const char *name) = 0;
 };
 
 #endif //__CS_IASKELETON_H__
--- plugins/mesh/skeleton/newskel/skeleton.h
+++ plugins/mesh/skeleton/newskel/skeleton.h
@@ -34,129 +34,11 @@
 CS_PLUGIN_NAMESPACE_BEGIN(Skeleton)
 {
 
-class BoneFactory;
-class SkeletonFactory;
-class AnimationFactory;
 
-class Bone
-{
-public:
-  csReversibleTransform &GetFullTransform ();
-private:
-  BoneFactory* fact;
-  Bone* parent;
-  csArray<Bone*> children;
 
-  // store relative (to parent bone) and local transform
-  csReversibleTransform rel_transform, full_transform;
-};
-class BoneFactory
-{
-public:
-  BoneFactory ();
-
-  void SetName (const char* n);
-  const char* GetName ();
-private:
-  csString name;
-};
-
-struct KeyFrame
-{
-  struct BoneDescription
-  {
-    BoneFactory* bone;
-    csQuaternion rot;
-    csVector3 pos;
-  };
-  csArray<BoneDescription> keyed_bones;
-  csTicks time;
-};
-class Animation
-{
-public:
-  Animation (const AnimationFactory *anim, const SkeletonAnimationParams &p);
-
-  const SkeletonAnimationParams &GetParameters ();
-
-  void Advance (csTicks elapsed);
-private:
-  const AnimationFactory* anim;
-  SkeletonAnimationParams p;
-  csTicks current_time;
-};
-class AnimationFactory
-{
-public:
-  void SetName (const char* n);
-  const char* GetName () const;
-
-  KeyFrame &CreateKeyFrame ();
-private:
-  csString name;
-  csArray<KeyFrame> keyframes;
-};
-class AnimationMixer
-{
-protected:
-  AnimationMixer ();
-
-  void UpdateMixer (csTicks current);
-
-  void AddAnimation (const Animation &anim, 
-      iNewSkeleton::MixLayerType mixtype);
-private:
-  // blend and overwrite mix layers
-  csArray<Animation> blend, overwrite;
-  // sum of blend weights from blend layer
-  float sum_blends;
-  // the time of the last update for this skeleton. is -1 on creation of this.
-  long last_update_time;
-};
-
-class Skeleton : public scfImplementation1<Skeleton, iNewSkeleton>,
-  public AnimationMixer
-{
-public:
-  Skeleton ();
-
-  void Update (csTicks current);
-
-  virtual bool PlayAnimation (const char* anim, SkeletonAnimationParams params,
-      MixLayerType mixtype);
-
-  virtual csReversibleTransform GetBoneTransformOffset (size_t boneid);
-private:
-  csArray<Bone> bones;
-  csArray<size_t> parent_bones;
-  SkeletonFactory* fact;
-};
-
-class SkeletonFactory :
-  public scfImplementation1<SkeletonFactory, iNewSkeletonFactory>
-{
-public:
-  SkeletonFactory ();
-
-  virtual void CreateBone (const char* name);
-
-  virtual void CreateAnimation (const char* name,
-      const csArray<SkeletonAnimationKeyFrame> &keyframes);
-
-  BoneFactory* FindBone (const char* name);
-
-  const csArray<AnimationFactory>::Iterator GetAnimFactIterator ();
-
-  void SetName (const char* n);
-  const char* GetName ();
-private:
-  csArray<BoneFactory> bones;
-  csArray<size_t> parent_bones;
-  csArray<AnimationFactory> anims;
-  csString name;
-};
 class SkeletonGraveyard :
-  public scfImplementation2<SkeletonGraveyard, iNewSkeletonGraveyard, iComponent>
+  public scfImplementation2<SkeletonGraveyard, iAmirSkeletonGraveyard,
+    iComponent>
 {
 public:
   SkeletonGraveyard (iBase *parent);
@@ -165,7 +47,7 @@ public:
   bool Initialize (iObjectRegistry* object_reg);
   bool HandleEvent (iEvent& ev);
 
-  virtual iNewSkeletonFactory* CreateSkeletonFactory ();
+  virtual iAmirSkeletonFactory* CreateFactory ();
 private:
 
   class SkelEventHandler :
@@ -193,8 +75,8 @@ private:
 
   iObjectRegistry* object_reg;
   csRef<iVirtualClock> vc;
-  csRefArray<Skeleton> skeletons;
-  csRefArray<SkeletonFactory> factories;
+  //csRefArray<Skeleton> skeletons;
+  //csRefArray<SkeletonFactory> factories;
   csEventID preprocess;
 };
 
