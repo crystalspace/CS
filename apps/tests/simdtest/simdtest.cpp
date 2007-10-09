448766242e9d90b6c0f02e76058b2b55d2e1e49f
Mike Gist <xordan@gmail.com>
2007-10-09 04:06:17 +0000

    - Change which didn't get merged.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@27859 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/tests/simdtest/simdtest.cpp
+++ apps/tests/simdtest/simdtest.cpp
@@ -19,7 +19,7 @@ Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #include "cssysdef.h"
 #include "csutil/processorspecdetection.h"
 
-int main()
+int main(int argc, char* argv[])
 {
     CS::Platform::ProcessorSpecDetection detect;
 
