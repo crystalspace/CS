e151b2694bd5b1609113ee74e18f83f85dc9e937
Marten Svanfeldt <developer@svanfeldt.com>
2007-10-23 20:28:39 +0000

    - Restored previously removed file
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@27996 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/csutil/scopedmutexlock.h
@@ -0,0 +1,9 @@
+// Obsoleted in 1.3
+#ifdef CS_COMPILER_GCC
+  #warning This file was superseded by csutil/scopedlock.h.h
+#endif
+#ifdef CS_COMPILER_MSVC
+  #pragma message ("This file was superseded by csutil/scopedlock.h")
+#endif
+
+#include "csutil/scopedlock.h"
