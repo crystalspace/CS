037618bdcb4311defbf49ca3db6c2a604ce0cd7d
Mike Gist <xordan@gmail.com>
2007-11-04 10:32:12 +0000

    - Mike Gist added the concept of 'proxy textures', where textures are created without an image loaded, and then fully loaded when they are needed. This allows for a library of textures to be shared between maps, but only the textures actually used by a given map to be loaded. Also added some unloading functions as part of this, although they can be used separately if wanted.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@28098 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ libs/cstool/unusedresourcehelper.cpp
@@ -0,0 +1,206 @@
+/*
+  Copyright (C) 2007 by Mike Gist
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with this library; if not, write to the Free
+  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+
+#include "cstool/unusedresourcehelper.h"
+#include "iutil/object.h"
+#include "iengine/region.h"
+
+namespace CS
+{
+    namespace Utility
+    {
+        namespace UnusedResourceHelper
+        {
+            void UnloadAllUnusedMaterials(iEngine* engine)
+            {
+                iMaterialList *matList = engine->GetMaterialList();
+
+                iRegionList *regList = engine->GetRegions();
+                for(int i=0; i<matList->GetCount(); i++)
+                {
+                    bool inRegion = false;
+                    iRegion* region;
+                    for(int j=0; j<regList->GetCount(); j++)
+                    {
+                        inRegion = inRegion || regList->Get(j)->FindMaterial(matList->Get(i)->QueryObject()->GetName());
+                        if(inRegion)
+                        {
+                            region = regList->Get(j);
+                            break;
+                        }
+                    }
+
+                    if(matList->Get(i)->GetRefCount() == 1 || (matList->Get(i)->GetRefCount() == 2 && inRegion))
+                    {
+                        if(inRegion)
+                            region->Remove(matList->Get(i)->QueryObject());
+                        matList->Remove(i);
+                        i--;
+                    }
+                }
+            }
+
+            void UnloadAllUnusedTextures(iEngine* engine)
+            {
+                iTextureList *texList = engine->GetTextureList();
+
+                iRegionList *regList = engine->GetRegions();
+                for(int i=0; i<texList->GetCount(); i++)
+                {
+                    bool inRegion = false;
+                    iRegion* region;
+                    for(int j=0; j<regList->GetCount(); j++)
+                    {
+                        inRegion = inRegion || regList->Get(j)->FindTexture(texList->Get(i)->QueryObject()->GetName());
+                        if(inRegion)
+                        {
+                            region = regList->Get(j);
+                            break;
+                        }
+                    }
+
+                    if(texList->Get(i)->GetRefCount() == 1 || (texList->Get(i)->GetRefCount() == 2 && inRegion))
+                    {
+                        if(inRegion)
+                            region->Remove(texList->Get(i)->QueryObject());
+                        texList->Remove(i);
+                        i--;
+                    }
+                }
+            }
+
+            void UnloadAllUnusedFactories(iEngine* engine)
+            {
+                iMeshFactoryList *factList = engine->GetMeshFactories();
+
+                iRegionList *regList = engine->GetRegions();
+                for(int i=0; i<factList->GetCount(); i++)
+                {
+                    bool inRegion = false;
+                    iRegion* region;
+                    for(int j=0; j<regList->GetCount(); j++)
+                    {
+                        inRegion = inRegion || regList->Get(j)->FindMeshFactory(factList->Get(i)->QueryObject()->GetName());
+                        if(inRegion)
+                        {
+                            region = regList->Get(j);
+                            break;
+                        }
+                    }
+
+                    if(factList->Get(i)->GetRefCount() == 1 || (factList->Get(i)->GetRefCount() == 2 && inRegion))
+                    {
+                        if(inRegion)
+                            region->Remove(factList->Get(i)->QueryObject());
+                        factList->Remove(i);
+                        i--;
+                    }
+                }
+            }
+
+            void UnloadUnusedMaterials(iEngine* engine, csWeakRefArray<iMaterialWrapper> materials)
+            {
+                iRegionList *regList = engine->GetRegions();
+
+                for(uint i=0; i<materials.GetSize(); i++)
+                {
+                    bool inRegion = false;
+                    iRegion* region;
+                    for(int j=0; j<regList->GetCount(); j++)
+                    {
+                        inRegion = inRegion || regList->Get(j)->FindMaterial(materials.Get(i)->QueryObject()->GetName());
+                        if(inRegion)
+                        {
+                            region = regList->Get(j);
+                            break;
+                        }
+                    }
+
+                    if(materials.Get(i)->GetRefCount() == 1 || (materials.Get(i)->GetRefCount() == 2 && inRegion))
+                    {
+                        if(inRegion)
+                            region->Remove(materials.Get(i)->QueryObject());
+                        engine->GetMaterialList()->Remove(materials.Get(i));
+                        materials.DeleteIndex(i);
+                        i--;
+                    }
+                }
+            }
+
+            void UnloadUnusedTextures(iEngine* engine, csWeakRefArray<iTextureWrapper> textures)
+            {
+                iRegionList *regList = engine->GetRegions();
+
+                for(uint i=0; i<textures.GetSize(); i++)
+                {
+                    bool inRegion = false;
+                    iRegion* region;
+                    for(int j=0; j<regList->GetCount(); j++)
+                    {
+                        inRegion = inRegion || regList->Get(j)->FindTexture(textures.Get(i)->QueryObject()->GetName());
+                        if(inRegion)
+                        {
+                            region = regList->Get(j);
+                            break;
+                        }
+                    }
+
+                    if(textures.Get(i)->GetRefCount() == 1 || (textures.Get(i)->GetRefCount() == 2 && inRegion))
+                    {
+                        if(inRegion)
+                            region->Remove(textures.Get(i)->QueryObject());
+                        engine->GetTextureList()->Remove(textures.Get(i));
+                        textures.DeleteIndex(i);
+                        i--;
+                    }
+                }
+            }
+
+            void UnloadUnusedFactories(iEngine* engine, csWeakRefArray<iMeshFactoryWrapper> factories)
+            {
+                iRegionList *regList = engine->GetRegions();
+
+                for(uint i=0; i<factories.GetSize(); i++)
+                {
+                    bool inRegion = false;
+                    iRegion* region;
+                    for(int j=0; j<regList->GetCount(); j++)
+                    {
+                        inRegion = inRegion || regList->Get(j)->FindMeshFactory(factories.Get(i)->QueryObject()->GetName());
+                        if(inRegion)
+                        {
+                            region = regList->Get(j);
+                            break;
+                        }
+                    }
+
+                    if(factories.Get(i)->GetRefCount() == 1 || (factories.Get(i)->GetRefCount() == 2 && inRegion))
+                    {
+                        if(inRegion)
+                            region->Remove(factories.Get(i)->QueryObject());
+                        engine->GetMeshFactories()->Remove(factories.Get(i));
+                        factories.DeleteIndex(i);
+                        i--;
+                    }
+                }
+            }
+        }
+    }
+}
