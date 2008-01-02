c38e40fc8045d992ef30f995c04084ca8b222390
Ronie Salgado <ronsaldo@users.sourceforge.net>
2008-01-02 04:51:19 +0000

    Added another missing file
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/feature/csharpbinding@28393 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ plugins/cscript/cscsharp/cspacesharp/const-nocls.cs
@@ -0,0 +1,65 @@
+
+using System;
+using System.Reflection;
+
+namespace CrystalSpace
+{
+  [CLSCompliant(false)]
+  public class NoCLSConstants
+  {
+     public const int CS_BOX_CORNER_xy = 0;
+     public const int CS_BOX_CORNER_xY = 1;
+     public const int CS_BOX_CORNER_Xy = 2;
+     public const int CS_BOX_CORNER_XY = 3;
+
+     public const int CS_BOX_EDGE_xy_Xy = 0;
+     public const int CS_BOX_EDGE_Xy_xy = 1;
+     public const int CS_BOX_EDGE_Xy_XY = 2;
+     public const int CS_BOX_EDGE_XY_Xy = 3;
+     public const int CS_BOX_EDGE_XY_xY = 4;
+     public const int CS_BOX_EDGE_xY_XY = 5;
+     public const int CS_BOX_EDGE_xY_xy = 6;
+     public const int CS_BOX_EDGE_xy_xY = 7;
+
+     public const int CS_BOX_CORNER_xyz = 0;
+     public const int CS_BOX_CORNER_xyZ = 1;
+     public const int CS_BOX_CORNER_xYz = 2;
+     public const int CS_BOX_CORNER_xYZ = 3;
+     public const int CS_BOX_CORNER_Xyz = 4;
+     public const int CS_BOX_CORNER_XyZ = 5;
+     public const int CS_BOX_CORNER_XYz = 6;
+     public const int CS_BOX_CORNER_XYZ = 7;
+
+     public const int CS_BOX_SIDE_x = 0;
+     public const int CS_BOX_SIDE_X = 1;
+     public const int CS_BOX_SIDE_y = 2;
+     public const int CS_BOX_SIDE_Y = 3;
+     public const int CS_BOX_SIDE_z = 4;
+     public const int CS_BOX_SIDE_Z = 5;
+
+     public const int CS_BOX_EDGE_Xyz_xyz = 0;
+     public const int CS_BOX_EDGE_xyz_Xyz = 1;
+     public const int CS_BOX_EDGE_xyz_xYz = 2;
+     public const int CS_BOX_EDGE_xYz_xyz = 3;
+     public const int CS_BOX_EDGE_xYz_XYz = 4;
+     public const int CS_BOX_EDGE_XYz_xYz = 5;
+     public const int CS_BOX_EDGE_XYz_Xyz = 6;
+     public const int CS_BOX_EDGE_Xyz_XYz = 7;
+     public const int CS_BOX_EDGE_Xyz_XyZ = 8;
+     public const int CS_BOX_EDGE_XyZ_Xyz = 9;
+     public const int CS_BOX_EDGE_XyZ_XYZ = 10;
+     public const int CS_BOX_EDGE_XYZ_XyZ = 11;
+     public const int CS_BOX_EDGE_XYZ_XYz = 12;
+     public const int CS_BOX_EDGE_XYz_XYZ = 13;
+     public const int CS_BOX_EDGE_XYZ_xYZ = 14;
+     public const int CS_BOX_EDGE_xYZ_XYZ = 15;
+     public const int CS_BOX_EDGE_xYZ_xYz = 16;
+     public const int CS_BOX_EDGE_xYz_xYZ = 17;
+     public const int CS_BOX_EDGE_xYZ_xyZ = 18;
+     public const int CS_BOX_EDGE_xyZ_xYZ = 19;
+     public const int CS_BOX_EDGE_xyZ_xyz = 20;
+     public const int CS_BOX_EDGE_xyz_xyZ = 21;
+     public const int CS_BOX_EDGE_xyZ_XyZ = 22;
+     public const int CS_BOX_EDGE_XyZ_xyZ = 23;
+  }
+};
