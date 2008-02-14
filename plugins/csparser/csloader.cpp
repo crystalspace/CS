7792a12a1c3e7872d47cf12a272119d277c7aed0
Jelle Hellemans <sueastside@users.sourceforge.net>
2008-02-14 15:59:01 +0000

    Sueastside added 'portals' loading to iLoader::Load().
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@28712 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/csparser/csloader.cpp
+++ plugins/csparser/csloader.cpp
@@ -859,6 +859,44 @@ csLoadResult csLoader::Load (iDocumentNode* node, iRegion* region,
     return rc;
   }
 
+  csRef<iDocumentNode> portalsnode = node->GetNode ("portals");
+  if (portalsnode)
+  {
+    const char* portalsname = override_name ? override_name :
+    	portalsnode->GetAttributeValue ("name");
+    if (ldr_context->CheckDupes () && portalsname)
+    {
+      iMeshWrapper* mw = Engine->FindMeshObject (portalsname);
+      if (mw)
+      {
+        csRef<iPortalContainer> pc = 
+          scfQueryInterface<iPortalContainer>(mw->GetMeshObject ());
+        if (pc)
+        {
+          AddToRegion (ldr_context, mw->QueryObject ());
+          rc.result = mw;
+          rc.success = true;
+          return rc;
+        }
+      }
+    }
+    if (ParsePortals (ldr_context, portalsnode, 0, 0, ssource))
+    {
+      iMeshWrapper* mw = ldr_context->GetRegion ()->FindMeshObject(portalsname);
+      if (mw)
+      {
+        mw->QueryObject()->SetName(portalsname);
+        rc.result = mw;
+        rc.success = true;
+        return rc;
+      }
+    }
+
+    rc.result = 0;
+    rc.success = false;
+    return rc;
+  }
+
   ReportError ("crystalspace.maploader.parse",
     "File doesn't seem to be a world, library, meshfact, or meshobj file!");
 
