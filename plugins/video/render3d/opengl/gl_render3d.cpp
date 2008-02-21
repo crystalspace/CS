0310326ebe1af070fe0d1b099f0846f6cc515358
Frank Richter <res@crystalspace3d.org>
2008-02-21 11:48:35 +0000

    res restored r28657 which got accidentally lost in r28744.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@28760 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/render3d/opengl/gl_render3d.cpp
+++ plugins/video/render3d/opengl/gl_render3d.cpp
@@ -1285,6 +1285,7 @@ bool csGLGraphics3D::BeginDraw (int drawflags)
       	| GL_COLOR_BUFFER_BIT;
     else
       clearMask = GL_DEPTH_BUFFER_BIT | stencilFlag;
+    glClearDepth (0.0); // @@@ Never really changes 
   }
   else if (drawflags & CSDRAW_CLEARSCREEN)
     clearMask = GL_COLOR_BUFFER_BIT;
