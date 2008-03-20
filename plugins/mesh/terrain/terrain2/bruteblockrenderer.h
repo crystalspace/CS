408cbfd0701fbc9ca19fcf8077cd29aae0adda28
Frank Richter <res@crystalspace3d.org>
2008-03-20 16:27:38 +0000

    Compile fix after merge
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/feature/rendermanager@29056 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/terrain/terrain2/bruteblockrenderer.h
+++ plugins/mesh/terrain/terrain2/bruteblockrenderer.h
@@ -27,6 +27,7 @@
 #include "iutil/comp.h"
 #include "iutil/strset.h"
 #include "ivideo/graph3d.h"
+#include "ivideo/shader/shader.h"
 
 CS_PLUGIN_NAMESPACE_BEGIN(Terrain2)
 {
@@ -95,7 +96,7 @@ public:
     return emptyPalette;
   }
 
-  inline iStringSet* GetStringSet ()
+  inline iShaderVarStringSet* GetStringSet ()
   {
     return stringSet;
   }
@@ -113,7 +114,7 @@ private:
 
   iObjectRegistry* objectRegistry;
   csRef<iGraphics3D> graph3d;
-  csRef<iStringSet> stringSet;
+  csRef<iShaderVarStringSet> stringSet;
 
   const csTerrainMaterialPalette* materialPalette;
   csTerrainMaterialPalette emptyPalette;  
