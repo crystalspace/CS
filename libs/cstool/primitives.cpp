d4443d6bbe7f6cee7f6077bb89571013be3a9252
Vincent Knecht <vince@iefr.ath.cx>
2008-04-08 18:59:29 +0000

    Merged revisions 27907 via svnmerge from https://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk
    ........
      r27907 | sunshine | 2007-10-14 14:00:33 +0200 (Sun, 14 Oct 2007) | 1 line
      
      Eric Sunshine eliminated several compilation warnings.
    ........
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/release/V1.2@29266 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- libs/cstool/primitives.cpp
+++ libs/cstool/primitives.cpp
@@ -320,7 +320,7 @@ void csPrimitives::GenerateSphere (const csEllipsoid& ellips, int num,
 
   csArray<int> prev_verticesT;
   csArray<int> prev_verticesB;
-  float u, v;
+  float u = 0.0f, v = 0.0f;
   int i, j;
 
   // Number of degrees between layers.
