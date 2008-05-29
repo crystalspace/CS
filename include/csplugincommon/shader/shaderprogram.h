7dca40c5a2556fd22485c3e6cfc95c85ff9a8ce6
Frank Richter <res@crystalspace3d.org>
2008-05-29 18:10:07 +0000

    Fix merge fallout
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/feature/rendermanager@29760 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csplugincommon/shader/shaderprogram.h
+++ include/csplugincommon/shader/shaderprogram.h
@@ -115,8 +115,6 @@ public:
     ProgramParam() : valid (false), name (CS::InvalidShaderVarStringID) { }
     /// Returns whether this parameter 
     bool IsConstant() const { return valid && var.IsValid(); }
-    /// Returns whether this parameter 
-    bool IsConstant() const { return valid && var.IsValid(); }
   };
 
   class CS_CRYSTALSPACE_EXPORT ProgramParamParser
