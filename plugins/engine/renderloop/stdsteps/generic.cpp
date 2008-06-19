c19339c0bf59755ef36b71d35225798794e621ad
Frank Richter <res@crystalspace3d.org>
2008-06-19 17:36:13 +0000

    Made generic render step provide inverse object to world TF as a SV
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@30018 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/engine/renderloop/stdsteps/generic.cpp
+++ plugins/engine/renderloop/stdsteps/generic.cpp
@@ -154,6 +154,7 @@ csPtr<iRenderStep> csGenericRenderStepFactory::Create ()
 
 CS::ShaderVarStringID csGenericRenderStep::fogplane_name;
 CS::ShaderVarStringID csGenericRenderStep::string_object2world;
+CS::ShaderVarStringID csGenericRenderStep::string_object2worldInv;
 CS::ShaderVarStringID csGenericRenderStep::light_ambient;
 
 csGenericRenderStep::csGenericRenderStep (
@@ -175,6 +176,7 @@ csGenericRenderStep::csGenericRenderStep (
   currentSettings = false;
   fogplane_name = stringsSvName->Request ("fogplane");
   string_object2world = stringsSvName->Request ("object2world transform");
+  string_object2worldInv = stringsSvName->Request ("object2world transform inverse");
   light_ambient = stringsSvName->Request ("light ambient");
 
   visible_meshes_index = 0;
@@ -229,6 +231,8 @@ void csGenericRenderStep::RenderMeshes (iRenderView* rview, iGraphics3D* g3d,
   }
   csRef<csShaderVariable> svO2W = 
     shadervars.Top ().GetVariable(string_object2world);
+  csRef<csShaderVariable> svO2WI = 
+    shadervars.Top ().GetVariable(string_object2worldInv);
 
   ShaderVarPusher pusher (Pusher);
   iShader* shader = pusher.shader;
@@ -253,6 +257,7 @@ void csGenericRenderStep::RenderMeshes (iRenderView* rview, iGraphics3D* g3d,
       if ((!portalTraversal) && mesh->portal != 0) continue;
 
       svO2W->SetValue (mesh->object2world);
+      svO2WI->SetValue (mesh->object2world.GetInverse());
 
       pusher.meshContext = meshContext;
       pusher.mesh = mesh;
@@ -433,6 +438,7 @@ void csGenericRenderStep::Perform (iRenderView* rview, iSector* sector,
 
   shadervars.Push (csShaderVariableContext ());
   shadervars.Top ().GetVariableAdd (string_object2world);
+  shadervars.Top ().GetVariableAdd (string_object2worldInv);
 
   csRef<csShaderVariable> sv;
   sv = shadervars.Top ().GetVariableAdd (light_ambient);
