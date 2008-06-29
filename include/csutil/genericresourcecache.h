3966787e634b692d6f59393d70a79037916f29e1
Frank Richter <res@crystalspace3d.org>
2008-06-29 01:04:53 +0000

    Move generally useful purge condition from gl3d to public API
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@30139 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/genericresourcecache.h
+++ include/csutil/genericresourcecache.h
@@ -135,6 +135,38 @@ namespace CS
       };
       
       /**
+       * Reuse condition: a resource is reused if only one reference is held 
+       * to it. (The resource type must be a csRef<>.)
+       */
+      class ReuseIfOnlyOneRef
+      {
+      public:
+	struct AddParameter
+	{
+	  AddParameter () {}
+	};
+	struct StoredAuxiliaryInfo
+	{
+	  template<typename ResourceCacheType>
+	  StoredAuxiliaryInfo (const ResourceCacheType& cache, 
+	    const AddParameter& param) {}
+	};
+	
+	template<typename ResourceCacheType>
+	void MarkActive (const ResourceCacheType& cache,
+	    StoredAuxiliaryInfo& elementInfo)
+	{ }
+  
+	template<typename ResourceCacheType>
+	bool IsReusable (const ResourceCacheType& cache,
+	  const StoredAuxiliaryInfo& elementInfo,
+	  const typename ResourceCacheType::CachedType& data)
+	{
+	  return data->GetRefCount() == 1;
+	}
+      };
+      
+      /**
        * Purge condition: a resource is purged after a certain time has passed
        */
       template<typename TimeType = uint>
@@ -180,6 +212,38 @@ namespace CS
 	}
       };
 
+      /**
+       * Purge condition: a resource is purged if only one reference is held 
+       * to it. (The resource type must be a csRef<>.)
+       */
+      class PurgeIfOnlyOneRef
+      {
+      public:
+	struct AddParameter
+	{
+	  AddParameter () {}
+	};
+	struct StoredAuxiliaryInfo
+	{
+	  template<typename ResourceCacheType>
+	  StoredAuxiliaryInfo (const ResourceCacheType& cache, 
+	    const AddParameter& param) {}
+	};
+	
+	template<typename ResourceCacheType>
+	void MarkActive (const ResourceCacheType& cache,
+	    StoredAuxiliaryInfo& elementInfo)
+	{ }
+  
+	template<typename ResourceCacheType>
+	bool IsPurgeable (const ResourceCacheType& cache,
+	  StoredAuxiliaryInfo& elementInfo,
+	  const typename ResourceCacheType::CachedType& data)
+	{
+	  return data->GetRefCount() == 1;
+	}
+      };
+      
     } // namespace ResourceCache
     
     /**
