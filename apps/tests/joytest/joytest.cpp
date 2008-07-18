1bc47f2a779101d067ba7b6613c4fa4ed2f686cf
Lukas Erlinghagen <erluk@users.sourceforge.net>
2008-07-18 01:19:27 +0000

    - Ported joytest to the current simple1 codebase
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@30418 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/tests/joytest/joytest.cpp
+++ apps/tests/joytest/joytest.cpp
@@ -16,44 +16,6 @@
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#include "cssysdef.h"
-
-#include "cstool/csview.h"
-#include "cstool/initapp.h"
-#include "cstool/genmeshbuilder.h"
-#include "csutil/cmdhelp.h"
-#include "csutil/cscolor.h"
-#include "csutil/event.h"
-#include "csutil/sysfunc.h"
-#include "iengine/camera.h"
-#include "iengine/engine.h"
-#include "iengine/light.h"
-#include "iengine/material.h"
-#include "iengine/mesh.h"
-#include "iengine/movable.h"
-#include "iengine/sector.h"
-#include "iengine/texture.h"
-#include "igraphic/imageio.h"
-#include "imap/loader.h"
-#include "imesh/object.h"
-#include "imesh/sprite3d.h"
-#include "iutil/csinput.h"
-#include "iutil/event.h"
-#include "iutil/eventq.h"
-#include "iutil/objreg.h"
-#include "iutil/plugin.h"
-#include "iutil/stringarray.h"
-#include "iutil/vfs.h"
-#include "iutil/virtclk.h"
-#include "ivaria/reporter.h"
-#include "ivaria/stdrep.h"
-#include "ivideo/fontserv.h"
-#include "ivideo/graph2d.h"
-#include "ivideo/graph3d.h"
-#include "ivideo/material.h"
-#include "ivideo/texture.h"
-#include "ivideo/txtmgr.h"
-
 #include "joytest.h"
 
 
@@ -62,18 +24,17 @@ CS_IMPLEMENT_APPLICATION
 //-----------------------------------------------------------------------------
 
 // The global pointer to simple
-Simple *simple;
 
-Simple::Simple (iObjectRegistry* object_reg)
+Simple::Simple ()
 {
-  Simple::object_reg = object_reg;
+  SetApplicationName ("CrystalSpace.JoyTest");
 }
 
 Simple::~Simple ()
 {
 }
 
-void Simple::SetupFrame ()
+void Simple::Frame ()
 {
   // First get elapsed time from the virtual clock.
   csTicks elapsed_time = vc->GetElapsedTicks ();
@@ -83,13 +44,13 @@ void Simple::SetupFrame ()
 
   iCamera* c = view->GetCamera();
   if (kbd->GetKeyState (CSKEY_RIGHT))
-    c->GetTransform ().RotateThis (CS_VEC_ROT_RIGHT, speed);
+    rotY += speed;
   if (kbd->GetKeyState (CSKEY_LEFT))
-    c->GetTransform ().RotateThis (CS_VEC_ROT_LEFT, speed);
+    rotY -= speed;
   if (kbd->GetKeyState (CSKEY_PGUP))
-    c->GetTransform ().RotateThis (CS_VEC_TILT_UP, speed);
+    rotX += speed;
   if (kbd->GetKeyState (CSKEY_PGDN))
-    c->GetTransform ().RotateThis (CS_VEC_TILT_DOWN, speed);
+    rotX -= speed;
   if (kbd->GetKeyState (CSKEY_UP))
     c->Move (CS_VEC_FORWARD * 4 * speed);
   if (kbd->GetKeyState (CSKEY_DOWN))
@@ -110,195 +71,190 @@ void Simple::SetupFrame ()
       if (q) q->GetEventOutlet()->Broadcast (csevQuit (object_reg));
     }
   }
-  // Tell 3D driver we're going to display 3D things.
-  if (!g3d->BeginDraw (engine->GetBeginDrawFlags () | CSDRAW_3DGRAPHICS))
-    return;
+  
+  csMatrix3 rot = csXRotMatrix3 (rotX) * csYRotMatrix3 (rotY);
+  csOrthoTransform ot (rot, c->GetTransform().GetOrigin ());
+  c->SetTransform (ot);
 
-  // Tell the camera to render into the frame buffer.
-  view->Draw ();
+  rm->RenderView (view);
 }
 
-void Simple::FinishFrame ()
+bool Simple::OnKeyboard(iEvent& ev)
 {
-  g3d->FinishDraw ();
-  g3d->Print (0);
-}
-
-bool Simple::HandleEvent (iEvent& ev)
-{
-
-  if (ev.Name == Process)
+  // We got a keyboard event.
+  csKeyEventType eventtype = csKeyEventHelper::GetEventType(&ev);
+  if (eventtype == csKeyEventTypeDown)
   {
-    simple->SetupFrame ();
-    return true;
+    // The user pressed a key (as opposed to releasing it).
+    utf32_char code = csKeyEventHelper::GetCookedCode(&ev);
+    if (code == CSKEY_ESC)
+    {
+      // The user pressed escape to exit the application.
+      // The proper way to quit a Crystal Space application
+      // is by broadcasting a csevQuit event. That will cause the
+      // main runloop to stop. To do that we get the event queue from
+      // the object registry and then post the event.
+      csRef<iEventQueue> q = 
+        csQueryRegistry<iEventQueue> (GetObjectRegistry());
+      if (q.IsValid()) q->GetEventOutlet()->Broadcast(
+        csevQuit(GetObjectRegistry()));
+    }
   }
-  else if (ev.Name == FinalProcess)
-  {
-    simple->FinishFrame ();
-    return true;
-  }
-  else if ((ev.Name == KeyboardDown) &&
-	   (csKeyEventHelper::GetCookedCode (&ev) == CSKEY_ESC))
-  {
-    csRef<iEventQueue> q (csQueryRegistry<iEventQueue> (object_reg));
-    if (q) q->GetEventOutlet()->Broadcast (csevQuit (object_reg));
-    return true;
-  }
-
   return false;
 }
 
-bool Simple::SimpleEventHandler (iEvent& ev)
+bool Simple::OnInitialize(int /*argc*/, char* /*argv*/ [])
 {
-  if (simple)
-    return simple->HandleEvent (ev);
-  else
-    return false;
-}
+  // RequestPlugins() will load all plugins we specify. In addition
+  // it will also check if there are plugins that need to be loaded
+  // from the config system (both the application config and CS or
+  // global configs). In addition it also supports specifying plugins
+  // on the commandline.
+  if (!csInitializer::RequestPlugins(GetObjectRegistry(),
+    CS_REQUEST_VFS,
+    CS_REQUEST_OPENGL3D,
+    CS_REQUEST_ENGINE,
+    CS_REQUEST_FONTSERVER,
+    CS_REQUEST_IMAGELOADER,
+    CS_REQUEST_LEVELLOADER,
+    CS_REQUEST_REPORTER,
+    CS_REQUEST_REPORTERLISTENER,
+    CS_REQUEST_JOYSTICK,
+    CS_REQUEST_END))
+    return ReportError("Failed to initialize plugins!");
 
-bool Simple::Initialize ()
-{
-  if (!csInitializer::RequestPlugins (object_reg,
-  	CS_REQUEST_VFS,
-	CS_REQUEST_OPENGL3D,
-	CS_REQUEST_ENGINE,
-	CS_REQUEST_FONTSERVER,
-	CS_REQUEST_IMAGELOADER,
-	CS_REQUEST_LEVELLOADER,
-	CS_REQUEST_REPORTER,
-	CS_REQUEST_REPORTERLISTENER,
-	CS_REQUEST_JOYSTICK,
-	CS_REQUEST_END))
-  {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-    	"crystalspace.application.joytest",
-	"Can't initialize plugins!");
-    return false;
-  }
+  // "Warm up" the event handler so it can interact with the world
+  csBaseEventHandler::Initialize(GetObjectRegistry());
 
-  Process = csevProcess (object_reg);
-  FinalProcess = csevFinalProcess (object_reg);
-  KeyboardDown = csevKeyboardDown (object_reg);
-
+  // Now we need to register the event handler for our application.
+  // Crystal Space is fully event-driven. Everything (except for this
+  // initialization) happens in an event.
+  // Rather than simply handling all events, we subscribe to the
+  // particular events we're interested in.
   csEventID events[] = {
-    Process,
-    FinalProcess,
-    KeyboardDown,
+    csevFrame (GetObjectRegistry()),
+    csevKeyboardEvent (GetObjectRegistry()),
     CS_EVENTLIST_END
   };
-  if (!csInitializer::SetupEventHandler (object_reg, SimpleEventHandler, events))
-  {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-	      "crystalspace.application.joytest",
-	      "Can't initialize event handler!");
-    return false;
-  }
+  if (!RegisterQueue(GetObjectRegistry(), events))
+    return ReportError("Failed to set up event handler!");
 
-  // Check for commandline help.
-  if (csCommandLineHelper::CheckHelp (object_reg))
-  {
-    csCommandLineHelper::Help (object_reg);
-    return false;
-  }
+  // Report success
+  return true;
+}
 
-  /// @@@ (ge) This is a redundant way to load a joystick.
-  // Attempt to load a joystick plugin.
-  /*csRef<iStringArray> joystickClasses =
-    iSCF::SCF->QueryClassList ("crystalspace.device.joystick.");
-  if (joystickClasses.IsValid())
-  {
-    csRef<iPluginManager> plugmgr = 
-      csQueryRegistry<iPluginManager> (object_reg);
-    for (size_t i = 0; i < joystickClasses->Length (); i++)
-    {
-      const char* className = joystickClasses->Get (i);
-      iBase* b = plugmgr->LoadPlugin (className);
+void Simple::OnExit()
+{
+  // Shut down the event handlers we spawned earlier.
+  drawer.Invalidate();
+  printer.Invalidate();
+}
 
-      csReport (object_reg, CS_REPORTER_SEVERITY_NOTIFY,
-	"crystalspace.application.joytest", "Attempt to load plugin '%s' %s",
-	className, (b != 0) ? "successful" : "failed");
-      if (b != 0) b->DecRef ();
-    }
-  }*/
-
-  // The virtual clock.
-  vc = csQueryRegistry<iVirtualClock> (object_reg);
-  if (vc == 0)
-  {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-    	"crystalspace.application.joytest",
-	"Can't find the virtual clock!");
-    return false;
-  }
-
-  // Find the pointer to engine plugin
-  engine = csQueryRegistry<iEngine> (object_reg);
-  if (engine == 0)
-  {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-    	"crystalspace.application.joytest",
-	"No iEngine plugin!");
-    return false;
-  }
-
-  loader = csQueryRegistry<iLoader> (object_reg);
-  if (loader == 0)
-  {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-    	"crystalspace.application.joytest",
-    	"No iLoader plugin!");
-    return false;
-  }
-
-  g3d = csQueryRegistry<iGraphics3D> (object_reg);
-  if (g3d == 0)
-  {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-    	"crystalspace.application.joytest",
-    	"No iGraphics3D plugin!");
-    return false;
-  }
-
-  kbd = csQueryRegistry<iKeyboardDriver> (object_reg);
-  if (kbd == 0)
-  {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-    	"crystalspace.application.joytest",
-    	"No iKeyboardDriver plugin!");
-    return false;
-  }
-  joy= csQueryRegistry<iJoystickDriver> (object_reg);
-  if (joy==0)
-  {
-    csReport (object_reg,CS_REPORTER_SEVERITY_ERROR,
-      "crystalspace.application.joytest",
-      "No iJoystickDriver plugin ?!");
-    return false;
-  }
-
+bool Simple::Application()
+{
   // Open the main system. This will open all the previously loaded plug-ins.
-  if (!csInitializer::OpenApplication (object_reg))
+  // i.e. all windows will be opened.
+  if (!OpenApplication(GetObjectRegistry()))
+    return ReportError("Error opening system!");
+
+  if (SetupModules())
   {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-    	"crystalspace.application.joytest",
-    	"Error opening system!");
-    return false;
+    // This calls the default runloop. This will basically just keep
+    // broadcasting process events to keep the game going.
+    Run();
   }
+
+  return true;
+}
+bool Simple::SetupModules ()
+{
+  // Now get the pointer to various modules we need. We fetch them
+  // from the object registry. The RequestPlugins() call we did earlier
+  // registered all loaded plugins with the object registry.
+  g3d = csQueryRegistry<iGraphics3D> (GetObjectRegistry());
+  if (!g3d) return ReportError("Failed to locate 3D renderer!");
+
+  engine = csQueryRegistry<iEngine> (GetObjectRegistry());
+  if (!engine) return ReportError("Failed to locate 3D engine!");
+
+  vc = csQueryRegistry<iVirtualClock> (GetObjectRegistry());
+  if (!vc) return ReportError("Failed to locate Virtual Clock!");
+
+  kbd = csQueryRegistry<iKeyboardDriver> (GetObjectRegistry());
+  if (!kbd) return ReportError("Failed to locate Keyboard Driver!");
+
+  joy= csQueryRegistry<iJoystickDriver> (object_reg);
+  if (!joy) return ReportError("Failed to locate Joystick Driver!");
+
+  loader = csQueryRegistry<iLoader> (GetObjectRegistry());
+  if (!loader) return ReportError("Failed to locate Loader!");
+
+  // We need a View to the virtual world.
+  view.AttachNew(new csView (engine, g3d));
+  iGraphics2D* g2d = g3d->GetDriver2D ();
+  // We use the full window to draw the world.
+  view->SetRectangle (0, 0, g2d->GetWidth (), g2d->GetHeight ());
 
   // First disable the lighting cache. Our app is simple enough
   // not to need this.
   engine->SetLightingCacheMode (0);
 
-  if (!loader->LoadTexture ("stone", "/lib/std/stone4.gif"))
-  {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-    	"crystalspace.application.joytest",
-    	"Error loading 'stone4' texture!");
-    return false;
-  }
-  iMaterialWrapper* tm = engine->GetMaterialList ()->FindByName ("stone");
+  // Here we create our world.
+  CreateRoom();
 
+  // Let the engine prepare all lightmaps for use and also free all images 
+  // that were loaded for the texture manager.
+  engine->Prepare ();
+  rm = engine->GetRenderManager();
+
+  // these are used store the current orientation of the camera
+  rotY = rotX = 0;
+
+  // Now we need to position the camera in our world.
+  view->GetCamera ()->SetSector (room);
+  view->GetCamera ()->GetTransform ().SetOrigin (csVector3 (0, 5, -3));
+
+  // We use some other "helper" event handlers to handle 
+  // pushing our work into the 3D engine and rendering it
+  // to the screen.
+  //drawer.AttachNew(new FrameBegin3DDraw (GetObjectRegistry (), view));
+  printer.AttachNew(new FramePrinter (GetObjectRegistry ()));
+
+  return true;
+}
+
+void Simple::CreateRoom ()
+{
+  // Load the texture from the standard library.  This is located in
+  // CS/data/standard.zip and mounted as /lib/std using the Virtual
+  // File System (VFS) plugin.
+  if (!loader->LoadTexture ("brick", "/lib/std/castle/brick1_d.jpg"))
+    ReportError("Error loading 'brick1_d' texture!");
+
+  iMaterialWrapper* tm = engine->GetMaterialList ()->FindByName ("brick");
+
+  // Add a normal map to the material.
+  {
+    /* Shader variables are identified by numeric IDs for performance reasons.
+    * The shader var string set translates string IDs to numeric IDs. */
+    csRef<iShaderVarStringSet> svStrings =
+      csQueryRegistryTagInterface<iShaderVarStringSet> (GetObjectRegistry(),
+      "crystalspace.shader.variablenameset");
+    // Load the normal map texture itself
+    csRef<iTextureHandle> normalMap = loader->LoadTexture (
+      "/lib/std/castle/brick1_n.jpg");
+    // Set this to avoid compression - makes for better quality here
+    normalMap->SetTextureClass ("normalmap");
+    // The normal map is attached to the material through a shader variable.
+    csShaderVariable* svNormalMap =
+      tm->GetMaterial()->GetVariableAdd (svStrings->Request ("tex normal"));
+    svNormalMap->SetValue (normalMap);
+  }
+
+  // We create a new sector called "room".
   room = engine->CreateSector ("room");
+
+  // Creating the walls for our room.
 
   // First we make a primitive for our geometry.
   using namespace CS::Geometry;
@@ -310,69 +266,55 @@ bool Simple::Initialize ()
 
   // Now we make a factory and a mesh at once.
   csRef<iMeshWrapper> walls = GeneralMeshBuilder::CreateFactoryAndMesh (
-      engine, room, "walls", "walls_factory", &box);
+    engine, room, "walls", "walls_factory", &box);
 
   csRef<iGeneralMeshState> mesh_state = scfQueryInterface<
     iGeneralMeshState> (walls->GetMeshObject ());
   mesh_state->SetShadowReceiving (true);
   walls->GetMeshObject ()->SetMaterialWrapper (tm);
 
+  // Now we need light to see something.
   csRef<iLight> light;
   iLightList* ll = room->GetLights ();
 
-  light = engine->CreateLight (0, csVector3 (-3, 5, 0), 10,
-  	csColor (1, 0, 0));
+  light = engine->CreateLight(0, csVector3(-3, 5, 0), 10, csColor(2, 0, 0));
   ll->Add (light);
 
-  light = engine->CreateLight (0, csVector3 (3, 5,  0), 10,
-  	csColor (0, 0, 1));
+  light = engine->CreateLight(0, csVector3(3, 5,  0), 10, csColor(0, 0, 2));
   ll->Add (light);
 
-  light = engine->CreateLight (0, csVector3 (0, 5, -3), 10,
-  	csColor (0, 1, 0));
+  light = engine->CreateLight(0, csVector3(0, 5, -3), 10, csColor(0, 2, 0));
   ll->Add (light);
-
-  engine->Prepare ();
-
-  view = csPtr<iView> (new csView (engine, g3d));
-  view->GetCamera ()->SetSector (room);
-  view->GetCamera ()->GetTransform ().SetOrigin (csVector3 (0, 5, -3));
-  iGraphics2D* g2d = g3d->GetDriver2D ();
-  view->SetRectangle (0, 0, g2d->GetWidth (), g2d->GetHeight ());
 
   iTextureManager* txtmgr = g3d->GetTextureManager ();
 
   // Load a texture for our sprite.
   iTextureWrapper* txt = loader->LoadTexture ("spark",
-  	"/lib/std/spark.png", CS_TEXTURE_3D, txtmgr, true);
+    "/lib/std/spark.png", CS_TEXTURE_3D, txtmgr, true);
   if (txt == 0)
   {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-    	"crystalspace.application.joytest",
-    	"Error loading texture!");
-    return false;
+    ReportError ("Error loading texture!");
+    return;
   }
 
   // Load a sprite template from disk.
   csRef<iMeshFactoryWrapper> imeshfact (
-  	loader->LoadMeshObjectFactory ("/lib/std/sprite1"));
+    loader->LoadMeshObjectFactory ("/lib/std/sprite1"));
   if (imeshfact == 0)
   {
-    csReport (object_reg, CS_REPORTER_SEVERITY_ERROR,
-    	"crystalspace.application.joytest",
-    	"Error loading mesh object factory!");
-    return false;
+    ReportError ("Error loading mesh object factory!");
+    return;
   }
 
   // Create the sprite and add it to the engine.
   csRef<iMeshWrapper> sprite (engine->CreateMeshWrapper (
-  	imeshfact, "MySprite", room,
-	csVector3 (-3, 5, 3)));
+    imeshfact, "MySprite", room,
+    csVector3 (-3, 5, 3)));
   csMatrix3 m; m.Identity (); m *= 5.;
   sprite->GetMovable ()->SetTransform (m);
   sprite->GetMovable ()->UpdateMove ();
   csRef<iSprite3DState> spstate (
-  	scfQueryInterface<iSprite3DState> (sprite->GetMeshObject ()));
+    scfQueryInterface<iSprite3DState> (sprite->GetMeshObject ()));
   spstate->SetAction ("default");
   //spstate->SetMixMode (CS_FX_SETALPHA (.5));
 
@@ -381,29 +323,21 @@ bool Simple::Initialize ()
   // can do this.
   sprite->SetZBufMode (CS_ZBUF_USE);
   sprite->SetRenderPriority (engine->GetObjectRenderPriority ());
-
-  return true;
 }
 
-void Simple::Start ()
-{
-  csDefaultRunLoop (object_reg);
-}
 
-/*---------------------------------------------------------------------*
- * Main function
- *---------------------------------------------------------------------*/
+/*-------------------------------------------------------------------------*
+* Main function
+*-------------------------------------------------------------------------*/
 int main (int argc, char* argv[])
 {
-  iObjectRegistry* object_reg = csInitializer::CreateEnvironment (argc, argv);
-
-  simple = new Simple (object_reg);
-  if (simple->Initialize ())
-    simple->Start ();
-  delete simple;
-  simple = 0;
-
-  csInitializer::DestroyApplication (object_reg);
-  return 0;
+  /* Runs the application. 
+  *
+  * csApplicationRunner<> is a small wrapper to support "restartable" 
+  * applications (ie where CS needs to be completely shut down and loaded 
+  * again). Joytest does not use that functionality itself, however, it
+  * allows you to later use "Simple.Restart();" and it'll just work.
+  */
+  return csApplicationRunner<Simple>::Run (argc, argv);
 }
 
