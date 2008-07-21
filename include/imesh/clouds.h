adf8d31b1a3274eab0b5bf2d5f27cdf0e5b0c44e
Julian Mautner <panther88@users.sourceforge.net>
2008-07-21 14:36:21 +0000

    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2008/clouds@30474 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/imesh/clouds.h
+++ include/imesh/clouds.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2002 by Jorrit Tyberghein
+    Copyright (C) 2008 by Julian Mautner
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -16,11 +16,13 @@
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#ifndef __CS_CSCLOUDSAPI_H__
-#define __CS_CSCLOUDSAPI_H__
+#ifndef __CS_IMESH_CLOUDS_H
+#define __CS_IMESH_CLOUDS_H
 
 #include <csutil/scf.h>
 #include <csutil/scf_implementation.h>
+
+#include "csutil/array.h"
 
 /**
 This APIs implementation is a supervisor-class, which takes 
@@ -40,7 +42,7 @@ struct iClouds : public virtual iBase
 	Does a single timestep of size dTime for the cloud-dynamics-simulation
 	If dTime is not set, the time is measured automatically, in order to achive realtime-simulation
 	*/
-	virtual const bool DoTimeStep(const double dTime = 0.f) = 0;
+	virtual const bool DoTimeStep(const float fTime = 0.f) = 0;
 
 	/**
 	This method does an amortized time step: Means it doesn't do all the computations
@@ -51,55 +53,13 @@ struct iClouds : public virtual iBase
 	of this method, only the first value is considered! 
 	If not set, the time is measured automatically, in order to achive realtime-simulation.
 	*/
-	virtual const bool DoAmortTimeStep(const double dTime = 0.f) = 0;
+	virtual const bool DoAmortTimeStep(const float fTime = 0.f) = 0;
 
 	/**
 	This method starts the rendering process. All clouds are rendered at
 	the calculated positions and formations.
 	*/
 	virtual const bool RenderClouds(/* Transformationmatrix? */) = 0;
-};
-
-//--------------------------------------------------------------------------------------------//
-
-/**
-This class does all the rendering and illumination stuff.
-The only input it needs is the condensed-water-mixing-ration
-scalar field!
-*/
-struct iCloudsRenderer : public virtual iBase
-{
-	SCF_INTERFACE(iCloudsRenderer, 0, 0, 1);
-
-	/**
-	Rendermethod. Renders the whole cloud scene.
-	*/
-	virtual const bool Render(const iField3<double>& aaaMixingRatios /*, const csMatrix& mTransformation */) = 0;
-};
-
-//--------------------------------------------------------------------------------------------//
-
-/**
-This class simulates all the physics behind the cloud dynamics. This methods will
-contain the bottleneck of the overall system. The output of the simulition
-is a 3d- scalar field containing all the condensed water mixing ratios.
-*/
-struct iCloudsDynamics : public virtual iBase
-{
-	SCF_INTERFACE(iCloudsDynamics, 0, 0, 1);
-
-	/**
-	Does n computation steps. The overall calculations are split into several
-	subtasks. This method computes n of those. If it gets a zero as input, it
-	performs all for an entire time-step
-	*/
-	virtual const bool DoComputationSteps(const UINT iStepCount, const double dTime = 0.) = 0;
-
-	/**
-	Returns the simulation-output. A scalarfield which contains all the condensed
-	water mixing ratios for the entire cloud-volume
-	*/
-	virtual inline const iField3<double> GetCondWaterMixingRatios() const = 0;
 };
 
 //--------------------------------------------------------------------------------------------//
@@ -159,5 +119,49 @@ struct iField3 : public virtual iBase
 	virtual const T GetValue(const UINT x, const UINT y, const UINT z) const = 0;
 };
 
+//--------------------------------------------------------------------------------------------//
 
-#endif // __CS_CSCLOUDSAPI_H__
+/**
+This class does all the rendering and illumination stuff.
+The only input it needs is the condensed-water-mixing-ration
+scalar field!
+*/
+struct iCloudsRenderer : public virtual iBase
+{
+	SCF_INTERFACE(iCloudsRenderer, 0, 0, 1);
+
+	/**
+	Rendermethod. Renders the whole cloud scene.
+	*/
+	virtual const bool Render(const iField3<float>& aaafMixingRatios /*, const csMatrix& mTransformation */) = 0;
+};
+
+//--------------------------------------------------------------------------------------------//
+
+/**
+This class simulates all the physics behind the cloud dynamics. This methods will
+contain the bottleneck of the overall system. The output of the simulition
+is a 3d- scalar field containing all the condensed water mixing ratios.
+*/
+struct iCloudsDynamics : public virtual iBase
+{
+	SCF_INTERFACE(iCloudsDynamics, 0, 0, 1);
+
+	/**
+	Does n computation steps. The overall calculations are split into several
+	subtasks. This method computes n of those. If it gets a zero as input, it
+	performs all for an entire time-step
+	*/
+	virtual const bool DoComputationSteps(const UINT iStepCount, const float fTime = 0.) = 0;
+
+	/**
+	Returns the simulation-output. A scalarfield which contains all the condensed
+	water mixing ratios for the entire cloud-volume
+	*/
+	virtual inline const iField3<float> GetCondWaterMixingRatios() const = 0;
+};
+
+//--------------------------------------------------------------------------------------------//
+
+
+#endif // __CS_IMESH_CLOUDS_H
