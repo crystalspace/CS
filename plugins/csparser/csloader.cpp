a2ffa9d318cfb8eeebfc70145c2d6c41dd629aaa
Mike Gist <xordan@gmail.com>
2008-07-27 13:48:07 +0000

    Merged revisions 30487,30553 via svnmerge from https://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk
    ................
      r30487 | mgist | 2008-07-23 11:18:22 +0100 (Wed, 23 Jul 2008) | 9 lines
      
      Merged revisions 30486 via svnmerge from 
      https://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2008/threadcomm
      
      ........
        r30486 | mgist | 2008-07-23 11:03:05 +0100 (Wed, 23 Jul 2008) | 1 line
        
        - Added a missing dupe check in LoadLibrary.
      ........
    ................
      r30553 | thebolt00 | 2008-07-26 21:45:46 +0100 (Sat, 26 Jul 2008) | 2 lines
      
      Fixed a bug whereby bruteblock renderer in terrain2 would crash if you don't have any material palette.
    ................
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/release/V1.4@30567 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/csparser/csloader.cpp
+++ plugins/csparser/csloader.cpp
@@ -2015,22 +2015,32 @@ bool csLoader::LoadLibrary (iLoaderContext* ldr_context, iDocumentNode* libnode,
         break;
       case XMLTOKEN_MESHFACT:
         {
-          csRef<iMeshFactoryWrapper> t = Engine->CreateMeshFactory (
-	    child->GetAttributeValue ("name"));
-	  if (t)
-	  {
-	    if (!LoadMeshObjectFactory (ldr_context, t, 0, child, 0, ssource))
-	    {
-	      // Error is already reported.
-	      return false;
-	    }
-	    else
-	    {
-	      AddToRegionOrCollection (ldr_context, t->QueryObject ());
-	    }
-	  }
-	}
-	break;
+          const char* meshfactname = child->GetAttributeValue ("name");
+          if(ldr_context->CheckDupes ())
+          {
+            iMeshFactoryWrapper* mfw = Engine->FindMeshFactory (meshfactname);
+            if(mfw)
+            {
+              AddToRegionOrCollection (ldr_context, mfw->QueryObject ());
+              break;
+            }
+          }
+
+          csRef<iMeshFactoryWrapper> t = Engine->CreateMeshFactory (meshfactname);
+          if (t)
+          {
+            if (!LoadMeshObjectFactory (ldr_context, t, 0, child, 0, ssource))
+            {
+              // Error is already reported.
+              return false;
+            }
+            else
+            {
+              AddToRegionOrCollection (ldr_context, t->QueryObject ());
+            }
+          }
+        }
+        break;
       case XMLTOKEN_PLUGINS:
 	if (!LoadPlugins (child))
 	  return false;
