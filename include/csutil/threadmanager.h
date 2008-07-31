be9e960f833f944be855a0a4df761e6a7370da2d
Mike Gist <xordan@gmail.com>
2008-07-31 16:10:02 +0000

    - Some reordering.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2008/threadcomm@30649 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/threadmanager.h
+++ include/csutil/threadmanager.h
@@ -274,6 +274,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[3]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -281,8 +283,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   ret.AttachNew(new returnClass(tm)); \
   args[1] = mempool->Store<csRef<iThreadReturn> >(ret); \
   args[2] = mempool->Store<T1>((T1)A1); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn> , T1>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -300,6 +300,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[4]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -308,8 +310,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[1] = mempool->Store<csRef<iThreadReturn> >(ret); \
   args[2] = mempool->Store<T1>((T1)A1); \
   args[3] = mempool->Store<T2>((T2)A2); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn> , T1, T2>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -327,6 +327,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[5]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -336,8 +338,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[2] = mempool->Store<T1>((T1)A1); \
   args[3] = mempool->Store<T2>((T2)A2); \
   args[4] = mempool->Store<T3>((T3)A3); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -356,6 +356,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4) const \
 { \
   void const** args = new void const*[6]; \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
   csRef<iThreadReturn> ret; \
@@ -365,8 +367,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[3] = mempool->Store<T2>((T2)A2); \
   args[4] = mempool->Store<T3>((T3)A3); \
   args[5] = mempool->Store<T4>((T4)A4); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -384,6 +384,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[7]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -395,8 +397,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[4] = mempool->Store<T3>((T3)A3); \
   args[5] = mempool->Store<T4>((T4)A4); \
   args[6] = mempool->Store<T5>((T5)A5); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -414,6 +414,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5, T6 A6) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[8]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -426,8 +428,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[5] = mempool->Store<T4>((T4)A4); \
   args[6] = mempool->Store<T5>((T5)A5); \
   args[7] = mempool->Store<T6>((T6)A6); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5, T6>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -445,6 +445,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5, T6 A6, T7 A7) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[9]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -458,8 +460,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[6] = mempool->Store<T5>((T5)A5); \
   args[7] = mempool->Store<T6>((T6)A6); \
   args[8] = mempool->Store<T7>((T7)A7); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5, T6, T7>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -477,6 +477,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5, T6 A6, T7 A7, T8 A8) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[10]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -491,8 +493,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[7] = mempool->Store<T6>((T6)A6); \
   args[8] = mempool->Store<T7>((T7)A7); \
   args[9] = mempool->Store<T8>((T8)A8); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5, T6, T7, T8>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -510,6 +510,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5, T6 A6, T7 A7, T8 A8, T9 A9) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[11]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -525,8 +527,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[8] = mempool->Store<T7>((T7)A7); \
   args[9] = mempool->Store<T8>((T8)A8); \
   args[10] = mempool->Store<T9>((T9)A9); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5, T6, T7, T8, T9>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -544,6 +544,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5, T6 A6, T7 A7, T8 A8, T9 A9, T10 A10) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[12]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -560,8 +562,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[9] = mempool->Store<T8>((T8)A8); \
   args[10] = mempool->Store<T9>((T9)A9); \
   args[11] = mempool->Store<T10>((T10)A10); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -579,6 +579,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5, T6 A6, T7 A7, T8 A8, T9 A9, T10 A10, T11 A11) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[13]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -596,8 +598,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[10] = mempool->Store<T9>((T9)A9); \
   args[11] = mempool->Store<T10>((T10)A10); \
   args[12] = mempool->Store<T11>((T11)A11); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -615,6 +615,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5, T6 A6, T7 A7, T8 A8, T9 A9, T10 A10, T11 A11, T12 A12) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[14]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -633,8 +635,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[11] = mempool->Store<T10>((T10)A10); \
   args[12] = mempool->Store<T11>((T11)A11); \
   args[13] = mempool->Store<T12>((T12)A12); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -652,6 +652,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5, T6 A6, T7 A7, T8 A8, T9 A9, T10 A10, T11 A11, T12 A12, T13 A13) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[15]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -671,8 +673,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[12] = mempool->Store<T11>((T11)A11); \
   args[13] = mempool->Store<T12>((T12)A12); \
   args[14] = mempool->Store<T13>((T13)A13); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -690,6 +690,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5, T6 A6, T7 A7, T8 A8, T9 A9, T10 A10, T11 A11, T12 A12, T13 A13, T14 A14) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[16]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -710,8 +712,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[13] = mempool->Store<T12>((T12)A12); \
   args[14] = mempool->Store<T13>((T13)A13); \
   args[15] = mempool->Store<T14>((T14)A14); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
@@ -729,6 +729,8 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
 } \
   iThreadReturn* function(T1 A1, T2 A2, T3 A3, T4 A4, T5 A5, T6 A6, T7 A7, T8 A8, T9 A9, T10 A10, T11 A11, T12 A12, T13 A13, T14 A14, T15 A15) const \
 { \
+  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
+  CS_ASSERT(tm.IsValid()); \
   void const** args = new void const*[17]; \
   TEventMemPool *mempool = new TEventMemPool; \
   args[0] = mempool; \
@@ -750,8 +752,6 @@ void QueueEvent(iThreadManager* tm, ThreadedCallable<T>* object, void (T::*metho
   args[14] = mempool->Store<T13>((T13)A13); \
   args[15] = mempool->Store<T14>((T14)A14); \
   args[16] = mempool->Store<T15>((T15)A15); \
-  csRef<iThreadManager> tm = csQueryRegistry<iThreadManager>(GetObjectRegistry()); \
-  CS_ASSERT(tm.IsValid()); \
   QueueEvent<type, csRef<iThreadReturn>, T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15>(tm, (ThreadedCallable*)this, &type::function##TC, args, useThreadQueue); \
   if(wait) \
   { \
