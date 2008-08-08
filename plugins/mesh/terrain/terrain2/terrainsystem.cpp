d842974bd33693b36224d654a0cab14b1e36fe68
Mike Gist <xordan@gmail.com>
2008-08-08 21:58:30 +0000

    Merged revisions 30728-30729 via svnmerge from https://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk
    ........
      r30728 | thebolt00 | 2008-08-08 19:16:53 +0100 (Fri, 08 Aug 2008) | 2 lines
      
      Terrain2: Reworked how cell bounding boxes are setup to give tighter AABBs and also support negative heights
    ........
      r30729 | thebolt00 | 2008-08-08 21:43:46 +0100 (Fri, 08 Aug 2008) | 2 lines
      
      Fixed simple portals in "real" rendermanagers. Problem was confusion between coordinate systems in portal polygon setup and clipping.
    ........
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2008/threadcomm@30730 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/terrain/terrain2/terrainsystem.cpp
+++ plugins/mesh/terrain/terrain2/terrainsystem.cpp
@@ -69,6 +69,8 @@ void csTerrainSystem::AddCell (csTerrainCell* cell)
   }
 
   boundingbox += cell->GetBBox ();
+
+  ShapeChanged ();
 }
 
 void csTerrainSystem::FireLoadCallbacks (csTerrainCell* cell)
@@ -103,6 +105,12 @@ void csTerrainSystem::FireHeightUpdateCallbacks (csTerrainCell* cell,
     heightDataCallbacks[i]->OnHeightUpdate (cell, rectangle);
   }
 }
+
+void csTerrainSystem::CellSizeUpdate (csTerrainCell* cell)
+{
+  ComputeBBox ();
+}
+
 
 iTerrainCell* csTerrainSystem::GetCell (const char* name, bool loadData)
 {
@@ -580,6 +588,20 @@ void csTerrainSystem::InvalidateMaterialHandles ()
 csColliderType csTerrainSystem::GetColliderType ()
 {
   return CS_TERRAIN_COLLIDER;
+}
+
+void csTerrainSystem::ComputeBBox ()
+{
+  boundingbox.Empty ();
+  boundingbox.StartBoundingBox ();
+  bbStarted = true;
+
+  for (size_t i = 0; i < cells.GetSize (); ++i)
+  {
+    boundingbox += cells[i]->GetBBox ();
+  }
+
+  ShapeChanged ();
 }
 
 }
