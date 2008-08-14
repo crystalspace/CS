78601ec72ad0db0e3410f0d805db40b619cd33d4
Frank Richter <res@crystalspace3d.org>
2008-08-14 13:11:37 +0000

    MSI generation stuff from andres
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@30775 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ scripts/msi/csguidregistry.py
@@ -0,0 +1,36 @@
+guid_registry = {
+    #We compute some uuids out of a base and a unique id (mostly component guids)
+    "base": '174182C0-BF94-11DC-B42B-0015582877AD',
+
+    #Upgrade codes for our installers. Those may not change.
+    "CS.upgrade_code": '3C8B4562-BF93-11DC-B42B-0015582877AD',
+    "CS.cel.upgrade_code": '64D0D104-BF93-11DC-B42B-0015582877AD',
+    "CS.b2CS.upgrade_code": '7552D0F4-BF93-11DC-B42B-0015582877AD',
+    "cel.upgrade_code": '7C953DF2-BF93-11DC-B42B-0015582877AD',
+    "b2cs.upgrade_code": '826602C0-BF93-11DC-B42B-0015582877AD',
+
+    #CS merge modules
+    "CS.doc.msm": '88888F92-BF93-11DC-B42B-0015582877AD',
+    "CS.vfs.msm": '8DDFD4F0-BF93-11DC-B42B-0015582877AD',
+    "CS.register.msm": '8E4AB45A-BF93-11DC-B42B-0015582877AD',
+    "CS.data.msm": '8EA81938-BF93-11DC-B42B-0015582877AD',
+    "CS.include.msm": '8F1B7996-BF93-11DC-B42B-0015582877AD',
+    "CS.arch.GCC.include.msm": '8F6CB040-BF93-11DC-B42B-0015582877AD',
+    "CS.arch.GCC.lib.msm": 'A6DDD416-BF93-11DC-B42B-0015582877AD',
+    "CS.arch.GCC.executable.msm": 'AC4E1EB0-BF93-11DC-B42B-0015582877AD',
+    "CS.arch.GCC.support.msm": 'B18119AA-BF93-11DC-B42B-0015582877AD',
+
+    #cel merge modules
+    "cel.doc.msm": 'BFEEA872-BF93-11DC-B42B-0015582877AD',
+    "cel.vfs.msm": 'C7E67122-BF93-11DC-B42B-0015582877AD',
+    "cel.register.msm": 'D27841CE-BF93-11DC-B42B-0015582877AD',
+    "cel.data.msm": 'DD058534-BF93-11DC-B42B-0015582877AD',
+    "cel.include.msm": 'E639D420-BF93-11DC-B42B-0015582877AD',
+    "cel.arch.GCC.include.msm": 'F65B66AC-BF93-11DC-B42B-0015582877AD',
+    "cel.arch.GCC.lib.msm": '039A60DE-BF94-11DC-B42B-0015582877AD',
+    "cel.arch.GCC.executable.msm": '06DF1D7A-BF94-11DC-B42B-0015582877AD',
+
+    "b2cs.blender.plugin.msm": 'FE7E2FC8-BFF6-11DC-BDF4-001302164650',
+
+    "python": '4E60ED45-1DBB-44AD-A77F-E17E9C8BDCFB',
+    }
