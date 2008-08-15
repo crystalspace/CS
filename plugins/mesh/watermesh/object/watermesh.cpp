551f01bad3fdb96f683fbd88e259905a80cdf8d2
Pavel Krajcevski <krajcevski@users.sourceforge.net>
2008-08-15 06:57:16 +0000

    Added doxymentation to watermesh files.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2008/water@30802 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/watermesh/object/watermesh.cpp
+++ plugins/mesh/watermesh/object/watermesh.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2004 by Jorrit Tyberghein
+Copyright (C) 2008 by Pavel Krajcevski
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Library General Public
--- plugins/mesh/watermesh/object/watermesh.h
+++ plugins/mesh/watermesh/object/watermesh.h
@@ -1,19 +1,19 @@
 /*
-    Copyright (C) 2004 by Jorrit Tyberghein
+Copyright (C) 2008 by Pavel Krajcevski
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Library General Public
+License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.`
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public
-    License along with this library; if not, write to the Free
-    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+You should have received a copy of the GNU Library General Public
+License along with this library; if not, write to the Free
+Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
 #ifndef __CS_WATERMESH_H__
@@ -145,11 +145,17 @@ private:
 
   void updateLocal();
 
-  void DrawFromNode(csOceanNode start, const csVector3 camPos, csPlane3 *planes, uint32 frustum_mask);
-  void DrawLeftFromNode(csOceanNode start, const csVector3 camPos, csPlane3 *planes, uint32 frustum_mask);
-  void DrawRightFromNode(csOceanNode start, const csVector3 camPos, csPlane3 *planes, uint32 frustum_mask);
-  void DrawTopFromNode(csOceanNode start, const csVector3 camPos, csPlane3 *planes, uint32 frustum_mask);
-  void DrawBottomFromNode(csOceanNode start, const csVector3 camPos, csPlane3 *planes, uint32 frustum_mask);
+  // These are the recursive functions used for placing ocean cells
+  void DrawFromNode(csOceanNode start, const csVector3 camPos, 
+    csPlane3 *planes, uint32 frustum_mask);
+  void DrawLeftFromNode(csOceanNode start, const csVector3 camPos, 
+    csPlane3 *planes, uint32 frustum_mask);
+  void DrawRightFromNode(csOceanNode start, const csVector3 camPos, 
+    csPlane3 *planes, uint32 frustum_mask);
+  void DrawTopFromNode(csOceanNode start, const csVector3 camPos, 
+    csPlane3 *planes, uint32 frustum_mask);
+  void DrawBottomFromNode(csOceanNode start, const csVector3 camPos, 
+    csPlane3 *planes, uint32 frustum_mask);
 
   void AddNode(csOceanNode start, float dist);
 
