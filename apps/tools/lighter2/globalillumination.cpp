6175797b09ebc08311f4f7d5fb2f3b72ea8b3eeb
Greg Hoffman <glh3586@users.sourceforge.net>
2008-08-18 20:00:23 +0000

    Added license boilerplates to the cpp files that I forgot to add.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2008/lighter2@30848 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/tools/lighter2/globalillumination.cpp
+++ apps/tools/lighter2/globalillumination.cpp
@@ -1,4 +1,20 @@
+/*
+  Copyright (C) 2008 by Greg Hoffman
 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with this library; if not, write to the Free
+  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #include "common.h"
 #include "Photonmap.h"
 #include "lighter.h"
