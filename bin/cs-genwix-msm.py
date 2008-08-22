9bbeffd1f30ddb08662062f462ba517aeb8268a4
Frank Richter <res@crystalspace3d.org>
2008-08-22 17:26:14 +0000

    Allow specification of subdirs for file lists put into an MSM.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@30934 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- bin/cs-genwix-msm.py
+++ bin/cs-genwix-msm.py
@@ -12,6 +12,9 @@ options.add_option("-o", "--out", dest="outfile",
                     help="Output file name", metavar="FILE")
 options.add_option("--id", dest="msmid",
                     help="Merge module ID (used to look up GUID)", metavar="ID")
+options.add_option("--subdirmap", dest="subdirmap",
+                    help="Subdirectory map to place files in. (Multiple allowed)", 
+                    action="append", metavar="LIST:DIR")
 options.add_option("--version", dest="version",
                     help="Package version", metavar="VERSION")
 
@@ -21,6 +24,12 @@ if not parsed_options.outfile:
     options.error("missing --out")
 if not parsed_options.msmid:
     options.error("missing --id")
+
+if parsed_options.subdirmap:
+    subdirmap = {}
+    for s in parsed_options.subdirmap:
+        (list, subdirs) = s.split (':', 1)
+        subdirmap[list] = subdirs
 
 (base, merge, dir) = generate_merge_module(id = parsed_options.msmid,
                                            path = [('TARGETDIR', 'SourceDir', None),
@@ -34,14 +43,25 @@ f = Fragment(
     parent = base,
     id = parsed_options.msmid)
 
-ref = DirectoryRef(
+dirref = DirectoryRef(
     parent = f,
     id = parsed_options.msmid + '_dir')
 
 for list in args:
+    listid = os.path.splitext(os.path.basename(list))[0]
+    thisdir = dirref
+    if subdirmap and subdirmap.has_key(listid):
+        dirid = parsed_options.msmid
+        list_subdir = subdirmap[listid]
+        for subdir in list_subdir.split('/'):
+            dirid = dirid + '.' + subdir
+            thisdir = Directory(
+                parent = thisdir,
+                id = dirid + '_d',
+                name = subdir)
     generate_from_file_list_txt (list,
                                  parent = merge,
-                                 prefix = parsed_options.msmid,
-                                 dir = ref)
+                                 prefix = parsed_options.msmid + '.' + listid,
+                                 dir = thisdir)
         
 base.save()
