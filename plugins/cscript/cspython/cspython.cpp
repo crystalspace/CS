0c8c1a28a699de6dea11151194ac2b51d5c16526
Frank Richter <res@crystalspace3d.org>
2008-09-29 18:56:01 +0000

    Tweaked the _DEBUG undefining for Python.h: Now, only undefine it for Python.h, but no standard headers. This is required to ensure (a) _DEBUG is set for standard headers on MSVC debug builds (b) _DEBUG is set consistently for standard headers on MSVC.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@31313 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/cscript/cspython/cspython.cpp
+++ plugins/cscript/cspython/cspython.cpp
@@ -17,10 +17,17 @@
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#if !defined(DEBUG_PYTHON) && defined(_DEBUG)
+#include <io.h>
+#include <stdarg.h>
+#ifndef DEBUG_PYTHON
 #undef _DEBUG
+#define RESTORE__DEBUG
 #endif
-
+#include <Python.h>
+#ifdef RESTORE__DEBUG
+#define _DEBUG
+#undef RESTORE__DEBUG
+#endif
 
 #include <stdio.h>
 
