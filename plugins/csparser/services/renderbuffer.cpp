488fdfccf346a3378367ad610cc5c7ffaeabc2dc
Frank Richter <res@crystalspace3d.org>
2008-11-28 14:17:25 +0000

    Bloop, remove debug code
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@31646 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/csparser/services/renderbuffer.cpp
+++ plugins/csparser/services/renderbuffer.cpp
@@ -1068,11 +1068,6 @@ csRef<iRenderBuffer> csTextSyntaxService::ReadRenderBuffer (iDataBuffer* buf,
   if (buf->GetSize() < sizeof (RenderBufferHeaderCommon)) return 0;
   RenderBufferHeaderCommon* header = 
     reinterpret_cast<RenderBufferHeaderCommon*> (buf->GetData());
-  printf("sizeof (RenderBufferHeaderCommon) = %u\n", sizeof (RenderBufferHeaderCommon));
-  printf("header->magic = %.4x, ", header->magic);
-  printf("header->compType = %.4x, ", header->compType);
-  printf("header->compCount = %.4x, ", header->compCount);
-  printf("header->elementCount = %.4x\n", header->elementCount);
 
   const uint16 magic = csLittleEndian::Convert (header->magic);
   if ((magic != RenderBufferHeaderCommon::MagicNormal)
