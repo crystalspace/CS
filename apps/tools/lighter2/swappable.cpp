1485f6f766a9d0f6899c96c8ee82bebceb9a1148
Frank Richter <res@crystalspace3d.org>
2008-12-21 10:26:12 +0000

    Merged revisions 31763,31767 via svnmerge from https://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk
    ........
      r31763 | res2002 | 2008-12-19 04:46:32 +0100 (Fri, 19 Dec 2008) | 1 line
      
      Fix swap cache size uninitialized on 64bit platforms
    ........
      r31767 | philwyett | 2008-12-19 06:49:36 +0100 (Fri, 19 Dec 2008) | 3 lines
      
       - Temporarily disable S3TC on Mesa Intel DRI. This eliminates a startup
         error message. Please report any issues or performance hits on trac.
    ........
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/release/V1.4@31793 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/tools/lighter2/swappable.cpp
+++ apps/tools/lighter2/swappable.cpp
@@ -33,6 +33,8 @@ namespace lighter
 #if CS_PROCESSOR_SIZE == 32
     //@@ Try to make sure we don't run out of virtual address space, don't use above 1GB for swapcache
     maxCacheSize = csMin (maxSize, size_t (1u<<30));
+#else
+    maxCacheSize = maxSize;
 #endif
   }
 
