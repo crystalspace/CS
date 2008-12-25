f6257f43fdddb18c66366353cb597a5f0d244acc
Mike Gist <xordan@gmail.com>
2008-12-25 07:39:29 +0000

    - Silenced some 'false' deprecation warnings.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@31818 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/iengine/camera.h
+++ include/iengine/camera.h
@@ -301,12 +301,16 @@ struct iCamera : public virtual iBase
   /// Get the hit-only-portals flag.
   virtual bool GetOnlyPortals () = 0;
 
+#include "csutil/deprecated_warn_off.h"
+
   /// Add a listener to this camera.
   CS_DEPRECATED_METHOD_MSG("Use iCameraListener instead")
   virtual void AddCameraSectorListener (iCameraSectorListener* listener) = 0;
   /// Remove a listener from this camera.
   CS_DEPRECATED_METHOD_MSG("Use iCameraListener instead")
   virtual void RemoveCameraSectorListener (iCameraSectorListener* listener) = 0;
+
+#include "csutil/deprecated_warn_on.h"
 
   /// Add a listener to this camera.
   virtual void AddCameraListener (iCameraListener* listener) = 0;
