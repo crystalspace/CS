1963147d8c2e5c99b272ebaf91cf9b5f364d15c7
Frank Richter <res@crystalspace3d.org>
2009-01-24 12:53:52 +0000

    Merged revisions 30763,30787,30802,30826,30851 via svnmerge from https://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2008/water
    ........
      r30763 | Krajcevski | 2008-08-13 20:51:46 +0200 (Wed, 13 Aug 2008) | 1 line
      
      Updated world file.
    ........
      r30787 | Krajcevski | 2008-08-15 00:02:28 +0200 (Fri, 15 Aug 2008) | 1 line
      
      Added manual entry for watermesh
    ........
      r30802 | Krajcevski | 2008-08-15 08:57:16 +0200 (Fri, 15 Aug 2008) | 1 line
      
      Added doxymentation to watermesh files.
    ........
      r30826 | Krajcevski | 2008-08-16 19:43:15 +0200 (Sat, 16 Aug 2008) | 1 line
      
      Updated shader configuration after merge
    ........
      r30851 | Krajcevski | 2008-08-18 23:39:54 +0200 (Mon, 18 Aug 2008) | 1 line
      
      Removed unnecessary plugin from world file
    ........
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@31995 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/imesh/watermesh.h
+++ include/imesh/watermesh.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2004 by Jorrit Tyberghein
+    Copyright (C) 2008 by Pavel Krajcevski
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -20,7 +20,7 @@
 #define __CS_IMESH_WATERMESH_H__
 
 /**\file
- * Tutorial mesh object
+ * Water mesh object
  */ 
 
 #include "csutil/scf.h"
@@ -29,114 +29,113 @@
 /**\addtogroup meshplugins
  * @{ */
 
-class csVector3;
-class csVector2;
-class csColor;
-
-struct csTriangle;
-struct iMaterialWrapper;
-
 /**
- * The proto mesh is a demonstration or tutorial mesh. It is
- * very simple and really unusable in games but it is a very good
- * start to make a new mesh object. 
- * 
- * The proto mesh supports:
- * - Primitive geometry (8 vertices, 12 triangles, just enough for a box).
- * - Setting of base color and per vertex color.
- * - Setting of vertices, texels, and normals.
- * - Material per mesh object.
- * - Sharing geometry in the factory.
- * - Collision detection.
- * - Direct creation of render buffers.
- * - Delayed creation of render buffers.
- * 
- * The general API for the proto factory. Here you define the
- * actual geometry which is shared between all proto mesh instances.
- * 
- * Main creators of instances implementing this interface:
- * - Protomesh mesh object plugin (crystalspace.mesh.object.protomesh)
- * - iMeshObjectType::NewFactory()
- *   
- * Main ways to get pointers to this interface:
- * - scfQueryInterface() on iMeshFactoryWrapper::GetMeshObjectFactory()
- *   
- * Main users of this interface:
- * - Protomesh Factory Loader plugin
-        (crystalspace.mesh.loader.factory.protomesh)
+ * The water mesh is a simple mesh representation for a horizontal square. It
+ * is best used in conjunction with a shader able to properly simulate the behavior
+ * of water. The mesh supports ocean rendering with extremely basic LOD.
+ *
+ * The different properties that can be set for the waves are given by the 
+ * amplitudes, phases, frequencies, and directions of combinations of sine and
+ * cosine waves used to permute the vertices. These values get passed to the proper
+ * shader via shader variables which permute the vertices in shader vertex programs.
+ *
+ * These are the wave properties as outlined in section 4.1 in this paper by
+ * Jerry Tessendorf, presented in 2004, which uses ideas from Fournier and 
+ * Reeves' paper from Siggraph '86 Proceedings:
+ *
+ * http://www.finelightvisualtechnology.com/docs/coursenotes2004.pdf
+ * http://www.iro.umontreal.ca/~poulin/fournier/papers/p75-fournier.pdf
+ *	
+ * TODO: Move from Gerstner waves to a statistical model representation using
+ * Fast Fourier Transforms as described in section 4.3 of Tessendorf's paper.
  *   
  */
-enum waterMeshType {
-	WATER_TYPE_LOCAL,
-	WATER_TYPE_OCEAN
-};
 
 struct iWaterFactoryState : public virtual iBase
 {
   SCF_INTERFACE (iWaterFactoryState, 2, 0, 0);
 
   /**
-   * After making a significant change to the vertices or triangles you
-   * probably want to let this object recalculate the bounding boxes
-   * and such. This function will invalidate the internal data structures
-   * so that they are recomputed.
+   * Types of water meshes. Note, that setting the type as
+   * WATER_TYPE_OCEAN ignores the values specified by 
+   * iWaterFactoryState::SetLength(), iWaterFactoryState::SetWidth()
+   * and iWaterFactoryState::SetGranularity()
+   */
+  enum waterMeshType {
+	/// Local water, used for a finite amount.
+    WATER_TYPE_LOCAL,
+    /// Ocean water, used for an infinite amount.
+    WATER_TYPE_OCEAN
+  };
+
+  /**
+   * Invalidates the render buffers. This function should be
+   * called when there has been a significant change to the
+   * dimensions and or granularity of the water mesh.
    */
   virtual void Invalidate () = 0;
 
+  /// Set the length of the mesh in the x direction.
   virtual void SetLength(uint length) = 0;
+  /// Get the length of the mesh in the x direction.
   virtual uint GetLength() = 0;
 
+  /// Set the width of the mesh in the z direction.
   virtual void SetWidth(uint width) = 0;
+  /// Get the width of the mesh in the z direction.
   virtual uint GetWidth() = 0;
 
+  /**
+   * Get the granularity of the mesh. This determines the
+   * number of vertices that get used per unit length. A
+   * higher granularity gives nicer effects for wave simulation
+   * and fluid dynamics, but is more expensive to draw. Default
+   * value is 1.
+   */
   virtual void SetGranularity(uint granularity) = 0;
+  /// Get the granularity of the mesh. See iWaterFactoryState::SetGranularity();
   virtual uint GetGranularity() = 0;
 
+  /// Set the murkiness (alpha) of the water.
   virtual void SetMurkiness(float murk) = 0;
+  /// Get the murkiness (alpha) of the water.
   virtual float GetMurkiness() = 0;
 	
+  /// Set the water type.
   virtual void SetWaterType(waterMeshType type) = 0;
+
+  /// Returns true if the factory represents an ocean mesh.
   virtual bool isOcean() = 0;
 
-  /*
-   *	These are the wave properties as outlined in section 4.1 in this paper by
-   *	Jerry Tessendorf, presented in 2004, which uses ideas from Fournier and 
-   *	Reeves' paper from Siggraph '86 Proceedings:
-   *
-   *	http://www.finelightvisualtechnology.com/docs/coursenotes2004.pdf
-   *	http://www.iro.umontreal.ca/~poulin/fournier/papers/p75-fournier.pdf
-   *	
-   *	TODO: Move from Gerstner waves to a statistical model representation using
-   *	Fast Fourier Transforms as described in section 4.3 of Tessendorf's paper.
-   */
+  // TODO: Implement a way to "add" a wave function instead of having three hardcoded.
+
+  /// Set the wave amplitudes.
   virtual void SetAmplitudes(float amp1, float amp2, float amp3) = 0;
+
+  /// Set the wave frequencies.
   virtual void SetFrequencies(float freq1, float freq2, float freq3) = 0;
+
+  /// Set the wave phases.
   virtual void SetPhases(float phase1, float phase2, float phase3) = 0;
+
+  /// Set the wave directions.
   virtual void SetDirections(csVector2 dir1, csVector2 dir2, csVector2 dir3) = 0;
 
-  //Size must be a power of two.
+  // Size must be a power of two. Not yet implemented.
   virtual csRef<iTextureWrapper> MakeFresnelTex(int size) = 0;
 };
 
 /**
- * This interface describes the API for the proto mesh object.
- * 
- * Main creators of instances implementing this interface:
- * - Proto mesh object plugin (crystalspace.mesh.object.protomesh)
- * - iMeshObjectFactory::NewInstance()
- *   
- * Main ways to get pointers to this interface:
- * - scfQueryInterface() on iMeshWrapper::GetMeshObject()
- *   
- * Main users of this interface:
- * - Protomesh Loader plugin (crystalspace.mesh.loader.protomesh)
- *   
+ * This interface describes the API for the water mesh object.
  */
 struct iWaterMeshState : public virtual iBase
 {
   SCF_INTERFACE (iWaterMeshState, 1, 0, 0);
 
+  /// Specify the normal map to be used with this object.
   virtual void SetNormalMap(iTextureWrapper *map) = 0;
+
+  /// Get a pointer to the normal map to be used with this object.
   virtual iTextureWrapper *GetNormalMap() = 0;
 };
 
