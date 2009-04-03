08939bb4ee597e34ed3a30e2a99b6e6b7d02651d
Mike Gist <xordan@gmail.com>
2009-04-03 14:03:34 +0000

    - Forced csGLBasicTextureHandle::Unload to only run in the main thread.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@32260 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/render3d/opengl/gl_txtmgr_basictex.h
+++ plugins/video/render3d/opengl/gl_txtmgr_basictex.h
@@ -119,6 +119,7 @@ public:
 #include "csutil/deprecated_warn_off.h"
 
 class csGLBasicTextureHandle :
+  public ThreadedCallable<csGLBasicTextureHandle>,
   public scfImplementation1<csGLBasicTextureHandle,
 			    iTextureHandle>
 {
@@ -209,7 +210,8 @@ protected:
   GLuint pbo;
   /// Upload the texture to GL.
   void Load ();
-  void Unload ();
+  // Needs to be done in the main thread.
+  THREADED_CALLABLE_DECL(csGLBasicTextureHandle, Unload, csThreadReturn, HIGH, true, false);
   
   CS::Graphics::TextureComparisonMode texCompare;
 public:
@@ -220,6 +222,9 @@ public:
   csArray<csGLUploadData>* uploadData;
   csWeakRef<csGLGraphics3D> G3D;
   TextureType texType;
+
+  iObjectRegistry* GetObjectRegistry() const;
+
   /// Format used for last Blit() call
   TextureBlitDataFormat texFormat;
   bool IsWasRenderTarget() const
