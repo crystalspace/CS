7a8744cf0cdcf3ae2fc051b4cc077ac371b02119
Mike Gist <xordan@gmail.com>
2009-04-08 17:42:30 +0000

    - Fixed genmeshify compile.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@32281 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/tools/genmeshify/thing/object/thing.h
+++ apps/tools/genmeshify/thing/object/thing.h
@@ -885,7 +885,7 @@ public:
    */
   virtual bool HitBeamObject (const csVector3& start, const csVector3& end,
   	csVector3& isect, float* pr, int* polygon_idx = 0,
-	iMaterialWrapper** material = 0);
+	iMaterialWrapper** material = 0, csArray<iMaterialWrapper*>* materials = 0);
 
   //----------------------------------------------------------------------
   // Various
