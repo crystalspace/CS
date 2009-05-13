7925b2b17cb736d254e459592b17ea65b1dfe428
Frank Richter <res@crystalspace3d.org>
2009-05-13 16:46:33 +0000

    Change particles linear&lincolor effectors 'time' parameter to 'rtime' to clearer indicate it's actually _remaining_ time.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@32387 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/particles/persist/standard/particlesldr.cpp
+++ plugins/mesh/particles/persist/standard/particlesldr.cpp
@@ -642,7 +642,11 @@ CS_PLUGIN_NAMESPACE_BEGIN(ParticlesLoader)
 	      return 0;
 
             float t (0.0f);
+            // 'Old' time attribute
             t = child->GetAttributeValueAsFloat ("time");
+            // Attribute mnemonic for 'remaining time'
+            csRef<iDocumentAttribute> attr = child->GetAttribute ("rtime");
+            if (attr) t = attr->GetValueAsFloat ();
             linEffector->AddParameterSet (param, t);
           }
           break;
@@ -675,7 +679,6 @@ CS_PLUGIN_NAMESPACE_BEGIN(ParticlesLoader)
         case XMLTOKEN_COLOR:
           {
             csColor4 c;
-            float t (0.0f);
 
             if (!synldr->ParseColor (child, c))
             {
@@ -683,7 +686,12 @@ CS_PLUGIN_NAMESPACE_BEGIN(ParticlesLoader)
                 "Error parsing color!");
             }
 
+            float t (0.0f);
+            // 'Old' time attribute
             t = child->GetAttributeValueAsFloat ("time");
+            // Attribute mnemonic for 'remaining time'
+            csRef<iDocumentAttribute> attr = child->GetAttribute ("rtime");
+            if (attr) t = attr->GetValueAsFloat ();
             colorEffector->AddColor (c, t);
           }
           break;
@@ -1362,7 +1370,7 @@ CS_PLUGIN_NAMESPACE_BEGIN(ParticlesLoader)
         colorNode->SetValue ("color");
 
         synldr->WriteColor (colorNode, c);
-        colorNode->SetAttributeAsFloat ("time", t);
+        colorNode->SetAttributeAsFloat ("rtime", t);
       }
 
       return true;
@@ -1383,7 +1391,7 @@ CS_PLUGIN_NAMESPACE_BEGIN(ParticlesLoader)
         linEffector->GetParameterSet (i, c, t);
         csRef<iDocumentNode> paramNode = effectorNode->CreateNodeBefore (CS_NODE_ELEMENT, 0);
         paramNode->SetValue ("param");
-        paramNode->SetAttributeAsFloat ("time", t);
+        paramNode->SetAttributeAsFloat ("rtime", t);
 
 	if (mask & CS_PARTICLE_MASK_COLOR)
 	{
