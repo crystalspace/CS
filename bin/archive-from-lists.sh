3c0e436b3ebb79bb0fbb3ff87267eb70ade55f49
Frank Richter <res@crystalspace3d.org>
2009-07-04 02:41:27 +0000

    Merged revisions 32539,32541-32543,32550-32551 via svnmerge from https://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/release/V1.4
    ........
      r32539 | res2002 | 2009-07-04 01:21:33 +0200 (Sat, 04 Jul 2009) | 1 line
      
      Create script to automatically update 0install feeds
    ........
      r32541 | res2002 | 2009-07-04 01:37:49 +0200 (Sat, 04 Jul 2009) | 1 line
      
      Fix missing quote
    ........
      r32542 | res2002 | 2009-07-04 01:45:10 +0200 (Sat, 04 Jul 2009) | 1 line
      
      Fix defaulting to '.' if no subdir is given
    ........
      r32543 | res2002 | 2009-07-04 03:01:07 +0200 (Sat, 04 Jul 2009) | 1 line
      
      Make archive contents more consistent between runs
    ........
      r32550 | res2002 | 2009-07-04 04:15:28 +0200 (Sat, 04 Jul 2009) | 1 line
      
      Fix how path of script itself is determined
    ........
      r32551 | res2002 | 2009-07-04 04:27:39 +0200 (Sat, 04 Jul 2009) | 1 line
      
      0install feeds for data, user's manual
    ........
    
    
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@32552 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- bin/archive-from-lists.sh
+++ bin/archive-from-lists.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 OLD_PWD=$PWD
+MY_PATH=$PWD/$0
 
 ARCHIVER="tar --use-compress-program=lzma -cvf"
 ARCHIVE_EXT=.tar.lzma
@@ -25,8 +26,7 @@ fullpath()
 addcopyentries()
 {
   listfilename=$1
-  listsubdir=$2
-  listsubdir=${listsubdir-.}
+  listsubdir=${2:-.}
   MY_IFS=$IFS
   IFS=$'\n'
   for entry in `cat $listfilename`
@@ -34,8 +34,26 @@ addcopyentries()
     srcfile=`echo "$entry" | sed -e "s/\(.*\):\(.*\)/\\1/"`
     dstfile=$listsubdir/`echo "$entry" | sed -e "s/\(.*\):\(.*\)/\\2/"`
     echo mkdir -p \"`dirname "$dstfile"`\"
-    echo cp -R \"`fullpath "$srcfile"`\" \"$dstfile\"
+    echo cp -pR \"`fullpath "$srcfile"`\" \"$dstfile\"
   done
+  IFS=$MY_IFS
+}
+
+addtouchentries()
+{
+  echo "" > touch.tmp
+  listfilename=$1
+  listsubdir=${2:-.}
+  MY_IFS=$IFS
+  IFS=$'\n'
+  for entry in `cat $listfilename`
+  do
+    srcfile=`echo "$entry" | sed -e "s/\(.*\):\(.*\)/\\1/"`
+    dstfile=$listsubdir/`echo "$entry" | sed -e "s/\(.*\):\(.*\)/\\2/"`
+    echo touch -r \"`dirname "$(fullpath "$srcfile")"`\" \"`dirname "$dstfile"`\" >> touch.tmp
+  done
+  cat touch.tmp | uniq
+  rm touch.tmp
   IFS=$MY_IFS
 }
 
@@ -48,12 +66,14 @@ do
     if [ -r "$listfile" ]
     then
       addcopyentries "$listfile" $listsubdir >> $COPYSCRIPT
+      addtouchentries "$listfile" $listsubdir >> $COPYSCRIPT
     fi
 done
 
 cd $TMPDIR/$ARCHIVE_NAME
 source $COPYSCRIPT
 cd ..
+touch -r $MY_PATH $TMPDIR/$ARCHIVE_NAME
 
 $ARCHIVER $OLD_PWD/$ARCHIVE_NAME$ARCHIVE_EXT $ARCHIVE_NAME
 
