09d0ea927db05a7abb1597d74b8b90474ef55247
Trond Varslot <varslot@users.sourceforge.net>
2009-08-27 03:50:11 +0000

    Fix for compile error using i686-apple-darwin9-gcc-4.0.1: it does not like the arguments of csMin being unnamed constants.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@32879 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/render3d/opengl/gl_render3d.cpp
+++ plugins/video/render3d/opengl/gl_render3d.cpp
@@ -1668,7 +1668,8 @@ bool csGLGraphics3D::ActivateBuffers (csRenderBufferHolder *holder,
   queueEntry.attrib = CS_VATTRIB_SECONDARY_COLOR;
   changeQueue.Push (queueEntry);
   
-  for (int i = 0; i < csMin (numTCUnits, 8); i++)
+  const int n = ( numTCUnits < 8 ) ? numTCUnits : 8 ;
+  for (int i = 0; i < n; i++)
   {
     queueEntry.buffer = holder->GetRenderBuffer (mapping[CS_VATTRIB_TEXCOORD0+i]);
     queueEntry.attrib = (csVertexAttrib)(CS_VATTRIB_TEXCOORD0+i);
