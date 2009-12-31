1d69bcfebe77e9bd83f164d13631b7604a4ced19
Stefano Angeleri <weltall2@users.sourceforge.net>
2009-12-31 11:39:23 +0000

    Added support for PushAll(csArray) and PushSmartAll(csArray) which copy all the content of a provided csArray into the current one. the Smart version just like the normal PushSmart adds only unique objects. (trac #732)
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@33442 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csutil/array.h
+++ include/csutil/array.h
@@ -775,6 +775,29 @@ public:
     size_t const n = Find (what);
     return (n == csArrayItemNotFound) ? Push (what) : n;
   }
+  
+  /**
+   * Push the elements of an array onto the tail end of the array.
+   * 
+   * \param origin The array to push at the end of this array.
+   */
+  void PushAll(csArray& origin)
+  {
+    for(size_t i = 0; i < origin.GetSize(); i++)
+      Push(origin.Get(i));
+  }
+  
+  /**
+   * Push the elements of an array onto the tail end of the array if it's elements aren't already present.
+   * If an element is found duplicate it's skipped.
+   * 
+   * \param origin The array to push at the end of this array.
+   */
+  void PushSmartAll(csArray& porigin)
+  {
+    for(size_t i = 0; i < origin.GetSize(); i++)
+      PushSmart(origin.Get(i));
+  }
 
   /// Pop an element from tail end of array.
   T Pop ()
