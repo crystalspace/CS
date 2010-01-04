a5e7bac0ed7e4393e1454f86e3a58d9efd1c2554
Christian Van Brussel <kickvb@users.sourceforge.net>
2010-01-04 13:24:11 +0000

    API fix
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@33481 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/imesh/lookat.h
+++ include/imesh/lookat.h
@@ -100,7 +100,7 @@ struct iLookAtAnimNodeFactory : public iSkeletonAnimNodeFactory2
  * at a target.
  * There are three types of constraints that will modify the 'LookAt' control:
  * - Whether or not a iBodyBoneJoint for the bone controlled has been defined while
- * creating the 'LookAt' factory (see LookAtManager::CreateAnimNodeFactory()).
+ * creating the 'LookAt' factory (see iLookAtManager::CreateAnimNodeFactory()).
  * - Whether or not SetAlwaysRotate() has been set.
  * - The maximum rotation speed allowed through SetMaximumSpeed().
  *
