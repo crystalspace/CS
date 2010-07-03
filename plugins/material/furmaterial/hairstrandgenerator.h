ee48f32980f0450bb6937b6dfeaff84b847ebcd1
Alexandru Voicu <avoicu@users.sourceforge.net>
2010-07-03 08:14:18 +0000

    Proper classes for Marschner (defined in .h and implemented in .cpp).
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2010/hair@34374 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/material/furmaterial/hairstrandgenerator.h
+++ plugins/material/furmaterial/hairstrandgenerator.h
@@ -36,28 +36,9 @@ CS_PLUGIN_NAMESPACE_BEGIN(FurMaterial)
   class MarschnerConstants
   {
   public:         
-    MarschnerConstants()
-    {
-      // Surface properties
-      aR = -10;
-      aTT = - (-10) / 2;
-      aTRT = - 3 * (-10) / 2;
-
-      bR = 5;
-      bTT = (5) / 2;
-      bTRT = 2 * (5);
-
-      // Fiber properties
-      eta = 1.55;
-      absorption = 0.2;
-      eccentricity = 0.85;
-
-      // Glints
-      kG = 0.5;
-      wc = 10;
-      Dh0 = 0.2;
-      DhM = 0.5;
-    }
+    CS_LEAKGUARD_DECLARE(MarschnerConstants);
+    MarschnerConstants();
+    virtual ~MarschnerConstants();
 
     // Surface properties
     float aR;
@@ -128,196 +109,51 @@ CS_PLUGIN_NAMESPACE_BEGIN(FurMaterial)
 }
 CS_PLUGIN_NAMESPACE_END(FurMaterial)
 
+
 class MarschnerHelper
 {
 public:
-
+  CS_LEAKGUARD_DECLARE(MarschnerHelper);
   // Gaussian distribution - http://en.wikipedia.org/wiki/Normal_distribution
-  static float GaussianDistribution(float sigma, float x_mu)
-  {
-    return ((1.0f / (fabs(sigma) * sqrt(2.0f * PI))) *
-      exp(-(x_mu * x_mu) / (2.0f * sigma * sigma)));
-  }
+  static float GaussianDistribution(float sigma, float x_mu);
 
   // Bravais - Miller index - http://en.wikipedia.org/wiki/Miller_index
-  static float BravaisIndex(float theta, float eta)
-  {
-    float sinTheta = sin(theta);
-    return (sqrt(eta * eta - sinTheta * sinTheta) / cos(theta));
-  }
+  static float BravaisIndex(float theta, float eta);
 
   // Fresnel Parallel - http://en.wikipedia.org/wiki/Fresnel_equations
-  static float FresnelParallel(float n2, float angle)
-  {
-    float R = 1;
-    float n1 = 1;
-    float cos_gammaI = cos(angle);
-    float a = ((n1 / n2) * sin(angle));
-    float b = a * a;
-
-    if (b > 1)
-      return R;
-
-    float cos_gammaT = sqrt(1 - b);
-
-    R = (n2 * cos_gammaI - n1 * cos_gammaT) / (n2 * cos_gammaI + n1 * cos_gammaT);
-
-    return csMin(1.0f, R * R);
-  }
+  static float FresnelParallel(float n2, float angle);
 
   // Fresnel Perpendicular - http://en.wikipedia.org/wiki/Fresnel_equations
-  static float FresnelPerpendicular(float n2, float angle)
-  {
-    float R = 1;
-    float n1 = 1;
-    float cos_gammaI = cos(angle);
-    float a = ((n1 / n2) * sin(angle));
-    float b = a * a;
+  static float FresnelPerpendicular(float n2, float angle);
 
-    if (b > 1)
-      return R;
-
-    float cos_gammaT = sqrt(1 - b);
-
-    R = (n1 * cos_gammaI - n2 * cos_gammaT) / (n1 * cos_gammaI + n2 * cos_gammaT);
-
-    return csMin(1.0f, R * R);
-  }
-  
   // Fresnel Equation - http://en.wikipedia.org/wiki/Fresnel_equations
-  static double Fresnel(float etaPerpendicular, float etaParallel, float angle)
-  {
-    return 0.5f * (FresnelPerpendicular(etaPerpendicular, angle) + 
-      FresnelParallel(etaParallel, angle));
-  }
+  static double Fresnel(float etaPerpendicular, float etaParallel, float angle);
 };
 
 class EquationsSolver
 {
 public:
+  CS_LEAKGUARD_DECLARE(EquationsSolver);
   // Solve a * x + b = 0
-  static csVector4 LinearSolver(float a, float b)
-  {
-    csVector4 roots = csVector4();
-
-    if (fabs(a) > EPSILON)
-    {
-      roots[0] = -b / a;
-      roots[3] = 1;
-    }
-    else
-      roots[3] = 0;
-
-    return roots;
-  }
+  static csVector4 LinearSolver(float a, float b);
 
   // Solve a * x ^ 2 + b * x + c = 0
-  static csVector4 QuadraticSolver(float a, float b, float c)
-  {
-    csVector4 roots;
+  static csVector4 QuadraticSolver(float a, float b, float c);
 
-    if (fabs(a) < EPSILON)
-      return LinearSolver(b, c);
-    else
-    {
-      roots = csVector4();
+  // Solve x ^ 3 + A * x ^ 2 + B * x + C = 0 - 
+  //  http://en.wikipedia.org/wiki/Cubic_function
+  static csVector4 NormalizedCubicSolver(float A, float B, float C);
 
-      float D = b * b - 4 * a * c;
-
-      if (fabs(D) < EPSILON)
-      {
-        roots[0] = -b / (2 * a);
-        roots[1] = -b / (2 * a);
-        roots[3] = 2;
-      }
-      else if (D > 0)
-      {
-        float delta = sqrt(D);
-        roots[0] = (-b + delta) / (2 * a);
-        roots[1] = (-b - delta) / (2 * a);
-        roots[3] = 2;
-      }
-      else
-        roots[3] = 0;
-    }
-
-    return roots;
-  }
-
-  // Solve x ^ 3 + A * x ^ 2 + B * x + C = 0 - http://en.wikipedia.org/wiki/Cubic_function
-  static csVector4 NormalizedCubicSolver(float A, float B, float C)
-  {
-    csVector4 roots;
-
-    if (fabs(C) < EPSILON)	//	x = 0 solution
-    {
-      roots = QuadraticSolver(1, A, B);
-      roots[ (int)roots.w ] = 0;
-      roots.w ++;
-    }
-    else
-    {
-      roots = csVector4();
-
-      float Q = (3 * B - A * A) / 9;
-      float R = (9 * A * B - 27 * C - 2 * A * A * A) / 54;
-      float D = Q * Q * Q + R * R;
-
-      if (D > 0)	// 1 root
-      {
-        float sqrtD = sqrt(D);
-        float s = SIGN(R + sqrtD) * pow(fabs(R + sqrtD), 1.0f / 3.0f);
-        float t = SIGN(R - sqrtD) * pow(fabs(R - sqrtD), 1.0f / 3.0f);
-
-        roots[0] = (-A / 3 + (s + t));
-        roots[3] = 1;
-      }
-      else	// 3 roots
-      {
-        float theta = acos(R / sqrt(-(Q * Q * Q)));
-        float sqrtQ = sqrt(-Q);
-        roots[0] = (2 * sqrtQ * cos(theta / 3) - A / 3);
-        roots[1] = (2 * sqrtQ * cos((theta + 2 * PI) / 3) - A / 3);
-        roots[2] = (2 * sqrtQ * cos((theta + 4 * PI) / 3) - A / 3);
-        roots[3] = 3;
-      }
-    }
-
-    return roots;
-  }
-
-  // Solve a * x ^ 3 + b * x ^ 2 + c * x  + d = 0 - http://en.wikipedia.org/wiki/Cubic_function
-  static csVector4 CubicSolver(float a, float b, float c, float d)
-  {
-    csVector4 roots;
-
-    if (fabs(a) < EPSILON)
-      roots = QuadraticSolver(b, c, d);
-    else
-      roots = NormalizedCubicSolver(b / a, c / a, d / a);
-
-    return roots;
-  }
+  // Solve a * x ^ 3 + b * x ^ 2 + c * x  + d = 0 - 
+  //  http://en.wikipedia.org/wiki/Cubic_function
+  static csVector4 CubicSolver(float a, float b, float c, float d);
 
   // Solve o(p,y) - phi = 0
-  static csVector4 Roots(float p, float etaPerpendicular, float phi)
-  {
-
-    float c = asin(1 / etaPerpendicular);
-    return CubicSolver(-8 * (p * c / (PI * PI * PI)), 0, 
-      (6 * p * c / PI - 2), p * PI - phi);
-  }
+  static csVector4 Roots(float p, float etaPerpendicular, float phi);
 
   // computes the derivative of the polynomial relative to h.
-  static float InverseFirstDerivate(float p, float etaPerpendicular, float h)
-  {
-    float gammaI = asin(h);
-    float c = asin( 1 / etaPerpendicular );
-    float dGamma = (6 * p * c / PI - 2) - 
-      3 * 8 * (p * c / (PI * PI * PI)) * gammaI * gammaI;
-
-    return sqrt(1 - h * h) / dGamma;
-  }
+  static float InverseFirstDerivate(float p, float etaPerpendicular, float h);
 };
+
 
 #endif // __HAIR_STRAND_GENERATOR_H__
