dd916e360a1fddb317256df60fa0c3632f4fc3bb
Alexandru Voicu <avoicu@users.sourceforge.net>
2010-08-09 10:02:38 +0000

    Added furmeshfactory files.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2010/hair@34750 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/mesh/furmesh/furmesh.cpp
+++ plugins/mesh/furmesh/furmesh.cpp
@@ -27,186 +27,6 @@
 CS_PLUGIN_NAMESPACE_BEGIN(FurMesh)
 {
   /********************
-  *  FurMeshFactory
-  ********************/
-
-  CS_LEAKGUARD_IMPLEMENT(FurMeshFactory);
-
-  FurMeshFactory::FurMeshFactory (iEngine *e, iObjectRegistry* reg, iMeshObjectType* type)
-    : scfImplementationType(this, e, reg, type), indexCount(0), vertexCount(0), 
-    indexBuffer(0), vertexBuffer(0), texcoordBuffer(0), tangentBuffer(0), binormalBuffer(0)
-  {
-  }
-
-  FurMeshFactory::~FurMeshFactory ()
-  {
-  }
-
-  csPtr<iMeshObject> FurMeshFactory::NewInstance ()
-  {
-    csRef<iMeshObject> ref;
-    ref.AttachNew (new FurMesh(Engine, object_reg, this));
-    return csPtr<iMeshObject> (ref);
-  }
-
-  /// geometry access
-  void FurMeshFactory::SetVertexCount (uint n)
-  {
-    vertexCount = n;
-
-    vertexBuffer = csRenderBuffer::CreateRenderBuffer (n, 
-      CS_BUF_STREAM, CS_BUFCOMP_FLOAT, 3);
-
-    texcoordBuffer = csRenderBuffer::CreateRenderBuffer (n, 
-      CS_BUF_STREAM, CS_BUFCOMP_FLOAT, 2);
-
-    normalBuffer = csRenderBuffer::CreateRenderBuffer (n, 
-      CS_BUF_STREAM, CS_BUFCOMP_FLOAT, 3);
-
-    binormalBuffer = csRenderBuffer::CreateRenderBuffer (n, 
-      CS_BUF_STREAM, CS_BUFCOMP_FLOAT, 3);
-
-    tangentBuffer = csRenderBuffer::CreateRenderBuffer (n, 
-      CS_BUF_STREAM, CS_BUFCOMP_FLOAT, 3);
-  }
-
-  void FurMeshFactory::SetTriangleCount (uint n)
-  {
-    indexCount = n;
-
-    indexBuffer = csRenderBuffer::CreateIndexRenderBuffer (3 * n, 
-      CS_BUF_STATIC, CS_BUFCOMP_UNSIGNED_INT, 0, vertexCount - 1);
-  }
-
-  uint FurMeshFactory::GetVertexCount()
-  {
-    return vertexCount;
-  }
-
-  uint FurMeshFactory::GetIndexCount()
-  {
-    return indexCount;
-  }
-
-  iRenderBuffer* FurMeshFactory::GetIndices ()
-  {
-    return indexBuffer;
-  }
-
-  bool FurMeshFactory::SetIndices (iRenderBuffer* renderBuffer)
-  {
-    indexBuffer = renderBuffer;
-    indexCount = (uint)indexBuffer->GetElementCount ();
-    return true;
-  }
-
-  iRenderBuffer* FurMeshFactory::GetVertices ()
-  {
-    return vertexBuffer;
-  }
-
-  bool FurMeshFactory::SetVertices (iRenderBuffer* renderBuffer)
-  {
-    if (renderBuffer->GetComponentCount () < 3)
-      return false;
-
-    vertexBuffer = renderBuffer;
-    vertexCount = (uint)vertexBuffer->GetElementCount ();
-
-    return true;
-  }
-
-  iRenderBuffer* FurMeshFactory::GetTexCoords ()
-  {
-    return texcoordBuffer;
-  }
-
-  bool FurMeshFactory::SetTexCoords (iRenderBuffer* renderBuffer)
-  {
-    if (renderBuffer->GetElementCount () < vertexCount)
-      return false;
-
-    texcoordBuffer = renderBuffer;    
-    return true;
-  }
-
-  iRenderBuffer* FurMeshFactory::GetNormals ()
-  {
-    return normalBuffer;
-  }
-
-  bool FurMeshFactory::SetNormals (iRenderBuffer* renderBuffer)
-  {
-    if (renderBuffer->GetElementCount () < vertexCount)
-      return false;
-
-    normalBuffer = renderBuffer;    
-    return true;
-  }
-
-  iRenderBuffer* FurMeshFactory::GetTangents ()
-  {
-    return tangentBuffer;
-  }
-
-  bool FurMeshFactory::SetTangents (iRenderBuffer* renderBuffer)
-  {
-    if (renderBuffer->GetElementCount () < vertexCount)
-      return false;
-
-    tangentBuffer = renderBuffer;    
-    return true;
-  }
-
-  iRenderBuffer* FurMeshFactory::GetBinormals ()
-  {
-    return binormalBuffer;
-  }
-
-  bool FurMeshFactory::SetBinormals (iRenderBuffer* renderBuffer)
-  { 
-    if (renderBuffer->GetElementCount () < vertexCount)
-      return false;
-
-    binormalBuffer = renderBuffer;    
-    return true;
-  }
-
-  /********************
-  *  FurMeshType
-  ********************/
-
-  SCF_IMPLEMENT_FACTORY (FurMeshType)
-
-    CS_LEAKGUARD_IMPLEMENT(FurMeshType);
-
-  FurMeshType::FurMeshType (iBase* parent) :
-  scfImplementationType (this, parent),
-    object_reg(0)
-  {
-  }
-
-  FurMeshType::~FurMeshType ()
-  {
-  }
-
-  // From iComponent
-  bool FurMeshType::Initialize (iObjectRegistry* r)
-  {
-    csRef<iEngine> e = csQueryRegistry<iEngine> (r);
-    Engine = e;
-    object_reg = r;
-    return true;
-  }
-
-  csPtr<iMeshObjectFactory> FurMeshType::NewFactory ()
-  {
-    csRef<iMeshObjectFactory> ref;
-    ref.AttachNew (new FurMeshFactory(Engine, object_reg, this));
-    return csPtr<iMeshObjectFactory> (ref);
-  }
-
-  /********************
   *  FurMesh
   ********************/
 
@@ -1041,15 +861,15 @@ CS_PLUGIN_NAMESPACE_BEGIN(FurMesh)
     for (size_t i = 0 ; i < hairStrands.GetSize() ; i ++)
     {
       csVector2 uv = csVector2(0);
-      csGuideHairReference *guideHairsReference = hairStrands.Get(i).guideHairs;
+      csGuideHairReference *guideHairReference = hairStrands.Get(i).guideHairs;
       
       for ( size_t j = 0 ; j < GUIDE_HAIRS_COUNT ; j ++ )
-        if ( guideHairsReference[j].index < guideHairs.GetSize() )
-          uv += guideHairsReference[j].distance * 
-            (guideHairs.Get(guideHairsReference[j].index).uv);
+        if ( guideHairReference[j].index < guideHairs.GetSize() )
+          uv += guideHairReference[j].distance * 
+            (guideHairs.Get(guideHairReference[j].index).uv);
         else
-          uv += guideHairsReference[j].distance * (guideHairsLOD.Get(
-            guideHairsReference[j].index - guideHairs.GetSize()).uv);
+          uv += guideHairReference[j].distance * (guideHairsLOD.Get(
+            guideHairReference[j].index - guideHairs.GetSize()).uv);
 
       densitymap.data[ 4 * ((int)(uv.x * densitymap.width) + 
         (int)(uv.y * densitymap.height) * densitymap.width ) + 2 ] = 255;
--- plugins/mesh/furmesh/furmesh.h
+++ plugins/mesh/furmesh/furmesh.h
@@ -25,8 +25,8 @@
 #include <csgfx/shadervarcontext.h>
 #include <imesh/genmesh.h>
 
+#include "furmeshfactory.h"
 #include "crystalspace.h"
-
 #include "csutil/scf_implementation.h"
 
 struct iObjectRegistry;
@@ -35,70 +35,6 @@ struct iObjectRegistry;
 
 CS_PLUGIN_NAMESPACE_BEGIN(FurMesh)
 {
-  class FurMeshFactory : public scfImplementationExt1<FurMeshFactory, 
-    csMeshFactory, iFurMeshFactory>
-  {
-  public:
-    CS_LEAKGUARD_DECLARE(FurMeshFactory);
-
-    FurMeshFactory (iEngine *e, iObjectRegistry* reg, iMeshObjectType* type);
-    virtual ~FurMeshFactory ();
-
-    virtual csPtr<iMeshObject> NewInstance ();
-    virtual csPtr<iMeshObjectFactory> Clone () { return 0; }
-  
-    /// geometry access
-    virtual void SetVertexCount (uint n);
-    virtual void SetTriangleCount (uint n);
-  
-    virtual uint GetVertexCount();
-    virtual uint GetIndexCount();
-
-    virtual iRenderBuffer* GetIndices ();
-    virtual bool SetIndices (iRenderBuffer* renderBuffer);
-    virtual iRenderBuffer* GetVertices ();
-    virtual bool SetVertices (iRenderBuffer* renderBuffer);
-    virtual iRenderBuffer* GetTexCoords ();
-    virtual bool SetTexCoords (iRenderBuffer* renderBuffer);
-    virtual iRenderBuffer* GetNormals ();
-    virtual bool SetNormals (iRenderBuffer* renderBuffer);
-    virtual iRenderBuffer* GetTangents ();
-    virtual bool SetTangents (iRenderBuffer* renderBuffer);
-    virtual iRenderBuffer* GetBinormals ();
-    virtual bool SetBinormals (iRenderBuffer* renderBuffer);
-
-  protected:
-    uint indexCount;
-    uint vertexCount;
-    csRef<iRenderBuffer> indexBuffer;
-    csRef<iRenderBuffer> vertexBuffer;
-    csRef<iRenderBuffer> texcoordBuffer;
-    csRef<iRenderBuffer> normalBuffer;
-    csRef<iRenderBuffer> tangentBuffer;
-    csRef<iRenderBuffer> binormalBuffer;
-  };
-
-  class FurMeshType : public 
-    scfImplementation2<FurMeshType,iFurMeshType,iComponent>
-  {
-  public:
-    CS_LEAKGUARD_DECLARE(FurMeshType);
-
-    FurMeshType (iBase* parent);
-    virtual ~FurMeshType ();
-
-    // From iComponent	
-    virtual bool Initialize (iObjectRegistry*);
-
-    // From iMeshObjectType
-    virtual csPtr<iMeshObjectFactory> NewFactory ();
-
-  private:
-    iObjectRegistry* object_reg;
-    /// pointer to the engine if available.
-    iEngine *Engine;
-  };
-
   struct csGuideHairReference
   {
     size_t index;
