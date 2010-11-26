6fc7b82f7a288a5afc4bc14b3c077ad1c5d6477b
rlydontknow <rlydontknow@users.sourceforge.net>
2010-11-26 20:48:06 +0000

    make float>csVector3 actually return greater than rather than greater or equal
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@35295 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/csgeom/vector3.h
+++ include/csgeom/vector3.h
@@ -210,9 +210,9 @@ public:
   inline friend bool operator< (const csVector3& v, float f)
   { return ABS(v.x)<f && ABS(v.y)<f && ABS(v.z)<f; }
 
-  /// Test if each component of a vector is less than a small epsilon value.
+  /// Test if each component of a vector is greater than a small epsilon value.
   inline friend bool operator> (float f, const csVector3& v)
-  { return ABS(v.x)<f && ABS(v.y)<f && ABS(v.z)<f; }
+  { return f>ABS(v.x) && f>ABS(v.y) && f>ABS(v.z); }
 
   /// Returns n-th component of the vector.
 #ifdef __STRICT_ANSI__
