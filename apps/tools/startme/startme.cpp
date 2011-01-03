282c0dacffc4730f2e84297f16ed43a5b66668a3
Jelle Hellemans <sueastside@users.sourceforge.net>
2011-01-03 21:06:39 +0000

    Seems default behavior is for mouse inputs to _not_ propagate :s
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@35616 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/tools/startme/startme.cpp
+++ apps/tools/startme/startme.cpp
@@ -302,7 +302,11 @@ bool StartMe::Application()
 
   LoadConfig ();
 
+  CEGUI::Window* description = cegui->GetWindowManagerPtr()->getWindow("Description");
+  description->setMouseInputPropagationEnabled(true);
+
   CEGUI::Window* logo = winMgr->getWindow("Logo");
+  logo->setMouseInputPropagationEnabled(true);
   logo->subscribeEvent(CEGUI::Window::EventMouseClick,
       CEGUI::Event::Subscriber(&StartMe::OnLogoClicked, this));
 
@@ -325,6 +329,7 @@ bool StartMe::Application()
     }
     root->addChildWindow(demos[i].window);
 
+    demos[i].window->setMouseInputPropagationEnabled(true);
     demos[i].window->subscribeEvent(CEGUI::Window::EventMouseClick,
       CEGUI::Event::Subscriber(&StartMe::OnClick, this));
   }
