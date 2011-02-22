70533d9485ef4b24cee83e3cd533eaaded6725d8
Christian Van Brussel <kickvb@users.sourceforge.net>
2011-02-22 17:27:46 +0000

    Added some tools to import some animeshes as new morph targets of another
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@35826 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ include/cstool/animeshtools.h
@@ -0,0 +1,120 @@
+/*
+  Copyright (C) 2011 Christian Van Brussel, Communications and Remote
+      Sensing Laboratory of the School of Engineering at the 
+      Universite catholique de Louvain, Belgium
+      http://www.tele.ucl.ac.be
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with this library; if not, write to the Free
+  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_CSTOOL_ANIMESHTOOLS_H__
+#define __CS_CSTOOL_ANIMESHTOOLS_H__
+
+/**\file
+ * Tools for the generation of CS::Mesh::iAnimatedMesh's
+ */
+
+#include "ivaria/reporter.h"
+
+struct iModelLoader;
+struct iObjectRegistry;
+
+namespace CS {
+namespace Mesh {
+
+struct iAnimatedMeshFactory;
+
+/**
+ * Helper class to generate new CS::Mesh::iAnimatedMesh's.
+ */
+class CS_CRYSTALSPACE_EXPORT AnimatedMeshTools
+{
+  static void ReportError (const char* msg, ...)
+  {
+#ifdef CS_DEBUG
+    va_list arg;
+    va_start (arg, msg);
+    csReportV (iSCF::SCF->object_reg, CS_REPORTER_SEVERITY_ERROR,
+	       "crystalspace.mesh.animeshtools", msg, arg);
+    va_end (arg);
+#endif
+  }
+
+  static void ReportWarning (const char* msg, ...)
+  {
+#ifdef CS_DEBUG
+    va_list arg;
+    va_start (arg, msg);
+    csReportV (iSCF::SCF->object_reg, CS_REPORTER_SEVERITY_WARNING,
+	       "crystalspace.mesh.animeshtools", msg, arg);
+    va_end (arg);
+#endif
+  }
+
+  static csRef<iAnimatedMeshFactory> LoadAnimesh (iModelLoader* loader,
+						  const char* factoryName,
+						  const char* filename);
+
+ public:
+
+  /**
+   * Import a mesh with its morph targets splitted in several different
+   * model files. This is useful eg to load directly models from the
+   * FaceGen software.
+   *
+   * The system will scan the provided directory, and load all animesh
+   * model files matching the given mask. All these animeshes will be
+   * imported in the base animesh as a new morph target instead of a
+   * new animesh.
+   *
+   * \param object_reg The object registry
+   * \param path A VFS path to the directory containing all the data files
+   * \param baseMesh The name of the file defining the base animesh where
+   * all morph targets will be imported.
+   * \param meshMask A regular expression to determine which files should
+   * be imported as a new morph target. Use for example ".*[.]3[dD][sS]"
+   * if you want all files terminating by ".3ds" or ".3DS".
+   * \param factoryName Name to be given to the newly created animesh
+   * factory.
+   * \note The animesh models with a different number of vertices than the
+   * base animesh won't be imported. These models are also supposed to have
+   * the same vertices in the same order.
+   */
+  static iAnimatedMeshFactory* ImportSplittedMesh
+    (iObjectRegistry* object_reg, const char* path, const char* baseMesh,
+     const char* meshMask, const char* factoryName);
+
+  /**
+   * Create a new morph target in an animesh by importing the vertices from
+   * another animesh. The two animeshes must have the same vertices in the
+   * same order.
+   *
+   * \param object_reg The object registry
+   * \param baseMesh The base animesh where the morph target will be imported.
+   * \param morphMesh The animesh to import in the other as a new morph target.
+   * \param morphName The name of the new morph target
+   * \param deleteMesh Whether or not the animesh \a morphMesh should be
+   * removed from the engine.
+   */
+  static bool ImportMorphMesh
+    (iObjectRegistry* object_reg, iAnimatedMeshFactory* baseMesh,
+     iAnimatedMeshFactory* morphMesh, const char* morphName,
+     bool deleteMesh = false);
+};
+
+} //namespace Mesh
+} //namespace CS
+
+#endif // __CS_CSTOOL_ANIMESHTOOLS_H__
---
+++ libs/cstool/animeshtools.cpp
@@ -0,0 +1,220 @@
+/*
+  Copyright (C) 2011 Christian Van Brussel, Communications and Remote
+      Sensing Laboratory of the School of Engineering at the 
+      Universite catholique de Louvain, Belgium
+      http://www.tele.ucl.ac.be
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with this library; if not, write to the Free
+  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+#include "cssysdef.h"
+
+#include "iengine/engine.h"
+#include "iengine/mesh.h"
+#include "imap/modelload.h"
+#include "imesh/animesh.h"
+#include "iutil/objreg.h"
+#include "iutil/plugin.h"
+#include "iutil/stringarray.h"
+#include "iutil/vfs.h"
+
+#include "csgfx/renderbuffer.h"
+#include "cstool/vfsdirchange.h"
+#include "csutil/regexp.h"
+#include "csutil/stringquote.h"
+
+#include "cstool/animeshtools.h"
+
+namespace CS {
+namespace Mesh {
+
+csRef<iAnimatedMeshFactory> AnimatedMeshTools::LoadAnimesh
+(iModelLoader* loader, const char* factoryName, const char* filename)
+{
+  // Load the base mesh
+  csRef<iMeshFactoryWrapper> factoryWrapper =
+    loader->Load (factoryName, filename);
+  if (!factoryWrapper)
+  {
+    ReportWarning ("Could not load mesh from file %s!",
+		   CS::Quote::Single (filename));
+    return (iAnimatedMeshFactory*) nullptr;
+  }
+
+  // Find the animesh interface
+  csRef<iAnimatedMeshFactory> factory =
+    scfQueryInterface<CS::Mesh::iAnimatedMeshFactory>
+    (factoryWrapper->GetMeshObjectFactory ());
+  if (!factory)
+  {
+    ReportWarning ("The mesh loaded from file %s is not an animesh!",
+		   CS::Quote::Single (filename));
+    return (iAnimatedMeshFactory*) nullptr;
+  }
+
+  return factory;
+}
+
+iAnimatedMeshFactory* AnimatedMeshTools::ImportSplittedMesh
+(iObjectRegistry* object_reg, const char* path, const char* baseMesh,
+ const char* meshMask, const char* factoryName)
+{
+  // Change the current working directory to the given path
+  csRef<iVFS> vfs = csQueryRegistry<iVFS> (object_reg);
+  csVfsDirectoryChanger diretoryChanger (vfs);
+  if (!diretoryChanger.ChangeToFull (path))
+  {
+    ReportError ("Could not find path %s!", CS::Quote::Single (path));
+    return nullptr;
+  }
+
+  // Temporary hack: use explicitely the assimp loader
+  csRef<iPluginManager> plugmgr = 
+    csQueryRegistry<iPluginManager> (object_reg);
+  csRef<iModelLoader> loader =
+    csLoadPlugin<iModelLoader> (plugmgr, "crystalspace.mesh.loader.assimp");
+
+  // Load the base mesh
+  csRef<iAnimatedMeshFactory> meshFact =
+    LoadAnimesh (loader, factoryName, baseMesh);
+  if (!meshFact)
+  {
+    ReportError ("Error loading base mesh %s!",
+		 CS::Quote::Single (baseMesh));
+    return nullptr;
+  }
+
+  // Find the base name of base mesh
+  csString baseName = baseMesh;
+  size_t index = baseName.FindLast ('/');
+  if (index != (size_t) -1)
+    baseName = baseName.Slice (index + 1);
+  index = baseName.FindLast ('.');
+  if (index != (size_t) -1)
+    baseName = baseName.Slice (0, index);
+  csRef<iDataBuffer> dataBuffer = vfs->GetRealPath (baseMesh);
+  csString realBaseName = dataBuffer->GetData ();
+
+  // Load all animesh files matching the mask
+  csRef<iStringArray> files = vfs->FindFiles (path);
+  csRegExpMatcher matcher (meshMask);
+  for (size_t i = 0; i < files->GetSize (); i++)
+  {
+    // Check if the file matches the mask
+    if (matcher.Match (files->Get (i)) != csrxNoError) 
+      continue;
+
+    // Check that it is not the base file
+    dataBuffer = vfs->GetRealPath (files->Get (i));
+    csString txt = dataBuffer->GetData ();
+    if (txt == realBaseName)
+      continue;
+
+    // Load the animesh
+    // TODO: don't load other data than the indices
+    csRef<iAnimatedMeshFactory> morphFact =
+      LoadAnimesh (loader, factoryName, files->Get (i));
+    if (!morphFact)
+      continue;
+
+    // Find the name of the morph target
+    csString name = files->Get (i);
+    size_t index = name.FindLast ('/');
+    if (index != (size_t) -1)
+      name = name.Slice (index + 1);
+    name = name.ReplaceAll (baseName, "");
+    index = name.FindLast ('.');
+    if (index != (size_t) -1)
+      name = name.Slice (0, index);
+    if (name.StartsWith ("_"))
+      name = name.Slice (1);
+
+    // Merge it with the base mesh
+    if (!ImportMorphMesh (object_reg, meshFact, morphFact, name, true))
+      continue;
+  }
+
+  return meshFact;
+}
+
+bool AnimatedMeshTools::ImportMorphMesh
+(iObjectRegistry* object_reg, iAnimatedMeshFactory* baseMesh,
+ iAnimatedMeshFactory* morphMesh, const char* morphName, bool deleteMesh)
+{
+  // Find a pointer to the engine
+  csRef<iEngine> engine;
+  if (deleteMesh)
+  {
+    engine = csQueryRegistry<iEngine> (object_reg);
+    if (!engine)
+    {
+      ReportError ("Could not find the engine");
+      return false;
+    }
+  }
+
+  // Check that the two meshes have the same count of vertices
+  if (baseMesh->GetVertexCount () != morphMesh->GetVertexCount ())
+  {
+    ReportWarning
+      ("The animesh for the morph target %s has a different count of vertices!",
+       CS::Quote::Single (morphName));
+
+    // Delete the mesh if needed
+    if (deleteMesh)
+    {
+      csRef<iMeshObjectFactory> factory =
+	scfQueryInterface<iMeshObjectFactory> (morphMesh);
+      engine->GetMeshFactories ()->Remove (factory->GetMeshFactoryWrapper ());
+    }
+
+    return false;
+  }
+
+  // Setup the offsets of the morph target
+  csRef<iRenderBuffer> baseBuffer = baseMesh->GetVertices ();
+  csVector3* baseIndices = (csVector3*) baseBuffer->Lock (CS_BUF_LOCK_NORMAL);
+
+  csRef<iRenderBuffer> morphBuffer;
+  if (deleteMesh)
+    morphBuffer = morphMesh->GetVertices ();
+  else
+    morphBuffer = csRenderBuffer::CreateRenderBuffer
+      (morphMesh->GetVertexCount (), CS_BUF_STATIC, CS_BUFCOMP_FLOAT, 3);
+  csVector3* morphIndices = (csVector3*) morphBuffer->Lock (CS_BUF_LOCK_NORMAL);
+
+  for (size_t i = 0; i < baseMesh->GetVertexCount (); i++)
+    morphIndices[i] = morphIndices[i] - baseIndices[i];
+  baseBuffer->Release ();
+  morphBuffer->Release ();
+
+  // Create the morph target
+  iAnimatedMeshMorphTarget* target =
+    baseMesh->CreateMorphTarget (morphName);
+  target->SetVertexOffsets (morphBuffer);
+  //target->Invalidate ();
+
+  // Delete the mesh if needed
+  if (deleteMesh)
+  {
+    csRef<iMeshObjectFactory> factory =
+      scfQueryInterface<iMeshObjectFactory> (morphMesh);
+    engine->GetMeshFactories ()->Remove (factory->GetMeshFactoryWrapper ());
+  }
+
+  return true;
+}
+
+} //namespace Mesh
+} //namespace CS
