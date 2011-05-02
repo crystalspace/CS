95ea71267fc4f6833ebbf437b69a3ab5d395628c
Jelle Hellemans <sueastside@users.sourceforge.net>
2011-05-02 21:25:04 +0000

    Added an iHTTPConnection interface and a default CURL implementation.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@36254 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ plugins/network/http/curl/httpfactory.h
@@ -0,0 +1,56 @@
+/*
+    Copyright (C) 2011 by Jelle Hellemans
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Library General Public
+    License as published by the Free Software Foundation; either
+    version 2 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Library General Public License for more details.
+
+    You should have received a copy of the GNU Library General Public
+    License along with this library; if not, write to the Free
+    Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __NETWORK_HTTPFACTORY_H__
+#define __NETWORK_HTTPFACTORY_H__
+
+#include "inetwork/http.h"
+
+#include <csutil/scf_implementation.h>
+
+#include <string>
+
+using namespace CS::Network::HTTP;
+
+CS_PLUGIN_NAMESPACE_BEGIN(CSHTTP)
+{
+class HTTPConnectionFactory : public scfImplementation2<HTTPConnectionFactory,iHTTPConnectionFactory,iComponent>
+{
+public:
+  HTTPConnectionFactory (iBase* parent);
+  virtual ~HTTPConnectionFactory ();
+  
+  // iComponent
+  virtual bool Initialize (iObjectRegistry* obj_reg);
+  
+  // iHTTPConnectionFactory
+  virtual csRef<iHTTPConnection> Create (const char* uri);
+  virtual void UseProxy (ProxySetting setting);
+  virtual void SetCustomProxy(const char* uri, const char* user=0, const char* password=0);
+
+private:
+  iObjectRegistry* object_reg;
+  ProxySetting setting;
+  std::string proxyURI;
+  std::string proxyUser;
+  std::string proxyPass;
+};
+}
+CS_PLUGIN_NAMESPACE_END(CSHTTP)
+
+#endif
