e31e7c842ccf8350223bcebb35d77e7fd284784d
Alexandru Voicu <avoicu@users.sourceforge.net>
2011-05-19 19:23:07 +0000

    Using CS::Utility::DemoApplication.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2011/selfshadow@36337 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- apps/demos/selfshadow/selfshadow.cpp
+++ apps/demos/selfshadow/selfshadow.cpp
@@ -18,233 +18,66 @@
   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
+#include "cssysdef.h"
+
+#include "iengine/campos.h"
 #include "selfshadow.h"
 
-//---------------------------------------------------------------------------
-
 SelfShadowDemo::SelfShadowDemo ()
+: DemoApplication ("CrystalSpace.SelfShadow")
 {
-  SetApplicationName ("CrystalSpace.SelfShadowDemo");
 }
 
-SelfShadowDemo::~SelfShadowDemo ()
+void SelfShadowDemo::PrintHelp ()
 {
+  csCommandLineHelper commandLineHelper;
+
+  // Printing help
+  commandLineHelper.PrintApplicationHelp
+    (GetObjectRegistry (), "selfshadowdemo", "selfshadowdemo", 
+      "Crystal Space's self shadow RM demo.");
 }
 
 void SelfShadowDemo::Frame ()
 {
-  // First get elapsed time from the virtual clock.
-  csTicks elapsed_time = vc->GetElapsedTicks ();
-  // Now rotate the camera according to keyboard state
-  float speed = (elapsed_time / 1000.0) * (0.06 * 20);
-
-  iCamera* c = view->GetCamera ();
-
-  if (kbd->GetKeyState (CSKEY_SHIFT))
-  {
-    // If the user is holding down shift, the arrow keys will cause
-    // the camera to strafe up, down, left or right from it's
-    // current position.
-    if (kbd->GetKeyState (CSKEY_RIGHT))
-      c->Move (CS_VEC_RIGHT * 4 * speed);
-    if (kbd->GetKeyState (CSKEY_LEFT))
-      c->Move (CS_VEC_LEFT * 4 * speed);
-    if (kbd->GetKeyState (CSKEY_UP))
-      c->Move (CS_VEC_UP * 4 * speed);
-    if (kbd->GetKeyState (CSKEY_DOWN))
-      c->Move (CS_VEC_DOWN * 4 * speed);
-  }
-  else
-  {
-    // left and right cause the camera to rotate on the global Y
-    // axis; page up and page down cause the camera to rotate on the
-    // _camera's_ X axis (more on this in a second) and up and down
-    // arrows cause the camera to go forwards and backwards.
-    if (kbd->GetKeyState (CSKEY_RIGHT))
-      rotY += speed;
-    if (kbd->GetKeyState (CSKEY_LEFT))
-      rotY -= speed;
-    if (kbd->GetKeyState (CSKEY_PGUP))
-      rotX += speed;
-    if (kbd->GetKeyState (CSKEY_PGDN))
-      rotX -= speed;
-    if (kbd->GetKeyState (CSKEY_UP))
-      c->Move (CS_VEC_FORWARD * 4 * speed);
-    if (kbd->GetKeyState (CSKEY_DOWN))
-      c->Move (CS_VEC_BACKWARD * 4 * speed);
-  }
-
-  // We now assign a new rotation transformation to the camera.  You
-  // can think of the rotation this way: starting from the zero
-  // position, you first rotate "rotY" radians on your Y axis to get
-  // the first rotation.  From there you rotate "rotX" radians on the
-  // your X axis to get the final rotation.  We multiply the
-  // individual rotations on each axis together to get a single
-  // rotation matrix.  The rotations are applied in right to left
-  // order .
-  csMatrix3 rot = csXRotMatrix3 (rotX) * csYRotMatrix3 (rotY);
-  csOrthoTransform ot (rot, c->GetTransform ().GetOrigin ());
-  c->SetTransform (ot);
-
-  rm->RenderView (view);
-}
-
-bool SelfShadowDemo::OnKeyboard (iEvent& ev)
-{
-  // We got a keyboard event.
-  csKeyEventType eventtype = csKeyEventHelper::GetEventType (&ev);
-  if (eventtype == csKeyEventTypeDown)
-  {
-    // The user pressed a key (as opposed to releasing it).
-    utf32_char code = csKeyEventHelper::GetCookedCode (&ev);
-    if (code == CSKEY_ESC)
-    {
-      // The user pressed escape to exit the application.
-      // The proper way to quit a Crystal Space application
-      // is by broadcasting a csevQuit event. That will cause the
-      // main runloop to stop. To do that we get the event queue from
-      // the object registry and then post the event.
-      csRef<iEventQueue> q = 
-        csQueryRegistry<iEventQueue> (GetObjectRegistry ());
-      if (q.IsValid ()) q->GetEventOutlet ()->Broadcast(
-      	csevQuit (GetObjectRegistry ()));
-    }
-  }
-
-  return false;
-}
-
-bool SelfShadowDemo::OnInitialize (int /*argc*/, char* /*argv*/ [])
-{
-  // RequestPlugins() will load all plugins we specify. In addition
-  // it will also check if there are plugins that need to be loaded
-  // from the config system (both the application config and CS or
-  // global configs). In addition it also supports specifying plugins
-  // on the commandline.
-  if (!csInitializer::RequestPlugins (GetObjectRegistry (),
-    CS_REQUEST_VFS,
-    CS_REQUEST_OPENGL3D,
-    CS_REQUEST_ENGINE,
-    CS_REQUEST_FONTSERVER,
-    CS_REQUEST_IMAGELOADER,
-    CS_REQUEST_LEVELLOADER,
-    CS_REQUEST_REPORTER,
-    CS_REQUEST_REPORTERLISTENER,
-    CS_REQUEST_END))
-    return ReportError ("Failed to initialize plugins!");
-
-  // "Warm up" the event handler so it can interact with the world
-  csBaseEventHandler::Initialize (GetObjectRegistry ());
-
-  // Now we need to register the event handler for our application.
-  // Crystal Space is fully event-driven. Everything (except for this
-  // initialization) happens in an event.
-  // Rather than simply handling all events, we subscribe to the
-  // particular events we're interested in.
-  csEventID events[] = {
-    csevFrame (GetObjectRegistry ()),
-    csevKeyboardEvent (GetObjectRegistry ()),
-    CS_EVENTLIST_END
-  };
-
-  if (!RegisterQueue (GetObjectRegistry (), events))
-    return ReportError ("Failed to set up event handler!");
-
-  // Report success
-  return true;
-}
-
-void SelfShadowDemo::OnExit ()
-{
-  // Shut down the event handlers we spawned earlier.
-  printer.Invalidate ();
+  // Default behavior from DemoApplication
+  DemoApplication::Frame ();
 }
 
 bool SelfShadowDemo::Application ()
 {
-  // Open the main system. This will open all the previously loaded plug-ins.
-  // i.e. all windows will be opened.
-  if (!OpenApplication (GetObjectRegistry ()))
-    return ReportError ("Error opening system!");
+  // Default behavior from DemoApplication
+  if (!DemoApplication::Application ())
+    return false;
 
-  if (SetupModules ())
-  {
-    // This calls the default runloop. This will basically just keep
-    // broadcasting process events to keep the game going.
-    Run ();
-  }
-
-  return true;
-}
-
-bool SelfShadowDemo::SetupModules ()
-{
-  // Now get the pointer to various modules we need. We fetch them
-  // from the object registry. The RequestPlugins() call we did earlier
-  // registered all loaded plugins with the object registry.
-  g3d = csQueryRegistry<iGraphics3D> (GetObjectRegistry ());
-  if (!g3d) return ReportError ("Failed to locate 3D renderer!");
-
-  engine = csQueryRegistry<iEngine> (GetObjectRegistry ());
-  if (!engine) return ReportError ("Failed to locate 3D engine!");
-
-  vc = csQueryRegistry<iVirtualClock> (GetObjectRegistry ());
-  if (!vc) return ReportError ("Failed to locate Virtual Clock!");
-
-  kbd = csQueryRegistry<iKeyboardDriver> (GetObjectRegistry ());
-  if (!kbd) return ReportError ("Failed to locate Keyboard Driver!");
-
-  loader = csQueryRegistry<iLoader> (GetObjectRegistry ());
-  if (!loader) return ReportError ("Failed to locate Loader!");
-
-  // We need a View to the virtual world.
-  view.AttachNew(new csView (engine, g3d));
-  iGraphics2D* g2d = g3d->GetDriver2D ();
-  // We use the full window to draw the world.
-  view->SetRectangle (0, 0, g2d->GetWidth (), g2d->GetHeight ());
-
-  // Here we create our world.
-  CreateRoom ();
-
-  // Let the engine prepare the meshes and textures.
-  engine->Prepare ();
-
-//   rm = engine->GetRenderManager ();
-
-  /* NOTE: Config settings for render managers are stored in 'engine.cfg' 
-   * and are needed when loading a render manager. Normally these settings 
-   * are added by the engine when it loads a render manager. However, since
-   * we are loading the deferred render manager manually we must also manually
-   * add the proper config file. */
+  // Manual loading RM
   csRef<iVFS> vfs = csQueryRegistry<iVFS> (GetObjectRegistry());
   csRef<iConfigManager> cfg = csQueryRegistry<iConfigManager> (GetObjectRegistry());
   cfg->AddDomain ("/config/engine.cfg", vfs, iConfigManager::ConfigPriorityPlugin);
 
-  rm = csLoadPlugin<iRenderManager> (GetObjectRegistry(), "crystalspace.rendermanager.shadow_pssm");
+  csRef<iRenderManager> rm = csLoadPlugin<iRenderManager> (GetObjectRegistry(), 
+    "crystalspace.rendermanager.shadow_pssm");
   if (!rm)
     return ReportError("Failed to load deferred Render Manager!");
 
   cfg->RemoveDomain ("/config/engine.cfg");
-  // These are used store the current orientation of the camera
-  rotY = rotX = 0;
- 
-  // Now we need to position the camera in our world.
-  view->GetCamera ()->SetSector (room);
-  view->GetCamera ()->GetTransform ().SetOrigin (csVector3 (0, 3, -7));
 
-  // We use some other "helper" event handlers to handle 
-  // pushing our work into the 3D engine and rendering it
-  // to the screen.
-  printer.AttachNew (new FramePrinter (GetObjectRegistry ()));
+  engine->SetRenderManager(rm);
+
+  // Create the scene
+  if (!CreateScene ())
+    return false;
+
+  // Run the application
+  Run();
 
   return true;
 }
 
-void SelfShadowDemo::CreateRoom ()
+bool SelfShadowDemo::CreateScene ()
 {
-  // Load the texture from the standard library.  This is located in
-  // CS/data/standard.zip and mounted as /lib/std using the Virtual
-  // File System (VFS) plugin.
+  printf ("Loading level...\n");
+
   if (!loader->LoadTexture ("stone", "/lib/std/stone4.gif"))
     ReportError ("Error loading %s texture!",
 		 CS::Quote::Single ("stone4"));
@@ -255,11 +88,9 @@ void SelfShadowDemo::CreateRoom ()
     (object_reg,"boxmaterial",csColor(0,1,0));
 
   // We create a new sector called "room".
-  room = engine->CreateSector ("room");
+  room = engine->CreateSector ("room");  
 
-  // Creating the walls for our room.
-
-  // First we make a primitive for our geometry.
+//   // Make a simple box which cast shadows
   using namespace CS::Geometry;
   DensityTextureMapper mapper (0.3f);
   TesselatedBox box (csVector3 (-5, 0, -5), csVector3 (5, 20, 5));
@@ -276,14 +107,14 @@ void SelfShadowDemo::CreateRoom ()
   wallsMeshstate->SetLighting (true);
 //   wallsMeshstate->SetShadowCasting(true);
 //   wallsMeshstate->SetShadowReceiving(true)
-  // Make a simple box which cast shadows
+
   Box simpleBox (3 * csVector3 (-.1, -.1, -.1), 3 * csVector3 (.1, .1, .1));
   // Now we make a factory and a mesh at once.
   csRef<iMeshWrapper> mesh = GeneralMeshBuilder::CreateFactoryAndMesh (
     engine, room, "cube", "cubeFact", &simpleBox);
   mesh->GetMovable()->SetPosition(csVector3(0, 2, 0));
   mesh->GetMeshObject ()->SetMaterialWrapper (simpleMaterial);
-  
+
   csRef<iGeneralMeshState> meshstate = scfQueryInterface<iGeneralMeshState> (
     mesh->GetMeshObject ());
   meshstate->SetLighting (true);
@@ -315,5 +146,18 @@ void SelfShadowDemo::CreateRoom ()
 // 
 //   light = engine->CreateLight (0, csVector3 (0, 5, -3), 10, csColor (0, 1, 0));
 //   ll->Add (light);
-}
 
+  // Setup the sector and the camera
+  view->GetCamera ()->SetSector (room);
+  view->GetCamera ()->GetTransform ().SetOrigin (csVector3 (0, 3, -7));
+  cameraManager->SetCamera(view->GetCamera());
+  cameraManager->SetCameraMode (CS::Utility::CAMERA_ROTATE);
+  cameraManager->SetMotionSpeed (10.0f);
+
+//   printf ("Precaching data...\n");
+//   engine->PrecacheDraw ();
+
+  printf ("Ready!\n");
+
+  return true;
+}
--- apps/demos/selfshadow/selfshadow.h
+++ apps/demos/selfshadow/selfshadow.h
@@ -10,7 +10,7 @@
 
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
   Library General Public License for more details.
 
   You should have received a copy of the GNU Library General Public
@@ -18,101 +18,26 @@
   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
-#ifndef __SELFSHADOWDEMO_H__
-#define __SELFSHADOWDEMO_H__
+#ifndef __SELFSHADOW_H__
+#define __SELFSHADOW_H__
 
 #include <crystalspace.h>
+#include "cstool/demoapplication.h"
 
-/**
- * The main class that runs the self shadow demo application.
- */
-class SelfShadowDemo : public csApplicationFramework, public csBaseEventHandler
+class SelfShadowDemo : public CS::Utility::DemoApplication
 {
-private:
-  /// A pointer to the 3D engine.
-  csRef<iEngine> engine;
-   
-  /// A pointer to the map loader plugin.
-  csRef<iLoader> loader;
-     
-  /// A pointer to the 3D renderer plugin.
-  csRef<iGraphics3D> g3d;
-	
-  /// A pointer to the keyboard driver.
-  csRef<iKeyboardDriver> kbd;
-	  
-  /// A pointer to the virtual clock.
-  csRef<iVirtualClock> vc;
+ private:
+  bool CreateScene ();
 
-  /// A pointer to the view which contains the camera.
-  csRef<iView> view;
-
-  /// The render manager, cares about selecting lights+meshes to render
-  csRef<iRenderManager> rm;
-
-  /// A pointer to the sector the camera will be in.
-  iSector* room;
-
-  /// Current orientation of the camera.
-  float rotX, rotY;
-
-  /// Event handlers to draw and print the 3D canvas on each frame
-  csRef<FramePrinter> printer;
-
-public:
-  bool SetupModules ();
-
-  /**
-   * Handle keyboard events - ie key presses and releases.
-   * This routine is called from the event handler in response to a 
-   * csevKeyboard event.
-   */
-  bool OnKeyboard (iEvent&);
-  
-  /**
-   * Setup everything that needs to be rendered on screen. This routine
-   * is called from the event handler in response to a csevFrame
-   * message, and is called in the "logic" phase (meaning that all
-   * event handlers for 3D, 2D, Console, Debug, and Frame phases
-   * will be called after this one).
-   */
-  void Frame ();
-  
-  /// Create the basic world for this demo
-  void CreateRoom ();
-    
-  /// Construct the demo.
+ public:
   SelfShadowDemo ();
 
-  /// Destructor.
-  ~SelfShadowDemo ();
+  //-- CS::Utility::DemoApplication
+  void PrintHelp ();
+  void Frame ();
 
-  /// Final cleanup.
-  void OnExit ();
-
-  /**
-   * Main initialization routine. This routine will set up some basic stuff
-   * (like load all needed plugins, setup the event handler, ...).
-   * In case of failure this routine will return false. You can assume
-   * that the error message has been reported to the user.
-   */
-  bool OnInitialize (int argc, char* argv[]);
-
-  /**
-   * Run the application.
-   * First, there are some more initialization (everything that is needed 
-   * by Simple1 to use Crystal Space), then this routine fires up the main
-   * event loop. This is where everything starts. This loop will  basically
-   * start firing events which actually causes Crystal Space to function.
-   * Only when the program exits this function will return.
-   */
+  //-- csApplicationFramework
   bool Application ();
-  
-  /* Declare the name by which this class is identified to the event scheduler.
-   * Declare that we want to receive the frame event in the "LOGIC" phase,
-   * and that we're not terribly interested in having other events
-   * delivered to us before or after other modules, plugins, etc. */
-  CS_EVENTHANDLER_PHASE_LOGIC("application.selfshadow")
 };
 
-#endif // __SELFSHADOWDEMO_H__
+#endif // __SELFSHADOW_H__
