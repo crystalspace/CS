61ce4fd5b2322cb6d03bdac1474914b25d44ed13
Alexandru Voicu <avoicu@users.sourceforge.net>
2011-06-04 18:08:50 +0000

    Added a new blank render target in ShadowOSM.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/soc2011/selfshadow@36429 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/rendermanager/osm/shadow_osm.h
+++ plugins/rendermanager/osm/shadow_osm.h
@@ -31,9 +31,33 @@ namespace RenderManager
   public:
     struct PersistentData
     {
+      csRef<iTextureHandle> accumBuffer;
+      csRef<iShaderManager> shaderManager;
+
       void Initialize (iObjectRegistry* objectReg,
         RenderTreeBase::DebugPersistent& dbgPersist)
       {
+	      csConfigAccess cfg (objectReg);
+        csRef<iGraphics3D> g3d =
+          csQueryRegistry<iGraphics3D> (objectReg);
+
+        // Creates the accumulation buffer.
+        int flags = CS_TEXTURE_2D | CS_TEXTURE_NOMIPMAPS | CS_TEXTURE_CLAMP | CS_TEXTURE_NPOTS;
+        const char *accumFmt = cfg->GetStr ("RenderManager.Deferred.AccumBufferFormat", "rgb16_f");
+        iGraphics2D *g2d = g3d->GetDriver2D ();
+
+        scfString errStr;
+        accumBuffer = g3d->GetTextureManager ()->CreateTexture (g2d->GetWidth (),
+          g2d->GetHeight (),
+          csimg2D,
+          accumFmt,
+          flags,
+          &errStr);
+
+        if (!accumBuffer)
+          csPrintf("Error initializing accumBuffer!\n");
+
+        shaderManager = csQueryRegistry<iShaderManager> (objectReg);
       }
       
       void UpdateNewFrame ()
@@ -44,7 +68,13 @@ namespace RenderManager
     class ViewSetup
     {
     public:
+      CS::RenderManager::RenderView* rview;
+
+      PersistentData& persist;
+      SingleRenderLayer depthRenderLayer;
+
       ViewSetup (PersistentData& persist, CS::RenderManager::RenderView* rview)
+         : persist (persist), rview(rview)
       {
         
       }
@@ -57,6 +87,27 @@ namespace RenderManager
       uint GetSublightNum() const { return (uint)1; }
 
       void ClearFrameData(){}
+
+      void AddShadowMapTarget (typename RenderTree::MeshNode* meshNode,
+        PersistentData& persist, const SingleRenderLayer& layerConfig,
+        RenderTree& renderTree, iLight* light, ViewSetup& viewSetup)
+      {
+        if (light->GetFlags().Check (CS_LIGHT_NOSHADOWS)) return;
+
+        uint currentFrame = viewSetup.rview->GetCurrentFrameNumber();
+
+//         CS_ALLOC_STACK_ARRAY(iTextureHandle*, texHandles, 1);
+        
+        typename RenderTree::ContextNode* shadowMapCtx = 
+          renderTree.CreateContext (viewSetup.rview);
+        shadowMapCtx->drawFlags |= (CSDRAW_CLEARSCREEN | CSDRAW_CLEARZBUFFER);
+
+        shadowMapCtx->renderTargets[rtaColor1].texHandle = persist.accumBuffer;        
+        shadowMapCtx->renderTargets[rtaColor1].subtexture = 0; 
+        renderTree.AddDebugTexture (persist.accumBuffer);
+
+//         csPrintf("New Target %u!\n", currentFrame);
+      }
     };
 
     typedef ViewSetup ShadowParameters;
@@ -64,15 +115,23 @@ namespace RenderManager
     ShadowOSM (PersistentData& persist,
       const LayerConfigType& layerConfig,
       typename RenderTree::MeshNode* node, 
-      ViewSetup& viewSetup) 
+      ViewSetup& viewSetup) : persist (persist), 
+      renderTree (node->GetOwner().owner), meshNode (node), 
+      viewSetup (viewSetup)
     {
     }
 
     csFlags GetLightFlagsMask () const { return csFlags (0); }
 
-    static bool NeedFinalHandleLight() { return false; }
+    static bool NeedFinalHandleLight() { return true; }
 
-    void FinalHandleLight (iLight* light, CachedLightData& lightData) {}
+    void FinalHandleLight (iLight* light, CachedLightData& lightData) 
+    { 
+      lightData.AddShadowMapTarget (meshNode, persist,
+        viewSetup.depthRenderLayer, renderTree, light, viewSetup);
+
+      lightData.ClearFrameData();
+    }
 
     size_t GetLightLayerSpread() const { return 1; }
 
@@ -80,6 +139,12 @@ namespace RenderManager
       iLight* light, CachedLightData& lightData,
       csShaderVariableStack* lightStacks,
       uint lightNum, uint subLightNum) { return 1; }
+
+  protected:
+    PersistentData& persist;
+    RenderTree& renderTree;
+    typename RenderTree::MeshNode* meshNode;
+    ViewSetup& viewSetup;
   };
 }
 }
