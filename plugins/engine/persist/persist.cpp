beb2bb255717b6024082d4a85267c94d70c2cb7c
Mike Gist <xordan@gmail.com>
2011-07-16 16:33:54 +0000

    - Added skeleton persist plugin for engine plugin resources.
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/branches/feature/loading@36653 8cc4aa7f-3514-0410-904f-f2cc9021211c

---
+++ plugins/engine/persist/persist.cpp
@@ -0,0 +1,55 @@
+/*
+  Copyright (C) 2011 by Michael Gist
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with this library; if not, write to the Free
+  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#include "cssysdef.h"
+
+#include "persist.h"
+
+CS_PLUGIN_NAMESPACE_BEGIN(Engine)
+{
+  SCF_IMPLEMENT_FACTORY(Persist)
+
+  Persist::Persist (iBase* pParent)
+  : scfImplementationType (this, pParent)
+  {
+  }
+
+  Persist::~Persist ()
+  {
+  }
+
+  bool Persist::Initialize (iObjectRegistry* objectReg_)
+  {
+    objectReg = objectReg_;
+    syntaxService = csQueryRegistry<iSyntaxService> (objectReg);
+
+    InitTokenTable (xmltokens);
+    return true;
+  }
+
+  virtual csPtr<iBase> Persist::Load (iDocumentNode* node)
+  {
+    return csRef<iBase> ();
+  }
+
+  virtual bool Persist::Save (iBase* resource, iDocumentNode* node)
+  {
+    return true;
+  }
+}
+CS_PLUGIN_NAMESPACE_END (Engine)
---
+++ plugins/engine/persist/persist.h
@@ -0,0 +1,62 @@
+/*
+  Copyright (C) 2011 by Michael Gist
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
+
+  You should have received a copy of the GNU Library General Public
+  License along with this library; if not, write to the Free
+  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __CS_ENGINE_PERSIST_H__
+#define __CS_ENGINE_PERSIST_H__
+
+CS_PLUGIN_NAMESPACE_BEGIN(Engine)
+{
+  class Persist : 
+    public scfImplementation3<Persist, 
+                              iResourceLoader,
+                              iResourceSaver,
+                              iComponent>
+  {
+  private:
+    iObjectRegistry* objectReg;
+    csRef<iSyntaxService> syntaxService;
+
+    csStringHash xmltokens;
+#define CS_TOKEN_ITEM_FILE \
+  "plugins/engine/persist/persist.tok"
+#include "cstool/tokenlist.h"
+#undef CS_TOKEN_ITEM_FILE
+
+  public:
+    /// Constructor.
+    Persist (iBase*);
+
+    /// Destructor.
+    virtual ~Persist ();
+
+    virtual bool Initialize (iObjectRegistry *object_reg);
+
+    //// iResourceLoader ////
+    virtual csPtr<iBase> Load (iDocumentNode* node);
+
+    //// iResourceSaver ////
+    virtual bool Save (iBase* resource, iDocumentNode* node);
+
+    //// iResourceLoader & iResourceSaver ////
+    virtual bool IsThreadSafe() { return true; }
+  };
+}
+CS_PLUGIN_NAMESPACE_END (Engine)
+
+#endif // __CS_ENGINE_PERSIST_H__
+
