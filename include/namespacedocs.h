1390589bc63bc19d89c4053116479179cd8a2521
Christian Van Brussel <kickvb@users.sourceforge.net>
2011-08-24 13:29:51 +0000

    Fixed missing API doc for the CS::Material namespace
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@36877 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- include/namespacedocs.h
+++ include/namespacedocs.h
@@ -91,6 +91,12 @@ namespace CS
   {}
 
   /**
+   * Material and texture related classes.
+   */
+  namespace Material
+  {}
+
+  /**
    * Mathematical functions and definitions
    */
   namespace Math
