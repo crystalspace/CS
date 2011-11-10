8a63d2e50c5daca736d08a8a38133b324f584314
Frank Richter <res@crystalspace3d.org>
2011-11-10 23:52:41 +0000

    wxgl: Use right name for WX Unicode support macro
    git-svn-id: http://crystal.svn.sourceforge.net/svnroot/crystal/CS/trunk@37082 8cc4aa7f-3514-0410-904f-f2cc9021211c

--- plugins/video/canvas/wxgl/GLWXDriver2D.cpp
+++ plugins/video/canvas/wxgl/GLWXDriver2D.cpp
@@ -758,7 +758,7 @@ void csGLCanvas::EmitKeyEvent(wxKeyEvent& event, bool down)
   utf32_char cskey_raw = 0, cskey_cooked = 0, cskey_cooked_new = 0;
   wxCodeToCSCode (event.GetKeyCode(), cskey_raw, cskey_cooked);
 
-#if wxHAS_UNICODE
+#if wxUSE_UNICODE
   cskey_cooked_new = event.GetUnicodeKey();
 #else
   // Argh! Seems there is no way to get the character code for non-ASCII keys
